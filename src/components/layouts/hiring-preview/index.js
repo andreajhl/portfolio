39b1d14e2bd48ef3c47455a44127a2dbda794f03
Duvan Vargas <duvandroid@gmail.com>
2019-11-02 02:05:01 -0500

    first contract preview version

---
+++ src/components/layouts/hiring-preview/index.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import "./styles.scss";
+
+class HiringsCardSectionLayout extends Component {
+
+    renderContractCards() {
+        return (
+            this.props.contracts.map((contract, key) => {
+                return (
+                    <div className="contract-card" key={key}>
+                        <div className="div-contract-info">
+                            <div className="card">
+                                <div className="card-header">
+                                    <div className="celebrity-avatar d-none d-md-block">
+                                        <img className="celebrity-avatar rounded mt-2"
+                                             width="40px"
+                                             src={contract.celebrity.avatar}
+                                             alt="avatar"/>
+                                    </div>
+                                    <div className="from-to">
+                                        <h6 className="mt-2 font-weight-bold">
+                                            Famoso:
+                                            <small>{contract.celebrity.ful_name}</small>
+                                        </h6>
+                                        <h6 className="mt-2 font-weight-bold">
+                                            Para:
+                                            <small>{contract.delivery_to}</small>
+                                        </h6>
+                                        <h6 className="mt-2 font-weight-bold">
+                                            De:
+                                            <small>{contract.delivery_from}</small>
+                                        </h6>
+                                    </div>
+                                    <div className="button-status">
+                                        {
+                                            contract.status <= 30
+                                                ?
+                                                <button className="btn btn-outline-dark" disabled>
+                                                    <span className="d-none d-md-block">Pendiente</span>
+                                                    <i className="ml-0 ml-sm-2 mt-0 mt-sm-1 fa fa-clock"/>
+                                                </button>
+                                                :
+                                                <button className="btn btn-outline-success">
+                                                    <span className="d-none d-md-block">Ver Video</span>
+                                                    <i className="ml-0 ml-sm-2 mt-0 mt-sm-1 fa fa-play"/>
+                                                </button>
+                                        }
+                                    </div>
+                                </div>
+                                <div className="card-body text-justify contract-instructions">
+                                    {contract.instructions}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        )
+    }
+
+    render() {
+        return (
+            <div className="HiringsCardSectionLayout">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-lg-8">
+                        <div className="f-main-padding mt-4 f-shadow rounded f-rounded">
+                            <div className="row section">
+                                <div className="col-12 text-center">
+                                    <h4 className="mt-3 font-weight-bold border-bottom pb-3">
+                                        Mis Contrataciones
+                                    </h4>
+                                </div>
+                                {this.renderContractCards()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+}
+
+//default props
+HiringsCardSectionLayout.defaultProps = {
+    contracts: []
+};
+
+export {HiringsCardSectionLayout};
