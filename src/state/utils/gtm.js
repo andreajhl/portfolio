06993f0c04d2f1e7fa115d0091c479caa3e871e8
Duvan Vargas <duvandroid@gmail.com>
2019-11-15 18:46:23 -0500

    gtm for production

--- src/state/utils/gtm.js
+++ src/state/utils/gtm.js
@@ -9,3 +9,10 @@ export const initialize = () => {
         })
     }
 };
+
+export const tagManagerDataLayer = (event, dataLayer) => {
+    window.dataLayer.push({
+        ...dataLayer,
+        event
+    });
+};
