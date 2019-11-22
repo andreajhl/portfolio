2a8a645080d13d495446ff1d2343306bc9efa9d7
Duvan Vargas <duvandroid@gmail.com>
2019-11-21 21:01:41 -0500

    referrals

--- src/components/containers/celebrity-request-form/index.js
+++ src/components/containers/celebrity-request-form/index.js
@@ -3,6 +3,7 @@ import "./styles.scss";
 import {FlagsSelect} from "../../layouts/flags-select";
 import {celebrityRequestOperations} from "../../../state/ducks/celebrity-requests";
 import {connect} from "react-redux";
+import {history} from "../../../routing/History";
 
 class CelebrityRequestForm extends Component {
 
@@ -10,20 +11,34 @@ class CelebrityRequestForm extends Component {
         super(props);
 
         this.state = {
-            countryAlpha3Code: "COL",
-            countryCellphoneCode: "+57",
-            cellphoneNumber: "",
-            fullName: "",
-            subDomain: "",
-            email: "",
-            socialNetworkName: "",
-            socialNetworkUsername: "",
-            referralCode: "",
+            data: {
+                countryAlpha3Code: "COL",
+                countryCellphoneCode: "+57",
+                cellphoneNumber: "",
+                fullName: "",
+                subDomain: "",
+                email: "",
+                socialNetworkName: "",
+                socialNetworkUsername: "",
+                referral: new URLSearchParams(history.location.search).get("r") || "",
+            },
+            errors: {
+                fullNameError: false,
+                subDomainError: false,
+                emailError: false,
+                cellphoneNumberError: false,
+                socialNetworkNameError: false,
+            }
         };
 
         this.handleInput = this.handleInput.bind(this);
         this.onSelectCountry = this.onSelectCountry.bind(this);
         this.saveCelebrityRequest = this.saveCelebrityRequest.bind(this);
+        this.close = this.close.bind(this);
+    }
+
+    close() {
+        window.location.reload()
     }
 
     onSelectCountry(country) {
@@ -34,16 +49,55 @@ class CelebrityRequestForm extends Component {
     }
 
     handleInput(event) {
-        this.setState({[event.target.name]: event.target.value})
+        const data = this.state.data;
+        data[event.target.name] = event.target.value;
+        this.setState({data})
+    }
+
+    isAValidForm() {
+        return this.state.data.fullName && this.state.data.subDomain && this.state.data.email && this.state.data.cellphoneNumber && this.state.data.socialNetworkName
     }
 
     saveCelebrityRequest() {
-        this.props.saveCelebrityRequest(this.state);
+        let fullNameError = false;
+        let subDomainError = false;
+        let emailError = false;
+        let cellphoneNumberError = false;
+        let socialNetworkNameError = false;
+
+        if (!this.state.data.fullName) {
+            fullNameError = true;
+        }
+        if (!this.state.data.subDomain) {
+            subDomainError = true;
+        }
+        if (!this.state.data.email) {
+            emailError = true;
+        }
+        if (!this.state.data.cellphoneNumber) {
+            cellphoneNumberError = true;
+        }
+        if (!this.state.data.socialNetworkName) {
+            socialNetworkNameError = true;
+        }
+
+        this.setState({
+            errors: {
+                fullNameError,
+                subDomainError,
+                emailError,
+                cellphoneNumberError,
+                socialNetworkNameError,
+            }
+        });
+        if (this.isAValidForm()) {
+            this.props.saveCelebrityRequest(this.state.data);
+        }
     }
 
-    render() {
+    renderForm() {
         return (
-            <div className="CelebrityRequestForm">
+            <>
                 <h4 className="font-weight-bold text-center mb-4">¡Aplica y Reserva tu Subdominio Ahora!</h4>
                 <label className="">Nombre
                     <small className="text-danger ml-1">*</small>
@@ -51,11 +105,11 @@ class CelebrityRequestForm extends Component {
                 <input
                     autoFocus={true}
                     type="text"
-                    className="form-control mb-3"
+                    className={"form-control mb-3" + (this.state.errors.fullNameError ? " border-danger " : "")}
                     placeholder="Escribe tu nombre"
                     name="fullName"
                     onChange={this.handleInput}
-                    value={this.state.fullName}
+                    value={this.state.data.fullName}
                 />
                 <label className="">Subdominio
                     <small className="text-danger ml-1">*</small>
@@ -63,17 +117,17 @@ class CelebrityRequestForm extends Component {
                 <div className="form-horizontal">
                     <input
                         disabled={true}
-                        className="form-control mb-3"
+                        className={"form-control mb-3"}
                         value="famosos.com/"
                         style={{flex: 1.2}}
                     />
                     <input
                         type="text"
-                        className="form-control mb-3"
+                        className={"form-control mb-3" + (this.state.errors.subDomainError ? " border-danger " : "")}
                         placeholder="Escribe tu Subdominio Ej: /andresito"
                         name="subDomain"
                         onChange={this.handleInput}
-                        value={this.state.subDomain}
+                        value={this.state.data.subDomain}
                         style={{flex: 2}}
                     />
                 </div>
@@ -82,11 +136,11 @@ class CelebrityRequestForm extends Component {
                 </label>
                 <input
                     type="email"
-                    className="form-control mb-3"
+                    className={"form-control mb-3" + (this.state.errors.emailError ? " border-danger " : "")}
                     placeholder="Escribe tu correo electrónico"
                     name="email"
                     onChange={this.handleInput}
-                    value={this.state.email}
+                    value={this.state.data.email}
                 />
                 <label className="">Télefono de contacto</label>
                 <div className="form-horizontal">
@@ -95,11 +149,11 @@ class CelebrityRequestForm extends Component {
                     />
                     <input
                         type="number"
-                        className="form-control mb-3"
+                        className={"form-control mb-3" + (this.state.errors.cellphoneNumberError ? " border-danger " : "")}
                         placeholder="Escribe tu número"
                         name="cellphoneNumber"
                         onChange={this.handleInput}
-                        value={this.state.cellphoneNumber}
+                        value={this.state.data.cellphoneNumber}
                     />
                 </div>
                 <label className="">Red Social
@@ -107,12 +161,13 @@ class CelebrityRequestForm extends Component {
                 </label>
                 <div className="form-horizontal">
                     <select
-                        className="form-control mb-3"
+                        className={"form-control mb-3" + (this.state.errors.socialNetworkNameError ? " border-danger " : "")}
                         style={{flex: 1}}
                         name="socialNetworkName"
                         onChange={this.handleInput}
-                        value={this.state.socialNetworkName}
+                        value={this.state.data.socialNetworkName}
                     >
+                        <option></option>
                         <option>Instagram</option>
                         <option>Facebook</option>
                         <option>Twitter</option>
@@ -122,24 +177,28 @@ class CelebrityRequestForm extends Component {
                     </select>
                     <input
                         type="text"
-                        className="form-control mb-3"
+                        className={"form-control" + (this.state.errors.socialNetworkUsernameError ? " border-danger " : "")}
                         placeholder="¿Como te encontramos? Ej: andresito"
                         name="socialNetworkUsername"
                         onChange={this.handleInput}
-                        value={this.state.socialNetworkUsername}
+                        value={this.state.data.socialNetworkUsername}
                         style={{flex: 2}}
                     />
                 </div>
-                <label className="">Código de referido</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Código de referido"
-                    name="referralCode"
-                    onChange={this.handleInput}
-                    value={this.state.referralCode}
-                />
-
+                {
+                    this.state.data.referral &&
+                    <>
+                        <label className="mt-3">Código de referido</label>
+                        <input
+                            type="text"
+                            className={"form-control" + this.state.errors.referralError ? " border-danger " : ""}
+                            placeholder="Código de referido"
+                            name="referral"
+                            onChange={this.handleInput}
+                            value={this.state.data.referral}
+                        />
+                    </>
+                }
                 {
                     this.props.saveCelebrityRequestError
                     &&
@@ -149,7 +208,7 @@ class CelebrityRequestForm extends Component {
                 }
                 <button
                     className="send-button"
-                    disabled={!this.state.cellphoneNumber}
+                    disabled={!this.state.data.cellphoneNumber}
                     onClick={this.saveCelebrityRequest}
                 >
                     {
@@ -163,6 +222,62 @@ class CelebrityRequestForm extends Component {
                             <span className={"text-white"}>Continuar</span>
                     }
                 </button>
+            </>
+        )
+    }
+
+    renderCongratsOne() {
+        return (
+            <>
+                <h4 className="font-weight-bold text-center mb-4">¡Tu solicitud ha sido enviada!</h4>
+                <p className="mt-4">Nuestro equipo analizará tu solicitud y se pondra en contacto contigo muy
+                    pronto.</p>
+                <div className={"text-center"}>
+                    <img src={"/assets/img/review.svg"} alt={"img"} width="300px"/>
+                </div>
+                <div className={"text-center mt-3"}>
+                    <button className="btn btn-primary" onClick={this.close}>Cerrar</button>
+                </div>
+            </>
+        )
+    }
+
+    renderCongratsTwo() {
+        return (
+            <>
+                <h4 className="font-weight-bold text-center mb-4">¡Tu solicitud ha sido enviada!</h4>
+                <p className="mt-4">Nuestro equipo analizará tu solicitud y se pondra en contacto contigo muy
+                    pronto.</p>
+                <p className="text-justify"><span className="font-weight-bold mr-2">Nota:</span> Si utilizas el link de
+                    referido de uno uno de los Famosos registrados en la plataforma tendrás mas oportunidades de ser
+                    aprobado, este link es único por cuenta y está disponible en la sección de Mi Perfil de la
+                    aplicación de los Famosos.</p>
+                <div className={"text-center"}>
+                    <img src={"/assets/img/review.svg"} alt={"img"} width="300px"/>
+                </div>
+                <div className={"text-center mt-3"}>
+                    <button className="btn btn-primary" onClick={this.close}>Cerrar</button>
+                </div>
+            </>
+        )
+    }
+
+    renderContent() {
+        if (!this.props.saveCelebrityRequestCompleted || this.props.saveCelebrityRequestError) {
+            return this.renderForm()
+        } else if (this.props.saveCelebrityRequestData.option === "REGISTERED_WITH_REFERRAL") {
+            window.scroll({top: 0,});
+            return this.renderCongratsOne()
+        } else if (this.props.saveCelebrityRequestData.option === "REGISTERED_WITHOUT_REFERRAL") {
+            window.scroll({top: 0,});
+            return this.renderCongratsTwo()
+        }
+    }
+
+    render() {
+        return (
+            <div className="CelebrityRequestForm">
+                {this.renderContent()}
             </div>
         );
     };
