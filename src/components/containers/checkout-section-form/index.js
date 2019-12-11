00e9b515c1aeb0361f7edcf88ba22bdd79c54bdc
Nano <mac@MacBook-Pro-de-mac.local>
2019-12-10 21:59:03 -0500

    edit views to payment method

--- src/components/containers/checkout-section-form/index.js
+++ src/components/containers/checkout-section-form/index.js
@@ -1,147 +1,143 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./styles.scss";
-import {PaymentMethodsSection} from "../../containers/payment-methods-section";
-import {ContractCheckoutSummary} from "../../containers/contract-checkout-summary";
-import {paymentsOperations} from "../../../state/ducks/payments";
-import {connect} from "react-redux";
-import {CheckoutBuyerData} from "../checkout-buyer-data";
-import {ContractCurrencyPayment} from "../contract-currency-payment";
-
+import { PaymentMethodsSection } from "../../containers/payment-methods-section";
+import { ContractCheckoutSummary } from "../../containers/contract-checkout-summary";
+import { paymentsOperations } from "../../../state/ducks/payments";
+import { connect } from "react-redux";
+import { CheckoutBuyerData } from "../checkout-buyer-data";
+import { ContractCurrencyPayment } from "../contract-currency-payment";
 
 class CheckoutSectionForm extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currency: "USD",
-            buyerData: {},
-            paymentMethod: {},
-            paymentType: {}
-        };
-
-        this.onSelectCurrency = this.onSelectCurrency.bind(this);
-        this.onBuyerDataChange = this.onBuyerDataChange.bind(this);
-
-        this.onSelectPaymentMethod = this.onSelectPaymentMethod.bind(this);
-        this.onSelectPaymentType = this.onSelectPaymentType.bind(this);
-        this.onPay = this.onPay.bind(this);
-    }
-
-    onSelectCurrency(currency) {
-        this.setState({
-            currency
-        })
-    }
-
-    onBuyerDataChange(buyerData) {
-        this.setState({
-            buyerData
-        })
-    }
-
-    onSelectPaymentMethod(paymentMethod) {
-        this.setState({
-            paymentMethod
-        })
-    }
-
-    onSelectPaymentType(paymentType) {
-        this.setState({
-            paymentType
-        })
-    }
-
-    onPay() {
-        let country = null;
-        if (this.state.currency === "ARS") {
-            country = "AR"
-        } else if (this.state.currency === "BOB") {
-            country = "BO"
-        } else if (this.state.currency === "BRL") {
-            country = "BR"
-        } else if (this.state.currency === "CLP") {
-            country = "CL"
-        } else if (this.state.currency === "CNY") {
-            country = "CN"
-        } else if (this.state.currency === "COP") {
-            country = "CO"
-        } else if (this.state.currency === "USD") {
-            country = "EC"
-        } else if (this.state.currency === "INR") {
-            country = "IN"
-        } else if (this.state.currency === "IDR") {
-            country = "ID"
-        } else if (this.state.currency === "MXN") {
-            country = "MX"
-        } else if (this.state.currency === "MAD") {
-            country = "MA"
-        } else if (this.state.currency === "PYG") {
-            country = "PY"
-        } else if (this.state.currency === "PEN") {
-            country = "PE"
-        } else if (this.state.currency === "ZAR") {
-            country = "ZA"
-        } else if (this.state.currency === "TRY") {
-            country = "TR"
-        } else if (this.state.currency === "UYU") {
-            country = "UY"
-        }
-        this.props.createContractPayment({
-            buyer_full_name: this.state.buyerData.full_name,
-            buyer_email: this.state.buyerData.email,
-            buyer_document: this.state.buyerData.document,
-            contract_reference: this.props.contractData.reference,
-            payment_method_id: this.state.paymentMethod.id,
-            country: country,
-        })
-    }
-
-    checkButtonAvailability() {
-        if (this.state.currency !== "USD") {
-            return this.state.buyerData.full_name && this.state.buyerData.email && this.state.buyerData.document && this.state.paymentMethod.name && !this.props.isLoading;
-        } else {
-
-        }
-    }
-
-    render() {
-        return (
-            <div className="CheckoutSectionForm">
-                <div className="row checkout-section mx-auto justify-content-center">
-                    <div className="col-lg-7 payment-methods">
-                        <ContractCurrencyPayment
-                            onSelectCurrency={this.onSelectCurrency}
-                        />
-                        <hr/>
-                        <PaymentMethodsSection
-                            onSelectPaymentType={this.onSelectPaymentType}
-                            onSelectPaymentMethod={this.onSelectPaymentMethod}
-                        />
-                        {
-                            this.state.currency !== "USD"
-                            &&
-                            <>
-                                <hr/>
-                                <CheckoutBuyerData
-                                    onBuyerDataChange={this.onBuyerDataChange}
-                                />
-                            </>
-                        }
-                    </div>
-                    <div className="col-lg-5 contract-summary">
-                        <ContractCheckoutSummary
-                            transactionFee={this.state.paymentMethod.fee}
-                            contractData={this.props.contractData}
-                            buttonPayDisabled={this.checkButtonAvailability()}
-                            onPay={this.onPay}
-                        />
-                    </div>
-                </div>
-            </div>
-        );
+    this.state = {
+      currency: "USD",
+      buyerData: {},
+      paymentMethod: {},
+      paymentType: {}
     };
 
+    this.onSelectCurrency = this.onSelectCurrency.bind(this);
+    this.onBuyerDataChange = this.onBuyerDataChange.bind(this);
+
+    this.onSelectPaymentMethod = this.onSelectPaymentMethod.bind(this);
+    this.onSelectPaymentType = this.onSelectPaymentType.bind(this);
+    this.onPay = this.onPay.bind(this);
+  }
+
+  onSelectCurrency(currency) {
+    this.setState({
+      currency
+    });
+  }
+
+  onBuyerDataChange(buyerData) {
+    this.setState({
+      buyerData
+    });
+  }
+
+  onSelectPaymentMethod(paymentMethod) {
+    this.setState({
+      paymentMethod
+    });
+  }
+
+  onSelectPaymentType(paymentType) {
+    this.setState({
+      paymentType
+    });
+  }
+
+  onPay() {
+    let country = null;
+    if (this.state.currency === "ARS") {
+      country = "AR";
+    } else if (this.state.currency === "BOB") {
+      country = "BO";
+    } else if (this.state.currency === "BRL") {
+      country = "BR";
+    } else if (this.state.currency === "CLP") {
+      country = "CL";
+    } else if (this.state.currency === "CNY") {
+      country = "CN";
+    } else if (this.state.currency === "COP") {
+      country = "CO";
+    } else if (this.state.currency === "USD") {
+      country = "EC";
+    } else if (this.state.currency === "INR") {
+      country = "IN";
+    } else if (this.state.currency === "IDR") {
+      country = "ID";
+    } else if (this.state.currency === "MXN") {
+      country = "MX";
+    } else if (this.state.currency === "MAD") {
+      country = "MA";
+    } else if (this.state.currency === "PYG") {
+      country = "PY";
+    } else if (this.state.currency === "PEN") {
+      country = "PE";
+    } else if (this.state.currency === "ZAR") {
+      country = "ZA";
+    } else if (this.state.currency === "TRY") {
+      country = "TR";
+    } else if (this.state.currency === "UYU") {
+      country = "UY";
+    }
+    this.props.createContractPayment({
+      buyer_full_name: this.state.buyerData.full_name,
+      buyer_email: this.state.buyerData.email,
+      buyer_document: this.state.buyerData.document,
+      contract_reference: this.props.contractData.reference,
+      payment_method_id: this.state.paymentMethod.id,
+      country: country
+    });
+  }
+
+  checkButtonAvailability() {
+    if (this.state.currency !== "USD") {
+      return (
+        this.state.buyerData.full_name &&
+        this.state.buyerData.email &&
+        this.state.buyerData.document &&
+        this.state.paymentMethod.name &&
+        !this.props.isLoading
+      );
+    } else {
+    }
+  }
+
+  render() {
+    return (
+      <div className="CheckoutSectionForm">
+        <div className="row checkout-section mx-auto justify-content-center">
+          <div className="col-12 col-sm-8 col-md-7 col-lg-7 payment-methods">
+            <ContractCurrencyPayment onSelectCurrency={this.onSelectCurrency} />
+            <hr />
+            <PaymentMethodsSection
+              onSelectPaymentType={this.onSelectPaymentType}
+              onSelectPaymentMethod={this.onSelectPaymentMethod}
+            />
+            {this.state.currency !== "USD" && (
+              <>
+                <hr />
+                <CheckoutBuyerData onBuyerDataChange={this.onBuyerDataChange} />
+              </>
+            )}
+          </div>
+          <div className="col-12 col-sm-8 col-md-7 col-lg-5 contract-summary mt-3">
+            <ContractCheckoutSummary
+              transactionFee={this.state.paymentMethod.fee}
+              contractData={this.props.contractData}
+              buttonPayDisabled={this.checkButtonAvailability()}
+              onPay={this.onPay}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 // Set propTypes
@@ -149,22 +145,25 @@ CheckoutSectionForm.propTypes = {};
 
 // Set defaultProps
 CheckoutSectionForm.defaultProps = {
-    contractData: {}
+  contractData: {}
 };
 
 // mapStateToProps
 const mapStateToProps = (state: any) => ({
-    currencyExchangeData: state.payments.currencyExchangeReducer.data,
-    isLoading: state.payments.createContractPaymentReducer.loading,
-    isCompleted: state.payments.createContractPaymentReducer.completed,
-    contractPayment: state.payments.createContractPaymentReducer.data,
+  currencyExchangeData: state.payments.currencyExchangeReducer.data,
+  isLoading: state.payments.createContractPaymentReducer.loading,
+  isCompleted: state.payments.createContractPaymentReducer.completed,
+  contractPayment: state.payments.createContractPaymentReducer.data
 });
 
 // mapStateToProps
 const mapDispatchToProps = {
-    createContractPayment: paymentsOperations.createContractPayment,
+  createContractPayment: paymentsOperations.createContractPayment
 };
 
 // Export Class
-const _CheckoutSectionForm = connect(mapStateToProps, mapDispatchToProps)(CheckoutSectionForm);
-export {_CheckoutSectionForm as CheckoutSectionForm};
+const _CheckoutSectionForm = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckoutSectionForm);
+export { _CheckoutSectionForm as CheckoutSectionForm };
--- src/components/containers/contract-checkout-summary/index.js
+++ src/components/containers/contract-checkout-summary/index.js
@@ -1,101 +1,127 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./styles.scss";
-import {paymentsOperations} from "../../../state/ducks/payments";
-import {connect} from "react-redux";
-import NumberFormat from 'react-number-format';
-
+import { paymentsOperations } from "../../../state/ducks/payments";
+import { connect } from "react-redux";
+import NumberFormat from "react-number-format";
 
 class ContractCheckoutSummary extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {};
 
-    constructor(props) {
-        super(props);
+    this.contractPrice = this.contractPrice.bind(this);
+    this.onPay = this.onPay.bind(this);
+  }
 
-        this.state = {};
+  returnNumber(number) {
+    return (
+      <NumberFormat
+        value={number}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={""}
+        renderText={value => <span>{value}</span>}
+      />
+    );
+  }
 
-        this.contractPrice = this.contractPrice.bind(this);
-        this.onPay = this.onPay.bind(this);
-    }
-
-    returnNumber(number) {
+  contractPrice() {
+    if (this.props.contractData.price) {
+      if (
+        this.props.currencyExchangeData.to !== "USD" &&
+        this.props.transactionFee
+      ) {
+        const price =
+          this.props.contractData.price * this.props.currencyExchangeData.rate;
+        return price + price * this.props.transactionFee;
+      } else if (this.props.currencyExchangeData.to) {
         return (
-            <NumberFormat
-                value={number}
-                displayType={'text'}
-                thousandSeparator={true}
-                prefix={''}
-                renderText={value => <span>{value}</span>}
-            />
-        )
-    }
-
-    contractPrice() {
-        if(this.props.contractData.price){
-            if (this.props.currencyExchangeData.to !== "USD" && this.props.transactionFee) {
-                const price = this.props.contractData.price * this.props.currencyExchangeData.rate;
-                return price + (price * this.props.transactionFee);
-            } else if (this.props.currencyExchangeData.to) {
-                return this.props.contractData.price * this.props.currencyExchangeData.rate;
-            } else {
-                return this.props.contractData.price
-            }
-        }else{
-            return 0
-        }
-    }
-
-    onPay() {
-        this.props.onPay()
-    }
-
-    render() {
-        return (
-            <div className="ContractCheckoutSummary f-rounded">
-                <div className="row contract-summary col-lg-12 justify-content-center f-shadow f-rounded">
-                    <div className="col-lg-12 custom-card-title f-rounded bg-primary">
-                        <h6 className="text-white">Resumen de la contratación</h6>
-                    </div>
-                    <div className="col-lg-12 contract-details">
-                        <div className="celebrity-name text-center">
-                            <img className="rounded-circle mx-auto"
-                                 src={!this.props.contractData.celebrity ? "/assets/img/avatar-blank.png" : this.props.contractData.celebrity.avatar}
-                                 width={"60px"}
-                                 alt={"avatar"}/>
-                        </div>
-                        <div className="from-to mt-4">
-                            <h6>
-                                <span className="text-colored">Para: <span
-                                    className="font-weight-bold">{this.props.contractData.delivery_to}</span> </span>
-                                <span className="text-colored ml-3">De: <span
-                                    className="font-weight-bold">{this.props.contractData.delivery_from}</span> </span>
-                            </h6>
-                        </div>
-                        <div className="instructions mt-4 text-justify">
-                            <small>{this.props.contractData.instructions}</small>
-                        </div>
-                        <div className="total mt-4">
-                            <div className="clearfix ">
-                                <h5 className="font-weight-bold float-left">Total:</h5>
-                                <h5 className="font-weight-bold float-right">
-                                    $ {this.returnNumber(this.contractPrice())} {this.props.currencyExchangeData.to ? this.props.currencyExchangeData.to : "USD"}
-                                </h5>
-                            </div>
-                        </div>
-                        <div className="contract-button mt-4 mx-auto text-center">
-                            <button
-                                onClick={this.onPay}
-                                className={"contract-button mx-auto hover cursor-pointer p-2 border bg-active " + (!this.props.buttonPayDisabled ? " disabled " : " ")}
-                                disabled={!this.props.buttonPayDisabled}
-                            >
-                                3. Pagar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          this.props.contractData.price * this.props.currencyExchangeData.rate
         );
-    };
+      } else {
+        return this.props.contractData.price;
+      }
+    } else {
+      return 0;
+    }
+  }
 
+  onPay() {
+    this.props.onPay();
+  }
+
+  render() {
+    return (
+      <div className="ContractCheckoutSummary f-rounded">
+        <div className="row contract-summary col-lg-12 justify-content-center f-shadow f-rounded">
+          <div className="col-lg-12 custom-card-title f-rounded bg-primary">
+            <h6 className="text-white">Resumen de la contratación</h6>
+          </div>
+          <div className="col-lg-12 contract-details">
+            <div className="celebrity-name text-center">
+              <img
+                className="rounded-circle mx-auto"
+                src={
+                  !this.props.contractData.celebrity
+                    ? "/assets/img/avatar-blank.png"
+                    : this.props.contractData.celebrity.avatar
+                }
+                width={"60px"}
+                alt={"avatar"}
+              />
+            </div>
+            <div className="from-to mt-4">
+              <h6>
+                <div className="col-12">
+                  <span className="text-colored">
+                    De:&nbsp;
+                    <span className="font-weight-bold">
+                      {this.props.contractData.delivery_from}
+                    </span>{" "}
+                  </span>
+                </div>
+                <div className="col-12">
+                  <span className="text-colored">
+                    Para:&nbsp;
+                    <span className="font-weight-bold">
+                      {this.props.contractData.delivery_to}
+                    </span>{" "}
+                  </span>
+                </div>
+              </h6>
+            </div>
+            <div className="instructions mt-4 text-justify">
+              <small>{this.props.contractData.instructions}</small>
+            </div>
+            <div className="total mt-4">
+              <div className="clearfix ">
+                <h5 className="font-weight-bold float-left">Total:</h5>
+                <h5 className="font-weight-bold float-right">
+                  $ {this.returnNumber(this.contractPrice())}{" "}
+                  {this.props.currencyExchangeData.to
+                    ? this.props.currencyExchangeData.to
+                    : "USD"}
+                </h5>
+              </div>
+            </div>
+            <div className="contract-button mt-4 mx-auto text-center">
+              <button
+                onClick={this.onPay}
+                className={
+                  "contract-button mx-auto hover cursor-pointer p-2 border bg-active " +
+                  (!this.props.buttonPayDisabled ? " disabled " : " ")
+                }
+                disabled={!this.props.buttonPayDisabled}
+              >
+                3. Pagar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 // Set propTypes
@@ -103,24 +129,26 @@ ContractCheckoutSummary.propTypes = {};
 
 // Set defaultProps
 ContractCheckoutSummary.defaultProps = {
-    buttonPayDisabled: false,
-    transactionFee: 0,
-    contractData: {},
-    onPay: () => {
-    }
+  buttonPayDisabled: false,
+  transactionFee: 0,
+  contractData: {},
+  onPay: () => {}
 };
 
 // mapStateToProps
 const mapStateToProps = (state: any) => ({
-    isLoading: state.payments.currencyExchangeReducer.loading,
-    currencyExchangeData: state.payments.currencyExchangeReducer.data,
+  isLoading: state.payments.currencyExchangeReducer.loading,
+  currencyExchangeData: state.payments.currencyExchangeReducer.data
 });
 
 // mapStateToProps
 const mapDispatchToProps = {
-    currencyExchange: paymentsOperations.currencyExchange,
+  currencyExchange: paymentsOperations.currencyExchange
 };
 
 // Export Class
-const _ContractCheckoutSummary = connect(mapStateToProps, mapDispatchToProps)(ContractCheckoutSummary);
-export {_ContractCheckoutSummary as ContractCheckoutSummary};
+const _ContractCheckoutSummary = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ContractCheckoutSummary);
+export { _ContractCheckoutSummary as ContractCheckoutSummary };
