ae05aee0ba2456419911c25b27543564b2975735
@duvanv <duvan@famosos.com>
2020-05-14 13:46:35 -0500

    waiting for the image

--- src/components/pages/create-contract/index.js
+++ src/components/pages/create-contract/index.js
@@ -125,7 +125,6 @@ class CreateContractPage extends Component {
 
   renderFromTo() {
     switch (this.state.contract_data.contract_type) {
-      default:
       case 1:
         return (
             <Row>
@@ -181,6 +180,8 @@ class CreateContractPage extends Component {
               </Col>
             </Row>
         );
+      default:
+        return
     }
   }
 
@@ -232,6 +233,20 @@ class CreateContractPage extends Component {
                           Para mi
                         </Form.Check.Label>
                       </Form.Check>
+                      <Form.Check type="radio">
+                        <Form.Check.Input
+                            type="radio"
+                            name="contract_type"
+                            value={2}
+                            checked={this.state.contract_data.contract_type === 3}
+                            onChange={this.handleValue}
+                        />
+                        <Form.Check.Label
+                            onClick={this.contractTypeLabelClick.bind(this, 3)}
+                        >
+                          Para mi marca o negocio <span className="new-label f-rounded">Nueva funcionalidad</span>
+                        </Form.Check.Label>
+                      </Form.Check>
                     </Form.Group>
                     {/*END CONTRACT TYPE*/}
 
@@ -239,120 +254,150 @@ class CreateContractPage extends Component {
                     {this.renderFromTo()}
                     {/*END FROM TO*/}
 
-                    {/*INSTRUCTIONS*/}
-                    <Form.Group>
-                      <Form.Label>
-                        <b>
-                          Mis instrucciones para {this.props.celebrity.full_name}{" "}
-                          son:
-                        </b>
-                      </Form.Label>
-                      <Form.Control
-                          as="textarea"
-                          rows="3"
-                          placeholder={
-                            this.state.contract_data.contract_type === 1
-                                ? "Ejemplo: Mi nombre es Duvan. ¡Mi hermana Anita es tu fan!. Ella está cumpliendo años el 12 de agosto y quisiera que le desearas un Feliz Cumpleaños de mi parte."
-                                : "Escribe aquí tus instrucciones"
-                          }
-                          name="instructions"
-                          value={this.state.contract_data.instructions}
-                          onChange={this.handleValue}
-                      />
-                      <Form.Text className="text-muted">
-                        Máximo 300 caracteres
-                      </Form.Text>
-                      {this.showErrorMessage("instructions")}
-                      {this.showErrorMessage("instructions_300")}
-                    </Form.Group>
-                    {/*END INSTRUCTIONS*/}
+                    {
+                      this.state.contract_data.contract_type <= 2
+                          ?
+                          <>
+                            {/*INSTRUCTIONS*/}
+                            <Form.Group>
+                              <Form.Label>
+                                <b>
+                                  Mis instrucciones para {this.props.celebrity.full_name}{" "}
+                                  son:
+                                </b>
+                              </Form.Label>
+                              <Form.Control
+                                  as="textarea"
+                                  rows="3"
+                                  placeholder={
+                                    this.state.contract_data.contract_type === 1
+                                        ? "Ejemplo: Mi nombre es Duvan. ¡Mi hermana Anita es tu fan!. Ella está cumpliendo años el 12 de agosto y quisiera que le desearas un Feliz Cumpleaños de mi parte."
+                                        : "Escribe aquí tus instrucciones"
+                                  }
+                                  name="instructions"
+                                  value={this.state.contract_data.instructions}
+                                  onChange={this.handleValue}
+                              />
+                              <Form.Text className="text-muted">
+                                Máximo 300 caracteres
+                              </Form.Text>
+                              {this.showErrorMessage("instructions")}
+                              {this.showErrorMessage("instructions_300")}
+                            </Form.Group>
+                            {/*END INSTRUCTIONS*/}
 
-                    {/*DELIVERY MAIL*/}
-                    <Form.Group>
-                      <Form.Label>
-                        <b>Correo electrónico de notificación:</b>
-                        <i className="fa fa-info-circle customTooltip ml-4">
+                            {/*DELIVERY MAIL*/}
+                            <Form.Group>
+                              <Form.Label>
+                                <b>Correo electrónico de notificación:</b>
+                                <i className="fa fa-info-circle customTooltip ml-4">
                             <span className="tooltipText">
                                 A este correo electrónico te noficaremos cuando el video esté listo.
                             </span>
-                        </i>
-                      </Form.Label>
-                      <Form.Control
-                          type="email"
-                          placeholder="correo@dominio.com"
-                          name="delivery_contact"
-                          value={this.state.contract_data.delivery_contact}
-                          onChange={this.handleValue}
-                      />
-                      {this.showErrorMessage("delivery_contact")}
-                    </Form.Group>
-                    {/*END DELIVERY MAIL*/}
+                                </i>
+                              </Form.Label>
+                              <Form.Control
+                                  type="email"
+                                  placeholder="correo@dominio.com"
+                                  name="delivery_contact"
+                                  value={this.state.contract_data.delivery_contact}
+                                  onChange={this.handleValue}
+                              />
+                              {this.showErrorMessage("delivery_contact")}
+                            </Form.Group>
+                            {/*END DELIVERY MAIL*/}
 
-                    {/* IS PUBLIC  */}
-                    <div className="mt-1">
-                      <Form.Check
-                          checked={this.state.contract_data.is_public}
-                          onChange={this.handleIsPublic}
-                          type="switch"
-                          id="custom-switch"
-                          label="Publicar este video en Famosos.com"
-                      />
-                    </div>
-                    {/* END IS PUBLIC */}
+                            {/* IS PUBLIC  */}
+                            <div className="mt-1">
+                              <Form.Check
+                                  checked={this.state.contract_data.is_public}
+                                  onChange={this.handleIsPublic}
+                                  type="switch"
+                                  id="custom-switch"
+                                  label="Publicar este video en Famosos.com"
+                              />
+                            </div>
+                            {/* END IS PUBLIC */}
+
+                            <br/>
+                            <div className="text-center">
+                              {this.state.errors.length ? (
+                                  <div className={"mb-2"}>
+                                    <small className="text-danger">
+                                      Tienes campos obligatorios por llenar
+                                    </small>
+                                  </div>
+                              ) : null}
+                              {this.props.saveClientContractError ? (
+                                  <div className={"mb-2"}>
+                                    <small className="text-danger">
+                                      El pago no pudo ser procesado
+                                    </small>
+                                  </div>
+                              ) : null}
+                              {this.state.tokenizeCardError ? (
+                                  <div className={"mb-2"}>
+                                    <small className="text-danger">
+                                      {this.state.tokenizeCardError}
+                                    </small>
+                                  </div>
+                              ) : null}
+                              <button
+                                  disabled={
+                                    this.props.isLoading || this.state.tokenizeCardLoading
+                                  }
+                                  type="button"
+                                  className="contract-button hover cursor-pointer p-2 border bg-active"
+                                  onClick={this.createContract}
+                              >
+                                {this.props.isLoading || this.state.tokenizeCardLoading ? (
+                                    <span
+                                        className="spinner-grow spinner-grow-sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                    />
+                                ) : (
+                                    <span className="text-white">
+                                      Continuar
+                                      <i
+                                          className="fa fa-arrow-right float-right text-white ml-2"
+                                          style={{fontSize: "26px", position: "absolute"}}
+                                      />
+                                    </span>
+                                )}
+                              </button>
+                              <br/>
+                              <div className="mt-4 mx-auto text-center">
+                                <img width="230px" src={"/assets/img/pago-seguro.png"} alt={"pago-seguro"}/>
+                              </div>
+                              <br/>
+                              <br className={"d-block d-md-none"}/>
+                            </div>
+                          </>
+                          :
+                          <>
+                            <img
+                                src="https://joashdukes.com/wp-content/uploads/2020/02/lipsum.jpg"
+                                width="100%"
+                            />
+                            <img
+                                src="https://joashdukes.com/wp-content/uploads/2020/02/lipsum.jpg"
+                                width="100%"
+                            />
+                            <div className="text-center mt-4">
+                              <a href={"https://api.whatsapp.com/send?phone=+17865207235&text=¡Hola! Estoy interesada/o en contratar a " + this.props.celebrity.full_name +" para que grabe un Video para promocionar mi empresa. ¿Me podrías explicar el proceso?"}
+                                 target="_blank"
+                                 className={"whatsapp-link"}
+                              >
+                                <div>
+                                  Continuar
+                                  <i className="fa fa-arrow-circle-right text-white ml-2"/>
+                                </div>
+                              </a>
+                            </div>
+                          </>
+                    }
                   </Form>
-                  <br/>
-                  <div className="text-center">
-                    {this.state.errors.length ? (
-                        <div className={"mb-2"}>
-                          <small className="text-danger">
-                            Tienes campos obligatorios por llenar
-                          </small>
-                        </div>
-                    ) : null}
-                    {this.props.saveClientContractError ? (
-                        <div className={"mb-2"}>
-                          <small className="text-danger">
-                            El pago no pudo ser procesado
-                          </small>
-                        </div>
-                    ) : null}
-                    {this.state.tokenizeCardError ? (
-                        <div className={"mb-2"}>
-                          <small className="text-danger">
-                            {this.state.tokenizeCardError}
-                          </small>
-                        </div>
-                    ) : null}
-                    <button
-                        disabled={
-                          this.props.isLoading || this.state.tokenizeCardLoading
-                        }
-                        className="contract-button hover cursor-pointer p-2 border bg-active"
-                        onClick={this.createContract}
-                    >
-                      {this.props.isLoading || this.state.tokenizeCardLoading ? (
-                          <span
-                              className="spinner-grow spinner-grow-sm"
-                              role="status"
-                              aria-hidden="true"
-                          />
-                      ) : (
-                          <span className="text-white">
-                        Continuar
-                        <i
-                            className="fa fa-arrow-right float-right text-white ml-2"
-                            style={{fontSize: "26px", position: "absolute"}}
-                        />
-                      </span>
-                      )}
-                    </button>
-                    <br/>
-                    <div className="mt-4 mx-auto text-center">
-                      <img width="230px" src={"/assets/img/pago-seguro.png"} alt={"pago-seguro"}/>
-                    </div>
-                    <br/>
-                    <br className={"d-block d-md-none"}/>
-                  </div>
                 </div>
               </div>
             </div>
