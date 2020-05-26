b1a6e6a74dec12984986e22c772a436878b20e48
@duvanv <duvan@famosos.com>
2020-05-26 00:46:51 -0500

    mix-panel

--- react-app/src/state/utils/mixPanel.js
+++ react-app/src/state/utils/mixPanel.js
@@ -2,21 +2,19 @@ import mixpanel from 'mixpanel-browser';
 
 mixpanel.init('3c06764cad281ce6f4ca117975c119ef');
 
-let env_check = process.env.REACT_APP_ENVIRONMENT === 'local';
-
 let actions = {
     identify: (id) => {
-        if (env_check) mixpanel.identify(id);
+        mixpanel.identify(id);
     },
     alias: (id) => {
-        if (env_check) mixpanel.alias(id);
+        mixpanel.alias(id);
     },
     track: (name, props) => {
-        if (env_check) mixpanel.track(name, props);
+        mixpanel.track(name, props);
     },
     people: {
         set: (props) => {
-            if (env_check) mixpanel.people.set(props);
+            mixpanel.people.set(props);
         },
     },
 };
