fb179a12b953fa79e4e6b565924c8d89279fd4b2
@duvanv <duvan@famosos.com>
2020-08-19 14:16:00 -0500

    loading page

--- react-app/src/components/pages/loading/index.js
+++ react-app/src/components/pages/loading/index.js
@@ -10,27 +10,33 @@ class LoadingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            readyState: false,
+            readyState: true,
         };
     }
 
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                readyState: true,
-            })
-        }, 2600);
-        // CHECK VISIT
+        // setTimeout(() => {
+        //     this.setState({
+        //         readyState: true,
+        //     })
+        // }, 2600);
+        // // CHECK VISIT
+        // const session = new Session();
+        // if (session.isFirstVisit()) {
+        //     setTimeout(() => {
+        //         window.location.href = "https://landing.famosos.com";
+        //     }, 3000)
+        // } else {
+        //     setTimeout(() => {
+        //         history._pushRoute(PATHS.HOME_PATH)
+        //     }, 3000)
+        // }
         const session = new Session();
         if (session.isFirstVisit()) {
-            setTimeout(() => {
-                window.location.href = "https://landing.famosos.com";
-            }, 3000)
+            window.location.href = "https://landing.famosos.com";
         } else {
-            setTimeout(() => {
-                history._pushRoute(PATHS.HOME_PATH)
-            }, 3000)
+            history._pushRoute(PATHS.HOME_PATH)
         }
     }
 
