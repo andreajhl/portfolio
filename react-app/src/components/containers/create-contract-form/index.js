13c895db6cc2139b6c11a3b0e36875fd2601ba16
@duvanv <duvan@famosos.com>
2020-08-20 16:42:02 -0500

    create contract ok

--- react-app/src/components/containers/create-contract-form/index.js
+++ react-app/src/components/containers/create-contract-form/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import "./styles.scss";
 import {connect} from "react-redux";
-import {Col, Form, Row} from "react-bootstrap";
+import {Form} from "react-bootstrap";
 import PropTypes from 'prop-types';
 import {contractOperations} from "../../../state/ducks/contracts";
 import * as GTM from "../../../state/utils/gtm";
@@ -11,7 +11,7 @@ class CreateContractForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            contract_data: {
+            contractData: {
                 celebrity: null,
                 contractType: 1,
                 deliveryFrom: "",
@@ -19,38 +19,13 @@ class CreateContractForm extends Component {
                 deliveryType: 1,
                 deliveryContact: "",
                 instructions: "",
-                price: 0,
                 isPublic: true
             },
-            errors: []
+            showErrors: false,
         };
-        this.handleValue = this.handleValue.bind(this);
         this.handleIsPublic = this.handleIsPublic.bind(this);
         this.createContract = this.createContract.bind(this);
     }
-
-    showErrorMessage(field) {
-        if (field === "instructions_300") {
-            return (
-                <>
-                    {this.state.errors.includes(field) && (
-                        <Form.Text className="text-danger">
-                            El campo contiene mas de 300 caracteres
-                        </Form.Text>
-                    )}
-                </>
-            );
-        } else {
-            return (
-                <>
-                    {this.state.errors.includes(field) && (
-                        <Form.Text className="text-danger">Campo obligatorio</Form.Text>
-                    )}
-                </>
-            );
-        }
-    }
-
 
     sendBusinessRequestGTMEvent = () => {
         GTM.tagManagerDataLayer("BUSINESS_REQUEST", this.props.celebrity);
@@ -58,132 +33,92 @@ class CreateContractForm extends Component {
         window.open("https://landing-business.famosos.com/form", "_blank")
     };
 
-    renderFromTo() {
-        switch (this.state.contract_data.contractType) {
-            case 1:
-                return (
-                    <Row>
-                        <Col sm="4">
-                            <Form.Group>
-                                <Form.Label>
-                                    <b>Para:</b>
-                                </Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Anita"
-                                    name="deliveryTo"
-                                    value={this.state.contract_data.deliveryTo}
-                                    onChange={this.handleValue}
-                                />
-                                {this.showErrorMessage("deliveryTo")}
-                            </Form.Group>
-                        </Col>
-                        <Col sm="4">
-                            <Form.Group>
-                                <Form.Label>
-                                    <b>De:</b>
-                                </Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Duvan"
-                                    name="deliveryFrom"
-                                    value={this.state.contract_data.deliveryFrom}
-                                    onChange={this.handleValue}
-                                />
-                                {this.showErrorMessage("deliveryFrom")}
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                );
-            case 2:
-                return (
-                    <Row>
-                        <Col sm="4">
-                            <Form.Group>
-                                <Form.Label>
-                                    <b>Mi nombre es:</b>
-                                </Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Duvan"
-                                    name="deliveryTo"
-                                    value={this.state.contract_data.deliveryTo}
-                                    onChange={this.handleValue}
-                                />
-                                {this.showErrorMessage("deliveryTo")}
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                );
-            default:
-                return
-        }
-    }
-
-
     createContract() {
-        if (!this.props.isLoading || !this.state.tokenizeCardLoading) {
-            const contract_data = this.state.contract_data;
-
-            contract_data.celebrityId = this.props.celebrityId;
-
-            const errors = [];
-            if (contract_data.contractType === 1 && !contract_data.deliveryFrom) {
-                errors.push("deliveryFrom");
-            }
-            if (!contract_data.deliveryTo) {
-                errors.push("deliveryTo");
-            }
-            if (!contract_data.deliveryContact || !contract_data.deliveryContact.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
-                errors.push("deliveryContact");
-            }
-            if (!contract_data.instructions) {
-                errors.push("instructions");
-            }
-            if (contract_data.instructions.length > 300) {
-                errors.push("instructions_300");
-            }
-            this.setState({errors});
-            if (errors.length) {
-                return false;
-            } else {
-                GTM.tagManagerDataLayer("CONTRACT_CREATED", contract_data);
-                this.setState({contract_data}, () => {
-                    this.props.saveClientContract(this.state.contract_data);
-                });
-            }
-        }
-    }
-
-    handleValue(event) {
-        const contract_data = this.state.contract_data;
-        if (event.target.name === "contractType") {
-            contract_data[event.target.name] = parseInt(event.target.value);
+        this.setState({
+            ...this.state,
+            showErrors: false,
+        });
+        if (this.deliveryFromValidator() ||
+            this.deliveryToValidator() ||
+            this.instructionsValidator() ||
+            this.deliveryContactValidator()) {
+            this.setState({
+                ...this.state,
+                showErrors: true,
+            })
         } else {
-            contract_data[event.target.name] = event.target.value;
+            const contractData = this.state.contractData;
+            contractData.celebrityId = this.props.celebrityId;
+            GTM.tagManagerDataLayer("CONTRACT_CREATED", contractData);
+            this.setState({contractData}, () => {
+                this.props.saveClientContract(this.state.contractData);
+            });
         }
-        this.setState({
-            contract_data: contract_data
-        });
     }
 
-    contractTypeLabelClick(value) {
-        const contract_data = this.state.contract_data;
-        contract_data["contractType"] = value;
+    handleInputChange = (event) => {
+        const contractData = this.state.contractData;
+        if (event.target.value.length <= 300) {
+            contractData[event.target.name] = event.target.value;
+            this.setState({
+                contractData: contractData
+            });
+        }
+    };
+
+    handleContractTypeChange = (value) => {
+        const contractData = this.state.contractData;
+        contractData.contractType = parseInt(value);
         this.setState({
-            contract_data: contract_data
+            ...this.state,
+            contractData
         });
-    }
+    };
 
     handleIsPublic() {
-        const contract_data = this.state.contract_data;
-        contract_data.isPublic = !contract_data.isPublic;
+        const contractData = this.state.contractData;
+        contractData.isPublic = !contractData.isPublic;
         this.setState({
-            contract_data: contract_data
+            contractData: contractData
         });
     }
 
+
+    deliveryFromValidator = () => {
+        if (this.state.contractData.contractType === 2 && !this.state.contractData.deliveryFrom.length) {
+            return "Campo requerido";
+        }
+        return null
+    };
+
+    deliveryToValidator = () => {
+        if (!this.state.contractData.deliveryTo.length) {
+            return "Campo requerido";
+        }
+        return null
+    };
+
+    instructionsValidator = () => {
+        if (!this.state.contractData.instructions.length) {
+            return "Campo requerido";
+        }
+        return null
+    };
+
+    deliveryContactValidator = () => {
+        if (!this.state.contractData.deliveryContact.length) {
+            return "Campo requerido";
+        } else if (!this.state.contractData.deliveryContact.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+            return "Este correo no es válido";
+        }
+        return null
+    };
+
+
     render() {
+
+        const contractData = this.state.contractData;
+
         return (
             <div className="CreateContractForm">
 
@@ -201,7 +136,11 @@ class CreateContractForm extends Component {
                     {/* DELIVERY TO OPTIONS */}
                     <div className="mt-3">
                         <h6 className="subtitle">¿Para quién es este video?</h6>
-                        <div className={"delivery-option"}>
+                        <div className={"delivery-option"}
+                             onClick={(e) => {
+                                 e.preventDefault();
+                                 this.handleContractTypeChange(1)
+                             }}>
                             <div className={"delivery-option-icon"}>
                                 <i className="far fa-grin-stars"/>
                             </div>
@@ -209,12 +148,18 @@ class CreateContractForm extends Component {
                                 <span>Para mi</span>
                             </div>
                             <div className="delivery-option-control">
-                                <div className={"switch off"}>
-                                    <span className="active"/>
+                                <div
+                                    className={"switch" + (contractData.contractType === 1 ? " on " : " off ")}
+                                >
+                                    <span className={"active"}/>
                                 </div>
                             </div>
                         </div>
-                        <div className={"delivery-option"}>
+                        <div className={"delivery-option"}
+                             onClick={(e) => {
+                                 e.preventDefault();
+                                 this.handleContractTypeChange(2)
+                             }}>
                             <div className={"delivery-option-icon"}>
                                 <i className="fas fa-gift"/>
                             </div>
@@ -222,8 +167,10 @@ class CreateContractForm extends Component {
                                 <span>Para un amigo o familiar</span>
                             </div>
                             <div className="delivery-option-control">
-                                <div className={"switch on"}>
-                                    <span className="active"/>
+                                <div
+                                    className={"switch" + (contractData.contractType === 2 ? " on " : " off ")}
+                                >
+                                    <span className={"active"}/>
                                 </div>
                             </div>
                         </div>
@@ -231,13 +178,12 @@ class CreateContractForm extends Component {
                         Si quieres un video comercial para tu marca haz clic
                             {" "}
                             <span className={"text-primary"} onClick={this.sendBusinessRequestGTMEvent}>aquí.</span>
-                    </span>
+                        </span>
                     </div>
-
                     <br/>
-
                     {/* FORM INPUTS */}
-                    <div className={"form-custom-horizontal-group"}>
+                    <div
+                        className={"form-custom-horizontal-group" + (contractData.contractType === 1 ? " hide " : "")}>
                         <div className={"form-custom-label"}>
                             <label>De:</label>
                         </div>
@@ -245,7 +191,14 @@ class CreateContractForm extends Component {
                             <input
                                 type={"text"}
                                 placeholder={"Duvan"}
+                                value={contractData.deliveryFrom}
+                                name={"deliveryFrom"}
+                                onChange={this.handleInputChange}
                             />
+                            <span
+                                className={"text-danger" + (this.state.showErrors ? " show-error " : " hide-error ")}>
+                                {this.deliveryFromValidator()}
+                            </span>
                         </div>
                     </div>
                     <div className={"form-custom-horizontal-group"}>
@@ -256,38 +209,68 @@ class CreateContractForm extends Component {
                             <input
                                 type={"text"}
                                 placeholder={"Ana"}
+                                value={contractData.deliveryTo}
+                                name={"deliveryTo"}
+                                onChange={this.handleInputChange}
                             />
+                            <span
+                                className={"text-danger" + (this.state.showErrors ? " show-error " : " hide-error ")}>
+                                {this.deliveryToValidator()}
+                            </span>
                         </div>
                     </div>
                     <div className={"mt-3"}>{""}</div>
                     <div className={"form-custom-vertical-group"}>
                         <label>¿Qué quieres que diga Alan Tacher?</label>
                         <textarea
-                            type={"text"}
                             rows={5}
-                            placeholder={"Ejemplo: Mi amiga Ana cumple años el 12 de agosto, me gustaría que la felicitaras."}
+                            placeholder={
+                                contractData.contractType === 2
+                                    ? "Ejemplo: Mi amiga Ana cumple años el 12 de agosto, me gustaría que la felicitaras."
+                                    : "Mis instrucciones son"
+                            }
+                            value={contractData.instructions}
+                            name={"instructions"}
+                            onChange={this.handleInputChange}
                         />
+                        <div
+                            className={"text-left" + (contractData.instructions.length === 300 ? " text-danger " : " text-muted ")}>
+                            {contractData.instructions.length}/300 caracteres permitidos
+                        </div>
+                        <span
+                            className={"text-danger" + (this.state.showErrors ? " show-error " : " hide-error ")}>
+                            {this.instructionsValidator()}
+                        </span>
                     </div>
                     <div className={"form-custom-vertical-group"}>
                         <label>Correo eléctronico de entrega</label>
                         <input
                             type={"email"}
                             placeholder={"correo@dominio.com"}
+                            value={contractData.deliveryContact}
+                            name={"deliveryContact"}
+                            onChange={this.handleInputChange}
                         />
+                        <span
+                            className={"text-danger" + (this.state.showErrors ? " show-error " : " hide-error ")}>
+                            {this.deliveryContactValidator()}
+                        </span>
                     </div>
-                    <div className={"mt-2"}>{""}</div>
+                    <div className={"mt-3"}>{""}</div>
                     <Form.Check
-                        checked={this.state.contract_data.isPublic}
-                        onChange={this.handleIsPublic}
                         type="switch"
                         id="custom-switch"
                         label="Publicar este video en Famosos.com"
+                        checked={contractData.isPublic}
+                        onChange={this.handleIsPublic}
                     />
                     <div className={"mt-4"}>{""}</div>
+                    <div
+                        className={"text-danger text-center" + (this.props.saveClientContractError != null ? " mb-3 " : "")}>
+                        {this.props.saveClientContractError}
+                    </div>
                     <button
-                        disabled={
-                            this.props.isLoading || this.state.tokenizeCardLoading
-                        }
+                        disabled={this.props.saveClientContractLoading}
                         type={"button"}
                         className={"btn f-contract-button text-align-center"}
                         onClick={this.createContract}
@@ -303,7 +286,6 @@ class CreateContractForm extends Component {
                         />
                         <br/>
                     </div>
-
                 </div>
             </div>
         );
@@ -329,6 +311,7 @@ CreateContractForm.defaultProps = {
 
 // mapStateToProps
 const mapStateToProps = (state) => ({
+    saveClientContractLoading: state.contracts.saveClientContractReducer.loading,
     saveClientContractError: state.contracts.saveClientContractReducer.error_data.error
 });
 
