b4140770d7c0a5475da84e86aca8473007d47d69
@duvanv <duvan@famosos.com>
2020-08-20 16:52:42 -0500

    SSR and form

--- server.js
+++ server.js
@@ -1,14 +1,122 @@
 const express = require('express');
 const path = require('path');
-
+const axios = require('axios');
+const fs = require('fs');
 const app = express();
+require("dotenv").config({path: ".env"});
+
+// default OG
+function defaultOG(data) {
+    data = data.replace(/\$OG_TITLE/g, 'Famosos.com');
+    data = data.replace(/\$OG_TYPE/g, 'website');
+    data = data.replace(/\$OG_URL/g, 'https://www.famosos.com');
+    data = data.replace(/\$OG_IMAGE/g, 'https://www.famosos.com/assets/img/favicon.png');
+    data = data.replace(/\$OG_SITE_NAME/g, 'Famosos.com');
+    data = data.replace(/\$OG_DESCRIPTION/g, 'Videos personalizados de tus Famosos favoritos. Reserva tu video y disfruta de experiencias únicas.');
+    data = data.replace(/\$OG_VIDEO/g, 'https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5');
+    data = data.replace(/\$OG_VIDEO_URL/g, 'https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5');
+    data = data.replace(/\$OG_VIDEO_SECURE_URL/g, 'https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5');
+    data = data.replace(/\$OG_VIDEO_WITH/g, '400');
+    data = data.replace(/\$OG_VIDEO_HEIGHT/g, '400');
+    return data;
+}
+
+// default response
+function defaultResponse(fs, filePath, res) {
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        res.send(defaultOG(data));
+    });
+}
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handles any requests that don't match the ones above
+// CELEBRITY PROFILE
+app.get('/:celebrity_username', async (req, res) => {
+    const filePath = path.resolve(__dirname, './build', 'index.html');
+    if (req.params.contract_reference !== "inicio" && req.params.contract_reference.length > 0) {
+        await axios
+            .get(process.env.REACT_APP_ENDPOINT + "custom-endpoints/celebrities/public-get/" + req.params.celebrity_username)
+            .then((r) => {
+                if (r.data.status === "OK") {
+                    fs.readFile(filePath, 'utf8', function (err, data) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        data = data.replace(/\$OG_TITLE/g, 'Famosos.com - ' + r.data.data.fullName);
+                        data = data.replace(/\$OG_TYPE/g, 'website');
+                        data = data.replace(/\$OG_URL/g, 'https://www.famosos.com/' + r.data.data.username);
+                        data = data.replace(/\$OG_IMAGE/g, r.data.data.avatar);
+                        data = data.replace(/\$OG_SITE_NAME/g, 'Famosos.com');
+                        data = data.replace(/\$OG_DESCRIPTION/g, "Perfil oficial de " + r.data.data.username + " en Famosos.com. Reserva tu video personalizado y disfruta de experiencias únicas.");
+                        data = data.replace(/\$OG_VIDEO/g, r.data.data.mainVideo);
+                        data = data.replace(/\$OG_VIDEO_URL/g, r.data.data.mainVideo);
+                        data = data.replace(/\$OG_VIDEO_SECURE_URL/g, r.data.data.mainVideo);
+                        data = data.replace(/\$OG_VIDEO_WITH/g, '400');
+                        data = data.replace(/\$OG_VIDEO_HEIGHT/g, '400');
+                        res.send(data);
+                    });
+                } else {
+                    defaultResponse(fs, filePath, res);
+                }
+            })
+            .catch((e) => {
+                defaultResponse(fs, filePath, res);
+            });
+    } else {
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            res.send(defaultOG(data));
+        });
+    }
+
+
+});
+
+// HIRING PREVIEW
+app.get('/hirings/:contract_reference', async (req, res) => {
+    const filePath = path.resolve(__dirname, './build', 'index.html');
+    await axios
+        .get(process.env.REACT_APP_ENDPOINT + "custom-endpoints/contracts/get-contract-by-reference/" + req.params.contract_reference)
+        .then((r) => {
+            if (r.data.status === "OK") {
+                fs.readFile(filePath, 'utf8', function (err, data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    data = data.replace(/\$OG_TITLE/g, 'Famosos.com - ' + r.data.data.celebrityData.fullName);
+                    data = data.replace(/\$OG_TYPE/g, 'website');
+                    data = data.replace(/\$OG_URL/g, 'https://www.famosos.com/' + r.data.data.celebrityData.username);
+                    data = data.replace(/\$OG_IMAGE/g, r.data.data.celebrityData.avatar);
+                    data = data.replace(/\$OG_SITE_NAME/g, 'Famosos.com');
+                    data = data.replace(/\$OG_DESCRIPTION/g, 'Video personalizado de ' + r.data.data.celebrityData.fullName + " hecho en Famosos.com. Tu lugar favorito para contratar a celebridades.");
+                    data = data.replace(/\$OG_VIDEO/g, r.data.data.media);
+                    data = data.replace(/\$OG_VIDEO_URL/g, r.data.data.media);
+                    data = data.replace(/\$OG_VIDEO_SECURE_URL/g, r.data.data.media);
+                    data = data.replace(/\$OG_VIDEO_WITH/g, '400');
+                    data = data.replace(/\$OG_VIDEO_HEIGHT/g, '400');
+                    res.send(data);
+                });
+            } else {
+                defaultResponse(fs, filePath, res);
+            }
+        })
+        .catch((e) => {
+            defaultResponse(fs, filePath, res);
+        });
+});
+
+
+// OTHER PAGES
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    // res.sendFile(path.join(__dirname + '/build/index.html'));
+    const filePath = path.resolve(__dirname, './build', 'index.html');
+    defaultResponse(fs, filePath, res);
 });
 
 const port = process.env.PORT || 5000;
