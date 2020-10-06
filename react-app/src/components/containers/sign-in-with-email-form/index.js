ddb0ecef1a68ecb6c8cd8865a5f08fa1e9438b61
German Solano <kontrolg1239@gmail.com>
2020-10-06 07:01:48 -0400

    Corregir error menor en SignWithEmail/n- El valor inicial del input era null, por lo tanto cambiaba de uncontrolled a controlled

--- react-app/src/components/containers/sign-in-with-email-form/index.js
+++ react-app/src/components/containers/sign-in-with-email-form/index.js
@@ -1,163 +1,168 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./styles.scss";
-import {authenticationOperations} from "../../../state/ducks/authentication";
-import {connect} from "react-redux";
-import {AuthTCLayout} from "../../layouts/auth-t&c";
-import {SignInMethodsForm} from "../sign-in-methods-form";
-import {history} from "../../../routing/History";
+import { authenticationOperations } from "../../../state/ducks/authentication";
+import { connect } from "react-redux";
+import { AuthTCLayout } from "../../layouts/auth-t&c";
+import { SignInMethodsForm } from "../sign-in-methods-form";
+import { history } from "../../../routing/History";
 import * as PATHS from "../../../routing/Paths";
 import * as GTM from "../../../state/utils/gtm";
 
 class SignInWithEmailForm extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            email: this.props.email,
-            password: ""
-        };
-
-        this.handleInput = this.handleInput.bind(this);
-        this.signInWithEmail = this.signInWithEmail.bind(this);
-        this.handleKeyPress = this.handleKeyPress.bind(this);
-        this.goToSignUp = this.goToSignUp.bind(this);
-        this.goToResetPassword = this.goToResetPassword.bind(this);
-    }
-
-    handleInput(event) {
-        this.setState({[event.target.name]: event.target.value})
-    }
-
-    handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            this.signInWithEmail();
-        }
+    this.state = {
+      email: this.props.email || "",
+      password: ""
     };
 
-    signInWithEmail(e) {
-        if (!this.props.signInWithEmailLoading) {
-            this.props.signInWithEmail({
-                email: this.state.email.trim(),
-                password: this.state.password,
-            });
-            GTM.tagManagerDataLayer(
-                "CLICK_ON_SEND_SMS_SECURITY_CODE",
-                {
-                    email: this.state.email,
-                }
-            );
-        }
+    this.handleInput = this.handleInput.bind(this);
+    this.signInWithEmail = this.signInWithEmail.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.goToSignUp = this.goToSignUp.bind(this);
+    this.goToResetPassword = this.goToResetPassword.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      this.signInWithEmail();
     }
+  };
 
-    goToSignUp() {
-        history._pushRoute(PATHS.SIGN_UP_WITH_SPECIFIC_FORM_PATH.replace(":form", "email-form"))
+  signInWithEmail(e) {
+    if (!this.props.signInWithEmailLoading) {
+      this.props.signInWithEmail({
+        email: this.state.email.trim(),
+        password: this.state.password
+      });
+      GTM.tagManagerDataLayer("CLICK_ON_SEND_SMS_SECURITY_CODE", {
+        email: this.state.email
+      });
     }
+  }
 
-    goToResetPassword() {
-        history._pushRoute(PATHS.RESET_PASSWORD_PATH)
-    }
+  goToSignUp() {
+    history._pushRoute(
+      PATHS.SIGN_UP_WITH_SPECIFIC_FORM_PATH.replace(":form", "email-form")
+    );
+  }
 
-    render() {
-        return (
-            <div className="SignInWithEmailForm">
-                <form
-                    onSubmit={(e) => e.preventDefault()}
-                    autoComplete="on"
-                    onScroll={() => {
-                        const a = document.activeElement;
-                        a.blur();
-                        a.focus();
-                    }}
-                >
-                    <div className="form-group">
-                    <h6>Ingresa con tu correo electrónico</h6>
-                    <input
-                        type="email"
-                        className="form-control mb-3"
-                        placeholder="Escribe tu correo"
-                        name="email"
-                        onChange={this.handleInput}
-                        value={this.state.email}
-                    />
-                    </div>
-                    <div className="form-group">
-                        <h6>Contraseña</h6>
-                        <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Escribe tu contraseña"
-                            name="password"
-                            onChange={this.handleInput}
-                            value={this.state.password}
-                            onKeyPress={this.handleKeyPress}
-                        />
-                        {
-                            this.props.signInWithEmailError
-                            &&
-                            <p className="instructions mt-4 text-danger">
-                                {this.props.signInWithEmailError}
-                            </p>
-                        }
-                    </div>
-                    <button className="send-button"
-                            disabled={!this.state.email || !this.state.password}
-                            onClick={this.signInWithEmail}
-                    >
-                        {
-                            this.props.signInWithEmailLoading
-                                ?
-                                <span className="text-white spinner-grow spinner-grow-sm"
-                                      role="status"
-                                      aria-hidden="true"
-                                />
-                                :
-                                <span className={"text-white"}>Continuar</span>
+  goToResetPassword() {
+    history._pushRoute(PATHS.RESET_PASSWORD_PATH);
+  }
 
-                        }
-                    </button>
-                </form>
-                <div className="mb-2">
-                    <div className="mt-2 cursor-pointer">
-                        <small className="font-weight-normal" onClick={this.goToSignUp}>
-                            Crear una cuenta
-                        </small>
-                    </div>
-                    <div className="mt-2 cursor-pointer">
-                        <small className="font-weight-normal" onClick={this.goToResetPassword}>
-                            Olvidé mi contraseña
-                        </small>
-                    </div>
-                </div>
-                <SignInMethodsForm cellphone={true} whatsapp={true}/>
-                <AuthTCLayout/>
-            </div>
-        );
-    };
-
+  render() {
+    return (
+      <div className="SignInWithEmailForm">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          autoComplete="on"
+          onScroll={() => {
+            const a = document.activeElement;
+            a.blur();
+            a.focus();
+          }}
+        >
+          <div className="form-group">
+            <h6>Ingresa con tu correo electrónico</h6>
+            <input
+              type="email"
+              className="form-control mb-3"
+              placeholder="Escribe tu correo"
+              name="email"
+              onChange={this.handleInput}
+              value={this.state.email}
+            />
+          </div>
+          <div className="form-group">
+            <h6>Contraseña</h6>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Escribe tu contraseña"
+              name="password"
+              onChange={this.handleInput}
+              value={this.state.password}
+              onKeyPress={this.handleKeyPress}
+            />
+            {this.props.signInWithEmailError && (
+              <p className="instructions mt-4 text-danger">
+                {this.props.signInWithEmailError}
+              </p>
+            )}
+          </div>
+          <button
+            className="send-button"
+            disabled={!this.state.email || !this.state.password}
+            onClick={this.signInWithEmail}
+          >
+            {this.props.signInWithEmailLoading ? (
+              <span
+                className="text-white spinner-grow spinner-grow-sm"
+                role="status"
+                aria-hidden="true"
+              />
+            ) : (
+              <span className={"text-white"}>Continuar</span>
+            )}
+          </button>
+        </form>
+        <div className="mb-2">
+          <div className="mt-2 cursor-pointer">
+            <small className="font-weight-normal" onClick={this.goToSignUp}>
+              Crear una cuenta
+            </small>
+          </div>
+          <div className="mt-2 cursor-pointer">
+            <small
+              className="font-weight-normal"
+              onClick={this.goToResetPassword}
+            >
+              Olvidé mi contraseña
+            </small>
+          </div>
+        </div>
+        <SignInMethodsForm cellphone={true} whatsapp={true} />
+        <AuthTCLayout />
+      </div>
+    );
+  }
 }
 
 // Set propTypes
-SignInWithEmailForm.propTypes = {};
+SignInWithEmailForm.propTypes = {
+  email: PropTypes.string
+};
 
 // Set defaultProps
 SignInWithEmailForm.defaultProps = {
-    email: ""
+  email: ""
 };
 
 // mapStateToProps
 const mapStateToProps = (state: any) => ({
-    signInWithEmailLoading: state.authentication.signInWithEmailReducer.loading,
-    signInWithEmailCompleted: state.authentication.signInWithEmailReducer.completed,
-    signInWithEmailError: state.authentication.signInWithEmailReducer.error_data.error,
-    signInWithEmailData: state.authentication.signInWithEmailReducer.data,
+  signInWithEmailLoading: state.authentication.signInWithEmailReducer.loading,
+  signInWithEmailCompleted:
+    state.authentication.signInWithEmailReducer.completed,
+  signInWithEmailError:
+    state.authentication.signInWithEmailReducer.error_data.error,
+  signInWithEmailData: state.authentication.signInWithEmailReducer.data
 });
 
 // mapStateToProps
 const mapDispatchToProps = {
-    signInWithEmail: authenticationOperations.signInWithEmail,
+  signInWithEmail: authenticationOperations.signInWithEmail
 };
 
 // Export Class
-const _SignInWithEmailForm = connect(mapStateToProps, mapDispatchToProps)(SignInWithEmailForm);
-export {_SignInWithEmailForm as SignInWithEmailForm};
+const _SignInWithEmailForm = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignInWithEmailForm);
+export { _SignInWithEmailForm as SignInWithEmailForm };
