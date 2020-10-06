63b2acc11eb8b1ae4d55e04cd6569445bf7fcdff
German Solano <kontrolg1239@gmail.com>
2020-10-06 12:10:51 -0400

    Cambiar CookiesConsent para que concuerde con el de la Landing

--- react-app/src/components/layouts/cookies-consent/index.js
+++ react-app/src/components/layouts/cookies-consent/index.js
@@ -1,59 +1,66 @@
-import React, {Component} from 'react';
-import 'react-flags-select/scss/react-flags-select.scss';
+import React, { Component } from "react";
+import "react-flags-select/scss/react-flags-select.scss";
 import "./styles.scss";
-import {history} from "../../../routing/History";
+import { history } from "../../../routing/History";
 import * as PATHS from "../../../routing/Paths";
 
 class CookiesConsent extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      showBanner: localStorage.getItem("hasAcceptedCookiesConsent") !== "true"
+    };
+  }
 
-        this.state = {
-            showBanner: !localStorage.getItem("showCookiesConsent")
-        }
+  goToTerms = () => {
+    history._pushRoute(PATHS.POLICIES_PATH);
+  };
+
+  hideBanner = () => {
+    localStorage.setItem("hasAcceptedCookiesConsent", true);
+    this.setState({
+      hideBanner: true
+    });
+  };
+
+  removeCookieConsent = ({ propertyName }) => {
+    if (propertyName === "opacity") {
+      this.setState({ showBanner: false });
     }
+  };
 
-    goToTerms = () => {
-        history._pushRoute(PATHS.POLICIES_PATH)
-    };
-
-
-    hideBanner = () => {
-        localStorage.setItem("showCookiesConsent", "false");
-        this.setState({
-            showBanner: false
-        })
-    };
-
-    render() {
-        if (this.state.showBanner) {
-            return (
-                <div className="CookiesConsent">
-                    <div className={"content"}>
-                        <div>
-                            En nuestro sitio web utilizamos cookies propias y de terceros para mejorar la experiencia de
-                            usuario, rendimiento, análisis y otro fines. Al hacer clic en aceptar o utilizar nuestro
-                            sitio, estarás aceptando el uso de estas cookies.
-                        </div>
-                        <div className={"text-justify mt-2 "}>
-                            <div className={"terms float-left"} onClick={this.goToTerms}>
-                                Ver las políticas de privacidad
-                            </div>
-                            <div className="button mr-1 font-weight-bold p-2 float-right" onClick={this.hideBanner}>
-                                Aceptar
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            );
-        } else {
-            return <div/>
-        }
-    };
-
+  render() {
+    return this.state.showBanner ? (
+      <div
+        className={`cookies-consent ${this.state.hideBanner ? "hidden" : ""}`}
+        onTransitionEnd={this.removeCookieConsent}
+      >
+        <div className="cookies-consent__text">
+          En nuestro sitio web utilizamos cookies propias y de terceros para
+          mejorar la experiencia de usuario, rendimiento, análisis y otro fines.
+          Al hacer clic en aceptar o utilizar nuestro sitio, estarás aceptando
+          el uso de estas cookies.
+        </div>
+        <div className="cookies-consent__options">
+          <a
+            onClick={this.goToTerms}
+            className="cookies-consent__terms-link float-left"
+          >
+            Ver las políticas de privacidad
+          </a>
+          <button
+            className="cookies-consent__accept-button"
+            onClick={this.hideBanner}
+          >
+            Aceptar
+          </button>
+        </div>
+      </div>
+    ) : null;
+  }
 }
 
 // Set defaultProps
 CookiesConsent.defaultProps = {};
-export {CookiesConsent};
+export { CookiesConsent };
