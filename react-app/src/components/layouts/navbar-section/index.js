f8366d6933f5468dc56f933930cf52a041409200
German Solano <german@famosos.com>
2020-11-03 17:37:56 -0400

    Maquetar nueva sección Hero

--- react-app/src/components/layouts/navbar-section/index.js
+++ react-app/src/components/layouts/navbar-section/index.js
@@ -96,6 +96,43 @@ const NavbarSectionLayout = (props) => {
           </div>
         </div>
       </div>
+      <div className="hero">
+        <div className="container hero__container">
+          <h1 className="hero__title mb-3">
+            Recibe videos personalizados de tus ídolos.
+          </h1>
+          <ol className="hero__steps p-0">
+            <li className="hero__steps-item">
+              <img
+                className="hero__steps-image"
+                src="/assets/img/paper-plain.svg"
+                alt="Enviando"
+              />
+              <p className="hero__steps-text">
+                Elige a tu Famoso y solicita el mensaje.
+              </p>
+            </li>
+            <li className="hero__steps-item">
+              <img
+                className="hero__steps-image"
+                src="/assets/img/credit-card-small-white.svg"
+                alt="Pagando"
+              />
+              <p className="hero__steps-text">Completa el proceso de pago.</p>
+            </li>
+            <li className="hero__steps-item">
+              <img
+                className="hero__steps-image"
+                src="/assets/img/play.svg"
+                alt="Recibiendo"
+              />
+              <p className="hero__steps-text">
+                ¡Recibe tu video personalizado!
+              </p>
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
