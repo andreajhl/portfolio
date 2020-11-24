8c9e14fe4f9b01d8b0cbd2aeaf1fd964558736ff
German Solano <german@famosos.com>
2020-11-24 14:01:56 -0400

    Añadir textos de sugerencia en los textos de prellenado en Contratar Famoso

--- react-app/src/constants/options.js
+++ react-app/src/constants/options.js
@@ -1,87 +1,87 @@
 export const occasionsData = {
   BIRTHDAY: {
-    icon: 'fa-birthday-cake',
-    title: 'Cumpleaños',
+    icon: "fa-birthday-cake",
+    title: "Cumpleaños",
     messages: [
-      '¡Hola (Famoso)! El XXXX cumplo XX años y quisiera que por favor me felicites. Soy tu fan y me encanta lo que haces.',
-      '¡Hola (Famoso)! Muchas gracias por esta oportunidad. (<<PARA>>) cumple XX años pronto. Quisiera que por favor le mandes una felicitación super especial. '
-    ],
+      "¡Hola (Famoso)! El [FECHA] cumplo [CANTIDAD] años y quisiera que por favor me felicites. Soy tu fan y me encanta lo que haces.",
+      "¡Hola (Famoso)! Muchas gracias por esta oportunidad. (<<PARA>>) cumple [CANTIDAD] años pronto. Quisiera que por favor le mandes una felicitación súper especial. "
+    ]
   },
   LOVE: {
-    icon: 'fa-heart',
-    title: 'Amor',
+    icon: "fa-heart",
+    title: "Amor",
     messages: [
-		'',
-		'¡Hola (Famoso)! Quisiera regalarle un saludo tuyo a (<<PARA>>) ya que ',
-    ],
+      "",
+      "¡Hola (Famoso)! Quisiera regalarle un saludo tuyo a (<<PARA>>) ya que "
+    ]
   },
   SPECIAL_OCCASION: {
-    icon: 'fa-glass-cheers',
-    title: 'Especial',
+    icon: "fa-glass-cheers",
+    title: "Especial",
     messages: [
-      '¡Hola (Famoso)! Tengo XXXX ocasión especial y me encantaría que me apoyes con un video personalizado felicitandome. ',
-      '¡Hola (Famoso)! Me encantaría soprender a (<<PARA>>) en esta ocasión especial. ',
-    ],
+      "¡Hola (Famoso)! Tengo [OCASIÓN] ocasión especial y me encantaría que me apoyes con un video personalizado felicitándome.",
+      "¡Hola (Famoso)! Me encantaría sorprender a (<<PARA>>) en esta ocasión especial."
+    ]
   },
   SONG: {
-    icon: 'fa-music',
-    title: 'Canción',
+    icon: "fa-music",
+    title: "Canción",
     messages: [
-      '¡Hola (Famoso)! ¿Podrías cantarme a un pedazo de la canción XXXX? Muchas gracias. ',
-      '¡Hola (Famoso)! ¿Podrías cantarle a (<<PARA>>) un pedazo de la canción XXXX?',
-    ],
+      "¡Hola (Famoso)! ¿Podrías cantarme un pedazo de la canción [CANCIÓN]? Muchas gracias.",
+      "¡Hola (Famoso)! ¿Podrías cantarle a (<<PARA>>) un pedazo de la canción [CANCIÓN]?"
+    ]
   },
   CHEER_UP: {
-    icon: 'fa-spa',
-    title: 'Ánimo',
+    icon: "fa-spa",
+    title: "Ánimo",
     messages: [
-      '¡Hola (Famoso)! Estos días he estado con los ánimos muy bajos. ¿Podrías por favor mandarme un mensaje de ánimo? Muchas gracias.',
-      '¡Hola (Famoso)! (<<PARA>>) está de ánimos muy bajos. ¿Podrías por favor mandarle un mensaje de ánimo? Muchas gracias. ',
-    ],
+      "¡Hola (Famoso)! Estos días he estado con los ánimos muy bajos. ¿Podrías por favor mandarme un mensaje de ánimo? Muchas gracias.",
+      "¡Hola (Famoso)! (<<PARA>>) está de ánimos muy bajos. ¿Podrías por favor mandarle un mensaje de ánimo? Muchas gracias."
+    ]
   },
   MAKE_SMILE: {
-    icon: 'fa-laugh',
-    title: 'Hacer sonreir',
+    icon: "fa-laugh",
+    title: "Hacer sonreír",
     messages: [
-      '',
-      '¡Hola (Famoso)! Quisiera que por favor le saques una sonrisa a (<<PARA>>). ',
-    ],
+      "",
+      "¡Hola (Famoso)! Quisiera que por favor le saques una sonrisa a (<<PARA>>)."
+    ]
   },
   MOTIVATION: {
-    icon: 'fa-bolt',
-    title: 'Motivación',
+    icon: "fa-bolt",
+    title: "Motivación",
     messages: [
-      '¡Hola (Famoso)! Estos días he estado muy desmotivado. ¿Me podrías enviar un mensaje motivacional? Muchas gracias. ',
-      '¡Hola (Famoso)! Por favor ayudame a motivar a (<<PARA>>). ',
-    ],
+      "¡Hola (Famoso)! Estos días he estado muy desmotivado. ¿Me podrías enviar un mensaje motivacional? Muchas gracias.",
+      "¡Hola (Famoso)! Por favor ayudame a motivar a (<<PARA>>)."
+    ]
   },
   JOKE: {
-    icon: 'fa-grin-squint',
-    title: 'Broma',
+    icon: "fa-grin-squint",
+    title: "Broma",
     messages: [
-      '¡Hola (Famoso)! Me encantaría que me hagas una broma. Soy tu fan y me río muchísimo contigo. Gracias por esta oportunidad.  ',
-      '¡Hola (Famoso)! Me encantaría hacerle una broma a (<<PARA>>). ',
-    ],
+      "¡Hola (Famoso)! Me encantaría que me hagas una broma. Soy tu fan y me río muchísimo contigo. Gracias por esta oportunidad.",
+      "¡Hola (Famoso)! Me encantaría hacerle una broma a (<<PARA>>)."
+    ]
   },
   HOPE: {
-    icon: 'fa-dove',
-    title: 'Esperanza',
+    icon: "fa-dove",
+    title: "Esperanza",
     messages: [
-      '',
-      '¡Hola (Famoso)! Estos meses han sido unos meses muy dificiles. ¿Podrías enviarle un mensaje de esperanza a (<<PARA>>)?',
-    ],
+      "",
+      "¡Hola (Famoso)! Estos meses han sido muy difíciles. ¿Podrías enviarle un mensaje de esperanza a (<<PARA>>)?"
+    ]
   },
   ASK_FOR_FORGIVENESS: {
-    icon: 'fa-hand-holding-heart',
-    title: 'Perdón',
+    icon: "fa-hand-holding-heart",
+    title: "Perdón",
     messages: [
-      '',
-      '¡Hola (Famoso)! Quisiera que (<<PARA>>) me perdone por XXXX. Me ayudas por favor diciendole este mensaje. Gracias.',
-    ],
+      "",
+      "¡Hola (Famoso)! Quisiera que (<<PARA>>) me perdone por [SITUACIÓN]. Me ayudas por favor diciéndole este mensaje. Gracias."
+    ]
   },
   OTHER: {
-    icon: 'fa-paper-plane',
-    title: 'Otro',
-    messages: ['¡Hola (Famoso)! ', '¡Hola (Famoso)! '],
-  },
+    icon: "fa-paper-plane",
+    title: "Otro",
+    messages: ["¡Hola (Famoso)! ", "¡Hola (Famoso)! "]
+  }
 };
