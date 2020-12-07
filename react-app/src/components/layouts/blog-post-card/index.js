5d36fc307a4c788ab4dba4b5741e5a6b90cddad6
Isaac Peña <isaac.pr99@gmail.com>
2020-12-07 13:55:24 -0400

    Se ajusta el height de los titulos de inicio para la seccion de blog-post

--- react-app/src/components/layouts/blog-post-card/index.js
+++ react-app/src/components/layouts/blog-post-card/index.js
@@ -11,8 +11,8 @@ const BlogPostCardLayout = ({ title, imageUrl, description, postUrl, idPost,hist
   return (
     <Card className="BlogPostCardLayout">
       <Card.Img variant="top" src={imageUrl} />
-      <Card.Body className="px-0">
-        <Card.Title className="font-weight-bold">{title}</Card.Title>
+      <Card.Body className="px-0" >
+        <Card.Title className="font-weight-bold" style={{minHeight:'50px'}}>{title}</Card.Title>
         <Card.Text>
           <span className="d-sm-none">{limitString(plainString, 80)}</span>
           <span className="d-none d-sm-block">
