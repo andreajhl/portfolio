84e94e05acffa07b6d0137090bbc01f56f55e4d5
Isaac Peña <isaac.pr99@gmail.com>
2020-12-07 14:02:33 -0400

    Se agrega limite de strings a titulos de blogs en inicio

--- react-app/src/components/layouts/blog-post-card/index.js
+++ react-app/src/components/layouts/blog-post-card/index.js
@@ -12,7 +12,7 @@ const BlogPostCardLayout = ({ title, imageUrl, description, postUrl, idPost,hist
     <Card className="BlogPostCardLayout">
       <Card.Img variant="top" src={imageUrl} />
       <Card.Body className="px-0" >
-        <Card.Title className="font-weight-bold" style={{minHeight:'50px'}}>{title}</Card.Title>
+        <Card.Title className="font-weight-bold" style={{minHeight:'50px'}}>{limitString(title, 70)}</Card.Title>
         <Card.Text>
           <span className="d-sm-none">{limitString(plainString, 80)}</span>
           <span className="d-none d-sm-block">
