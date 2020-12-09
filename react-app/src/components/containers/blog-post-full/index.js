0294c0649ca055fe31356c901ccf848131ddf63b
German Solano <german@famosos.com>
2020-12-09 05:40:07 -0400

    Correcciones menores en sección de Blogs

--- react-app/src/components/containers/blog-post-full/index.js
+++ react-app/src/components/containers/blog-post-full/index.js
@@ -1,35 +1,43 @@
-import React from 'react';
-import {Card, Button} from 'react-bootstrap';
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
-import './styles.scss';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2
+} from "react-html-parser";
+import "./styles.scss";
 
-
-function transform(node,index) {
-    // do not render any <span> tags
-    if (node.type === 'tag' && node.name === 'figure') {
-      return null;
-    }
-    if (node.type === 'tag' && node.name === 'strong') {
-      node.attribs.class = 'blog__text-bold';
-      node.name = 'span';
-      return convertNodeToElement(node, index, transform);
+function transform(node, index) {
+  // do not render any <span> tags
+  if (node.type === "tag" && node.name === "figure") {
+    return null;
+  }
+  if (node.type === "tag" && node.name === "strong") {
+    node.attribs.class = "blog__text-bold";
+    node.name = "span";
+    return convertNodeToElement(node, index, transform);
   }
 }
-const BlogPostFull = ({title, thumbnail, content,pubDate}) => {
-  const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const pubDateParse= new Date(pubDate);
+const BlogPostFull = ({ title, thumbnail, content, pubDate }) => {
+  const optionsDate = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+  const pubDateParse = new Date(pubDate);
   const htmlToParse = content;
-  const JSX = ReactHtmlParser(htmlToParse, {transform});
+  const JSX = ReactHtmlParser(htmlToParse, { transform });
   return (
-    <Card className='mb-5'>
-      <Card.Img variant='top' alt={title} fluid='true' src={thumbnail} />
+    <Card className="mb-5">
+      <Card.Img variant="top" alt={title} fluid="true" src={thumbnail} />
       <Card.Body>
-        <Card.Title className='mb-4 mt-2' as='h2'>
+        <Card.Title className="mb-4 mt-2" as="h2">
           {title}
         </Card.Title>
-        <Card.Text className='container-blog'>{JSX}</Card.Text>
+        <Card.Text className="container-blog">{JSX}</Card.Text>
       </Card.Body>
-      <Card.Footer className='text-muted'>
+      <Card.Footer className="text-muted">
         {pubDateParse.toLocaleDateString(undefined, optionsDate)}
       </Card.Footer>
     </Card>
--- react-app/src/components/pages/blog-results/index.js
+++ react-app/src/components/pages/blog-results/index.js
@@ -1,51 +1,68 @@
-import React, { Component, Fragment,useState, useEffect } from 'react';
-import {connect} from 'react-redux';
-import { PageContainer } from '../../layouts';
-import './styles.scss';
-import {Container,Row, Col} from 'react-bootstrap';
-import BlogPostCards from '../../containers/blog-post-card-details';
-import BlogPostCardShimmer from '../../layouts/blog-post-card-shimmer';
+import React, { Component, Fragment, useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { PageContainer } from "../../layouts";
+import "./styles.scss";
+import { Container, Row, Col } from "react-bootstrap";
+import BlogPostCards from "../../containers/blog-post-card-details";
+import BlogPostCardShimmer from "../../layouts/blog-post-card-shimmer";
 import MetaTags from "react-meta-tags";
 import * as mediumApiService from "../../../state/utils/mediumApiService";
 import { blogOperations } from "../../../state/ducks/blog";
 
-
-const Blog = ({blogsData,blogsDataLoading,blogsDataCompleted, saveBlogData,getBlogData}) => { 
+const Blog = ({
+  blogsData,
+  blogsDataLoading,
+  blogsDataCompleted,
+  saveBlogData,
+  getBlogData
+}) => {
   useEffect(() => {
-    if(!blogsDataCompleted){
+    if (!blogsDataCompleted) {
       getBlogData();
     }
   }, []);
   let renderPosts;
 
-  if(blogsDataLoading){
-    renderPosts =(<Fragment><BlogPostCardShimmer/><BlogPostCardShimmer/><BlogPostCardShimmer/></Fragment>);
-  }
-  else{   
-    renderPosts = blogsData.map(({ title, thumbnail, description, link,pubDate},index) => (
-      <BlogPostCards
-        title={title}
-        thumbnail={thumbnail}
-        description={description}
-        link={link}
-        pubDate={pubDate}
-        idPost={index}
-        key={index}
-      />
-    )); 
+  if (blogsDataLoading) {
+    renderPosts = (
+      <Fragment>
+        <BlogPostCardShimmer />
+        <BlogPostCardShimmer />
+        <BlogPostCardShimmer />
+      </Fragment>
+    );
+  } else {
+    renderPosts = blogsData.map(
+      ({ title, thumbnail, description, link, pubDate }, index) => (
+        <BlogPostCards
+          title={title}
+          thumbnail={thumbnail}
+          description={description}
+          link={link}
+          pubDate={pubDate}
+          idPost={index}
+          key={index}
+        />
+      )
+    );
   }
 
-  return (  
+  return (
     <Fragment>
       <MetaTags>
-        <title>"Famosos.com - Videos personalizados de tus famosos favoritos.",</title>
-        <meta name='description' content='Un blog acerca de tus famosos favoritos que forman parte de nuestra plataforma. Entérate con quien puedes conectarte' />
+        <title>
+          Famosos.com - Videos personalizados de tus famosos favoritos.
+        </title>
+        <meta
+          name="description"
+          content="Un blog acerca de tus famosos favoritos que forman parte de nuestra plataforma. Entérate con quien puedes conectarte"
+        />
       </MetaTags>
       <PageContainer>
         <Container>
           <Row>
-            <Col md='9' className='mx-auto'>
-              <h1 className='font-weight-bold'>Blogs</h1>
+            <Col md="9" className="mx-auto">
+              <h1 className="font-weight-bold">Blogs</h1>
               {renderPosts}
             </Col>
           </Row>
@@ -53,12 +70,10 @@ const Blog = ({blogsData,blogsDataLoading,blogsDataCompleted, saveBlogData,getBl
       </PageContainer>
     </Fragment>
   );
-}
+};
 
 // mapStateToProps
-const mapStateToProps = ({
-  blog,
-}) => ({
+const mapStateToProps = ({ blog }) => ({
   blogsData: blog.blogsPostMediumReducer.data,
   blogsDataLoading: blog.blogsPostMediumReducer.loading,
   isBlogsDataFetch: blog.blogsPostMediumReducer.completed,
@@ -67,9 +82,8 @@ const mapStateToProps = ({
 
 const mapDispatchToProps = {
   saveBlogData: blogOperations.saveBlogData,
-  getBlogData: blogOperations.getBlogData,
+  getBlogData: blogOperations.getBlogData
 };
 
-const _Blog = connect(
-  mapStateToProps,mapDispatchToProps)(Blog);
-export {_Blog as Blog_Results};
+const _Blog = connect(mapStateToProps, mapDispatchToProps)(Blog);
+export { _Blog as Blog_Results };
