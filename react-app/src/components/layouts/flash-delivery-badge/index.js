50aa15ce5b35cd3875a50b22565cc7554e3191dd
German Solano <german@famosos.com>
2020-12-15 13:38:58 -0400

    style(flash-delivery): Ajustar el componente para implementarlo en el perfil del famoso

--- react-app/src/components/layouts/flash-delivery-badge/index.js
+++ react-app/src/components/layouts/flash-delivery-badge/index.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { flashDeliveryCelebritiesUsernames } from "../../../constants/flashDeliveryCelebritiesUsernames";
 import "./styles.scss";
 
-const FlashDeliveryBadgeLayout = ({ className }) => (
-  <span className={`FlashDeliveryLayout ${className ? className : ""}`}>
-    <span className="FlashDeliveryLayout__title">Entrega Flash</span>
-    <i className="FlashDeliveryLayout__icon fa fa-bolt text-warning" />
-  </span>
-);
+const FlashDeliveryBadgeLayout = ({
+  celebrityUsername,
+  className,
+  color,
+  showTime
+}) => {
+  const celebrityIsFlashDelivery = flashDeliveryCelebritiesUsernames.find(
+    (username) => username === celebrityUsername
+  );
+  return celebrityIsFlashDelivery ? (
+    <span
+      className={`FlashDeliveryLayout ${
+        className ? className : ""
+      } FlashDeliveryLayout-color-${color ? color : "white"}`}
+    >
+      <span className="FlashDeliveryLayout__title">Entrega Flash</span>
+      {showTime ? (
+        <span className="FlashDeliveryLayout__time">24 hrs.</span>
+      ) : null}
+      <i className="FlashDeliveryLayout__icon fa fa-bolt text-warning" />
+    </span>
+  ) : null;
+};
 
 export { FlashDeliveryBadgeLayout };
