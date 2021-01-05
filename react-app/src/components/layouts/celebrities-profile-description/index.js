8909bb65ff5ae5a1d4794784537b57315e76b06d
German Solano <german@famosos.com>
2021-01-05 08:52:06 -0400

    style(Celebrity-profile): Mejorar formato en CelebrityProfileDescription

--- react-app/src/components/layouts/celebrities-profile-description/index.js
+++ react-app/src/components/layouts/celebrities-profile-description/index.js
@@ -1,49 +1,53 @@
-import React, {useState} from 'react';
-import {Collapse} from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import { Collapse } from "react-bootstrap";
+import PropTypes from "prop-types";
 import "./styles.scss";
-import limitString from '../../../utils/limitString';
-const CelebritiesProfileDescription = ({descriptionText}) => {
-    const [showMore, setShowMore] = useState(false);
-    const handlerShowMore = () => {
-      setShowMore((prevState) => !prevState);
-    };
-    const textDescription = (
-      <span className='container-celebrities-profile__text-span'>
-        {descriptionText}
-      </span>
-    );
-    return (
-      <div className={`container-celebrities-profile-description`}>
-        {descriptionText.length > 200 ? (
-          <>
-            <Collapse
-              className={`container-celebrities-profile-description__collapse`}
-              in={showMore}
-            >
-              <div>{textDescription}</div>
-            </Collapse>
-            <div className='container-celebrities-profile-description__toggle-description'>
-              {showMore ? null : (
-                <i
-                  className={`fas fa-angle-double-down`}
-                  onClick={() => handlerShowMore()}
-                ></i>
-              )}
-            </div>
-          </>
-        ) : (
-          <div>{textDescription}</div>
-        )}
-      </div>
-    );
-}
+
+const CelebritiesProfileDescription = ({ descriptionText }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore((previousShowMore) => !previousShowMore);
+  };
+
+  const descriptionTextSpan = (
+    <span className="container-celebrities-profile__text-span">
+      {descriptionText}
+    </span>
+  );
+
+  return (
+    <div className={`container-celebrities-profile-description`}>
+      {descriptionText.length > 200 ? (
+        <>
+          <Collapse
+            className={`container-celebrities-profile-description__collapse`}
+            in={showMore}
+          >
+            <div>{descriptionTextSpan}</div>
+          </Collapse>
+          <div className="container-celebrities-profile-description__toggle-description">
+            {showMore ? null : (
+              <i
+                className={`fas fa-angle-double-down`}
+                onClick={toggleShowMore}
+              />
+            )}
+          </div>
+        </>
+      ) : (
+        <div>{descriptionTextSpan}</div>
+      )}
+    </div>
+  );
+};
 
 CelebritiesProfileDescription.defaultProps = {
-    descriptionText: ""
-}
-CelebritiesProfileDescription.propTypes = {
-    descriptionText: PropTypes.string
-}
+  descriptionText: ""
+};
 
-export {CelebritiesProfileDescription};
+CelebritiesProfileDescription.propTypes = {
+  descriptionText: PropTypes.string
+};
+
+export { CelebritiesProfileDescription };
