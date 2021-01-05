523a6ef930647fde0e34c8663e10c01472b96375
German Solano <german@famosos.com>
2021-01-05 08:42:03 -0400

    fix(Celebrity-profile): Corregir margenes en Tiempo de Respuesta del famoso

--- react-app/src/components/layouts/celebrities-response-time/index.js
+++ react-app/src/components/layouts/celebrities-response-time/index.js
@@ -1,30 +1,27 @@
-import React,{useEffect} from 'react';
-import PropTypes from 'prop-types';
-const CelebritiesResponseTime = ({turnAroundTime}) => {
-    console.log(turnAroundTime)
-    const getTurnAroundText = () => {
-      if (turnAroundTime < 1) {
-        return <span>Pocas horas</span>;
-      } else if (turnAroundTime === 1) {
-        return (
-          <span >
-            {parseInt(turnAroundTime)} día
-          </span>
-        );
-      } else {
-        return (
-          <span >
-            {parseInt(turnAroundTime)} días
-          </span>
-        );
-      }
-    };
-  
-    return (
-        <span style={{ fontSize: '12px' }}>Respuesta promedio : {getTurnAroundText()}</span>
-    );
-}
-CelebritiesResponseTime.propTypes={
-    turnAroundTime: PropTypes.number
-}
-export default CelebritiesResponseTime;
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+const getTurnAroundText = (turnAroundTime) => {
+  if (turnAroundTime < 1) {
+    return "Pocas horas";
+  } else if (turnAroundTime === 1) {
+    return `${parseInt(turnAroundTime)} día`;
+  } else {
+    return `${parseInt(turnAroundTime)} días`;
+  }
+};
+
+const CelebritiesResponseTime = ({ turnAroundTime }) => {
+  return (
+    <span className="CelebritiesResponseTime">
+      Respuesta promedio: <span>{getTurnAroundText(turnAroundTime)}</span>
+    </span>
+  );
+};
+
+CelebritiesResponseTime.propTypes = {
+  turnAroundTime: PropTypes.number
+};
+
+export { CelebritiesResponseTime };
--- react-app/src/components/layouts/celebrity-details/index.js
+++ react-app/src/components/layouts/celebrity-details/index.js
@@ -6,8 +6,8 @@ import { CelebrityFavoriteButton } from "../celebrity-favorite-button";
 import { HireThisCelebrityButton } from "../hire-this-celebrity-button";
 import { CelebrityContractPrice } from "../celebrity-contract-price";
 import "./styles.scss";
-import {CelebritiesProfileDescription} from "../celebrities-profile-description";
-import CelebritiesResponseTime from "../celebrities-response-time";
+import { CelebritiesProfileDescription } from "../celebrities-profile-description";
+import { CelebritiesResponseTime } from "../celebrities-response-time";
 
 const CelebrityInfo = ({
   fullName,
@@ -22,30 +22,30 @@ const CelebrityInfo = ({
     case "1":
       return (
         <>
-          <h4 className='CelebrityDetails__full-name'>{fullName}</h4>
-          <div className='d-flex align-items-center mb-2'>
+          <h4 className="CelebrityDetails__full-name">{fullName}</h4>
+          <div className="d-flex align-items-center mb-2">
             <CountryFlag countryCode={countryCode} />
-            <span className='ml-3 mt-1 CelebrityDetails__category'>
+            <span className="ml-3 mt-1 CelebrityDetails__category">
               {categoryTitle}
             </span>
             <CelebrityFavoriteButton
-              className='d-none d-md-block CelebrityDetails__fav-button-desktop'
+              className="d-none d-md-block CelebrityDetails__fav-button-desktop"
               celebrityId={celebrityId}
-              outlinedImageSource='assets/img/heart-regular-outlined.svg'
-              width='2.5rem'
+              outlinedImageSource="assets/img/heart-regular-outlined.svg"
+              width="2.5rem"
             />
           </div>
-          <div className='d-flex justify-content-between mb-2 mb-md-4 align-items-center'>
+          <div className="d-flex justify-content-between align-items-center">
             <CelebrityContractPrice contractTypes={contractTypes} />
             <CelebrityFavoriteButton
-              className='d-md-none'
+              className="d-md-none"
               celebrityId={celebrityId}
-              outlinedImageSource='assets/img/heart-regular-outlined.svg'
-              width='1.5rem'
+              outlinedImageSource="assets/img/heart-regular-outlined.svg"
+              width="1.5rem"
             />
           </div>
-          <div className='mb-2'>
-          <CelebritiesResponseTime turnAroundTime={turnAround} />
+          <div className="mt-md-2 mb-md-4">
+            <CelebritiesResponseTime turnAroundTime={turnAround} />
           </div>
         </>
       );
@@ -93,9 +93,11 @@ const CelebrityInfo = ({
               width="2.5rem"
             />
           </div>
-          <CelebritiesResponseTime turnAroundTime={turnAround}/>
-          <div className="d-flex justify-content-between mb-2 mb-md-4 align-items-center">
+          <div className="d-flex justify-content-between align-items-center">
             <CelebrityContractPrice contractTypes={contractTypes} />
+          </div>
+          <div className="mt-md-2 mb-md-4">
+            <CelebritiesResponseTime turnAroundTime={turnAround} />
           </div>
         </>
       );
@@ -119,14 +121,14 @@ const CelebrityDetails = ({
 }) => {
   return (
     <Container
-      className={`mx-auto CelebrityDetails ${variant === '1' ? 'mb-0' : ''}`}
+      className={`mx-auto CelebrityDetails ${variant === "1" ? "mb-0" : ""}`}
     >
-      <Row className='justify-content-md-center align-items-center my-3'>
-        <Col xs='auto d-md-none'>
-          <ProfilePicture avatar={avatar} width='139px' />
+      <Row className="justify-content-md-center align-items-center my-3">
+        <Col xs="auto d-md-none">
+          <ProfilePicture avatar={avatar} width="139px" />
         </Col>
-        <Col xs='auto d-none d-md-block'>
-          <ProfilePicture avatar={avatar} width='200px' />
+        <Col xs="auto d-none d-md-block">
+          <ProfilePicture avatar={avatar} width="200px" />
         </Col>
         <Col>
           <CelebrityInfo
@@ -138,34 +140,34 @@ const CelebrityDetails = ({
             contractTypes={contractTypes}
             turnAround={turnAround}
           />
-          <Col className='d-none d-md-block mx-0 px-0'>
+          <Col className="d-none d-md-block mx-0 px-0">
             <HireThisCelebrityButton
               celebrityFullName={fullName}
               celebrityUsername={username}
               text={
                 // variant.startsWith("1")
-                /* ?  */ 'Quiero un video de'
+                /* ?  */ "Quiero un video de"
                 // : "Obtén un video de"
               }
-              width='100%'
-              fontSize='1.25em'
+              width="100%"
+              fontSize="1.25em"
             />
           </Col>
         </Col>
       </Row>
       <Row>
-        <Col className='mx-3 my-3 px-0'>
+        <Col className="mx-3 my-3 px-0">
           <CelebritiesProfileDescription descriptionText={description} />
         </Col>
       </Row>
       <Row>
-        <Col className='d-md-none'>
+        <Col className="d-md-none">
           <HireThisCelebrityButton
             celebrityFullName={fullName}
             celebrityUsername={username}
-            text='Quiero un video de'
-            width='100%'
-            fontSize='1.25em'
+            text="Quiero un video de"
+            width="100%"
+            fontSize="1.25em"
           />
         </Col>
       </Row>
