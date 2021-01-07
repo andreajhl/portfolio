4e466c85d3f78e7c2996664d32c70d20e50f1cbf
German Solano <german@famosos.com>
2021-01-07 14:56:57 -0400

    fix(Celebrity-profile): Corregir estilos del botón de Suscribirse a este famoso

--- react-app/src/components/layouts/subscribe-to-this-celebrity-button/index.js
+++ react-app/src/components/layouts/subscribe-to-this-celebrity-button/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "react-bootstrap";
 import { SUBSCRIPTION } from "../../../routing/Paths";
 import * as GTM from "../../../state/utils/gtm";
+import { parseFullName } from "parse-full-name";
+import { CallToActionButton } from "../call-to-action-button";
 
 const SubscribeToThisCelebrityButton = ({
   className,
@@ -12,28 +13,49 @@ const SubscribeToThisCelebrityButton = ({
   fontSize,
   width
 }) => {
- 
+  const registerSubscribeToThisCelebrityButtonEvent = (eventName) => {
+    GTM.tagManagerDataLayer(eventName + "_SUBSCRIBE_TO_THIS_CELEBRITY_BUTTON", {
+      path: window.location.pathname,
+      widget: "SubscribeToThisCelebrityButton",
+      text,
+      celebrityFullName,
+      celebrityUsername
+    });
+  };
+
+  const parsedFullName = parseFullName(
+    celebrityFullName,
+    "all",
+    true,
+    false,
+    true
+  );
+
+  const fullNameWords = celebrityFullName.split(" ");
+
+  const displayName =
+    parsedFullName.first === "Papá"
+      ? `${fullNameWords[0]} ${fullNameWords[1]}`
+      : parsedFullName.first || parsedFullName.last;
+
   return (
     <NavLink
-      to={SUBSCRIPTION.replace(':celebrity_username', celebrityUsername)}
+      to={SUBSCRIPTION.replace(":celebrity_username", celebrityUsername)}
+      onClick={() => registerSubscribeToThisCelebrityButtonEvent("CLICK")}
+      onMouseOver={() => registerSubscribeToThisCelebrityButtonEvent("HOVER")}
     >
-      <Button
-        style={{
-          backgroundColor: '#FB177D',
-          borderRadius: '5px',
-          color: 'white',
-          border: 'none',
-          padding: '0.75em',
-          fontSize,
-          width,
-        }}
-        className={`font-weight-bold ${className ? className : ''}`}
-      >
-        {text}
-        {celebrityFullName ? ' ' + celebrityFullName.split(' ')[0] : ''}
-      </Button>
+      {
+        <CallToActionButton
+          fontSize={fontSize}
+          width={width}
+          className={className}
+        >
+          {text}
+          {celebrityFullName ? " " + displayName : ""}
+        </CallToActionButton>
+      }
     </NavLink>
   );
 };
 
-export {  SubscribeToThisCelebrityButton };
+export { SubscribeToThisCelebrityButton };
