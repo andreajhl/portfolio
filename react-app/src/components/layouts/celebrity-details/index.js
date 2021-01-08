bd64ae41cff645eaa0a24607ec1c101d660a6550
German Solano <german@famosos.com>
2021-01-08 11:37:02 -0400

    refactor(Celebrity-profile): Mover CelebrityInfo a su archivo propio
    Para reducir el tamaño del archivo de CelebrityDetails, se movió a su el componente CelebrityInfo y se le aplicó un refactor

--- react-app/src/components/layouts/celebrity-details/index.js
+++ react-app/src/components/layouts/celebrity-details/index.js
@@ -1,113 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ProfilePicture } from "../profile-picture";
-import { CountryFlag } from "../../containers/celebrity-country-flag";
-import { CelebrityFavoriteButton } from "../celebrity-favorite-button";
 import { HireThisCelebrityButton } from "../hire-this-celebrity-button";
 import { SubscribeToThisCelebrityButton } from "../subscribe-to-this-celebrity-button";
-import { CelebrityContractPrice } from "../celebrity-contract-price";
-import "./styles.scss";
 import { CelebritiesProfileDescription } from "../celebrities-profile-description";
-import { CelebritiesResponseTime } from "../celebrities-response-time";
 import { FlashDeliveryBadgeLayout } from "../flash-delivery-badge";
-
-const CelebrityInfo = ({
-  fullName,
-  countryCode,
-  categoryTitle,
-  celebrityId,
-  contractTypes,
-  turnAround,
-  variant
-}) => {
-  switch (variant) {
-    case "1":
-      return (
-        <>
-          <h4 className="CelebrityDetails__full-name">{fullName}</h4>
-          <div className="d-flex align-items-center mb-2">
-            <CountryFlag countryCode={countryCode} />
-            <span className="ml-3 mt-1 CelebrityDetails__category">
-              {categoryTitle}
-            </span>
-            <CelebrityFavoriteButton
-              className="d-none d-md-block CelebrityDetails__fav-button-desktop"
-              celebrityId={celebrityId}
-              outlinedImageSource="assets/img/heart-regular-outlined.svg"
-              width="2.5rem"
-            />
-          </div>
-          <div className="d-flex justify-content-between align-items-center">
-            <CelebrityContractPrice contractTypes={contractTypes} />
-            <CelebrityFavoriteButton
-              className="d-md-none"
-              celebrityId={celebrityId}
-              outlinedImageSource="assets/img/heart-regular-outlined.svg"
-              width="1.5rem"
-            />
-          </div>
-          <div className="mt-md-2 mb-md-4">
-            <CelebritiesResponseTime turnAroundTime={turnAround} />
-          </div>
-        </>
-      );
-
-    case "1.1":
-      return (
-        <>
-          <CelebrityFavoriteButton
-            className="mb-3"
-            celebrityId={celebrityId}
-            outlinedImageSource="assets/img/heart-regular-outlined.svg"
-            width="1.25rem"
-          />
-          <h4 className="CelebrityDetails__full-name">{fullName}</h4>
-          <div className="d-flex align-items-center mb-2 mb-md-3">
-            <CountryFlag countryCode={countryCode} />
-            <span className="ml-3 mt-1 CelebrityDetails__category">
-              {categoryTitle}
-            </span>
-          </div>
-        </>
-      );
-
-    case "2":
-      return (
-        <>
-          <div className="text-right mb-2">
-            <CelebrityFavoriteButton
-              className="d-md-none"
-              celebrityId={celebrityId}
-              outlinedImageSource="assets/img/heart-regular-outlined.svg"
-              width="1.25rem"
-            />
-          </div>
-          <h4 className="CelebrityDetails__full-name">{fullName}</h4>
-          <div className="d-flex align-items-center mb-2">
-            <CountryFlag countryCode={countryCode} />
-            <span className="ml-3 mt-1 CelebrityDetails__category">
-              {categoryTitle}
-            </span>
-            <CelebrityFavoriteButton
-              className="d-none d-md-inline ml-auto"
-              celebrityId={celebrityId}
-              outlinedImageSource="assets/img/heart-regular-outlined.svg"
-              width="2.5rem"
-            />
-          </div>
-          <div className="d-flex justify-content-between align-items-center">
-            <CelebrityContractPrice contractTypes={contractTypes} />
-          </div>
-          <div className="mt-md-2 mb-md-4">
-            <CelebritiesResponseTime turnAroundTime={turnAround} />
-          </div>
-        </>
-      );
-
-    default:
-      break;
-  }
-};
+import { CelebrityInfo } from "../celebrity-info";
+import "./styles.scss";
 
 const CelebrityDetails = ({ celebrity, variant }) => {
   const {
