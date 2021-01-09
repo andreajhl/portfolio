32fd52522121ee64c851311a002ac70bc4ad49fb
German Solano <german@famosos.com>
2021-01-09 05:47:26 -0400

    feat(connection-error-page): Añadir primera versión del layout

---
+++ react-app/src/components/layouts/network-error-message/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { CallToActionButton } from "../call-to-action-button";
+import "./styles.scss";
+
+const NetworkConnectionErrorLayout = (props) => {
+  return (
+    <div className="NetworkConnectionErrorLayout">
+      <section className="NetworkConnectionErrorLayout__section">
+        <div className="NetworkConnectionErrorLayout__container mx-auto text-center p-4">
+          <h4 className="font-weight-light text-center mb-0">
+            Ha ocurrido un error. Por favor, comprueba tu conexión a internet e
+            intenta nuevamente.
+          </h4>
+          <img
+            width="200px"
+            style={{ opacity: "0.2", marginBottom: "0.35rem" }}
+            src="/assets/img/wifi-connection-error.png"
+            alt="Error de conexión"
+          />
+          <br />
+          <CallToActionButton
+            width="200px"
+            onClick={() => window.location.reload()}
+            className="text-uppercase"
+          >
+            Reintentar
+          </CallToActionButton>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export { NetworkConnectionErrorLayout };
