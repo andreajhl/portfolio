e9b2374a94450affec2338520892f6d0a44aa223
German Solano <german@famosos.com>
2021-01-09 06:23:48 -0400

    refactor: Añadir botón de call to action rosado de menor jerarquia

---
+++ react-app/src/components/layouts/less-important-call-to-action-button/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+const LessImportantCallToActionButton = ({
+  fontSize,
+  width,
+  className,
+  children,
+  onClick
+}) => {
+  return (
+    <Button
+      style={{
+        fontSize,
+        width
+      }}
+      className={`LessImportantCallToActionButton ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
+};
+
+LessImportantCallToActionButton.propTypes = {
+  fontSize: PropTypes.string,
+  width: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  onClick: PropTypes.func
+};
+
+LessImportantCallToActionButton.defaultProps = {
+  fontSize: "1rem",
+  width: "auto",
+  className: "",
+  children: null,
+  onClick: () => {}
+};
+
+export { LessImportantCallToActionButton };
