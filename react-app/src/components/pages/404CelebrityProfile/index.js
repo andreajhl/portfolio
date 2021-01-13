92d77d34dfcdcf23bb683034d0c5ae5feec1ff18
German Solano <german@famosos.com>
2021-01-12 20:35:38 -0400

    feat(404-celebrity-page): Añadir banner responsive

--- react-app/src/components/pages/404CelebrityProfile/index.js
+++ react-app/src/components/pages/404CelebrityProfile/index.js
@@ -1,55 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./styles.scss";
-import {PageContainer} from "../../layouts/page-container";
+import { PageContainer } from "../../layouts/page-container";
 import MetaTags from "react-meta-tags";
-import {history} from "../../../routing/History";
+import { history } from "../../../routing/History";
 import * as ROUTING_PATHS from "../../../routing/Paths";
-
+import { NavLink } from "react-router-dom";
 
 class FourZeroFourCelebrityProfile extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {};
+  }
 
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <>
-                <PageContainer applyFetchCelebrities={false} showFooter={false}>
-                    <div className="SignInPage">
-                        <div className="section">
-                            <div className="auth-container mx-auto text-center p-4">
-                                <h3 className="font-weight-light text-center">
-                                    No se encontró un famoso con el usuario
-                                    {" "}
-                                    <span className={"font-weight-bold"}>
-                                        {this.props.match.params.celebrity_username}
-                                    </span>
-                                </h3>
-                                <br/>
-                                <img width="200px"
-                                     style={{opacity: "0.2"}}
-                                     src="/assets/img/sad-face-in-rounded-square.svg" alt="sad-face"/>
-                                <br/>
-                                <br/>
-                                <button className={"btn btn-primary"} onClick={(e) => {
-                                    e.preventDefault();
-                                    history._pushRoute(
-                                        ROUTING_PATHS.HOME_PATH
-                                    );
-                                }}>Volver a inicio
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </PageContainer>
-            </>
-        );
-    };
-
+  render() {
+    return (
+      <>
+        <PageContainer applyFetchCelebrities={false} showFooter={false}>
+          <header className="FourZeroFourCelebrityProfile__banner container">
+            <img
+              src="/assets/img/index-header-backeground.png"
+              alt="Famosa con teléfono"
+              className="FourZeroFourCelebrityProfile__banner-img order-1 order-md-0"
+            />
+            <div className="FourZeroFourCelebrityProfile__banner-content p-4">
+              <h3 className="FourZeroFourCelebrityProfile__banner-title order-1 order-md-0">
+                No se encontró un <br />
+                famoso con el usuario{" "}
+                <span className="FourZeroFourCelebrityProfile__banner-username">
+                  {this.props.match.params.celebrity_username}
+                </span>
+              </h3>
+              <NavLink
+                to={ROUTING_PATHS.HOME_PATH}
+                className="FourZeroFourCelebrityProfile__banner-home-link"
+              >
+                <i className="fa fa-arrow-left FourZeroFourCelebrityProfile__banner-arrow-left" />{" "}
+                Volver al inicio
+              </NavLink>
+            </div>
+          </header>
+        </PageContainer>
+      </>
+    );
+  }
 }
 
-export {FourZeroFourCelebrityProfile};
-
+export { FourZeroFourCelebrityProfile };
