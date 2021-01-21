3252776e2bb9337a251fecd17d92fedbb01523a1
German Solano <german@famosos.com>
2021-01-21 16:23:07 -0400

    feat: Restringuir los parametros que se pasan desde la url en vista de busqueda

--- react-app/src/components/pages/celebrities-results/index.js
+++ react-app/src/components/pages/celebrities-results/index.js
@@ -12,6 +12,7 @@ import { celebrityOperations } from "../../../state/ducks/celebrities";
 import { updateQueryParamsInitialState } from "../../../state/ducks/celebrities/reducers";
 import * as GTM from "../../../state/utils/gtm";
 import { CelebritiesAdditionalResultsLayout } from "../../layouts/celebrities-additional-results";
+import pick from "lodash.pick";
 
 const mapStateToProps = ({ celebrities }) => {
   return {
@@ -41,6 +42,16 @@ const hasSearched = (listParams) => {
   );
 };
 
+const allowedParams = [
+  "search",
+  "limit",
+  "offset",
+  "country_id",
+  "category_id"
+];
+
+window.pick = pick;
+
 const CelebritiesResultsPage = ({
   fetchCelebrities,
   isLoading,
@@ -54,9 +65,10 @@ const CelebritiesResultsPage = ({
 }) => {
   const [offset, setOffset] = useState(updateQueryParamsInitialState.offset);
   const queryString = location.search;
-  const listParams = useMemo(() => queryStringToJSON(queryString), [
-    queryString
-  ]);
+  const listParams = useMemo(
+    () => pick(queryStringToJSON(queryString), allowedParams),
+    [queryString]
+  );
 
   useEffect(() => requestCancel, [requestCancel]);
 
