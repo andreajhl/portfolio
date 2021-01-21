da0db048d64553ec38f6203f70bea6069bbbd803
jhonvegamas <jhonvegamas@gmail.com>
2021-01-21 09:43:31 -0500

    feature(Banner): Mostrar el banner una sola vez

--- react-app/src/components/layouts/banner-promo/index.js
+++ react-app/src/components/layouts/banner-promo/index.js
@@ -8,23 +8,12 @@ import {getDiscountCouponBanner} from "../../../state/ducks/discount_coupons/act
 class BannerPromoLayout extends Component {
     constructor() {
         super();
-        let dateFinish = moment(new Moment().add(10, "minutes"), "YYYY-MM-DD HH:mm:ss");
-        if (localStorage.getItem('new_user') != null) {
-            if (localStorage.getItem("discount_coupon_banner") != null) {
-                dateFinish = moment(localStorage.getItem("discount_coupon_banner"));
-            } else {
-                localStorage.setItem("discount_coupon_banner", dateFinish);
-            }
-        }else {
-            dateFinish = moment(new Moment().subtract(10, "minutes"), "YYYY-MM-DD HH:mm:ss");
-        }
-
         this.state = {
             showDiv: true,
             diffTime: null,
             coupon: "",
             discount: 0,
-            dateFinish: dateFinish,
+            dateFinish: null,
         };
         this.calculateDiff();
     }
@@ -32,9 +21,16 @@ class BannerPromoLayout extends Component {
     componentDidMount(): void {
         this.runTimer();
         getDiscountCouponBanner().then(res => {
+            let dateFinish = moment(new Moment().add(10, "minutes"), "YYYY-MM-DD HH:mm:ss");
+            if (localStorage.getItem("discount_coupon_banner") != null) {
+                dateFinish = moment(localStorage.getItem("discount_coupon_banner"));
+            } else {
+                localStorage.setItem("discount_coupon_banner", dateFinish);
+            }
             this.setState({
                 coupon: res['couponCode'],
                 discount: parseFloat(res['discount_amount']) * 100,
+                dateFinish: dateFinish,
             });
         });
     }
@@ -59,10 +55,10 @@ class BannerPromoLayout extends Component {
     render() {
         return (
             <>
-                <div className={this.state.showDiv ? "display-none" : ""}>
+                <div className={this.state.showDiv && this.state.dateFinish != null ? "display-none" : ""}>
                     <div
                         className="ContentBanner row mx-auto p-0 text-center align-items-center justify-content-center">
-                        <div className="col-md-2 text-style text-center">Usa el código: {this.state.coupon}</div>
+                        <div className="col-md-3 text-style text-center">Usa el código: {this.state.coupon}</div>
                         <div className="col-md-2 text-style high-text text-center">{this.state.discount}% de descuento
                         </div>
                         <div className="col-md-3 text-center align-items-center justify-content-center">
