7025eab9c04a2b667c2ac1858eae388e29014301
Isaac Peña <isaac.pr99@gmail.com>
2021-01-22 12:35:06 -0400

    feat: Mensaje de error para compra minima al aplicar cupon

--- react-app/src/components/containers/discount-coupon-form/index.js
+++ react-app/src/components/containers/discount-coupon-form/index.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {paymentsOperations} from "../../../state/ducks/payments";
-import './index.scss';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { paymentsOperations } from "../../../state/ducks/payments";
+import { ContractPriceLayout } from "../../layouts/contract-price";
+import "./index.scss";
 // mapStateToProps
-const mapStateToProps = ({payments}) => ({
+
+const mapStateToProps = ({ payments }) => ({
   contract: payments.getContractToPayReducer.data,
-  couponData: payments.fetchDiscountCouponReducer
+  couponData: payments.fetchDiscountCouponReducer,
+  currencyExchangeData: payments.currencyExchangeReducer.data
 });
 // mapStateToProps
 const mapDispatchToProps = {
@@ -14,55 +16,88 @@ const mapDispatchToProps = {
 };
 
 class index extends Component {
-    constructor(props){
-        super(props);
-        this.state= {
-          discountCoupon: "",
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      discountCoupon: ""
+    };
+  }
+
+  handleErrorMessage = (message) => {
+    const error = message.split(" ");
+    if (error[0] === "MINIMUN_PURCHASE_ERROR") {
+      const minimumPurchaseAmount = parseInt(error[1]);
+      const contractPrice =
+        minimumPurchaseAmount * this.props.currencyExchangeData.rate;
+      return (
+        <React.Fragment>
+          Este cupón es válido para compras mayores o iguales a
+          <ContractPriceLayout
+            classes='font-weight-bold text-danger coupon-error'
+            price={contractPrice}
+            currency={this.props.currencyExchangeData.to}
+            rounding={true}
+          />
+        </React.Fragment>
+      );
+    } else {
+      return message;
     }
-    handleChange = (event)=>{
-      this.setState({ discountCoupon: event.target.value})
-    }
-    handleSubmit = (event) =>{
-      event.preventDefault();
-      this.props.checkoutDiscountCoupon(this.props.contract.reference, this.state.discountCoupon);
-    }
-   
-    render() {
-        return (
-          <form onSubmit={this.handleSubmit} className='coupon-container'>
-            <label htmlFor='coupon-input'>Ingresa aquí tu cupón de descuento</label>
-            <div className='input-group mb-3'>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Ingrese un cupón'
-                aria-label=''
-                aria-describedBy='basic-addon1'
-                id='coupon-input'
-                value={this.state.discountCoupon}
-                onChange={(event) => this.handleChange(event)}
-              />
-              {this.props.couponData.completed ? (
-                <div className='input-group-append'>
-                  <span className='input-group-text text-success' >
-                    CUPÓN AGREGADO
-                  </span>
-                </div>
-              ) : (
-                <div className='input-group-append'>
-                  <button className='btn btn-primary' type='submit'>
-                    Aplicar
-                  </button>
-                </div>
-              )}
+  };
+
+  handleChange = (event) => {
+    this.setState({ discountCoupon: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.checkoutDiscountCoupon(
+      this.props.contract.reference,
+      this.state.discountCoupon
+    );
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className='coupon-container'>
+        <label htmlFor='coupon-input'>Ingresa aquí tu cupón de descuento</label>
+        <div className='input-group mb-3'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Ingrese un cupón'
+            aria-label=''
+            aria-describedBy='basic-addon1'
+            id='coupon-input'
+            value={this.state.discountCoupon}
+            onChange={(event) => this.handleChange(event)}
+          />
+          {this.props.couponData.completed ? (
+            <div className='input-group-append'>
+              <span className='input-group-text text-success'>
+                CUPÓN AGREGADO
+              </span>
             </div>
-            <span className='font-weight-bold text-danger coupon-error'>
-              {this.props.couponData.error_data ? `CUPÓN NO VALIDO: ${this.props.couponData.error_data}` : null}
-            </span>
-          </form>
-        );
-    }
+          ) : (
+            <div className='input-group-append'>
+              <button className='btn btn-primary' type='submit'>
+                Aplicar
+              </button>
+            </div>
+          )}
+        </div>
+        <span className='font-weight-bold text-danger coupon-error'>
+          {this.props.couponData.error_data ? (
+            <React.Fragment>
+              <span className='font-weight-bold text-danger'>
+                CUPÓN NO VALIDO:{" "}
+              </span>
+              {this.handleErrorMessage(this.props.couponData.error_data)}
+            </React.Fragment>
+          ) : null}
+        </span>
+      </form>
+    );
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(index);
