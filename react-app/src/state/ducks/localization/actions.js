7e2eab797cee2ec9bcbbe7b823a78a3fbb25fdfc
German Solano <german@famosos.com>
2021-01-25 11:47:31 -0400

    feat: Añadir lógica de Redux necesaria para obtener el countryCode del usuario

---
+++ react-app/src/state/ducks/localization/actions.js
@@ -0,0 +1,18 @@
+import * as TYPES from "./types";
+import getLocationInfo from "../../utils/localizationApiService";
+import {
+  handleApiResponseFailure,
+  handleApiResponseSuccess
+} from "../../utils";
+
+export const getCountryCode = () => (dispatch) => {
+  const TYPE = TYPES.GET_COUNTRY_CODE_REQUEST;
+  dispatch({ type: TYPE });
+  getLocationInfo({ fields: "country_code" })
+    .then((data) => handleApiResponseSuccess(dispatch, TYPE, data))
+    .catch(() =>
+      handleApiResponseFailure(dispatch, TYPE, {
+        errorMessage: "There was an error getting the localization information."
+      })
+    );
+};
