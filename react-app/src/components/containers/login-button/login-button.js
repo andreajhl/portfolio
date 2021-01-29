b7c0c212504cdbcc4ad2465dfa2f047115cdcd90
Isaac Peña <isaac.pr99@gmail.com>
2021-01-29 12:21:39 -0400

    feat: Agregar login con popup en auth/sign-in

--- react-app/src/components/containers/login-button/login-button.js
+++ react-app/src/components/containers/login-button/login-button.js
@@ -3,16 +3,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = (props) => {
   const { className, redirectUrl } = props;
-  const { loginWithRedirect } = useAuth0();
-  const handlerClickToLogin = () => {
-    loginWithRedirect();
-  };
+  const { loginWithPopup, isLoading, isAuthenticated } = useAuth0();
+  // const handlerClickToLogin = () => {
+  //   loginWithRedirect();
+  // };
   return (
     <button
-      className={`btn btn-outline-primary  btn-sm ${
-        className ? className : ""
-      }`}
-      onClick={() => handlerClickToLogin()}
+      className={`btn btn-outline-primary  ${className ? className : ""}`}
+      onClick={() => loginWithPopup()}
     >
       Ingresar
     </button>
