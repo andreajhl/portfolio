8bd96328715c62d378ac5176f440911fecdcf6ba
Isaac Peña <isaac.pr99@gmail.com>
2021-01-29 17:21:13 -0400

    feat: Agregar logica para realizar login con popup o redireccionamiento dependiendo del dispositivo del usuario

--- react-app/src/components/containers/login-button/login-button.js
+++ react-app/src/components/containers/login-button/login-button.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { AUTH_SUCCESS } from "../../../routing/Paths";
 
 const LoginButton = (props) => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   const { className, redirectUrl } = props;
-  const { loginWithPopup, isLoading, isAuthenticated } = useAuth0();
-  // const handlerClickToLogin = () => {
-  //   loginWithRedirect();
-  // };
+  const {
+    loginWithPopup,
+    isLoading,
+    isAuthenticated,
+    loginWithRedirect
+  } = useAuth0();
+  const handlerClickToLogin = () => {
+    if (isMobile) {
+      loginWithRedirect({ redirectUri: window.location.origin + AUTH_SUCCESS });
+    } else {
+      loginWithPopup();
+    }
+  };
   return (
     <button
       className={`btn btn-outline-primary  ${className ? className : ""}`}
-      onClick={() => loginWithPopup()}
+      onClick={() => handlerClickToLogin()}
     >
       Ingresar
     </button>
