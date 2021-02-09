d8e91786d949ce16779d8df5f4d88dce9259bcff
German Solano <german@famosos.com>
2021-02-09 07:44:23 -0400

    refactor(search-engine-optimization): Mover metas dinamicos a componente personalizado CustomHead

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,11 +15,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-          />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <meta name="theme-color" content="#fff" />
           <meta
             name="google-site-verification"
@@ -31,42 +27,11 @@ class MyDocument extends Document {
             href="/assets/img/favicon.png"
             type="image/ico"
           />
-
-          <title key="title">
-            Famosos.com - Videos personalizados de tus famosos favoritos.
-          </title>
-          <meta
-            property="og:title"
-            content="Famosos.com - Videos personalizados de tus famosos favoritos."
-          />
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://www.famosos.com" />
-          <meta
-            property="og:image"
-            content="/assets/img/famosos-share-icon.png"
-          />
           <meta property="og:site_name" content="Famosos.com" />
-          <meta
-            property="og:description"
-            content="Videos personalizados de tus Famosos favoritos. Reserva tu video y disfruta de experiencias únicas."
-          />
-
-          <meta
-            property="og:video"
-            content="https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5"
-          />
-          <meta
-            property="og:video:url"
-            content="https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5"
-          />
-          <meta
-            property="og:video:secure_url"
-            content="https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5"
-          />
           <meta property="og:video:type" content="video/mp4" />
           <meta property="og:video:width" content="400" />
           <meta property="og:video:height" content="400" />
-
           <meta
             name="robots"
             content={isProdEnvironment ? "index" : "noindex"}
---
+++ react-app/components/common/helpers/custom-head/index.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from "react";
+import Head from "next/head";
+
+type CustomHeadProps = {
+  children?: ReactNode;
+  title?: string;
+  description?: string;
+  ogUrl?: string;
+  ogImage?: string;
+  ogVideo?: string;
+};
+
+const CustomHead = ({
+  title = "Famosos.com - Videos personalizados de tus famosos favoritos.",
+  description = "Videos personalizados de tus Famosos favoritos. Reserva tu video y disfruta de experiencias únicas.",
+  ogUrl = "https://www.famosos.com",
+  ogImage = "/assets/img/famosos-share-icon.png",
+  ogVideo = "https://famosos-output-videos.s3.amazonaws.com/videos/8/143/201912030248-353316-143.mp4#t=0.5",
+  children
+}: CustomHeadProps) => {
+  return (
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
+      <title key="title">{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={ogUrl} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:video" content={ogVideo} />
+      <meta property="og:video:url" content={ogVideo} />
+      <meta property="og:video:secure_url" content={ogVideo} />
+      {children}
+    </Head>
+  );
+};
+
+export default CustomHead;
