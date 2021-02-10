170f00ed97aa829b47a95c9218b1b8a3db19f679
Isaac Peña <isaac.pr99@gmail.com>
2021-02-10 16:31:06 -0400

    fix: Se cambia configuracion de Auth0 para permitir la extraccion del idToken

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { CelebritiesPage } from "react-app/src/components/pages/celebrities";
 import isBrowser from "react-app/src/utils/isBrowser";
 import auth0 from "../lib/auth0";
 
-const Home = ({ token, session }) => {
-  const [data, setData] = useState({});
+const Home = ({ session }) => {
   useEffect(() => {
-    if (isBrowser && token) {
-      console.log(token, "token");
-      console.log(session, "session");
-      if (token) {
-        localStorage.setItem("_a0_", token);
+    if (isBrowser && session) {
+      if (session.idToken) {
+        localStorage.setItem("_a0_", session.idToken);
       }
     }
   }, []);
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+
   return (
     <>
       <CustomHead />
@@ -31,10 +26,10 @@ export async function getServerSideProps({ req, res }) {
   if (typeof window === "undefined") {
     const session = await auth0.getSession(req);
     if (session) {
-      const tokenCache = auth0.tokenCache(req, res);
-      const { accessToken } = await tokenCache.getAccessToken();
       if (session) {
-        return { props: { token: accessToken } };
+        return {
+          props: { session: session }
+        };
       }
     }
   }
