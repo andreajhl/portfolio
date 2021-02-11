73ad7f2d4a678d938834d3b1cba76c8f6b9e87bd
Isaac Peña <isaac.pr99@gmail.com>
2021-02-11 13:53:09 -0400

    feat: Se actualiza vista perfil para realizar logout via auth0-react

--- pages/my-account/profile.tsx
+++ pages/my-account/profile.tsx
@@ -1,31 +1,7 @@
-import { useEffect } from "react";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { ClientProfilePage } from "react-app/src/components/pages/client-profile";
-import auth0 from "../../lib/auth0";
-import { LOGIN_API_ROUTE } from "../../contants/paths";
-import isBrowser from "react-app/src/utils/isBrowser";
-import { Session } from "react-app/src/state/utils/session";
 
-export async function getServerSideProps({ req, res }) {
-  if (typeof window === "undefined") {
-    const session = await auth0.getSession(req);
-    if (!session || !session.user) {
-      res.writeHead(302, {
-        Location: LOGIN_API_ROUTE
-      });
-      res.end();
-      return;
-    }
-    return { props: { session: session } };
-  }
-}
-
-const Profile = ({ session }) => {
-  useEffect(() => {
-    const sessionInstance = new Session();
-    localStorage.setItem(sessionInstance.sessionName, session.idToken);
-  }, [session]);
-  isBrowser;
+const Profile = () => {
   return (
     <>
       <CustomHead />
