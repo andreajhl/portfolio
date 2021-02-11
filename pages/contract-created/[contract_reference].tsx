8d29c9a7b4d35e338cbd4b5e2f6cc51b9618358f
German Solano <german@famosos.com>
2021-02-11 11:22:43 -0400

    refactor(pages): Migrar Contract Created

---
+++ pages/contract-created/[contract_reference].tsx
@@ -0,0 +1,22 @@
+import { GetServerSideProps } from "next";
+import CustomHead from "react-app/src/components/common/helpers/custom-head";
+import { ContractCreatedPage } from "react-app/src/components/pages/contract-created";
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params: { contract_reference }
+}) => {
+  return {
+    props: { contract_reference }
+  };
+};
+
+const PaymentMethods = ({ contract_reference }) => {
+  return (
+    <>
+      <CustomHead />
+      <ContractCreatedPage contractReference={contract_reference} />
+    </>
+  );
+};
+
+export default PaymentMethods;
--- react-app/src/state/ducks/contracts/actions.js
+++ react-app/src/state/ducks/contracts/actions.js
@@ -73,17 +73,15 @@ export const getContractWithPayments = (contractReference) => {
       .then((res) => {
         if ("status" in res.data && res.data.status === "ERROR") {
           handleApiResponseFailure(dispatch, TYPE, res);
-          // Other actions
           history._pushRoute(ROUTING_PATHS.HOME_PATH);
         } else {
           handleApiResponseSuccess(dispatch, TYPE, res);
-          // Other actions
           dispatch({ type: `${TYPE}_COMPLETED`, payload: res });
         }
       })
       .catch((err) => {
-        history._pushRoute(ROUTING_PATHS.HOME_PATH);
         handleApiErrors(dispatch, TYPE, err);
+        history._pushRoute(ROUTING_PATHS.HOME_PATH);
       });
   };
 };
@@ -453,13 +451,9 @@ export const updateContract = (body) => {
   };
 };
 
-export const saveContractToPayClear = () => {
-  return (dispatch) => {
-    dispatch({
-      type: TYPES.SAVE_CONTRACT_TO_PAY_CLEAR
-    });
-  };
-};
+export const saveContractToPayClear = () => ({
+  type: TYPES.SAVE_CONTRACT_TO_PAY_CLEAR
+});
 
 export const updateContractIsPublic = async (body) => {
   const FINAL_PATH = API_PATHS.UPDATE_CONTRACT_IS_PUBLIC;
