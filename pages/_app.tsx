25aa7de00319dbe5d0fa0b02e8d8ab6f1fc10147
Isaac Peña <isaac.pr99@gmail.com>
2021-02-11 12:33:46 -0400

    feat: Se agrega Auth0Provider a _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import Auth0ProviderWithHistory from "lib/auth0-provider-with-history";
 import React from "react";
 import { wrapper } from "react-app/src/state/store";
 import "react-app/src/styles.scss";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  return (
+    <Auth0ProviderWithHistory>
+      <Component {...pageProps} />;
+    </Auth0ProviderWithHistory>
+  );
+};
 
 // App.getInitialProps = async ({ Component, ctx }) => {
 //   const pageProps = Component.getInitialProps
