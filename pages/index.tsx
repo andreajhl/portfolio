1778f94b6278de04ff8a83ea3a9543e6e4d06ca9
Isaac Peña <isaac.pr99@gmail.com>
2021-02-11 16:48:06 -0400

    fix: Se agrega redirectUri a Auth0Provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { CelebritiesPage } from "react-app/src/components/pages/celebrities";
-import isBrowser from "react-app/src/utils/isBrowser";
-import auth0 from "../lib/auth0";
+// import isBrowser from "react-app/src/utils/isBrowser";
+// import auth0 from "../lib/auth0";
 
 const Home = ({ session }) => {
-  useEffect(() => {
-    if (isBrowser && session) {
-      if (session.idToken) {
-        localStorage.setItem("_a0_", session.idToken);
-      }
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (isBrowser && session) {
+  //     if (session.idToken) {
+  //       localStorage.setItem("_a0_", session.idToken);
+  //     }
+  //   }
+  // }, []);
 
   return (
     <>
@@ -22,19 +22,19 @@ const Home = ({ session }) => {
 };
 
 // This gets called on every request
-export async function getServerSideProps({ req, res }) {
-  if (typeof window === "undefined") {
-    const session = await auth0.getSession(req);
-    if (session) {
-      if (session) {
-        return {
-          props: { session: session }
-        };
-      }
-    }
-  }
+// export async function getServerSideProps({ req, res }) {
+//   if (typeof window === "undefined") {
+//     const session = await auth0.getSession(req);
+//     if (session) {
+//       if (session) {
+//         return {
+//           props: { session: session }
+//         };
+//       }
+//     }
+//   }
 
-  return { props: {} };
-}
+//   return { props: {} };
+// }
 
 export default Home;
