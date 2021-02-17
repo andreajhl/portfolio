8c0f4fa90a610374854c18c7420205db1b55df67
German Solano <german@famosos.com>
2021-02-17 07:06:00 -0400

    refactor(performance-optimization): Cambiar snippets de analitica a su versión minificada para aumentar la velocidad de su ejecución

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,31 +39,16 @@ class MyDocument extends Document {
           {/* Global Site Code Pixel - Facebook Pixel  */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `!(function (f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function () {
-          n.callMethod
-            ? n.callMethod.apply(n, arguments)
-            : n.queue.push(arguments);
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = "2.0";
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s);
-      })(
-        window,
-        document,
-        "script",
-        "https://connect.facebook.net/en_US/fbevents.js"
-      );
-      fbq("init", "430558828300567");
-      fbq("track", "PageView");`
+              __html: `
+              !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', "430558828300567");fbq("track", "PageView");`
             }}
           />
           <noscript>
@@ -81,76 +66,17 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-          !(function () {
-            var analytics = (window.analytics = window.analytics || []);
-            if (!analytics.initialize)
-              if (analytics.invoked)
-                window.console &&
-                  console.error &&
-                  console.error("Segment snippet included twice.");
-              else {
-                analytics.invoked = !0;
-                analytics.methods = [
-                  "trackSubmit",
-                  "trackClick",
-                  "trackLink",
-                  "trackForm",
-                  "pageview",
-                  "identify",
-                  "reset",
-                  "group",
-                  "track",
-                  "ready",
-                  "alias",
-                  "debug",
-                  "page",
-                  "once",
-                  "off",
-                  "on",
-                  "addSourceMiddleware",
-                  "addIntegrationMiddleware",
-                  "setAnonymousId",
-                  "addDestinationMiddleware"
-                ];
-                analytics.factory = function (e) {
-                  return function () {
-                    var t = Array.prototype.slice.call(arguments);
-                    t.unshift(e);
-                    analytics.push(t);
-                    return analytics;
-                  };
-                };
-                for (var e = 0; e < analytics.methods.length; e++) {
-                  var key = analytics.methods[e];
-                  analytics[key] = analytics.factory(key);
-                }
-                analytics.load = function (key, e) {
-                  var t = document.createElement("script");
-                  t.type = "text/javascript";
-                  t.async = !0;
-                  t.src =
-                    "https://cdn.segment.com/analytics.js/v1/" +
-                    key +
-                    "/analytics.min.js";
-                  var n = document.getElementsByTagName("script")[0];
-                  n.parentNode.insertBefore(t, n);
-                  analytics._loadOptions = e;
-                };
-                analytics.SNIPPET_VERSION = "4.13.1";
-                analytics.load("QFYr0foJEBPV2edPGtHTqT5dpsxAUAPa");
-                analytics.page({
-                  ENV: "${String(
-                    process.env.NEXT_PUBLIC_ENVIRONMENT
-                  ).toUpperCase()}",
-                  isReactRouting: false,
-                  path: window.location.pathname,
-                  userAgent: navigator.userAgent,
-                  vendor: navigator.vendor,
-                  receivedAt: new Date()
-                });
-              }
-          })();
-          `
+          !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+          analytics.load("QFYr0foJEBPV2edPGtHTqT5dpsxAUAPa");
+          analytics.page({
+            ENV: "${String(process.env.NEXT_PUBLIC_ENVIRONMENT).toUpperCase()}",
+            isReactRouting: false,
+            path: window.location.pathname,
+            userAgent: navigator.userAgent,
+            vendor: navigator.vendor,
+            receivedAt: new Date()
+          });
+          }}();`
             }}
           />
         </Head>
