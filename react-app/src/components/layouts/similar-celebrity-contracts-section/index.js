bef5d8eeda00cfe9a042b75504c410a54992d99d
German Solano <german@famosos.com>
2021-02-17 22:24:42 -0400

    fix(components): Añadir getWindow para evitar error en SimilarCelebrityContractSection

--- react-app/src/components/layouts/similar-celebrity-contracts-section/index.js
+++ react-app/src/components/layouts/similar-celebrity-contracts-section/index.js
@@ -13,6 +13,7 @@ import { VideoShimmerCardLayout } from "../video-shimmer-card";
 import { Session } from "../../../state/utils/session";
 
 import * as GTM from "../../../state/utils/gtm";
+import getWindow from "react-app/src/utils/getWindow";
 
 class SimilarCelebrityContractsSectionLayout extends Component {
   constructor(props) {
@@ -82,7 +83,7 @@ class SimilarCelebrityContractsSectionLayout extends Component {
 
   analyticsData = {
     widget: this.constructor.name,
-    path: window.location.pathname,
+    path: getWindow().location.pathname,
     celebrityUsername: this.props.celebrityUsername
   };
 
