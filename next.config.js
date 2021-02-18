4e35cafbecec4abe8e2189a13d71808d8a8a9d8f
Isaac Peña <isaac.pr99@gmail.com>
2021-02-18 14:13:19 -0400

    fix: Agregar dominio via.placeholder.com a configuracion nextjs

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ module.exports = {
       "firebasestorage.googleapis.com",
       "d3dxo4xx2lwk55.cloudfront.net",
       "dqb0851cl2gjs.cloudfront.net",
-      "restcountries.eu"
+      "restcountries.eu",
+      "via.placeholder.com"
     ]
   }
 };
