6968d016df342fed06860f6c744721d14cb9f9ec
Isaac Peña <isaac.pr99@gmail.com>
2021-02-19 16:27:13 -0400

    feat: Se agrega vista para resumen de compra con estado pendiente

---
+++ react-app/src/components/layouts/resumen-contract-pending/index.tsx
@@ -0,0 +1,278 @@
+import React from "react";
+import styled from "styled-components";
+import ResumenStatusPayment from "../../containers/resumen-status-payment";
+
+const ResumenContractPending = ({ resumen }) => {
+  const HeroDiv = styled.div`
+    background-color: black;
+    height: 180px;
+    display: flex;
+    justify-content: center;
+    flex-flow: column;
+    align-items: center;
+  `;
+  const HeroTextApproved = styled.div`
+    h2 {
+      text-align: center;
+      color: white;
+      font-weight: bold;
+      margin-top: 20px;
+      font-size: 22px;
+    }
+    /* font-weight: bold; */
+  `;
+
+  const ResumendContractDiv = styled.div`
+    display: flex;
+    height: min-height;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    margin: 0 auto;
+    margin-bottom: 2rem;
+    @media (min-width: 1024px) {
+      min-height: 60vh;
+    }
+  `;
+
+  const ContainerDetailsContractDiv = styled.div`
+    display: flex;
+    flex: 1 1 40%;
+    padding-top: 40px;
+    align-content: center;
+    justify-content: center;
+    background-color: #fcfcfc;
+    flex-flow: column;
+  `;
+
+  const DetailsContracDiv = styled.div`
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 500px;
+  `;
+
+  const InstructionsDetails = styled.div`
+    display: flex;
+    flex-flow: column;
+  `;
+
+  const SpanBoldGray = styled.span`
+    font-weight: bold;
+    color: #535353;
+  `;
+
+  const SpanGray = styled.span`
+    color: #535353;
+  `;
+
+  const CelebrityDetails = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    img {
+      height: 70px;
+    }
+    div {
+      flex: 0 1 200px;
+      span {
+        font-weight: bold;
+        text-align: center;
+        font-size: 0.85rem;
+      }
+    }
+  `;
+
+  const ResumenContainer = styled.div`
+    @media (min-width: 600px) {
+      width: 80%;
+      margin: 0 auto;
+    }
+  `;
+
+  const ResumenPaymentRejected = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column;
+    flex: 0 0 80%;
+    @media (min-width: 600px) {
+      flex: 0 0 50%;
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+    }
+    @media (min-width: 1024px) {
+      flex: 0 0 50%;
+      width: 80%;
+      align-items: flex-start;
+      justify-content: center;
+    }
+  `;
+  const PaymentRejectedActions = styled.div`
+    display: flex;
+    flex-flow: column;
+    margin-top: 20px;
+  `;
+
+  const ButtonPink = styled.button`
+    width: "80%";
+    background-color: #fb177d;
+    border-radius: "5px";
+    margin-top: 20px;
+    height: 55px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    span {
+      color: white;
+    }
+    @media (min-width: 768px) {
+      width: 70%;
+      height: 60px;
+    }
+    @media (min-width: 1024px) {
+      width: 50%;
+      height: 60px;
+      margin-top: 50px;
+    }
+  `;
+
+  return (
+    <>
+      <HeroDiv>
+        <i
+          style={{
+            fontSize: "48px",
+            color: "white"
+          }}
+          className="far fa-clock"
+        />
+
+        <HeroTextApproved>
+          <h2>Tu pago esta pendiente de aprobación.</h2>
+        </HeroTextApproved>
+      </HeroDiv>
+
+      <ResumendContractDiv>
+        <ContainerDetailsContractDiv>
+          <DetailsContracDiv>
+            <CelebrityDetails>
+              <img
+                className="rounded-circle"
+                src={resumen.celebrity.avatar}
+                alt="Avatar Famoso"
+              ></img>
+
+              <span
+                className="text-center font-weight-bold"
+                style={{
+                  color: "black"
+                }}
+              >
+                Video personalizado de {resumen.celebrity.fullName}
+              </span>
+            </CelebrityDetails>
+            <hr className="w-100"></hr>
+            <InstructionsDetails>
+              <div className="d-flex">
+                <div
+                  className="d-flex"
+                  style={{
+                    width: "70%"
+                  }}
+                >
+                  {resumen.contract.deliveryTo ? (
+                    <SpanBoldGray>
+                      Para : <SpanGray>{resumen.contract.deliveryTo}</SpanGray>
+                    </SpanBoldGray>
+                  ) : null}
+                  {resumen.contract.deliveryFrom ? (
+                    <SpanBoldGray>
+                      De : <SpanGray>{resumen.contract.deliveryFrom}</SpanGray>
+                    </SpanBoldGray>
+                  ) : null}
+                </div>
+                <div>Prueba</div>
+              </div>
+              <div className="d-flex mt-3 flex-column">
+                <SpanBoldGray>Mensaje</SpanBoldGray>
+                <SpanGray className="mt-2">
+                  {resumen.contract.instructions}
+                </SpanGray>
+              </div>
+            </InstructionsDetails>
+            <hr className="w-100"></hr>
+            <span
+              style={{
+                fontSize: "12px",
+                color: "#535353"
+              }}
+            >
+              *Puedes editar las instrucciones de tu video mientras está
+              pendiente de grabación.
+            </span>
+            <hr className="w-100"></hr>
+          </DetailsContracDiv>
+        </ContainerDetailsContractDiv>
+        <ResumenPaymentRejected>
+          <ResumenContainer>
+            <hr className="d-none d-md-block "></hr>
+            <ResumenStatusPayment
+              textColor={"#000000"}
+              status={resumen.contract.status}
+              idFollow={resumen.contract.reference}
+              paymentDate={resumen.contract.createdAt}
+            />
+            <hr></hr>
+
+            <PaymentRejectedActions>
+              <div className="d-flex flex-column">
+                <span
+                  className="mt-4"
+                  style={{
+                    color: "#535353"
+                  }}
+                >
+                  El proceso de aprobación puede tardar alrededor de 24 hrs.
+                </span>
+                <span
+                  className="mt-4"
+                  style={{
+                    color: "#535353"
+                  }}
+                >
+                  Te notificaremos a correocliente@dominio.com cuando tu pago
+                  haya sido aprobado.
+                </span>
+                <span
+                  className="mt-4"
+                  style={{
+                    color: "#535353"
+                  }}
+                >
+                  Puedes consultar el estado de tu video en “Mis Contrataciones”
+                </span>
+              </div>
+              <ButtonPink className="btn">
+                <span className="font-weight-bold">
+                  Ir nuevamente a mis contrataciones{" "}
+                </span>
+                <i
+                  style={{
+                    fontSize: "20px",
+                    color: "white"
+                  }}
+                  className="fas fa-long-arrow-alt-right"
+                ></i>
+              </ButtonPink>
+            </PaymentRejectedActions>
+          </ResumenContainer>
+        </ResumenPaymentRejected>
+      </ResumendContractDiv>
+    </>
+  );
+};
+
+export default ResumenContractPending;
