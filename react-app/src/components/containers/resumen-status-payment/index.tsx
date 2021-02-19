0a659668a6daf6944a2c059db1158b0e00597bd1
Isaac Peña <isaac.pr99@gmail.com>
2021-02-19 16:55:04 -0400

    feat: Se agrega nuevo parametro para permitir modificar el color del texto

--- react-app/src/components/containers/resumen-status-payment/index.tsx
+++ react-app/src/components/containers/resumen-status-payment/index.tsx
@@ -6,13 +6,15 @@ type ResumenStatusPaymentsProps = {
   paymentDate: string;
   idFollow: string;
   idTransaction?: string;
+  textColor?: string;
 };
 
 const ResumenStatusPayment = ({
   status,
   paymentDate,
   idFollow,
-  idTransaction
+  idTransaction,
+  textColor
 }: ResumenStatusPaymentsProps) => {
   const ContainerResumenStatusPayment = styled.div`
     display: flex;
@@ -20,15 +22,21 @@ const ResumenStatusPayment = ({
   `;
 
   const SpanGray = styled.span`
-    color: #838383;
+    color: ${(props) => (props.textColor ? props.textColor : "#838383")};
   `;
   return (
     <ContainerResumenStatusPayment>
-      <SpanGray className="mt-1">ESTADO DEL PAGO: {status}</SpanGray>
-      <SpanGray className="mt-1">FECHA DE PAGO: {paymentDate}</SpanGray>
-      <SpanGray className="mt-1">ID DEL SEGUIMIENTO: {idFollow}</SpanGray>
+      <SpanGray textColor={textColor} className="mt-3">
+        ESTADO DEL PAGO: {status}
+      </SpanGray>
+      <SpanGray textColor={textColor} className="mt-3">
+        FECHA DE PAGO: {paymentDate}
+      </SpanGray>
+      <SpanGray textColor={textColor} className="mt-3">
+        ID DEL SEGUIMIENTO: {idFollow}
+      </SpanGray>
       {idTransaction ? (
-        <SpanGray className="mt-1">ID DE TRANSACCIÓN: {idTransaction}</SpanGray>
+        <SpanGray className="mt-3">ID DE TRANSACCIÓN: {idTransaction}</SpanGray>
       ) : null}
     </ContainerResumenStatusPayment>
   );
