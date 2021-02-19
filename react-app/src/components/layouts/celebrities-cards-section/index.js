20e70d9f0982ce2e9d1d42d6ac084c209485d54a
German Solano <german@famosos.com>
2021-02-19 12:10:53 -0400

    feat(components): Implementar React-Window en secciones del Home para mejorar performance

--- react-app/src/components/layouts/celebrities-cards-section/index.js
+++ react-app/src/components/layouts/celebrities-cards-section/index.js
@@ -1,14 +1,51 @@
 import React, { useState, useRef, useEffect, useMemo } from "react";
 import debounce from "lodash.debounce";
+import { FixedSizeList } from "react-window";
 import { CelebrityCardLayout } from "../celebrity-card";
 import { VideoCardLayout } from "../video-card";
-
 import { NavLink } from "react-app/src/components/common/routing";
 import { SEARCH_PATH } from "../../../routing/Paths";
 import { jsonToQueryString } from "../../../state/utils/apiService";
 import * as GTM from "../../../state/utils/gtm";
 import getMoreFrequentIds from "../../../utils/getMoreFrequentIds";
 import getWindow from "react-app/src/utils/getWindow";
+import Maybe from "../../common/helpers/maybe";
+
+const celebrityCardWidth = 150;
+const videoCardWidth = 258;
+const celebrityCardSectionHeight = 219;
+const videoCardSectionHeight = 360;
+const cardGap = 12;
+
+const getColumn = (isVideoCardSection, celebritiesSectionId) => ({
+  data,
+  index,
+  style
+}) => {
+  const celebrity = data[index];
+  const isLastItem = index >= data.length - 1;
+  return (
+    <div
+      style={isLastItem ? { ...style, width: style.width - cardGap } : style}
+    >
+      <Maybe
+        it={isVideoCardSection}
+        orElse={<CelebrityCardLayout celebrity={celebrity} />}
+      >
+        <VideoCardLayout
+          celebrityId={celebrity.id}
+          celebrityAvatar={celebrity.avatar}
+          celebrityUsername={celebrity.username}
+          celebrityFullName={celebrity.fullName}
+          videoOccasion={celebrity.occasion}
+          videoUrl={celebrity.videoUrl}
+          videoPosterUrl={celebrity.videoPosterUrl}
+          videoKey={`${celebritiesSectionId}-${celebrity.id}`}
+        />
+      </Maybe>
+    </div>
+  );
+};
 
 const initialState = {
   showLeftScrollButton: false,
@@ -39,7 +76,7 @@ const CelebritiesCardsSectionLayout = ({
   };
 
   const scrollTo = (direction) => () => {
-    const cardListElement = cardListRef.current;
+    const cardListElement = cardListRef.current._outerRef;
     const { offsetWidth } = cardListElement;
     cardListElement.scrollBy({
       left: direction === "right" ? offsetWidth : offsetWidth * -1,
@@ -52,7 +89,11 @@ const CelebritiesCardsSectionLayout = ({
   };
 
   const setScrollButtonsVisibility = debounce(() => {
-    const { scrollLeft, offsetWidth, scrollWidth } = cardListRef.current;
+    const {
+      scrollLeft,
+      offsetWidth,
+      scrollWidth
+    } = cardListRef.current._outerRef;
     setShowLeftScrollButton(scrollLeft !== 0);
     setShowRightScrollButton(scrollLeft + offsetWidth !== scrollWidth);
     GTM.tagManagerDataLayer("SCROLL_CELEBRITY_SECTION_LIST", {
@@ -62,7 +103,7 @@ const CelebritiesCardsSectionLayout = ({
   }, 100);
 
   useEffect(() => {
-    const cardListElement = cardListRef.current;
+    const cardListElement = cardListRef.current._outerRef;
     setShowRightScrollButton(
       cardListElement.scrollWidth > cardListElement.offsetWidth
     );
@@ -97,6 +138,14 @@ const CelebritiesCardsSectionLayout = ({
       searchMoreResultsPath
     });
 
+  const { celebritySectionType } = celebritiesSection;
+  const isVideoCardSection = celebritySectionType !== "CELEBRITY_CARD";
+
+  const Column = useMemo(
+    () => getColumn(isVideoCardSection, celebritiesSection.id),
+    []
+  );
+
   return (
     <section
       className={`celebrities-section-layout container pr-0 ${
@@ -104,12 +153,13 @@ const CelebritiesCardsSectionLayout = ({
           ? "celebrities-sections-videos"
           : ""
       }`}
+      onMouseEnter={registerCelebritySectionHover}
     >
       <header className="celebrities-section__header d-flex justify-content-between">
         <h2 className={`celebrities-section-layout__title`}>
           {celebritiesSection.title}
         </h2>
-        {hasMoreResults ? (
+        <Maybe it={hasMoreResults}>
           <NavLink
             to={moreResultsPath || searchMoreResultsPath}
             className="mb-1 font-weight-bold mr-3 mr-sm-0 flex-shrink-0"
@@ -117,55 +167,44 @@ const CelebritiesCardsSectionLayout = ({
           >
             Ver más
           </NavLink>
-        ) : null}
+        </Maybe>
       </header>
-      {showLeftScrollButton ? (
+      <Maybe it={showLeftScrollButton}>
         <button
           className="celebrities-section-layout__scroll-to-button d-none d-md-block"
           onClick={scrollTo("left")}
         >
           <i className="fa fa-chevron-left text-white" />
         </button>
-      ) : null}
-      <ul
-        className="celebrities-section-layout__cards-list"
+      </Maybe>
+      <FixedSizeList
+        height={
+          isVideoCardSection
+            ? videoCardSectionHeight
+            : celebrityCardSectionHeight
+        }
         ref={cardListRef}
+        width={345}
+        layout="horizontal"
+        itemCount={celebrities.length}
+        itemData={celebrities}
         onScroll={setScrollButtonsVisibility}
-        onMouseOver={registerCelebritySectionHover}
+        className="celebrities-section-layout__cards-list"
+        useIsScrolling
+        itemSize={
+          (isVideoCardSection ? videoCardWidth : celebrityCardWidth) + cardGap
+        }
       >
-        {celebrities.length > 0
-          ? celebrities.map((celebrity, index) => (
-              <li
-                key={`${celebritiesSection.id}-${celebrity.id}`}
-                className="celebrities-section-layout__card-item"
-              >
-                {celebritiesSection.celebritySectionType !==
-                "CELEBRITY_CARD" ? (
-                  <VideoCardLayout
-                    celebrityId={celebrity.id}
-                    celebrityAvatar={celebrity.avatar}
-                    celebrityUsername={celebrity.username}
-                    celebrityFullName={celebrity.fullName}
-                    videoOccasion={celebrity.occasion}
-                    videoUrl={celebrity.videoUrl}
-                    videoPosterUrl={celebrity.videoPosterUrl}
-                    videoKey={`${celebritiesSection.id}-${celebrity.id}`}
-                  />
-                ) : (
-                  <CelebrityCardLayout celebrity={celebrity} />
-                )}
-              </li>
-            ))
-          : null}
-      </ul>
-      {showRightScrollButton ? (
+        {Column}
+      </FixedSizeList>
+      <Maybe it={showRightScrollButton}>
         <button
           className="celebrities-section-layout__scroll-to-button scroll-to-right-button d-none d-md-block"
           onClick={scrollTo("right")}
         >
           <i className="fa fa-chevron-right text-white" />
         </button>
-      ) : null}
+      </Maybe>
     </section>
   );
 };
