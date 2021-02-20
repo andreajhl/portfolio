27e89d1580421ede928101b0cf02204e48bc0a0d
German Solano <german@famosos.com>
2021-02-20 12:56:24 -0400

    feat(development-flow): Añadir bundle-analyzer para detectar librería que afecten el peso total de la app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+});
+
+module.exports = withBundleAnalyzer({
   compress: true,
   images: {
     domains: [
@@ -11,4 +15,4 @@ module.exports = {
       "via.placeholder.com"
     ]
   }
-};
+});
