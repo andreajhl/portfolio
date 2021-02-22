5379777e3926289a0834ed5e2abccc67e71ce9f3
Isaac Peña <isaac.pr99@gmail.com>
2021-02-22 17:48:22 -0400

    refactor: Se agrega nuevo metodos de pagos

--- constants/availablePaymentsMethods.ts
+++ constants/availablePaymentsMethods.ts
@@ -1,6 +1,7 @@
 export const AVAILABLE_PAYMENTS_METHODS = {
-    CARD : 'Tarjeta de crédito o débito',
-    BANK_TRANSFER: 'Transferencia bancaria',
-    TICKET: 'Efectivo',
-    PAYPAL: 'Paypal'
-}
+  CREDIT_CARD: "Tarjeta de crédito",
+  DEBIT_CARD: "Tarjeta de Debito",
+  BANK_TRANSFER: "Transferencia bancaria",
+  TICKET: "Efectivo",
+  PAYPAL: "Paypal"
+};
