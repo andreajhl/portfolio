d7fb0d23d035901fc5e9c59449a988916ae958d7
Isaac Peña <isaac.pr99@gmail.com>
2021-02-22 17:47:49 -0400

    refactor: Se agregan inputs dinamicos para seleccion de metodos de pago

---
+++ react-app/src/components/containers/dLocal-form-card/index.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState, useRef } from "react";
+import getWindow from "react-app/src/utils/getWindow";
+import { findDOMNode } from "react-dom";
+import scriptLoader from "react-async-script-loader";
+import { LoaderLayout } from "../../layouts/loader";
+const DLOCALKEY = process.env.NEXT_PUBLIC_DLOCAL_API_KEY;
+const CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_KEY;
+const scriptSrc = "https://js-sandbox.dlocal.com/";
+
+const DLocalFormCard = ({
+  isScriptLoaded,
+  isScriptLoadSucceed,
+  onScriptLoaded,
+  handleStartPayment,
+  paymentInProcess
+}) => {
+  const [card, setCard] = useState(null);
+  const [buyerName, setBuyerName] = useState("");
+  const [tokenError, settokenError] = useState("");
+  const [dLocalInstance, setDLocalInstance] = useState(null);
+  const handlerSubmitCreditCardDetails = (e) => {
+    e.preventDefault();
+    dLocalInstance
+      .createToken(card, {
+        name: buyerName
+      })
+      .then((result) => {
+        handleStartPayment(result.token);
+      })
+      .catch((err) => {
+        err.result
+          ? settokenError(err.result)
+          : settokenError("Los datos de su tarjeta no son validos");
+      });
+  };
+  const inputEl = useRef(null);
+  const style = {
+    base: {
+      fontSize: "16px",
+      color: "#32325d"
+    }
+  };
+  useEffect(() => {
+    if (isScriptLoaded && isScriptLoadSucceed) {
+      console.log("Script loaded");
+      const dlocalInstance = window.dlocal(DLOCALKEY);
+      const fields = dlocalInstance.fields({
+        locale: "es",
+        country: "CO"
+      });
+      const card = fields.create("card", { style: style });
+      card.mount(findDOMNode(inputEl.current));
+      setCard(card);
+      setDLocalInstance(dlocalInstance);
+    }
+  }, [isScriptLoaded, isScriptLoadSucceed]);
+  return isScriptLoaded ? (
+    <form className="d-flex flex-column">
+      <div className="form-group">
+        <label htmlFor="card-holdername" className="font-weight-bold">
+          CardHolder Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          value={buyerName}
+          onChange={(e) => setBuyerName(e.target.value)}
+          id="card-holdername"
+        ></input>
+        <label htmlFor="card-field mt-4">Numero de tarjeta</label>
+        <div id="card-field" ref={inputEl}></div>
+        <div id="card-errors" role="alert"></div>
+        <div>{tokenError}</div>
+      </div>
+      <button
+        onClick={(e) => handlerSubmitCreditCardDetails(e)}
+        className="btn btn-primary mx-auto"
+        style={{
+          backgroundColor: `${paymentInProcess ? "white" : "#FB177D"}`,
+          height: "50px",
+          borderRadius: "10px",
+          width: "80%"
+        }}
+      >
+        <LoaderLayout></LoaderLayout>
+      </button>
+    </form>
+  ) : (
+    <LoaderLayout></LoaderLayout>
+  );
+};
+
+export default scriptLoader(scriptSrc)(DLocalFormCard);
