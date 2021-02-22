cda15334b0c7e3464d0e48b773e2684b050c288d
Isaac Peña <isaac.pr99@gmail.com>
2021-02-22 17:49:17 -0400

    styles: Se realizan ajustes de estilos en inputs de formulario

--- react-app/src/components/containers/d-local-payments-form/index.tsx
+++ react-app/src/components/containers/d-local-payments-form/index.tsx
@@ -16,22 +16,31 @@ const DLocalPaymentsForm = ({
   return (
     <form>
       <div className="form-group">
-        <label>Nombre</label>
+        <label className="font-weight-bold">Nombre</label>
         <input
           type="text"
           onChange={(e) => setBuyerFullName(e.target.value)}
           className="form-control"
+          style={{
+            borderRadius: "10px"
+          }}
           placeholder="Escribe aquí tu nombre"
         ></input>
-        <label>Email</label>
+        <label className="font-weight-bold">Email</label>
         <input
+          style={{
+            borderRadius: "10px"
+          }}
           type="email"
           onChange={(e) => setBuyerEmail(e.target.value)}
           className="form-control"
           placeholder="Escribe aquí tu correo electronico"
         ></input>
-        <label>Id</label>
+        <label className="font-weight-bold">Cedula de identidad</label>
         <input
+          style={{
+            borderRadius: "10px"
+          }}
           type="text"
           onChange={(e) => setBuyerDocument(e.target.value)}
           className="form-control"
