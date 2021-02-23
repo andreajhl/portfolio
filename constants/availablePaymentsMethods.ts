55d773b9bb4230aca918dacc4573cd028fbd63e8
Isaac Peña <isaac.pr99@gmail.com>
2021-02-23 08:31:50 -0400

    refactor: Se corrijen mayusculas en texto

--- constants/availablePaymentsMethods.ts
+++ constants/availablePaymentsMethods.ts
@@ -1,7 +1,7 @@
 export const AVAILABLE_PAYMENTS_METHODS = {
-  CREDIT_CARD: "Tarjeta de crédito",
-  DEBIT_CARD: "Tarjeta de Debito",
-  BANK_TRANSFER: "Transferencia bancaria",
+  CREDIT_CARD: "Tarjeta de Crédito",
+  DEBIT_CARD: "Tarjeta de Débito",
+  BANK_TRANSFER: "Transferencia Bancaria",
   TICKET: "Efectivo",
   PAYPAL: "Paypal"
 };
