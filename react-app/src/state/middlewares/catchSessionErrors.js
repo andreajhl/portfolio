21dda434f509fea836a27c8d29fa1b11f81173fe
German Solano <german@famosos.com>
2021-02-23 19:22:10 -0400

    refactor: Cambiar condicional de sesión invalida

--- react-app/src/state/middlewares/catchSessionErrors.js
+++ react-app/src/state/middlewares/catchSessionErrors.js
@@ -6,7 +6,9 @@ const INVALID_TOKEN_ERRORS = [
   "Invalid audience.",
   "no token string was provided",
   "Unable to find appropriate key.",
-  "illegal base64 data at input byte 121"
+  "illegal base64 data at input byte 121",
+  "signing method (alg) is unavailable.",
+  "invalid character '!' looking for beginning of object key string"
 ];
 
 const UNAUTHORIZED_STATUS_CODE = 401;
@@ -14,9 +16,10 @@ const UNAUTHORIZED_STATUS_CODE = 401;
 export const invalidSessionWatcher = (store) => (next) => (action) => {
   if (
     action.type.endsWith(_FAILURE) &&
-    action.payload?.data?.api_error?.response?.status ===
-      UNAUTHORIZED_STATUS_CODE &&
-    INVALID_TOKEN_ERRORS.includes(action.payload?.data?.error)
+    (String(action.payload?.data?.error).startsWith("invalid token") ||
+      (action.payload?.data?.api_error?.response?.status ===
+        UNAUTHORIZED_STATUS_CODE &&
+        INVALID_TOKEN_ERRORS.includes(action.payload?.data?.error)))
   ) {
     return store.dispatch(setRedirectUnauthorized(true));
   }
