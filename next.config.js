2616c44012d5b4048e11ee4241728b8dc9677254
Isaac Peña <isaac.pr99@gmail.com>
2021-02-24 14:52:38 -0400

    fix: Se agrega dominio de imagenes en configuracion next

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   images: {
     domains: [
       "famosos-media.s3.amazonaws.com",
+      "famosos-output-media-testing.s3.amazonaws.com",
       "firebasestorage.googleapis.com",
       "d3dxo4xx2lwk55.cloudfront.net",
       "dqb0851cl2gjs.cloudfront.net",
