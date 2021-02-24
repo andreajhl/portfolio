04d7dc2839d5c6de3df51abe34d91d6207805267
Isaac Peña <isaac.pr99@gmail.com>
2021-02-24 16:34:01 -0400

    feat: Se crea objeto con textos correspondiente a cada estado de un pago

---
+++ constants/paymentStatus.ts
@@ -0,0 +1,11 @@
+export const paymentStatus = {
+  10: "Pago Creado",
+  20: "Pago Cancelado",
+  30: "Pago Rechazado",
+  40: "Pago Pendiente",
+  50: "Reembolso Fallido",
+  60: "Fallo Autorización de Cobro",
+  70: "Reembolso exitoso",
+  80: "Pago Autorizado",
+  90: "Pago Completado"
+};
