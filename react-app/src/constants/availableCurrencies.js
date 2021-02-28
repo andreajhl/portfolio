389707ef7294ee14fc9a722c4235d5c9126a3ac8
German Solano <german@famosos.com>
2021-02-28 14:06:39 -0400

    feat(components): Añadir componente PriceLayout

---
+++ react-app/src/constants/availableCurrencies.js
@@ -0,0 +1,135 @@
+export const AVAILABLE_CURRENCIES = [
+  {
+    name: "ARS",
+    label: "Pesos Argentinos",
+    symbol: "$",
+    implemented_by_dlocal: true,
+    flag: "https://restcountries.eu/data/arg.svg",
+    document_name: "DNI o CUIT",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "100"
+  },
+  {
+    name: "BRL",
+    label: "Real Brasileño",
+    symbol: "$",
+    implemented_by_dlocal: true,
+    flag: "https://restcountries.eu/data/bra.svg",
+    document_name: "CPF o CNPJ",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "5"
+  },
+  {
+    name: "CAD",
+    label: "Dólares Canadienses",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "https://datosmacro.expansion.com/img/flag/CAg.png",
+    document_name: "SIN",
+    decimal_separator: ".",
+    thousand_separator: ",",
+    round: "1"
+  },
+  {
+    name: "CLP",
+    label: "Pesos Chilenos",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "https://restcountries.eu/data/chl.svg",
+    document_name: "CI/RUT",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "1000"
+  },
+  {
+    name: "COP",
+    label: "Pesos Colombianos",
+    symbol: "$",
+    implemented_by_dlocal: true,
+    flag: "https://restcountries.eu/data/col.svg",
+    document_name: "CC",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "1000"
+  },
+  {
+    name: "EUR",
+    label: "Euros",
+    symbol: "€",
+    implemented_by_dlocal: false,
+    flag:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/1200px-Flag_of_Europe.svg.png",
+    document_name: "DNI",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "1"
+  },
+  {
+    name: "DOP",
+    label: "Pesos Dominicanos",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "https://restcountries.eu/data/dom.svg",
+    document_name: "Cédula",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "50"
+  },
+  {
+    name: "MXN",
+    label: "Pesos Mexicanos",
+    symbol: "$",
+    implemented_by_dlocal: true,
+    flag: "https://restcountries.eu/data/mex.svg",
+    document_name: "CURP",
+    decimal_separator: ".",
+    thousand_separator: ",",
+    round: "5"
+  },
+  {
+    name: "PEN",
+    label: "Sol",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "https://restcountries.eu/data/per.svg",
+    document_name: "DNI",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "5"
+  },
+  {
+    name: "PYG",
+    label: "Pesos Paraguayos",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "https://restcountries.eu/data/pry.svg",
+    document_name: "CI",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "10000"
+  },
+  {
+    name: "USD",
+    label: "Dólares",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "/assets/img/usa.svg",
+    document_name: "SSN",
+    decimal_separator: ".",
+    thousand_separator: ",",
+    round: "1"
+  },
+  {
+    name: "UYU",
+    label: "Pesos Uruaguayos",
+    symbol: "$",
+    implemented_by_dlocal: false,
+    flag: "https://restcountries.eu/data/ury.svg",
+    document_name: "CI",
+    decimal_separator: ",",
+    thousand_separator: ".",
+    round: "50"
+  }
+];
