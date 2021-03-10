49c1e3e69b08cdaf484af3d2f704b4db70a16f13
Isaac Peña <isaac.pr99@gmail.com>
2021-03-10 17:10:40 -0400

    feat: Agregar Dispatch para endpoint LIST_USERS_SUBSCRIPTIONS_ALARMS

--- react-app/src/state/ducks/subscription-celebrity-alarm/actions.js
+++ react-app/src/state/ducks/subscription-celebrity-alarm/actions.js
@@ -28,3 +28,30 @@ export const subscribeToEmailNotifications = (celebrity_id) => {
       });
   });
 };
+
+export const listUsersSubscriptionsAlarms = () => {
+  return (dispatch) => {
+    const TYPE = types.GET_USERS_SUBSCRIPTIONS_ALARMS_REQUEST;
+    const FINAL_PATH = API_PATHS.LIST_USERS_SUBSCRIPTIONS_ALARMS;
+    dispatch({ type: TYPE, payload: {} });
+    return apiService({
+      method: "GET",
+      action: TYPE,
+      path: FINAL_PATH,
+      async: true,
+      params: null,
+      body: null
+    })
+      .then((res) => {
+        if ("status" in res.data && res.data.status === "ERROR") {
+          handleApiResponseFailure(dispatch, TYPE, res);
+        } else {
+          handleApiResponseSuccess(dispatch, TYPE, res);
+          dispatch({ type: `${TYPE}_COMPLETED`, payload: res });
+        }
+      })
+      .catch((err) => {
+        handleApiErrors(dispatch, TYPE, err);
+      });
+  };
+};
--- react-app/src/state/ducks/subscription-celebrity-alarm/paths.js
+++ react-app/src/state/ducks/subscription-celebrity-alarm/paths.js
@@ -1,3 +1,4 @@
 export const MODEL = `subscription_celebrity_alarm`;
 
 export const SUBSCRIBE_CELEBRITY_ALARM = `custom-endpoints/${MODEL}/subscription`;
+export const LIST_USERS_SUBSCRIPTIONS_ALARMS = `custom-endpoints/${MODEL}/list`;
