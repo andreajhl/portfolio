2868cc67372059fee2bf4a463133dc2e07d5136d
Isaac Peña <isaac.pr99@gmail.com>
2021-03-10 17:11:37 -0400

    feat: Conectar dispatch de redux con boton

--- react-app/src/components/layouts/subscription-to-availability-notification/index.tsx
+++ react-app/src/components/layouts/subscription-to-availability-notification/index.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CallToActionButton } from "../call-to-action-button";
 import { parseFullName } from "parse-full-name";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useRouter } from "next/router";
 import { useLoginHandler } from "react-app/src/utils/useLoginHandler";
-import { subscribeToEmailNotifications } from "react-app/src/state/ducks/celebrities/actions";
+import { subscribeToEmailNotifications } from "react-app/src/state/ducks/subscription-celebrity-alarm/actions";
+import { connect } from "react-redux";
+import { subscriptionCelebrityAlarmOperations } from "../../../state/ducks/subscription-celebrity-alarm";
 
 type SubscriptionToAvailabilityNotificationProps = {
   className?: string;
-  text: string | React.ReactNode;
+  text?: string | React.ReactNode;
   celebrityFullName: string;
   celebrityUsername: string;
   showCelebrityName: boolean;
   fontSize: string;
   width: string;
+  celebrityId: number;
+  userSubscriptionsCelebrityAlarmsFetchCompleted: boolean;
+  userSubscriptionsCelebrityAlarmsData: {
+    results: Array<any>;
+    totalResults: number;
+  };
+  userSubscriptionsCelebrityAlarmsErrorData: { error: string };
+  userSubscriptionsCelebrityAlarmsIsLoading: boolean;
+  listUsersSubscriptionsAlarms: () => {};
 };
 
 const SubscriptionToAvailabilityNotification = ({
@@ -23,27 +34,40 @@ const SubscriptionToAvailabilityNotification = ({
   celebrityUsername,
   showCelebrityName,
   fontSize,
-  width
+  width,
+  celebrityId,
+  userSubscriptionsCelebrityAlarmsFetchCompleted,
+  userSubscriptionsCelebrityAlarmsData,
+  userSubscriptionsCelebrityAlarmsErrorData,
+  userSubscriptionsCelebrityAlarmsIsLoading,
+  listUsersSubscriptionsAlarms
 }: SubscriptionToAvailabilityNotificationProps) => {
   const router = useRouter();
   const { isLoading, isAuthenticated } = useAuth0();
   const fullNameWords = celebrityFullName.split(" ");
   const triggerLogin = useLoginHandler();
-
-  const handleSuscriptionRequest = () => {
-    // registerThisEventClick("CLICK");
+  useEffect(() => {
+    listUsersSubscriptionsAlarms();
+  }, [listUsersSubscriptionsAlarms]);
+  const handleSuscriptionRequest = async () => {
     if (!isAuthenticated) {
-      // TODO: En caso de ser login con redirect, se ha de setear en localStorage un key para realizar la suscripción al retornar
-      //   localStorage.setItem(
-      //     "finalRedirect",
-      //     "/" + celebrityUsername + "/contratar"
-      //   );
       triggerLogin();
     } else {
-      // TODO: agregar celebrity_id
-      // subscribeToEmailNotifications()
-      console.log("Trigger dispatch");
+      await subscribeToEmailNotifications(celebrityId)
+        .then((res) => {
+          router.reload();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
+
+  const checkSubscriptionToThisCelebrity = () => {
+    const result = userSubscriptionsCelebrityAlarmsData.results.some(
+      (celebrityData) => celebrityData.celebrityId === celebrityId
+    );
+    return result;
   };
 
   const parsedFullName = parseFullName(
@@ -58,7 +82,7 @@ const SubscriptionToAvailabilityNotification = ({
       ? fullNameWords.slice(0, 2).join(" ")
       : parsedFullName.first || parsedFullName.last;
 
-  return (
+  return userSubscriptionsCelebrityAlarmsFetchCompleted ? (
     <CallToActionButton
       onClick={handleSuscriptionRequest}
       fontSize={fontSize}
@@ -66,9 +90,34 @@ const SubscriptionToAvailabilityNotification = ({
       className={className}
     >
       {text}
+      {checkSubscriptionToThisCelebrity()
+        ? "No recibir notificación cuando este disponible"
+        : "Notificarme cuando este disponible"}
       {celebrityFullName && showCelebrityName ? " " + displayName : ""}
     </CallToActionButton>
-  );
+  ) : null;
 };
 
-export default SubscriptionToAvailabilityNotification;
+const mapStateToProps = ({ subscriptionCelebrityAlarm }) => {
+  return {
+    userSubscriptionsCelebrityAlarmsFetchCompleted:
+      subscriptionCelebrityAlarm.userSubscriptionsCelebrityAlarms.completed,
+    userSubscriptionsCelebrityAlarmsData:
+      subscriptionCelebrityAlarm.userSubscriptionsCelebrityAlarms.data,
+    userSubscriptionsCelebrityAlarmsErrorData:
+      subscriptionCelebrityAlarm.userSubscriptionsCelebrityAlarms.error_data,
+    userSubscriptionsCelebrityAlarmsIsLoading:
+      subscriptionCelebrityAlarm.userSubscriptionsCelebrityAlarms.loading
+  };
+};
+const mapDispatchToProps = {
+  listUsersSubscriptionsAlarms:
+    subscriptionCelebrityAlarmOperations.listUsersSubscriptionsAlarms
+};
+
+const _SubscriptionToAvailabilityNotification = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SubscriptionToAvailabilityNotification);
+
+export { _SubscriptionToAvailabilityNotification as SubscriptionToAvailabilityNotification };
