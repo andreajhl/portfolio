3dce9f1dea8ad9e4e5aaa4146a82ab027fd839b1
Isaac Peña <isaac.pr99@gmail.com>
2021-03-16 17:45:32 -0400

    feat: Agregar componente con lista de URLs para navegar en la web

---
+++ desktop-app/components/containers/webpage-list-links/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./styles.module.scss";
+const WebPageListLinks = () => {
+  return (
+    <ul className={styles.WebPageListLinks}>
+      <li>Preguntas Frecuentes</li>
+      <li>Terminos y Condiciones</li>
+      <li>Politica y Privacidad</li>
+      <li>Ayuda</li>
+      <li>Blog</li>
+      <li>Prensa</li>
+    </ul>
+  );
+};
+
+export default WebPageListLinks;
