d6ac27ce9f9c9febf1efea88cc65e4cbb2f1ab36
Isaac Peña <isaac.pr99@gmail.com>
2021-03-16 17:18:20 -0400

    feat: Crear componente para CTA de nuevos celebrities

---
+++ desktop-app/components/containers/celebrities-form-cta/index.tsx
@@ -0,0 +1,10 @@
+const CelebritiesFormCTA = () => {
+  return (
+    <div>
+      <p>¿Eres una celebridad o influencer? ¡Haz parte de nuestra familia!</p>
+      <button>Quiero Unirme</button>
+    </div>
+  );
+};
+
+export default CelebritiesFormCTA;
