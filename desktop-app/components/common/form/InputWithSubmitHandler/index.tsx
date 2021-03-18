0ad637d012bc0d15a51929972c6a12ac6375ccea
Isaac Peña <isaac.pr99@gmail.com>
2021-03-18 02:30:37 -0400

    feat: Agregar prop para manejar Placeholder dinamico

--- desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
+++ desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
@@ -1,15 +1,17 @@
 import styles from "./styles.module.scss";
 type InputWithSubmitHandlerProps = {
   className?: string;
+  placeHolderInput: string;
   btnType?: string;
 };
 const InputWithSubmitHandler = ({
   className = "",
+  placeHolderInput,
   btnType = "btn-primary"
 }: InputWithSubmitHandlerProps) => {
   return (
     <div className={`${styles.InputWithSubmitHandler} ${className}`}>
-      <input className={styles.Input} placeholder={"E-mail"}></input>
+      <input className={styles.Input} placeholder={placeHolderInput}></input>
       <button className={`btn ${btnType} ${styles.SubmitButton}`}>
         Suscribirme
       </button>
