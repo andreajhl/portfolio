dc468376dfd8be5079c38e8b1f79dbb5868a9db6
Isaac Peña <isaac.pr99@gmail.com>
2021-03-18 02:42:19 -0400

    feat: Agregar prop para manejar texto de boton dinamicamente

--- desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
+++ desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
@@ -1,19 +1,21 @@
 import styles from "./styles.module.scss";
 type InputWithSubmitHandlerProps = {
   className?: string;
-  placeHolderInput: string;
   btnType?: string;
+  placeHolderInput: string;
+  placeHolderButton: string;
 };
 const InputWithSubmitHandler = ({
   className = "",
   placeHolderInput,
-  btnType = "btn-primary"
+  btnType = "btn-primary",
+  placeHolderButton
 }: InputWithSubmitHandlerProps) => {
   return (
     <div className={`${styles.InputWithSubmitHandler} ${className}`}>
       <input className={styles.Input} placeholder={placeHolderInput}></input>
       <button className={`btn ${btnType} ${styles.SubmitButton}`}>
-        Suscribirme
+        {placeHolderButton}
       </button>
     </div>
   );
