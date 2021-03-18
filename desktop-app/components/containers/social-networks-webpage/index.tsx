1eb3b39be8f771a7983816c4316a280fa4a2b55f
Isaac Peña <isaac.pr99@gmail.com>
2021-03-18 01:21:44 -0400

    feat: Agregar <a> tag con enlaces

--- desktop-app/components/containers/social-networks-webpage/index.tsx
+++ desktop-app/components/containers/social-networks-webpage/index.tsx
@@ -2,9 +2,27 @@ import styles from "./styles.module.scss";
 const SocialNetWorksWebPage = () => {
   return (
     <div className={styles.SocialNetWorksWebPage}>
-      <i className="fab fa-linkedin-in"></i>
-      <i className="fab fa-instagram"></i>
-      <i className="fab fa-facebook-f"></i>
+      <a
+        href="https://www.instagram.com/famosos/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-instagram"></i>
+      </a>
+      <a
+        href="https://www.facebook.com/contratafamosos"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-facebook-f"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/company/famosos-inc/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-linkedin-in"></i>
+      </a>
     </div>
   );
 };
