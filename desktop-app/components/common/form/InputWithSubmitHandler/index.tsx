3e8c6e9f92945ed1d100f399761751fb25374406
Isaac Peña <isaac.pr99@gmail.com>
2021-03-18 03:14:10 -0400

    feat: Agregar prop para aceptar css in js

--- desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
+++ desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
@@ -2,17 +2,22 @@ import styles from "./styles.module.scss";
 type InputWithSubmitHandlerProps = {
   className?: string;
   btnType?: string;
+  style?: object;
   placeHolderInput: string;
   placeHolderButton: string;
 };
 const InputWithSubmitHandler = ({
   className = "",
   placeHolderInput,
+  style,
   btnType = "btn-primary",
   placeHolderButton
 }: InputWithSubmitHandlerProps) => {
   return (
-    <div className={`${styles.InputWithSubmitHandler} ${className}`}>
+    <div
+      style={{ ...style }}
+      className={`${styles.InputWithSubmitHandler} ${className}`}
+    >
       <input className={styles.Input} placeholder={placeHolderInput}></input>
       <button className={`btn ${btnType} ${styles.SubmitButton}`}>
         {placeHolderButton}
