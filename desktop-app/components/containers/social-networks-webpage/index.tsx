706cec04d2302843a4e248211c4869cfae616e6f
Isaac Peña <isaac.pr99@gmail.com>
2021-03-17 20:03:00 -0400

    feat: Crear componente para reenderizar redes sociales de Famosos

---
+++ desktop-app/components/containers/social-networks-webpage/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+const SocialNetWorksWebPage = () => {
+  return (
+    <div className={styles.SocialNetWorksWebPage}>
+      <i className="fab fa-linkedin-in"></i>
+      <i className="fab fa-instagram"></i>
+      <i className="fab fa-facebook-f"></i>
+    </div>
+  );
+};
+
+export default SocialNetWorksWebPage;
