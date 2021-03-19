e20df1abb3fdd81dd287310b431a89a39bcb52cb
German Solano <german@famosos.com>
2021-03-19 15:31:51 -0400

    refactor(components): Remover clase "row" de los componentes debido a los margenes negativos que añade

--- desktop-app/components/home/hero-section/index.tsx
+++ desktop-app/components/home/hero-section/index.tsx
@@ -4,32 +4,30 @@ function HeroSection() {
   return (
     <section className={styles.HeroSection}>
       <div className="container">
-        <div className="row">
-          <header className={"col-5 " + styles.HeroSectionHeader}>
-            <h1 className={styles.HeroSectionTitle}>
-              Los <span className="text-primary font-weight-bold ml-2"></span>{" "}
-              <br />a un clic de distancia.
-            </h1>
-            <p className={styles.HeroSectionCopy}>
-              Experiencias personalizadas
-              <br /> con tus famosos favoritos.
-            </p>
-            <ul className={styles.HeroSectionSteps}>
-              <li>
-                <i className="fa fa-star" />
-                <p>Elije a tu Famoso</p>
-              </li>
-              <li>
-                <i className="fa fa-comment-alt" />
-                <p>Personaliza el mensaje</p>
-              </li>
-              <li>
-                <i className="fa fa-play" />
-                <p>¡Disfruta tu video!</p>
-              </li>
-            </ul>
-          </header>
-        </div>
+        <header className={styles.HeroSectionHeader}>
+          <h1 className={styles.HeroSectionTitle}>
+            Los <span className="text-primary font-weight-bold ml-2"></span>{" "}
+            <br />a un clic de distancia.
+          </h1>
+          <p className={styles.HeroSectionCopy}>
+            Experiencias personalizadas
+            <br /> con tus famosos favoritos.
+          </p>
+          <ul className={styles.HeroSectionSteps}>
+            <li>
+              <i className="fa fa-star" />
+              <p>Elije a tu Famoso</p>
+            </li>
+            <li>
+              <i className="fa fa-comment-alt" />
+              <p>Personaliza el mensaje</p>
+            </li>
+            <li>
+              <i className="fa fa-play" />
+              <p>¡Disfruta tu video!</p>
+            </li>
+          </ul>
+        </header>
       </div>
       <div className={styles.HeroSectionContent}>
         <img
