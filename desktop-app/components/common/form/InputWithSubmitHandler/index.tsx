6fe74d1c57a842ee35074fd48e17a8390b0c621d
German Solano <german@famosos.com>
2021-03-22 09:58:02 -0400

    feat(<SearchBarSection />): Realizar busqueda al clickear el botón de "Buscar"

--- desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
+++ desktop-app/components/common/form/InputWithSubmitHandler/index.tsx
@@ -5,21 +5,35 @@ type InputWithSubmitHandlerProps = {
   style?: object;
   placeHolderInput: string;
   placeHolderButton: string;
+  setInputValue?: (value: string) => void;
+  inputValue?: string;
+  onSubmit?: () => void;
 };
 const InputWithSubmitHandler = ({
   className = "",
   placeHolderInput,
   style,
   btnType = "btn-primary",
-  placeHolderButton
+  placeHolderButton,
+  setInputValue = () => {},
+  inputValue = "",
+  onSubmit = () => {}
 }: InputWithSubmitHandlerProps) => {
   return (
     <div
       style={{ ...style }}
       className={`${styles.InputWithSubmitHandler} ${className}`}
     >
-      <input className={styles.Input} placeholder={placeHolderInput}></input>
-      <button className={`btn ${btnType} ${styles.SubmitButton}`}>
+      <input
+        className={styles.Input}
+        placeholder={placeHolderInput}
+        value={inputValue}
+        onChange={({ target: { value } }) => setInputValue(value)}
+      />
+      <button
+        className={`btn ${btnType} ${styles.SubmitButton}`}
+        onClick={onSubmit}
+      >
         {placeHolderButton}
       </button>
     </div>
--- desktop-app/components/home/search-bar-section/index.tsx
+++ desktop-app/components/home/search-bar-section/index.tsx
@@ -1,7 +1,18 @@
 import InputWithSubmitHandler from "desktop-app/components/common/form/InputWithSubmitHandler";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { SEARCH_PATH } from "react-app/src/routing/Paths";
 import styles from "./styles.module.scss";
 
 const SearchBarSection = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const router = useRouter();
+  function goToSearch() {
+    if (!inputValue) return;
+    router.push(SEARCH_PATH + "?limit=20&search=" + String(inputValue));
+  }
+
   return (
     <div className={styles.SearchBarSection}>
       <p>Explora entre más de 1,000 famosos</p>
@@ -9,6 +20,9 @@ const SearchBarSection = () => {
         style={{ height: "50px", width: "600px" }}
         placeHolderButton={"Buscar"}
         placeHolderInput={"Ej: Pibe Valderrama, Músicos, México..."}
+        setInputValue={setInputValue}
+        inputValue={inputValue}
+        onSubmit={goToSearch}
       />
     </div>
   );
