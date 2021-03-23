c9e7d51e34c56c05b40f68e35cbc2925924f3375
Isaac Peña <isaac.pr99@gmail.com>
2021-03-23 12:28:07 -0400

    feat(<AppLinksBanner/>): Agregar asersion en test para cantidad de elementos <a>

--- desktop-app/components/containers/app-links-banner/indext.test.js
+++ desktop-app/components/containers/app-links-banner/indext.test.js
@@ -8,4 +8,11 @@ describe("<AppLinksBanner/>", () => {
   it("should render without crash", () => {
     expect(wrapper.length).toEqual(1);
   });
+
+  describe("<a> tags should be correctly render", () => {
+    const linksElements = wrapper.find("a");
+    it("should contain 3 <a> tags", () => {
+      expect(linksElements.length).toEqual(3);
+    });
+  });
 });
