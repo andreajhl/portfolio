964aa5cef039a514b338deb9f6cae585eb32a0b7
German Solano <german@famosos.com>
2021-03-24 12:01:35 -0400

    feat(pages): Añadir componente <CelebrityProfilePAge /> parcialmente

---
+++ desktop-app/components/pages/celebrity-profile/index.tsx
@@ -0,0 +1,12 @@
+import PageContainer from "desktop-app/components/layouts/page-container";
+import React from "react";
+
+function CelebrityProfilePage() {
+  return (
+    <PageContainer>
+      <h1>Hello Page</h1>
+    </PageContainer>
+  );
+}
+
+export { CelebrityProfilePage };
