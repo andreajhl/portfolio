c50c196777d2969e313b44aa15210ec4fccfe026
Isaac Peña <isaac.pr99@gmail.com>
2021-03-25 09:35:49 -0400

    feat: Crear type para card reviews

--- desktop-app/components/common/cards/review/index.tsx
+++ desktop-app/components/common/cards/review/index.tsx
@@ -4,13 +4,15 @@ import classes from "classnames";
 import { StarIcon } from "../../icons";
 import StarRatingDisplay from "../../star-rating/display";
 import styles from "./styles.module.scss";
+import { CardReviewProps } from "../../../../types/cardReviewProps";
+
 const CardReview = ({
-  stars,
-  celebrity_name,
+  contract_stars,
+  user_full_name,
   date,
-  comment,
+  contract_review,
   showBox = true
-}) => {
+}: CardReviewProps) => {
   return (
     <div
       className={classes(styles.CardReview, {
@@ -20,13 +22,16 @@ const CardReview = ({
       <div className={styles.ReviewDetails}>
         <StarIcon></StarIcon>
         <span className={styles.ReviewStarValue}>
-          {Number(stars).toFixed(1)}
+          {Number(contract_stars).toFixed(1)}
         </span>
         <span className={styles.ReviewDate}>{date}</span>
-        <StarRatingDisplay editing={false} value={stars}></StarRatingDisplay>
+        <StarRatingDisplay
+          editing={false}
+          value={contract_stars}
+        ></StarRatingDisplay>
       </div>
-      <p>{celebrity_name}</p>
-      <p>{comment}</p>
+      <p>{user_full_name}</p>
+      <p>{contract_review}</p>
     </div>
   );
 };
