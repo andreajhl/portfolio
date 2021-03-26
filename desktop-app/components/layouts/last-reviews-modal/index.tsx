e4485856d78dc7c144cb86f88a67a1f3f5ce4769
Isaac Peña <isaac.pr99@gmail.com>
2021-03-26 14:08:32 -0400

    fix: Conectar con redux

--- desktop-app/components/layouts/last-reviews-modal/index.tsx
+++ desktop-app/components/layouts/last-reviews-modal/index.tsx
@@ -1,31 +1,29 @@
 import { AnimatedPopup } from "desktop-app/components/common/animated-popup";
-import { CardReviewProps } from "desktop-app/types/cardReviewProps";
-import React, { ReactNode } from "react";
-import { CelebrityReviewCardLayout } from "react-app/src/components/layouts/celebrity-review-card";
+import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import Popup from "reactjs-popup";
+import { connect } from "react-redux";
 import CardReview from "../../../components/common/cards/review";
 import styles from "./styles.module.scss";
+
+// mapStateToProps
+const mapStateToProps = ({ celebrities }) => ({
+  isLoading: celebrities.fetchReviewsReducer.loading,
+  reviews: celebrities.fetchReviewsReducer.data.results,
+  paginationData: celebrities.fetchReviewsReducer.data.informationPage
+});
+
+type StateProps = ReturnType<typeof mapStateToProps>;
 
 type LastReviewsModalProps = {
   children: {
     triggerElement: JSX.Element;
   };
-  reviews: Array<CardReviewProps>;
-  fetchMoreData: () => void;
-};
+} & StateProps;
 
-// mapStateToProps
-const mapStateToProps = (state) => ({
-  isLoading: state.celebrities.fetchPublicContractsReducer.loading,
-  publicContracts: state.celebrities.fetchPublicContractsReducer.data.results
-});
-
-const LastReviewsModal = ({
-  children,
-  reviews,
-  fetchMoreData
-}: LastReviewsModalProps) => {
+const LastReviewsModal = ({ children, reviews }: LastReviewsModalProps) => {
+  const fetchMoreData = () => {
+    console.log("fetchMoreData");
+  };
   return (
     <AnimatedPopup trigger={children.triggerElement} modal>
       <div className={styles.LastReviewsModal}>
@@ -61,4 +59,7 @@ const LastReviewsModal = ({
     </AnimatedPopup>
   );
 };
-export default LastReviewsModal;
+
+const _LastReviewsModal = connect(mapStateToProps)(LastReviewsModal);
+
+export { _LastReviewsModal as LastReviewsModal };
