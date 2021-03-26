780cc2deb0e26eb06706cfc16998c995663ca09c
Isaac Peña <isaac.pr99@gmail.com>
2021-03-26 07:57:11 -0400

    feat: Crear componente con descripcion de pasos para adquirir video mensaje

---
+++ desktop-app/components/contract-steps/index.tsx
@@ -0,0 +1,74 @@
+import styles from "./styles.module.scss";
+import {
+  CommentAlt,
+  ShoppingBag,
+  SurpriseBox
+} from "../../components/common/icons/index";
+import classes from "classnames";
+import Maybe from "../common/helpers/maybe";
+import { connect } from "react-redux";
+
+// mapStateToProps
+const mapStateToProps = ({ celebrities }) => ({
+  publicContracts: celebrities.fetchPublicContractsReducer.data.results
+});
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+
+type ContractStepsProps = StateProps;
+const ContractSteps = ({ publicContracts }: ContractStepsProps) => {
+  const getVariant = (totalContracts) => {
+    let variant = "NormalLayout";
+    if (totalContracts >= 3 || totalContracts === 0) {
+      variant = "FullLayout";
+    }
+    if (totalContracts === 1) {
+      variant = "PartialLayout";
+    }
+    return variant;
+  };
+  return (
+    <div
+      className={styles[`ContractSteps${getVariant(publicContracts?.length)}`]}
+    >
+      <h2>Cómo tener un video personalizado de tu Famoso favorito.</h2>
+      <div className={styles.ContractStepsDescription}>
+        <div className={styles.ContractStep}>
+          <div className={styles.ContractStepTitle}>
+            <CommentAlt></CommentAlt> <p>Personaliza el mensaje de tu video.</p>
+          </div>
+
+          <span>
+            Especifica la celebración, dedicatoria o motivo del video. Puedes
+            contar algunos detalles para que tu video quede lo más especial
+            posible.
+          </span>
+        </div>
+        <div className={styles.ContractStep}>
+          <div className={styles.ContractStepTitle}>
+            <ShoppingBag /> <p>Completa el proceso de compra.</p>
+          </div>
+          <span>
+            Elige el método de pago que prefieras y completa el proceso. Los
+            pagos son 100% seguros. ¡En 7 días o menos recibirás tu video!
+          </span>
+        </div>
+        <div className={styles.ContractStep}>
+          <div className={styles.ContractStepTitle}>
+            <SurpriseBox /> <p>¡Disfruta la experiencia!</p>
+          </div>
+          <span>
+            Una vez que tu video esté listo te notificaremos para que corras a
+            verlo, descargarlo y compartirlo.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const _ContractSteps = connect(mapStateToProps)(ContractSteps);
+
+export default ContractSteps;
+
+export { _ContractSteps as ContractSteps };
