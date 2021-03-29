6636eadc28646ef5ad2e2e3ea58f5ad8c2a4ede6
German Solano <german@famosos.com>
2021-03-29 16:01:47 -0400

    feat(<CreateContractWizard />): Añadir hook useForm para controlar los formularios del wizard

--- desktop-app/components/celebrity-profile/create-contract-wizard/index.tsx
+++ desktop-app/components/celebrity-profile/create-contract-wizard/index.tsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Wizard, Steps as StepsList, Step } from "react-albus";
 import styles from "./styles.module.scss";
 import { WizardTopNavigation } from "desktop-app/components/wizard-top-navigation";
+import useForm from "lib/hooks/useForm";
 
 const mapStateToProps = (state) => ({ ...state });
 
@@ -16,7 +17,19 @@ type CreateContractWizardProps = {
 } & StateProps &
   DispatchProps;
 
+const initialValues = {
+  deliveryTo: "",
+  deliveryFrom: ""
+};
+
 function CreateContractWizard({ celebrity }: CreateContractWizardProps) {
+  const form = useForm<typeof initialValues>({
+    initialValues,
+    onSubmit(values) {
+      console.log(values);
+    }
+  });
+
   return (
     <div className={styles.CreateContractWizard}>
       <Wizard>
@@ -27,6 +40,12 @@ function CreateContractWizard({ celebrity }: CreateContractWizardProps) {
               Video personalizado
               <br /> de {celebrity.fullName}
             </h3>
+            <input
+              type="text"
+              name="deliveryFrom"
+              value={form.values.deliveryFrom}
+              onChange={form.onChangeField}
+            />
           </Step>
           <Step id="detalles1">
             <h3>
