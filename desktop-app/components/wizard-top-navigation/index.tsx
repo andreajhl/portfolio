dce732463bd5702bad8a6934b208af5f57afad40
German Solano <german@famosos.com>
2021-03-29 15:14:40 -0400

    feat(components): Añadir <WizardTopNavigation />

---
+++ desktop-app/components/wizard-top-navigation/index.tsx
@@ -0,0 +1,59 @@
+import styles from "./styles.module.scss";
+import { WithWizard } from "react-albus";
+import { LeftArrowIcon } from "../common/icons";
+import classes from "classnames";
+import Maybe from "../common/helpers/maybe";
+
+type WizardTopNavigationProps = {
+  className?: string;
+  enableNavigation?: boolean;
+  onNavigationClick?: (any) => void;
+};
+
+function WizardTopNavigation({
+  className = "",
+  enableNavigation = false,
+  onNavigationClick = () => {}
+}: WizardTopNavigationProps) {
+  function getClassName(steps, step, index, stepItem) {
+    if (steps.indexOf(step) === index) {
+      return styles.WizardTopNavigationStepDoing;
+    } else if (steps.indexOf(step) > index || stepItem.isDone) {
+      stepItem.isDone = true;
+      return styles.WizardTopNavigationStepDone;
+    }
+  }
+
+  function itemClick(stepItem, push) {
+    if (!enableNavigation) return;
+    push(stepItem.id);
+    // topNavigationClick(stepItem, push);
+  }
+
+  return (
+    <WithWizard>
+      {({ steps, step, previous, push }) => (
+        <div className={classes(styles.WizardTopNavigation, className)}>
+          <Maybe it={steps.indexOf(step) !== 0}>
+            <LeftArrowIcon onClick={previous} />
+          </Maybe>
+          <ul className={styles.WizardTopNavigationStepsList}>
+            {steps.map((stepItem, index) => (
+              <li
+                className={classes(
+                  styles.WizardTopNavigationStep,
+                  enableNavigation &&
+                    styles.WizardTopNavigationStepEnabledNavigation,
+                  getClassName(steps, step, index, stepItem)
+                )}
+                onClick={() => itemClick(stepItem, push)}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
+    </WithWizard>
+  );
+}
+
+export { WizardTopNavigation };
