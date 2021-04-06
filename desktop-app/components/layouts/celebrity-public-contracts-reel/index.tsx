23b63e09c34daac4a53a3aa96a71f9243727e3d1
German Solano <german@famosos.com>
2021-04-06 09:08:54 -0400

    refactor(<CelebrityPublicContractsReel />): :fire: Remover data de prueba y mover props para reducir tamaño del archivo

--- desktop-app/components/layouts/celebrity-public-contracts-reel/index.tsx
+++ desktop-app/components/layouts/celebrity-public-contracts-reel/index.tsx
@@ -5,53 +5,12 @@ import { CardsReelSection } from "../cards-section-reel";
 import { connect } from "react-redux";
 import styles from "./styles.module.scss";
 import Maybe from "react-app/src/components/common/helpers/maybe";
-const mockData = [
-  {
-    contract_id: 9951,
-    contract_reference: "202102231457-1532892-9951",
-    contract_delivery_to: "Juanito",
-    user_full_name: "Isaac Peña",
-    contract_media:
-      "https://famosos-output-videos-testing.s3.amazonaws.com/videos/609/9951/famosos-videos-personalizados-enriquearce-202102231457-1532892-9951-crf-video-watermark480.mp4",
-    video_poster_url:
-      "https://famosos-output-videos-testing.s3.amazonaws.com/videos/609/9951/famosos-videos-personalizados-enriquearce-202102231457-1532892-9951-crf-video-poster480.jpg"
-  }
-  // {
-  //   contract_id: 9950,
-  //   contract_reference: "202102231451-1855905-9950",
-  //   contract_delivery_to: "Juanito",
-  //   user_full_name: "Isaac Peña",
-  //   contract_media:
-  //     "https://famosos-output-videos-testing.s3.amazonaws.com/videos/609/9950/famosos-videos-personalizados-enriquearce-202102231451-1855905-9950-crf-video-watermark480.mp4",
-  //   video_poster_url:
-  //     "https://famosos-output-videos-testing.s3.amazonaws.com/videos/609/9950/famosos-videos-personalizados-enriquearce-202102231451-1855905-9950-crf-video-poster480.jpg"
-  // }
-];
 
-const publicContractsVideoReelProps = {
-  itemWidth: 263,
-  itemHeight: 350,
-  buttonsStyle: {
-    size: 49,
-    top: 175,
-    transform: "translateY(-50%)"
-  },
-  gap: 27,
-  children: (contract) => (
-    <ContractVideo
-      videoUrl={contract.contract_media}
-      videoPosterUrl={contract.video_poster_url}
-    />
-  )
-};
-
-// mapStateToProps
 const mapStateToProps = (state) => ({
   isLoading: state.celebrities.fetchPublicContractsReducer.loading,
   publicContracts: state.celebrities.fetchPublicContractsReducer.data.results
 });
 
-// mapStateToProps
 const mapDispatchToProps = {
   listPublicContracts: celebrityOperations.listPublicContracts
 };
@@ -67,6 +26,19 @@ type CelebrityPublicContractsReelProps = {
 } & StateProps &
   DispatchProps;
 
+const buttonsStyle = {
+  size: 49,
+  top: 175,
+  transform: "translateY(-50%)"
+};
+
+const renderContractVideo = (contract) => (
+  <ContractVideo
+    videoUrl={contract.contract_media}
+    videoPosterUrl={contract.video_poster_url}
+  />
+);
+
 const CelebrityPublicContractsReel = ({
   isLoading,
   publicContracts,
@@ -80,18 +52,18 @@ const CelebrityPublicContractsReel = ({
     <Maybe it={publicContracts.length > 0}>
       <div>
         <CardsReelSection
-          title={<h2>Últimos videos</h2>}
+          title="Últimos videos"
           className={
-            publicContracts.length < 4 ? styles.ReelContainerFitContent : ""
+            publicContracts.length < 3 ? styles.ReelContainerFitContent : ""
           }
           itemCount={publicContracts.length}
           itemData={publicContracts}
-          itemWidth={publicContractsVideoReelProps.itemWidth}
-          itemHeight={publicContractsVideoReelProps.itemHeight}
-          buttonsStyle={publicContractsVideoReelProps.buttonsStyle}
-          gap={publicContractsVideoReelProps.gap}
+          itemWidth={263}
+          itemHeight={350}
+          buttonsStyle={buttonsStyle}
+          gap={27}
         >
-          {publicContractsVideoReelProps.children}
+          {renderContractVideo}
         </CardsReelSection>
       </div>
     </Maybe>
