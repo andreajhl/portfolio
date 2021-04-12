0847f2b84f209ab61e0cf6ad948f3a393c0cae00
Isaac Peña <isaac.pr99@gmail.com>
2021-04-12 13:46:58 -0400

    fix: remover pt de los locales configurados en nextjs

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = withBundleAnalyzer({
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["es", "en", "pt"],
+    locales: ["es", "en"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "es"
