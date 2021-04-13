7244114ab00d2e53b627bbe8ccf9218942da7d26
Isaac Peña <isaac.pr99@gmail.com>
2021-04-13 17:00:38 -0400

    feat: :sparkles: agregar props para cambiar estilos de componente

--- desktop-app/components/common/badge/index.tsx
+++ desktop-app/components/common/badge/index.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { XIcon } from "../icons";
 import styles from "./styles.module.scss";
+import classes from "classnames";
+
 type BadgeProps = {
   text: string;
   showIcon?: boolean;
   iconVariant?: React.ReactNode;
   onClick?: () => void;
+  style?: React.CSSProperties;
+  className?: string;
 };
 
 function Badge({
   text,
   showIcon = true,
+  style,
   iconVariant = <XIcon className={styles.XIcon} />,
+  className,
   onClick
 }: BadgeProps) {
   return (
-    <div className={styles.Badge}>
+    <div
+      className={classes(styles.Badge, className)}
+      style={{
+        ...style
+      }}
+    >
       <span className={styles.BadgeText}>{text}</span>
       {showIcon ? (
         <div className={styles.IconContainer} onClick={() => onClick()}>
