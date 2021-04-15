774479eba60d5922be4889c6a8570c752f7a45e3
Isaac Peña <isaac.pr99@gmail.com>
2021-04-15 11:29:17 -0400

    feat(<CreateContractForm/>): habilitar numeros largos en PhoneInput component

--- react-app/src/components/containers/create-contract-form/index.js
+++ react-app/src/components/containers/create-contract-form/index.js
@@ -538,6 +538,7 @@ class CreateContractForm extends Component {
           <div className={"form-custom-vertical-group"}>
             <label>¿Quieres recibir el video a tu WhatsApp? (Opcional)</label>
             <PhoneInput
+              enableLongNumbers={true}
               enableSearch
               searchClass="d-flex align-items-center p-2"
               searchPlaceholder="Buscar país"
