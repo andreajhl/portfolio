c1eb7544e2ae247fc8959b0fa57066dc55b7556e
German Solano <german@famosos.com>
2021-04-22 07:36:23 -0400

    refactor(<ContractInfo />): :truck: Abstraer <ContractInfoHeader />

---
+++ desktop-app/components/payments-methods/contract-info-header/index.tsx
@@ -0,0 +1,47 @@
+import { ProfilePicture } from "react-app/src/components/layouts/profile-picture";
+import { occasionsData } from "react-app/src/constants/options";
+import styles from "./styles.module.scss";
+
+type ContractInfoHeaderProps = {
+  occasion: string;
+  celebrityAvatar: string;
+  celebrityFullName: string;
+};
+
+function ContractInfoHeader({
+  occasion,
+  celebrityAvatar,
+  celebrityFullName
+}: ContractInfoHeaderProps) {
+  const occasionKey = occasion || "OTHER";
+  const contractOccasion = occasionsData[occasionKey];
+  return (
+    <>
+      <div className={styles.ContractInfoCelebrityInfo}>
+        <ProfilePicture
+          imageStyles={{
+            height: 102,
+            objectFit: "cover",
+            marginRight: "40px"
+          }}
+          width={102}
+          avatar={"/assets/img/hero-background.jpg" || celebrityAvatar}
+        />
+        <h1 className={styles.ContractInfoTitle}>
+          Video personalizado de {celebrityFullName}
+        </h1>
+      </div>
+      <div className={styles.ContractInfoOccasion}>
+        <div className={styles.ContractInfoOccasionIcon}>
+          <img
+            src={`/assets/img/occasions/${occasionKey}.png`}
+            alt={contractOccasion.title}
+          />
+        </div>
+        {contractOccasion.title}
+      </div>
+    </>
+  );
+}
+
+export { ContractInfoHeader };
--- desktop-app/components/payments-methods/contract-info/index.tsx
+++ desktop-app/components/payments-methods/contract-info/index.tsx
@@ -1,3 +1,5 @@
+import { ContractInfoHeader } from "desktop-app/components/payments-methods/contract-info-header";
+import { ContractPriceSummary } from "desktop-app/components/contract-price-summary";
 import { ContractDataForm } from "desktop-app/components/payments-methods/contract-data-form";
 import { ProfilePicture } from "react-app/src/components/layouts/profile-picture";
 import { occasionsData } from "react-app/src/constants/options";
@@ -20,37 +22,16 @@ function ContractInfo({
   deliveryFrom,
   instructions
 }: ContractInfoProps) {
-  const occasionKey = occasion || "OTHER";
-  const contractOccasion = occasionsData[occasionKey];
-
   return (
     <div className={styles.ContractInfo}>
       <header
         className={`${styles.ContractInfoSection} ${styles.ContractInfoHeader}`}
       >
-        <div className={styles.ContractInfoCelebrityInfo}>
-          <ProfilePicture
-            imageStyles={{
-              height: 102,
-              objectFit: "cover",
-              marginRight: "40px"
-            }}
-            width={102}
-            avatar={"/assets/img/hero-background.jpg" || celebrityAvatar}
-          />
-          <h1 className={styles.ContractInfoTitle}>
-            Video personalizado de {celebrityFullName}
-          </h1>
-        </div>
-        <div className={styles.ContractInfoOccasion}>
-          <div className={styles.ContractInfoOccasionIcon}>
-            <img
-              src={`/assets/img/occasions/${occasionKey}.png`}
-              alt={contractOccasion.title}
-            />
-          </div>
-          {contractOccasion.title}
-        </div>
+        <ContractInfoHeader
+          occasion={occasion}
+          celebrityAvatar={celebrityAvatar}
+          celebrityFullName={celebrityFullName}
+        />
       </header>
       <div
         className={`${styles.ContractInfoSection} ${styles.ContractInfoData}`}
