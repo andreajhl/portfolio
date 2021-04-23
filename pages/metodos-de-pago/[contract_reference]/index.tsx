b596b76fc0a9dc9b46c3a0fce559c5e18081cab4
German Solano <german@famosos.com>
2021-04-23 19:08:51 -0400

    refactor(<PaymentMethods />): :zap: Añadir imports dinamicos

--- pages/metodos-de-pago/[contract_reference]/index.tsx
+++ pages/metodos-de-pago/[contract_reference]/index.tsx
@@ -1,12 +1,23 @@
 import { GetServerSideProps } from "next";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
-import { PaymentMethodsPage } from "react-app/src/components/pages/payment-methods";
-import { PaymentMethodsPage as DesktopPaymentMethodsPage } from "desktop-app/components/pages/payment-methods";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import LoadingPage from "react-app/src/components/layouts/loading-page";
 import isMobile from "lib/utils/isMobile";
 import Maybe from "desktop-app/components/common/helpers/maybe";
 import { useDesktopClass } from "lib/hooks/useDesktopClass";
+import dynamic from "next/dynamic";
+
+const PaymentMethodsPage = dynamic<{ contractReference: string }>(() =>
+  import("react-app/src/components/pages/payment-methods").then(
+    (mod) => mod.PaymentMethodsPage
+  )
+);
+
+const DesktopPaymentMethodsPage = dynamic<{ contractReference: string }>(() =>
+  import("desktop-app/components/pages/payment-methods").then(
+    (mod) => mod.PaymentMethodsPage
+  )
+);
 
 export const getServerSideProps: GetServerSideProps = async ({
   params: { contract_reference },
