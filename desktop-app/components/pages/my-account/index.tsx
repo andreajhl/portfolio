48193a50e82ce7a74a782748e131d24b9973117a
Isaac Peña <isaac.pr99@gmail.com>
2021-04-26 16:46:37 -0400

    feat: agregar componente page para vista /my-account

---
+++ desktop-app/components/pages/my-account/index.tsx
@@ -0,0 +1,24 @@
+import PageContainer from "desktop-app/components/layouts/page-container";
+import { PageHeading } from "desktop-app/components/layouts/page-heading";
+import React from "react";
+
+function ClientProfilePage() {
+  return (
+    <PageContainer>
+      <PageHeading showBackButton={false}>Mi Perfil</PageHeading>
+      <main>
+        {/* INFORMACION DE CUENTA */}
+
+        {/* MULTIPLE SETTINGS */}
+
+        {/* FAMOSOS FAVORITOS */}
+
+        {/* VIDEOS FAVORITOS */}
+
+        <span>hola mundo</span>
+      </main>
+    </PageContainer>
+  );
+}
+
+export { ClientProfilePage };
