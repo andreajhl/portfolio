f16726b68b0f8954d0a1ad88b2e135d3e32a6e72
German Solano <german@famosos.com>
2021-04-28 18:49:23 -0400

    feat(<MyHiringsCardHeader />): :lipstick: Modificar color de texto cuando el contrato está cancelado

--- desktop-app/components/my-hirings/my-hirings-card-header/index.tsx
+++ desktop-app/components/my-hirings/my-hirings-card-header/index.tsx
@@ -1,7 +1,12 @@
+import {
+  COMPLETED,
+  contractIsCancelled,
+} from "desktop-app/constants/contractStatuses";
 import MyHiringsContract from "desktop-app/types/myHiringsContract";
 import Maybe from "../../common/helpers/maybe";
 import { MyHiringsCardStatusBadge } from "../my-hirings-card-status-badge";
 import getValidDate from "./getValidDate";
+import classes from "classnames";
 import styles from "./styles.module.scss";
 
 type MyHiringsCardHeaderProps = {
@@ -11,7 +16,7 @@ type MyHiringsCardHeaderProps = {
 function getFormattedDate(date) {
   const dateObject = getValidDate(date);
   const monthString = dateObject.toLocaleDateString("es", {
-    month: "short"
+    month: "short",
   });
   return [dateObject.getDate(), monthString, dateObject.getFullYear()].join(
     "/"
@@ -26,8 +31,6 @@ function getExpectedDeliveryDate(paymentDate) {
   return dateObject;
 }
 
-const COMPLETED_STATUS = 40;
-
 function MyHiringsCardHeader({ contractData }: MyHiringsCardHeaderProps) {
   return (
     <header className={styles.MyHiringsCardHeader}>
@@ -36,13 +39,19 @@ function MyHiringsCardHeader({ contractData }: MyHiringsCardHeaderProps) {
           <h2>Video personalizado de {contractData.celebrityData.fullName}</h2>
           <MyHiringsCardStatusBadge status={contractData.status} />
         </div>
-        <div className={styles.MyHiringsCardHeaderInfo}>
+        <div
+          className={classes(
+            styles.MyHiringsCardHeaderInfo,
+            contractIsCancelled(contractData.status) &&
+              styles.MyHiringsCardHeaderInfoDisabled
+          )}
+        >
           <span>
             Fecha de solicitud: {getFormattedDate(contractData.paymentDate)}
           </span>
           <span>
             <Maybe
-              it={contractData.status === COMPLETED_STATUS}
+              it={contractData.status === COMPLETED}
               orElse={"Fecha estimada de grabación:"}
             >
               Fecha de grabación:
