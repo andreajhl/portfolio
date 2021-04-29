8b36d566089aefe1b0b72523d00423bbf8f19f56
German Solano <german@famosos.com>
2021-04-29 12:12:20 -0400

    feat(<MyHiringsCardNotificationInfo />): :lipstick: Ocultar botones de edición cuando no se pueda editar el contrato

--- desktop-app/components/my-hirings/my-hirings-card-notification-info/index.tsx
+++ desktop-app/components/my-hirings/my-hirings-card-notification-info/index.tsx
@@ -2,8 +2,21 @@ import { DeliveryCellphoneEditableInput } from "desktop-app/components/my-hiring
 import { DeliveryContactEditableInput } from "desktop-app/components/my-hirings/delivery-contact-editable-input";
 import MyHiringsContract from "desktop-app/types/myHiringsContract";
 import classes from "classnames";
-import { COMPLETED } from "desktop-app/constants/contractStatuses";
+import {
+  canEditContract,
+  COMPLETED,
+} from "desktop-app/constants/contractStatuses";
+import Maybe from "desktop-app/components/common/helpers/maybe";
 import styles from "./styles.module.scss";
+
+function InfoField({ title, value }: { title: string; value: string }) {
+  return (
+    <div className={styles.InfoWrapper}>
+      <h4 className={styles.InfoTitle}>{title}</h4>
+      <span className={styles.InfoValue}>{value}</span>
+    </div>
+  );
+}
 
 type MyHiringsCardNotificationInfoProps = {
   contractData: MyHiringsContract;
@@ -12,18 +25,41 @@ type MyHiringsCardNotificationInfoProps = {
 function MyHiringsCardNotificationInfo({
   contractData,
 }: MyHiringsCardNotificationInfoProps) {
+  const canEdit = canEditContract(contractData.status);
+
+  const deliveryCellphone = "+52 55 4375 0949";
+
   return (
     <div
       className={classes(
         styles.MyHiringsCardDeliveryInfo,
-        contractData.status === COMPLETED && styles.MyHiringsCardDeliveryInfoCompleted
+        contractData.status === COMPLETED &&
+          styles.MyHiringsCardDeliveryInfoCompleted
       )}
     >
-      <DeliveryContactEditableInput
-        deliveryContact={contractData.deliveryContact}
-        className={styles.EditableInputField}
-      />
-      <DeliveryCellphoneEditableInput className={styles.EditableInputField} />
+      <Maybe
+        it={canEdit}
+        orElse={
+          <>
+            <InfoField
+              title="Correo electrónico de notificación"
+              value={contractData.deliveryContact}
+            />
+            <Maybe it={Boolean(deliveryCellphone)}>
+              <InfoField
+                title="Whatsapp de notificación"
+                value={deliveryCellphone}
+              />
+            </Maybe>
+          </>
+        }
+      >
+        <DeliveryContactEditableInput
+          deliveryContact={contractData.deliveryContact}
+          className={styles.EditableInputField}
+        />
+        <DeliveryCellphoneEditableInput className={styles.EditableInputField} />
+      </Maybe>
     </div>
   );
 }
