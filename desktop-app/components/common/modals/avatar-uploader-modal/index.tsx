e809a029b9d74b0506c2fa9945427393aba81dcf
German Solano <german@famosos.com>
2021-05-01 18:07:34 -0400

    feat(<AvatarUploaderModal />): :sparkles: Finalizar la conexión de los componentes y las callbacks

--- desktop-app/components/common/modals/avatar-uploader-modal/index.tsx
+++ desktop-app/components/common/modals/avatar-uploader-modal/index.tsx
@@ -1,10 +1,11 @@
 import { FileUploaderModal } from "../file-uploader-modal";
-import { useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import {
   Crop,
   ImageCropper,
 } from "desktop-app/components/common/widgets/image-cropper";
 import styles from "./styles.module.scss";
+import { Dispatch, SetStateAction } from "react";
 
 const initialCropValue: Crop = {
   aspect: 1,
@@ -15,27 +16,45 @@ const initialCropValue: Crop = {
 type AvatarUploaderModalProps = {
   isOpen: boolean;
   initialImageSrc: string;
+  setIsOpen?: Dispatch<SetStateAction<boolean>>;
+  onImageUploaded?: (url: string, image: Blob) => void;
 };
 
 function AvatarUploaderModal({
   isOpen,
   initialImageSrc,
+  onImageUploaded = function () {},
+  setIsOpen = function () {},
 }: AvatarUploaderModalProps) {
   const [crop, setCrop] = useState<Crop>(initialCropValue);
-  const [image, setImage] = useState(null);
 
   return (
-    <FileUploaderModal isOpen={isOpen}>
-      <ImageCropper
-        className={styles.ImageCropper}
-        crop={crop}
-        onChange={setCrop}
-        circularCrop
-        ruleOfThirds
-        locked={false}
-        imageSrc={initialImageSrc}
-        onCropImage={setImage}
-      />
+    <FileUploaderModal
+      title="Recorta tu foto del perfil"
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      onFileUploaded={(...params) => {
+        setCrop(initialCropValue);
+        onImageUploaded(...params);
+      }}
+    >
+      {(setImageToUpload) => (
+        <ImageCropper
+          className={styles.ImageCropper}
+          crop={crop}
+          onChange={setCrop}
+          keepSelection
+          circularCrop
+          ruleOfThirds
+          imageAlt="Previsualización de cortado"
+          onImageLoaded={(image) => {
+            image.draggable = false;
+            image.className = styles.PreviewImage;
+          }}
+          imageSrc={initialImageSrc}
+          onCropImage={setImageToUpload}
+        />
+      )}
     </FileUploaderModal>
   );
 }
