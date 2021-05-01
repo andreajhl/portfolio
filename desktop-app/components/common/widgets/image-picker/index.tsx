2857b2ee7f37f57f51e6e968be518b58844fb747
German Solano <german@famosos.com>
2021-05-01 12:01:14 -0400

    feat(<ImagePicker />): :sparkles: Añadir elementos, logica y estilos iniciales

---
+++ desktop-app/components/common/widgets/image-picker/index.tsx
@@ -0,0 +1,40 @@
+import classes from "classnames";
+import styles from "./styles.module.scss";
+
+const ONLY_IMAGE_FORMATS = "image/*,image/heif,image/heic";
+
+type ImagePickerProps = {
+  id?: string;
+  previewImageSrc: string;
+  label?: string;
+  onPickImage?: (imageFile: File) => void;
+};
+
+function ImagePicker({
+  id = "image-picker-input",
+  previewImageSrc,
+  label = "Seleccionar imagen",
+  onPickImage = function () {},
+}: ImagePickerProps) {
+  return (
+    <div className={styles.Container}>
+      <img
+        className={styles.Preview}
+        alt="Previsualización"
+        src={previewImageSrc}
+      />
+      <input
+        type="file"
+        id={id}
+        accept={ONLY_IMAGE_FORMATS}
+        hidden
+        onChange={({ target }) => onPickImage(target?.files?.[0])}
+      />
+      <label htmlFor={id} className={classes("btn", styles.CTAUploadPhoto)}>
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export { ImagePicker };
