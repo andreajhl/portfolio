07a859e68110a8bae1dddd435ad2467f478304e6
German Solano <german@famosos.com>
2021-05-02 11:24:34 -0400

    feat(<CloseModalButton />): :lipstick: Añadir variante clara

--- desktop-app/components/common/button/close-modal-button/index.tsx
+++ desktop-app/components/common/button/close-modal-button/index.tsx
@@ -2,17 +2,25 @@ import { ButtonHTMLAttributes } from "react";
 import classes from "classnames";
 import styles from "./styles.module.scss";
 
-type CloseModalButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type CloseModalButtonProps = {
+  variant: "dark" | "light";
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 function CloseModalButton({
   className = "",
   children,
+  variant = "dark",
   ...props
 }: CloseModalButtonProps) {
   return (
     <button
       type="button"
-      className={classes("btn", styles.CloseModalButton, className)}
+      className={classes(
+        "btn",
+        styles.CloseModalButton,
+        styles[variant],
+        className
+      )}
       {...props}
     >
       <i className="fa fa-times" />
