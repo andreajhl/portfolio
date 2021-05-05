7512a14bb582d5b8d69a7eed63d96f3910e53640
Isaac Peña <isaac.pr99@gmail.com>
2021-05-05 14:55:58 -0400

    feat: implementar skeleton item en componente

--- desktop-app/components/user-profile/notification-slang-form/index.tsx
+++ desktop-app/components/user-profile/notification-slang-form/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 import { connect } from "react-redux";
 import styles from "./styles.module.scss";
 
@@ -12,19 +13,27 @@ type DispatchProps = typeof mapDispatchToProps;
 type NotificationSlangFormProps = StateProps & DispatchProps;
 // TODO: conectar con endpoint creado en branch master
 function NotificationSlangForm(props: NotificationSlangFormProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 5000);
+  }, []);
   return (
     <div className={styles.NotificationSlangFormContainer}>
       <label htmlFor="notifications_slang_select">
-        Idioma de Notificaciones:
+        Idioma de la página <br></br> y notificaciones
       </label>
-      <select
-        className={styles.NotificationsSlangFormSelect}
-        name="notifications_slang"
-        id="notifications_slang_select"
-      >
-        <option value="es">Español</option>
-        <option value="en">Ingles</option>
-      </select>
+      {!isLoading ? (
+        <select
+          className={styles.NotificationsSlangFormSelect}
+          name="notifications_slang"
+          id="notifications_slang_select"
+        >
+          <option value="es">Español</option>
+          <option value="en">Ingles</option>
+        </select>
+      ) : (
+        <Skeleton height={40} width={156} />
+      )}
     </div>
   );
 }
