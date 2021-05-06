8aa4446402ed90386a67f5f273a490f9d0867f0e
German Solano <german@famosos.com>
2021-05-06 11:53:01 -0400

    refactor(<Dropdown />): :recycle: Añadir librería de Popup para mejorar la interactividad

--- desktop-app/components/common/button/dropdown/index.tsx
+++ desktop-app/components/common/button/dropdown/index.tsx
@@ -1,55 +1,74 @@
 import classes from "classnames";
-import React, { useState } from "react";
+import {
+  DetailedReactHTMLElement,
+  cloneElement,
+  ReactNode,
+  Children as ReactChildren,
+} from "react";
+import { PopupProps } from "reactjs-popup/dist/types";
+import { AnimatedPopup } from "../../animated-popup";
 import styles from "./styles.module.scss";
 
 const addDropdownItemClass = (
-  child: React.DetailedReactHTMLElement<{ className?: string }, HTMLElement>
+  child: DetailedReactHTMLElement<{ className?: string }, HTMLElement>
 ) =>
-  React.cloneElement(child, {
-    className: classes("dropdown-item", child?.props?.className)
+  cloneElement(child, {
+    className: classes("dropdown-item", child?.props?.className),
   });
+
+type DropdownProps = {
+  id?: string;
+  buttonChildren: ReactNode;
+  children?:
+    | ReactNode
+    | ((isOpen: boolean, closePopup: () => void) => ReactNode);
+  showArrow?: boolean;
+  menuPosition?: PopupProps["position"];
+  className?: string;
+  buttonClassName?: string;
+  menuClassName?: string;
+  showClassName?: string;
+};
 
 function Dropdown({
   id = "dropdown-icon-button",
   buttonChildren,
   children,
-  className = "",
+  showArrow = false,
+  menuPosition,
   buttonClassName = "",
   menuClassName = "",
-  showClassName = ""
-}) {
-  const [isOpen, setIsOpen] = useState(false);
+}: DropdownProps) {
+  const trigger = (isOpen: boolean): JSX.Element => (
+    <button
+      className={classes(styles.DropdownButton, buttonClassName)}
+      aria-haspopup="true"
+      aria-expanded={isOpen}
+      id={id}
+      type="button"
+    >
+      {buttonChildren}
+    </button>
+  );
 
-  function toggleIsOpen() {
-    setIsOpen((isOpen) => !isOpen);
-  }
-
-  const childNodes =
-    typeof children === "function" ? children(isOpen, toggleIsOpen) : children;
+  const dropdownMenu = (closePopup: () => void, isOpen: boolean) => (
+    <div
+      aria-labelledby={id}
+      className={classes(styles.DropdownMenu, menuClassName)}
+    >
+      {ReactChildren.map(
+        typeof children === "function"
+          ? children(isOpen, closePopup)
+          : children,
+        addDropdownItemClass
+      )}
+    </div>
+  );
 
   return (
-    <div className={classes("dropdown", { show: isOpen }, className)}>
-      <button
-        className={classes(styles.DropdownButton, buttonClassName)}
-        onClick={toggleIsOpen}
-        aria-haspopup="true"
-        aria-expanded={isOpen}
-        id={id}
-        type="button"
-      >
-        {buttonChildren}
-      </button>
-      <div
-        aria-labelledby={id}
-        className={classes(
-          "dropdown-menu",
-          { [showClassName]: isOpen, show: isOpen },
-          menuClassName
-        )}
-      >
-        {React.Children.map(childNodes, addDropdownItemClass)}
-      </div>
-    </div>
+    <AnimatedPopup position={menuPosition} trigger={trigger} arrow={showArrow}>
+      {dropdownMenu}
+    </AnimatedPopup>
   );
 }
 
