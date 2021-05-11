c3dcdfa258fb923e80f3b2f5cff12055757a1d30
German Solano <german@famosos.com>
2021-05-11 09:15:30 -0400

    feat(<CardColorSelector />): :sparkles: Añadir elementos y logica

---
+++ constants/hiring-preview-configuration.ts
@@ -0,0 +1,12 @@
+export const availableCardColors = [
+  "#F8F8FE",
+  "#FFE3F0",
+  "#FFF5CA",
+  "#E3F8FF",
+  "#E3FFE6",
+] as const;
+
+export const availablePageBackgroundsUrls = [
+  null,
+  "/assets/hirings-backgrounds/first.jpg",
+] as const;
