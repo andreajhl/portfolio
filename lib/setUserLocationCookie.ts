d1588f3e49279690f6e2a0bf1defa30918569962
Isaac Peña <isaac.pr99@gmail.com>
2021-05-12 02:21:46 -0400

    feat: :sparkles: obtener en primera visita de sesion a la PW datos sobre moneda de usuario y setearlos en cookies
    se consuma IPStack para obtener codigo de la moneda del usuario y con este dato se realiza un fetch para setear el TRM actual

--- lib/setUserLocationCookie.ts
+++ lib/setUserLocationCookie.ts
@@ -1,14 +1,15 @@
-import { parse, serialize } from "cookie";
-import {
-  USER_LOCATION_KEY,
-  USER_IP_ADDRESS,
-  USER_CURRENCY_CODE
-} from "constants/keys";
+import { parse } from "cookie";
+import { USER_LOCATION_KEY, USER_IP_ADDRESS } from "constants/keys";
 import axios from "axios";
 import isBot from "isbot";
 import { IncomingMessage, ServerResponse } from "http";
 import { DocumentContext } from "next/document";
 import debug from "react-app/src/utils/debug";
+import findAvailableCurrencyByName from "react-app/src/utils/findAvailableCurrencyByName";
+import {
+  serializeCurrencyCurrentData,
+  serializeUserLocationCookies
+} from "./serializeCookies";
 
 const ONE_YEAR_IN_MILLISECONDS = 365 * 24 * 3600 * 1000;
 const invalidIpAddresses = ["127.0.0.1", "::1"];
@@ -53,7 +54,9 @@ const getUserLocationData = async (
         currency_code: ""
       };
     debug("Se va a llamar a IPStack con la IP", userIp);
-    return await getIpData(userIp);
+    const response = await getIpData(userIp);
+    debug("IPStack posee response", JSON.stringify(response));
+    return response;
   } catch (error) {
     return {
       country_code: "",
@@ -62,30 +65,43 @@ const getUserLocationData = async (
   }
 };
 
-async function setLocationCookieHeader(
+async function getLocationCookieHeader(
   req: IncomingMessage,
   res: ServerResponse
 ) {
   const userLocationValue = await getUserLocationData(req);
   const userIpAddress = getUserIp(req);
-  res.setHeader(
-    "Set-Cookie",
-    serialize(USER_LOCATION_KEY, userLocationValue.country_code, {
-      maxAge: ONE_YEAR_IN_MILLISECONDS
-    })
-  );
-  res.setHeader(
-    "Set-Cookie",
-    serialize(USER_CURRENCY_CODE, userLocationValue.currency_code, {
-      maxAge: ONE_YEAR_IN_MILLISECONDS
-    })
-  );
-  res.setHeader(
-    "Set-Cookie",
-    serialize(USER_IP_ADDRESS, userIpAddress, {
-      maxAge: ONE_YEAR_IN_MILLISECONDS
-    })
-  );
+  return {
+    ...userLocationValue,
+    userIpAddressLocation: userIpAddress
+  };
+}
+async function getCurrencyCurrentTRMCookieHeader(currency: string) {
+  let currencyCode = null;
+  if (findAvailableCurrencyByName(currency)) {
+    currencyCode = currency;
+  } else {
+    currencyCode = "USD";
+  }
+  const FINAL_PATH =
+    process.env.NEXT_PUBLIC_ENDPOINT +
+    "custom-endpoints/gateway-payment-methods/currency-exchange";
+  const response = await axios.get<{
+    data: {
+      from: string;
+      rate: number;
+      to: string;
+    };
+  }>(FINAL_PATH, {
+    params: {
+      from: "USD",
+      to: currencyCode
+    }
+  });
+  return {
+    currencyCurrentTRM: response.data?.data?.rate || "",
+    currentCurrencyTRMCode: response.data?.data?.to
+  };
 }
 
 const setUserLocationCookie = async ({
@@ -96,9 +112,29 @@ const setUserLocationCookie = async ({
   if (isBot(req.headers["user-agent"])) {
     return debug("Este es un bot solicitando", req.url);
   }
+
   const cookies = parse(req?.headers?.cookie || "");
-  if (USER_LOCATION_KEY in cookies && USER_IP_ADDRESS in cookies) return;
-  await setLocationCookieHeader(req, res);
+  let newCookiesSerializes = [];
+
+  if (!(USER_LOCATION_KEY in cookies) || !(USER_IP_ADDRESS in cookies)) {
+    const locationCookies = await getLocationCookieHeader(req, res);
+    const currencyCurrentData = await getCurrencyCurrentTRMCookieHeader(
+      locationCookies.currency_code
+    );
+    newCookiesSerializes.push(...serializeUserLocationCookies(locationCookies));
+    newCookiesSerializes.push(
+      ...serializeCurrencyCurrentData(currencyCurrentData)
+    );
+  } else {
+    const currencyCurrentData = await getCurrencyCurrentTRMCookieHeader(
+      cookies["currency_code"]
+    );
+    newCookiesSerializes.push(
+      ...serializeCurrencyCurrentData(currencyCurrentData)
+    );
+  }
+
+  res.setHeader("Set-Cookie", newCookiesSerializes);
 };
 
 export default setUserLocationCookie;
