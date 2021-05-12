0545fc681fc9e44c59de35506022d698506d2b5b
German Solano <german@famosos.com>
2021-05-12 06:11:12 -0400

    feat(<HiringShareInWhatsapp />): :sparkles: Añadir componentes

---
+++ pages/hirings/[contract_reference]/share-in-whatsapp.tsx
@@ -0,0 +1,26 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import CustomHead from "react-app/src/components/common/helpers/custom-head";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { LoaderLayout } from "react-app/src/components/layouts/loader";
+import { HiringSharePage } from "desktop-app/components/pages/hiring-share-in-whatsapp";
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params: { contract_reference },
+}: GetServerSidePropsContext) => {
+  return {
+    props: { contract_reference },
+  };
+};
+
+const HiringShareInWhatsapp = ({ contract_reference }) => {
+  return (
+    <>
+      <CustomHead />
+      <HiringSharePage contractReference={contract_reference} />
+    </>
+  );
+};
+
+export default withAuthenticationRequired(HiringShareInWhatsapp, {
+  onRedirecting: LoaderLayout,
+});
