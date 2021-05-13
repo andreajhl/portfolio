f84248e90015bf9907af7bf91ba360abd95c2184
German Solano <german@famosos.com>
2021-05-13 14:00:58 -0400

    feat(<WhatsappSharePreview />): :sparkles: Añadir imagen de poster de video

--- desktop-app/components/share-in-whatsapp/whatsapp-share-preview/index.tsx
+++ desktop-app/components/share-in-whatsapp/whatsapp-share-preview/index.tsx
@@ -15,12 +15,19 @@ function WhatsappSharePreview({
   return (
     <div className={styles.WhatsappSharePreview}>
       <div className={styles.MessageToast}>
-        ¡Hola {deliveryTo}! <br />
-        Mira el regalo que {deliveryFrom} te ha hecho a través de Famosos.com{" "}
-        <br /> <br />
-        <span className={styles.Hyperlink}>
-          https://www.famosos.com{getHiringPreviewPath(contractReference)}
-        </span>
+        <img
+          className={styles.MessageImg}
+          src="https://d3dxo4xx2lwk55.cloudfront.net/videos/609/7310/famosos-videos-personalizados-enriquearce-202011032150-1933443-7310-crf-video-poster480.jpg"
+          alt="Poster de video"
+        />
+        <div className={styles.MessageText}>
+          ¡Hola {deliveryTo}! <br />
+          Mira el regalo que {deliveryFrom} te ha hecho a través de Famosos.com{" "}
+          <br /> <br />
+          <span className={styles.Hyperlink}>
+            https://www.famosos.com{getHiringPreviewPath(contractReference)}
+          </span>
+        </div>
       </div>
     </div>
   );
