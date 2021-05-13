e774a1bd0c805d1f89aefa6ea8a3d746996cbe98
German Solano <german@famosos.com>
2021-05-13 15:07:48 -0400

    feat(<HiringShareInMailPage />): :sparkles: Añadir <MailSharePreview />

--- desktop-app/components/pages/hiring-share-in-mail/index.tsx
+++ desktop-app/components/pages/hiring-share-in-mail/index.tsx
@@ -1,6 +1,6 @@
 import PageContainer from "desktop-app/components/layouts/page-container";
 import { ShareDetailsForm } from "desktop-app/components/layouts/share-details-form";
-import { WhatsappSharePreview } from "desktop-app/components/share-in-whatsapp/whatsapp-share-preview";
+import { MailSharePreview } from "desktop-app/components/mail-share-preview";
 import styles from "./styles.module.scss";
 
 const HiringShareInMailPage = () => {
@@ -8,11 +8,25 @@ const HiringShareInMailPage = () => {
   return (
     <PageContainer>
       <div className={"container " + styles.Container}>
-        <WhatsappSharePreview
-          deliveryTo="Duvan"
-          deliveryFrom="German"
-          contractReference="202102201838-7015192-15162"
-        />
+        <MailSharePreview
+          className={styles.SharePreview}
+          to="erikadiaz.dcg@gmail.com"
+          subject="Luis te ha enviado un regalo muy especial."
+        >
+          <img
+            className={styles.Poster}
+            src="/assets/img/mail-share-img.png"
+            alt="Poster"
+          />
+          <p className={styles.BodyText}>
+            ¡Hola Ana! <br />
+            <br /> Tu mensaje va aquí.
+            <br />
+            <br /> Atte: Luis <br />
+            <br />
+            <br /> Para ver tu regalo haz clic aquí.
+          </p>
+        </MailSharePreview>
         <div className={styles.ShareDetailsFormContainer}>
           <ShareDetailsForm type="mail" />
         </div>
