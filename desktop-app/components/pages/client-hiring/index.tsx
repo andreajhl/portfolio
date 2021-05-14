e556688010487191177e7d944bbff2952817e4e6
German Solano <german@famosos.com>
2021-05-14 07:41:10 -0400

    feat(useGetContract): :sparkles: Abstraer desde <ClientHiringPage />

--- desktop-app/components/pages/client-hiring/index.tsx
+++ desktop-app/components/pages/client-hiring/index.tsx
@@ -1,52 +1,14 @@
 import PageContainer from "desktop-app/components/layouts/page-container";
-import { useEffect } from "react";
-import { getContract } from "react-app/src/state/ducks/contracts/actions";
-import { useRouter } from "next/router";
-import { CLIENT_HIRINGS } from "constants/paths";
-import { connect } from "react-redux";
 import { ClientHiringForOther } from "desktop-app/components/client-hiring/client-hiring-for-other";
 import Maybe from "desktop-app/components/common/helpers/maybe";
-
-const mapStateToProps = (state) => ({
-  isLoading: state.contracts.getContractReducer.loading,
-  contract: state.contracts.getContractReducer.data,
-  isCompleted: state.contracts.getContractReducer.completed,
-  isFailed: state.contracts.getContractReducer.failed,
-});
-
-const mapDispatchToProps = {
-  getContract,
-};
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-
-type DispatchProps = typeof mapDispatchToProps;
+import useGetContract from "../../../../lib/hooks/useGetContract";
 
 type ClientHiringPageProps = {
   contractReference: string;
-} & StateProps &
-  DispatchProps;
+};
 
-function ClientHiringPage({
-  contractReference,
-  getContract,
-  contract,
-  isFailed,
-}: ClientHiringPageProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    console.log({ contractReference });
-    if (!contractReference) return;
-    getContract(contractReference);
-  }, []);
-
-  useEffect(() => {
-    if (!isFailed) return;
-    router.push(CLIENT_HIRINGS);
-  }, [isFailed, router]);
-
-  console.log(contract);
+function ClientHiringPage({ contractReference }: ClientHiringPageProps) {
+  const { contract } = useGetContract(contractReference, true);
 
   return (
     <PageContainer showFooter={false}>
@@ -57,9 +19,4 @@ function ClientHiringPage({
   );
 }
 
-const _ClientHiringPage = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ClientHiringPage);
-
-export { _ClientHiringPage as ClientHiringPage };
+export { ClientHiringPage };
---
+++ lib/hooks/useGetContract.ts
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { getContract } from "react-app/src/state/ducks/contracts/actions";
+import { useRouter } from "next/router";
+import { CLIENT_HIRINGS } from "constants/paths";
+import { useDispatch, useSelector } from "react-redux";
+import ClientContractType from "desktop-app/types/clientContract";
+
+type StatusType = "loading" | "failed" | "completed";
+
+type StateType = {
+  contract: ClientContractType;
+  status: StatusType;
+};
+
+const contractSelector = ({ contracts: { getContractReducer } }) => {
+  let status: StatusType = "loading";
+  if (getContractReducer.failed) status = "failed";
+  if (getContractReducer.completed) status = "completed";
+
+  const state: StateType = {
+    contract: getContractReducer.data,
+    status,
+  };
+
+  return state;
+};
+
+function useGetContract(contractReference: string, redirectOnFailure = false) {
+  const { push } = useRouter();
+  const state = useSelector(contractSelector);
+  const dispatch = useDispatch();
+
+  const isSameContract = state?.contract?.reference === contractReference;
+
+  useEffect(() => {
+    if (!contractReference || isSameContract) {
+      return;
+    }
+    dispatch(getContract(contractReference));
+  }, [isSameContract, contractReference, dispatch]);
+
+  useEffect(() => {
+    if (!redirectOnFailure) return;
+    if (state.status !== "failed") return;
+    push(CLIENT_HIRINGS);
+  }, [redirectOnFailure, state.status, push]);
+
+  return state;
+}
+
+export default useGetContract;
