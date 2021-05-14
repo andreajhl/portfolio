7d9d05301305f2e8505ba5d93436815e20346c29
German Solano <german@famosos.com>
2021-05-14 16:43:46 -0400

    feat(<ClientHiringPage />): :sparkles: Añadir animación de regalo

--- desktop-app/components/pages/client-hiring/index.tsx
+++ desktop-app/components/pages/client-hiring/index.tsx
@@ -2,6 +2,7 @@ import PageContainer from "desktop-app/components/layouts/page-container";
 import { ClientHiringForOther } from "desktop-app/components/client-hiring/client-hiring-for-other";
 import Maybe from "desktop-app/components/common/helpers/maybe";
 import useGetContract from "../../../../lib/hooks/useGetContract";
+import { GiftAnimationWrapper } from "desktop-app/components/layouts/gift-animation-wrapper";
 
 type ClientHiringPageProps = {
   contractReference: string;
@@ -12,9 +13,14 @@ function ClientHiringPage({ contractReference }: ClientHiringPageProps) {
 
   return (
     <PageContainer showFooter={false}>
-      <Maybe it={contract.celebrityData && contract.contractType !== 2}>
-        <ClientHiringForOther contractData={contract} />
-      </Maybe>
+      <GiftAnimationWrapper
+        deliveryTo={contract.deliveryTo}
+        deliveryFrom={contract.deliveryFrom}
+      >
+        <Maybe it={contract.celebrityData && contract.contractType !== 2}>
+          <ClientHiringForOther contractData={contract} />
+        </Maybe>
+      </GiftAnimationWrapper>
     </PageContainer>
   );
 }
