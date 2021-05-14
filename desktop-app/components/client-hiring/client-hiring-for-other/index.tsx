d99619ed9542d822ecaee4d334c1042f456f8f86
Isaac Peña <isaac.pr99@gmail.com>
2021-05-14 04:14:26 -0400

    feat: importar componentes y ajustar layout

--- desktop-app/components/client-hiring/client-hiring-for-other/index.tsx
+++ desktop-app/components/client-hiring/client-hiring-for-other/index.tsx
@@ -4,6 +4,8 @@ import { DeliverVideoCard } from "../deliver-video-card";
 import classes from "classnames";
 import styles from "./styles.module.scss";
 import ClientContractType from "desktop-app/types/clientContract";
+import VideoActionButtons from "desktop-app/components/common/cards/video/action-buttons";
+import { ContractReviewVideoForOtherCard } from "desktop-app/components/client-hiring/contract-review-video-for-other-card";
 
 type ClientHiringForOtherProps = {
   contractData: ClientContractType;
@@ -23,13 +25,25 @@ function ClientHiringForOther({ contractData }: ClientHiringForOtherProps) {
         />
       </div>
       <div className={styles.RightSide}>
-        <ViewerClientVideo
-          avatar={contractData.celebrityData.avatar}
-          fullName={contractData.celebrityData.fullName}
-          username={contractData.celebrityData.username}
-          videoUrl={contractData.media}
-          videoPosterUrl="https://d3dxo4xx2lwk55.cloudfront.net/videos/609/14805/famosos-videos-personalizados-enriquearce-202102181551-7872249-14805-crf-video-poster480.jpg"
-        />
+        <div className={styles.VideoPlayer}>
+          <ViewerClientVideo
+            avatar={contractData.celebrityData.avatar}
+            fullName={contractData.celebrityData.fullName}
+            username={contractData.celebrityData.username}
+            videoUrl={contractData.media}
+            videoPosterUrl="https://d3dxo4xx2lwk55.cloudfront.net/videos/609/14805/famosos-videos-personalizados-enriquearce-202102181551-7872249-14805-crf-video-poster480.jpg"
+          />
+        </div>
+        <div className={styles.VideoActionsWrapper}>
+          <div>
+            <ContractReviewVideoForOtherCard contractData={contractData} />
+          </div>
+          <div>
+            <VideoActionButtons
+              videoURL={contractData.media}
+            ></VideoActionButtons>
+          </div>
+        </div>
       </div>
     </div>
   );
