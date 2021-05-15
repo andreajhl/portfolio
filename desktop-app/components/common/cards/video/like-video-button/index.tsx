d98d9a89983f08443634ad69b06b84ba0235fd8c
German Solano <german@famosos.com>
2021-05-15 07:17:30 -0400

    feat(<LikeVideoButton />): :sparkles: Añadir propiedad para cambiar color de fondo

--- desktop-app/components/common/cards/video/like-video-button/index.tsx
+++ desktop-app/components/common/cards/video/like-video-button/index.tsx
@@ -1,19 +1,31 @@
 import { LikeButton } from "desktop-app/components/common/button/like";
 import classes from "classnames";
 import styles from "./styles.module.scss";
+import { CSSProperties } from "react";
 
 type LikeVideoButtonProps = {
   onClick: () => void;
   isLiked: boolean;
+  backgroundColor?: CSSProperties["backgroundColor"];
 };
 
-function LikeVideoButton({ onClick, isLiked }: LikeVideoButtonProps) {
+function LikeVideoButton({
+  onClick,
+  isLiked,
+  backgroundColor = "white",
+}: LikeVideoButtonProps) {
   return (
     <button
       onClick={onClick}
       className={classes("btn btn-outline", styles.ActionButton)}
+      style={{ backgroundColor }}
     >
-      <LikeButton className={styles.LikeButtonModifier} />
+      <LikeButton
+        className={classes(
+          styles.LikeButtonModifier,
+          backgroundColor !== "white" && styles.LikeButtonLight
+        )}
+      />
     </button>
   );
 }
