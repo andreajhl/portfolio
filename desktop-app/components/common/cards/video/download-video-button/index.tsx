cc6cfbc7c663f937463c810479e2a437fef5b631
German Solano <german@famosos.com>
2021-05-15 07:15:23 -0400

    feat(<DownloadVideoButton />): :sparkles: Añadir propiedad para cambiar color de fondo

--- desktop-app/components/common/cards/video/download-video-button/index.tsx
+++ desktop-app/components/common/cards/video/download-video-button/index.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import { DownloadIcon } from "desktop-app/components/common/icons";
 import styles from "./styles.module.scss";
 import classes from "classnames";
+import { CSSProperties } from "react";
+
 type DownloadVideoButtonProps = {
   videoURL?: string;
+  backgroundColor?: CSSProperties["backgroundColor"];
 };
 
-function DownloadVideoButton({ videoURL }: DownloadVideoButtonProps) {
+function DownloadVideoButton({
+  backgroundColor = "white",
+  videoURL,
+}: DownloadVideoButtonProps) {
   return (
     <a href={videoURL} target="_blank" rel="noopener noreferrer" download>
-      <button className={classes("btn btn-outline", styles.ActionButton)}>
-        <DownloadIcon />
+      <button
+        className={classes("btn btn-outline", styles.ActionButton)}
+        style={{ backgroundColor }}
+      >
+        <DownloadIcon
+          className={backgroundColor !== "white" ? styles.WhiteIcon : ""}
+        />
       </button>
     </a>
   );
