4d334c1f4a9133ee82ebbd90160898513f10ba00
German Solano <german@famosos.com>
2021-05-16 09:23:50 -0400

    fix(<GiftPreviewMain />): :speech_balloon: Corregir textos predefinidos

--- desktop-app/components/layouts/gift-preview-main/index.tsx
+++ desktop-app/components/layouts/gift-preview-main/index.tsx
@@ -6,12 +6,9 @@ import { VideoContractFeed } from "../video-contract-feed";
 import ClientContractType from "desktop-app/types/clientContract";
 
 const defaultHiringConfiguration: HiringPreviewConfigurationType = {
-  cardTitle: `¡Feliz
-  cumpleaños
-  mi amor!`,
-  cardMessage: `Mi amor hermosa, te dedico esta canción con todo mi corazón.
-  Te amo infinito. Que tengas un cumpleaños hermoso. 
-  Con todo mi amor, Luis.`,
+  cardTitle: "¡Feliz cumpleaños\nmi amor!",
+  cardMessage:
+    "Mi amor hermosa, te dedico esta canción con todo mi corazón.\nTe amo infinito. Que tengas un cumpleaños hermoso.\n\n\nCon todo mi amor, Luis.",
   cardColor: "#E8E8FF",
   pageBackgroundUrl: "/assets/img/hirings-preview-backgrounds/background-1.png",
   actionButtonsBackgroundColor: "#000000",
