034fc2bc30687b66a0fcb4a4d026aa0c53da13f3
Duvan Vargas <duvan@famosos.com>
2021-05-21 18:45:29 -0500

    feat: add error in applyas celebrity form

--- pages/forms/aplicar.tsx
+++ pages/forms/aplicar.tsx
@@ -2,12 +2,13 @@ import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { CelebrityRequestPage } from "react-app/src/components/pages/celebrity-request";
 
 const CelebrityRequest = () => {
-  return (
-    <>
-      <CustomHead />
-      <CelebrityRequestPage />
-    </>
-  );
+  throw 'Esto es una prueba';
+  // return (
+  //   <>
+  //     <CustomHead />
+  //     <CelebrityRequestPage />
+  //   </>
+  // );
 };
 
 export default CelebrityRequest;
