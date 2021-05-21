94889d914a6f526cdae0be8f893e6a6e0280a029
German Solano <german@famosos.com>
2021-05-21 16:21:33 -0400

    feat(<HomePage />): :sparkles: Conectar endpoint nuevo de secciones de famosos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,14 @@
-import isMobile from "lib/utils/isMobile";
+import isMobileDevice from "lib/utils/isMobile";
 import { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import Maybe from "react-app/src/components/common/helpers/maybe";
-import { fetchCelebritySections } from "react-app/src/state/ducks/celebrity-sections/actions";
 import { wrapper } from "react-app/src/state/store";
 import getCookie from "react-app/src/utils/getCookie";
 import { useDesktopClass } from "../lib/hooks/useDesktopClass";
-import UAParser from "ua-parser-js";
-import debug from "react-app/src/utils/debug";
-import { parse, serialize } from "cookie";
-
-// import isBrowser from "react-app/src/utils/isBrowser";
-// import auth0 from "../lib/auth0";
+import { parse } from "cookie";
+import { fetchLandings } from "react-app/src/state/ducks/landings/actions";
+import { fetchCelebritySections } from "react-app/src/state/ducks/celebrity-sections/actions";
 
 const HomePage = dynamic<{ userLocation: string }>(() =>
   import("desktop-app/components/pages/home").then((mod) => mod.HomePage)
@@ -27,8 +23,11 @@ const CelebritiesPage = dynamic<{ isMobile: boolean }>(() =>
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(
   async ({ req, store, query }) => {
     const cookies = parse(req?.headers?.cookie || "");
+    const isMobile = isMobileDevice(req.headers["user-agent"]);
 
-    await fetchCelebritySections({
+    const fetchAction = isMobile ? fetchCelebritySections : fetchLandings;
+
+    await fetchAction({
       landingId: query.landingId,
       alpha2Code: cookies["userLocation"],
       limit: 10,
@@ -37,7 +36,7 @@ export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps
 
     return {
       props: {
-        isMobile: isMobile(req.headers["user-agent"]),
+        isMobile,
         userLocation: getCookie("userLocation", req?.headers?.cookie) || "",
       },
     };
