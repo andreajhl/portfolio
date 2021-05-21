c2bf48d325dbe0bc7d884e7e26a09076386823fa
Duvan Vargas <duvan@famosos.com>
2021-05-21 17:10:45 -0500

    feat: add NEXT_PUBLIC_SENTRY_DSN to env variables feat: SENTRY_DSN to env variables feat: NEXT_PUBLIC_SENTRY_ENV to env variables feat: SENTRY_ENV to env variables feat: NEXT_TRACK_SENTRY_ERRORS to env variables feat: TRACK_SENTRY_ERRORS to env variables

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,12 +2,14 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENV = process.env.SENTRY_ENV || process.env.NEXT_PUBLIC_SENTRY_ENV;
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://212f8940d5164776a49072b6b92b4a58@o472999.ingest.sentry.io/5764122',
+  environment: SENTRY_ENV || "production"
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
