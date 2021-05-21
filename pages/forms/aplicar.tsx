f16c997300a30532a833edf003e1effcc05e7f9a
Duvan Vargas <duvan@famosos.com>
2021-05-21 18:59:25 -0500

    fix: remove button demo in CelebrityRequestPage

--- pages/forms/aplicar.tsx
+++ pages/forms/aplicar.tsx
@@ -2,13 +2,12 @@ import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { CelebrityRequestPage } from "react-app/src/components/pages/celebrity-request";
 
 const CelebrityRequest = () => {
-  throw 'Esto es una prueba';
-  // return (
-  //   <>
-  //     <CustomHead />
-  //     <CelebrityRequestPage />
-  //   </>
-  // );
+  return (
+    <>
+      <CustomHead />
+      <CelebrityRequestPage />
+    </>
+  );
 };
 
 export default CelebrityRequest;
