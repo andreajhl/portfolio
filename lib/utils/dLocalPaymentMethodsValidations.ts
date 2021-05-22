5d3d5f9a41104dc0701d28e129b1625bfcc47848
Isaac Peña <isaac.pr99@gmail.com>
2021-05-22 04:17:24 -0400

    feat: agregar datos de methodos de pagos faltantes

---
+++ lib/utils/dLocalPaymentMethodsValidations.ts
@@ -0,0 +1,11 @@
+import {
+  AVAILABLE_PAYMENTS_METHODS_DLOCAL,
+  DLOCAL_PAYMENT_METHODS_WITH_CARD_REQUIRED,
+} from "./../../constants/availablePaymentsMethods";
+export const isAValidDLocalPaymentMethod = (paymentMethod) =>
+  AVAILABLE_PAYMENTS_METHODS_DLOCAL.some((method) => method === paymentMethod);
+
+export const isADLocalPaymentMethodWithCardRequired = (paymentMethod) =>
+  DLOCAL_PAYMENT_METHODS_WITH_CARD_REQUIRED.some(
+    (method) => method === paymentMethod
+  );
