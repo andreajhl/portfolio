d561eec5c90c5de6cc7b713b7c7282f747ca96c4
Isaac Peña <isaac.pr99@gmail.com>
2021-05-22 05:14:21 -0400

    feat: implementar flow de pago completo via dLocal

--- desktop-app/components/payments-methods/payments-methods-selector-card/index.tsx
+++ desktop-app/components/payments-methods/payments-methods-selector-card/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DLocalPersonalInfoForm } from "../dLocal-personal-info-form";
 import PaymentMethodsAvailableList from "../payment-methods-available-list";
 import styles from "./styles.module.scss";
@@ -32,68 +32,6 @@ type PaymentsMethodsSelectorCardProps = {
   contractReference: string;
 } & PropsFromRedux;
 
-const AVAILABLE_PAYMENTS_METHODS_DLOCAL = [
-  "CREDIT_CARD",
-  "DEBIT_CARD",
-  "BANK_TRANSFER",
-  "TICKET",
-] as const;
-const AVAILABLE_PAYMENTS_METHOD_PAYPAL = ["PAYPAL", "STRIPE"] as const;
-const AVAILABLE_PAYMENTS_METHOD_STRIPE = ["STRIPE"] as const;
-
-const ALL_AVAILABLE_PAYMENTS_METHODS = [
-  ...AVAILABLE_PAYMENTS_METHODS_DLOCAL,
-  ...AVAILABLE_PAYMENTS_METHOD_PAYPAL,
-  ...AVAILABLE_PAYMENTS_METHOD_STRIPE,
-] as const;
-
-type all_payments_methods = typeof ALL_AVAILABLE_PAYMENTS_METHODS[number];
-type PaymentMethodsAvailableListProps = {
-  paymentMethodType:
-    | "CREDIT_CARD"
-    | "DEBIT_CARD"
-    | "BANK_TRANSFER"
-    | "TICKET"
-    | "PAYPAL"
-    | "STRIPE";
-  // availablePaymentMethods?: {
-  //   id: number;
-  //   identifier: string;
-  //   name: string;
-  //   brand: string;
-  //   redirect: boolean;
-  //   logo: string;
-  // }[];
-}[];
-const mock_payments_methods: PaymentMethodsAvailableListProps = [
-  {
-    paymentMethodType: "STRIPE",
-    // availablePaymentMethods: [
-    //   {
-    //     id: 1,
-    //     identifier: "STRIPE",
-    //     name: "Stripe Cards",
-    //     brand: "",
-    //     redirect: false,
-    //     logo: "https://famosos-media.s3.amazonaws.com/Logo_stripe.jpg",
-    //   },
-    // ],
-  },
-  {
-    paymentMethodType: "PAYPAL",
-    // availablePaymentMethods: [
-    //   {
-    //     id: 54,
-    //     identifier: "PAYPAL",
-    //     name: "Paypal",
-    //     brand: "",
-    //     redirect: true,
-    //     logo: "paypal",
-    //   },
-    // ],
-  },
-];
-
 function PaymentsMethodsSelectorCard({
   contractPrice,
   contractReference,
@@ -114,20 +52,27 @@ function PaymentsMethodsSelectorCard({
   useEffect(() => {
     listPaymentGateways(currencyExchangeData.to);
   }, [currencyExchangeData.to]);
-  console.dir(paymentMethodsAvailable);
+  const DLocalPersonalInfoFormRef = useRef(null);
   const [dLocalBuyerFormData, setDLocalBuyerFormData] = useState({
     buyer_name: "",
     email_address: "",
     identification_document: "",
   });
+  const [errorMessageForDLocalForm, setErrorMessageForDLocalForm] = useState(
+    ""
+  );
 
   return (
     <div className={styles.PaymentsMethodsSelectorCard}>
+      <h2 className={styles.PaymentMethodFormTitle}>
+        1. Datos de la persona que realiza el pago.
+      </h2>
       <Maybe it={!userInformationLoading}>
-        <div className={styles.PaymentMethodFormSection}>
-          <h2 className={styles.PaymentMethodFormTitle}>
-            1. Datos de la persona que realiza el pago.
-          </h2>
+        <div
+          className={styles.PaymentMethodFormSection}
+          tabIndex={-1}
+          ref={DLocalPersonalInfoFormRef}
+        >
           <DLocalPersonalInfoForm
             initialValues={{
               buyer_name: userInformation.fullName,
@@ -136,6 +81,7 @@ function PaymentsMethodsSelectorCard({
                 userInformation.identification_document || "",
             }}
             onChangeValues={setDLocalBuyerFormData}
+            errorMessage={errorMessageForDLocalForm}
           />
         </div>
       </Maybe>
@@ -144,9 +90,14 @@ function PaymentsMethodsSelectorCard({
           2. Selecciona un Método de Pago.
         </h2>
         <PaymentMethodsAvailableList
+          onBuyerDataIncomplete={() => {
+            DLocalPersonalInfoFormRef.current.focus();
+            setErrorMessageForDLocalForm("Por favor ingrese todos los datos");
+          }}
           contractPrice={contractPrice}
           contractReference={contractReference}
           payment_methods={paymentMethodsAvailable}
+          buyerData={dLocalBuyerFormData}
         />
       </div>
     </div>
