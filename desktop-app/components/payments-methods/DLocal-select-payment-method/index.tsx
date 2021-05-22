669099e817622cd5654e2af2b8f65dde7848844d
Isaac Peña <isaac.pr99@gmail.com>
2021-05-22 05:01:58 -0400

    feat: crear componente para metodos de pago con select

---
+++ desktop-app/components/payments-methods/DLocal-select-payment-method/index.tsx
@@ -0,0 +1,116 @@
+import Maybe from "desktop-app/components/common/helpers/maybe";
+import WarningMessage from "desktop-app/components/common/warning-message";
+import React, { useRef, useState } from "react";
+import { LoaderLayout } from "react-app/src/components/layouts/loader";
+import styles from "./styles.module.scss";
+
+type DLocalSelectPaymentMethodProps = {
+  paymentMethodType: string;
+  paymentsMethodsAvailable: Array<{
+    brand: string;
+    id: number;
+    identifier: string;
+    logo: string;
+    name: string;
+    redirect: boolean;
+  }>;
+  onStartPayment: (option: { name: string; paymentMethodId: string }) => void;
+  disabled: boolean;
+  paymentInProcess: boolean;
+};
+function DLocalSelectPaymentMethod({
+  paymentsMethodsAvailable,
+  paymentMethodType,
+  disabled,
+  paymentInProcess,
+  onStartPayment,
+}: DLocalSelectPaymentMethodProps) {
+  const inputLabel = useRef<HTMLInputElement>(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [currentOption, setCurrentOption] = useState({
+    name: "",
+    paymentMethodId: "",
+  });
+  const handleChangePaymentMethod = (name, paymentMethodId) => {
+    setErrorMessage("");
+    setCurrentOption({ name: name, paymentMethodId: paymentMethodId });
+  };
+
+  const isMissingOptionSelected = () =>
+    Object.values(currentOption).includes("");
+
+  const handleStartPayment = () => {
+    if (!isMissingOptionSelected()) {
+      onStartPayment(currentOption);
+    } else {
+      setErrorMessage("Por favor seleccione un método de pago");
+    }
+  };
+  return (
+    <div className={styles.DLocalSelectPaymentMethodWraper} ref={inputLabel}>
+      {paymentsMethodsAvailable.map((paymentMethod, index) => (
+        <div>
+          <div
+            key={`paymentMethod-${paymentMethodType}-${paymentMethod.name}-${index}`}
+          >
+            <label
+              className={styles.Label}
+              htmlFor={`paymentMethod-${paymentMethodType}-${paymentMethod.name}-${index}`}
+            >
+              <span className={styles.RadioInput}>
+                <input
+                  type="radio"
+                  name={`paymentMethod-${paymentMethodType}`}
+                  value={paymentMethod.name}
+                  checked={currentOption.name === paymentMethod.name}
+                  onChange={() =>
+                    handleChangePaymentMethod(
+                      paymentMethod.name,
+                      paymentMethod.id
+                    )
+                  }
+                  id={`paymentMethod-${paymentMethodType}-${paymentMethod.name}-${index}`}
+                />
+                <span className={styles.RadioControl} />
+              </span>
+              {index < 3 ? (
+                <img
+                  alt="Card Logo"
+                  style={{
+                    position: "absolute",
+                    left: "90%",
+                  }}
+                  className="rounded-circle"
+                  height="30px"
+                  src={paymentMethod.logo}
+                />
+              ) : null}
+              <span className={styles.RadioLabel}>{paymentMethod.name}</span>
+            </label>
+          </div>
+        </div>
+      ))}
+      <Maybe it={errorMessage !== ""}>
+        <WarningMessage
+          className={styles.WarningMessage}
+          message={errorMessage}
+        />
+      </Maybe>
+      <button
+        onClick={(e) => handleStartPayment()}
+        disabled={disabled}
+        className="btn btn-primary"
+        style={{
+          backgroundColor: `${paymentInProcess ? "white" : "#FB177D"}`,
+          height: "50px",
+          borderRadius: "10px",
+          width: "100%",
+        }}
+      >
+        {paymentInProcess ? <LoaderLayout /> : "Pagar"}
+      </button>
+    </div>
+  );
+}
+
+export default DLocalSelectPaymentMethod;
