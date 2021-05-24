9cc87b923ab909e30837fe9ac9dd996298155ac1
German Solano <german@famosos.com>
2021-05-24 09:17:31 -0400

    feat(<CategoryFilter />): :alien: Actualizar endpoint a version nueva

--- react-app/src/state/ducks/celebrity-categories/paths.js
+++ react-app/src/state/ducks/celebrity-categories/paths.js
@@ -1,4 +1,5 @@
 export const MODEL = `celebrity-categories`;
 
 export const PUBLIC_LIST = `custom-endpoints/${MODEL}/public-list`;
+export const PUBLIC_LIST_V2 = `custom-endpoints/${MODEL}/v2/public-list`;
 export const PUBLIC_GET = `custom-endpoints/${MODEL}/public-get/`;
