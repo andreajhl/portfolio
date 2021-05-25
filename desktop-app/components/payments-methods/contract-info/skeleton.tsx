8d47a5a1c4c3c1d37df2a0c2279d04b250c62654
German Solano <german@famosos.com>
2021-05-25 09:58:32 -0400

    feat(<ContractDataForm />): :sparkles: Añadir skeleton

---
+++ desktop-app/components/payments-methods/contract-info/skeleton.tsx
@@ -0,0 +1,27 @@
+import { ContractDataFormSkeleton } from "../contract-data-form/skeleton";
+import { ContractInfoHeaderSkeleton } from "../contract-info-header/skeleton";
+import styles from "./styles.module.scss";
+
+function ContractInfoSkeleton() {
+  return (
+    <div className={styles.ContractInfo}>
+      <header
+        className={`${styles.ContractInfoSection} ${styles.ContractInfoHeader}`}
+      >
+        <ContractInfoHeaderSkeleton />
+      </header>
+      <div
+        className={`${styles.ContractInfoSection} ${styles.ContractInfoData}`}
+      >
+        <ContractDataFormSkeleton />
+      </div>
+      <div
+        className={`${styles.ContractInfoSection} ${styles.ContractInfoPricing}`}
+      >
+        {/* <ContractPriceSummary /> */}
+      </div>
+    </div>
+  );
+}
+
+export default ContractInfoSkeleton;
