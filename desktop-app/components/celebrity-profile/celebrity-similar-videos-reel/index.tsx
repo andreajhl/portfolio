8eb3e38990976777c697342440ac8191876a0164
German Solano <german@famosos.com>
2021-05-26 17:04:39 -0400

    feat(<CelebritySimilarVideosReel />): :sparkles: Añadir skeleton

--- desktop-app/components/celebrity-profile/celebrity-similar-videos-reel/index.tsx
+++ desktop-app/components/celebrity-profile/celebrity-similar-videos-reel/index.tsx
@@ -1,18 +1,40 @@
 import { CardsReelSection } from "desktop-app/components/layouts/cards-section-reel";
 import CelebritySectionVideoCard from "desktop-app/components/layouts/celebrity-section-video-card";
+import CelebritySectionVideoCardSkeleton from "desktop-app/components/layouts/celebrity-section-video-card/skeleton";
+import getArrayOfLength from "lib/utils/getArrayOfLength";
 import { getCelebrityFromSimilarCelebrity } from "lib/utils/getCelebrityFromSimilarCelebrity";
 import { useEffect } from "react";
 import Maybe from "react-app/src/components/common/helpers/maybe";
 import { fetchSimilarContracts } from "react-app/src/state/ducks/contracts/actions";
+import { RootState } from "react-app/src/state/store";
 import { connect } from "react-redux";
 
-const mapStateToProps = (state) => ({
-  isLoading: state.contracts.fetchSimilarContractsReducer.loading,
-  similarVideos: state.contracts.fetchSimilarContractsReducer.data.results
-});
+function renderVideoCard(similarVideo) {
+  if (!similarVideo) return <CelebritySectionVideoCardSkeleton />;
+  const celebrity = getCelebrityFromSimilarCelebrity(similarVideo);
+  return (
+    <CelebritySectionVideoCard
+      username={celebrity.username}
+      videoUrl={similarVideo?.contractMediaUrl}
+      fullName={celebrity.fullName}
+      videoPosterUrl={similarVideo?.contractPosterUrl}
+      avatar={celebrity.avatar}
+    />
+  );
+}
+
+const mapStateToProps = (state: RootState) => {
+  const isLoading = state.contracts.fetchSimilarContractsReducer.loading;
+  return {
+    isLoading,
+    similarVideos: isLoading
+      ? getArrayOfLength(10)
+      : state.contracts.fetchSimilarContractsReducer.data.results,
+  };
+};
 
 const mapDispatchToProps = {
-  getSimilarVideos: fetchSimilarContracts
+  getSimilarVideos: fetchSimilarContracts,
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
@@ -23,23 +45,10 @@ type CelebritySimilarVideosReelProps = {
 } & StateProps &
   DispatchProps;
 
-function renderVideoCard(similarVideo) {
-  const celebrity = getCelebrityFromSimilarCelebrity(similarVideo);
-  return (
-    <CelebritySectionVideoCard
-      username={celebrity.username}
-      videoUrl={similarVideo.contractMediaUrl}
-      fullName={celebrity.fullName}
-      videoPosterUrl={similarVideo.contractPosterUrl}
-      avatar={celebrity.avatar}
-    />
-  );
-}
-
 function CelebritySimilarVideosReel({
   celebrityUsername,
   similarVideos,
-  getSimilarVideos
+  getSimilarVideos,
 }: CelebritySimilarVideosReelProps) {
   useEffect(() => {
     if (!celebrityUsername) return;
@@ -57,7 +66,7 @@ function CelebritySimilarVideosReel({
         buttonsStyle={{
           size: 49,
           top: 176,
-          transform: "translateY(-50%)"
+          transform: "translateY(-50%)",
         }}
         gap={26.75}
       >
