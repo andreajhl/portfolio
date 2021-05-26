18c1c2179e7ec7c0a34835683d9364081e15d443
Isaac Peña <isaac.pr99@gmail.com>
2021-05-26 15:01:35 -0400

    feat: :poop: add new components for displaying total results and pagination

--- desktop-app/components/my-hirings/my-hirings-cards-section/index.tsx
+++ desktop-app/components/my-hirings/my-hirings-cards-section/index.tsx
@@ -7,11 +7,13 @@ import {
 } from "desktop-app/constants/contractStatuses";
 import { useEffect } from "react";
 import { listClientContracts } from "react-app/src/state/ducks/contracts/actions";
-import { connect } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
 import styles from "./styles.module.scss";
 import { MyHiringsCardSkeleton } from "desktop-app/components/my-hirings/my-hirings-card-skeleton";
 import MyHiringsContract from "desktop-app/types/myHiringsContract";
 import Maybe from "desktop-app/components/common/helpers/maybe";
+import { RootState } from "react-app/src/state/store";
+import Pagination from "desktop-app/components/common/pagination";
 
 const allowedStatuses = [PAYED_BY_CLIENT, COMPLETED, REJECTED, EXPIRED];
 
@@ -22,27 +24,29 @@ const loadingSkeletons = (
   </>
 );
 
-const mapStateToProps = ({ contracts }) => ({
+const mapStateToProps = ({ contracts }: RootState) => ({
   contracts:
     contracts.listClientContractsReducer.data?.filter?.(({ status }) =>
       allowedStatuses.includes(status)
     ) || [],
   isCompleted: contracts.listClientContractsReducer.completed,
+  contractData: contracts.listClientContractsReducer,
 });
 
 const mapDispatchToProps = {
   listClientContracts,
 };
 
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
 
-type MyHiringsCardsSectionProps = {} & StateProps & DispatchProps;
+type MyHiringsCardsSectionProps = {} & PropsFromRedux;
 
 function MyHiringsCardsSection({
   isCompleted,
   listClientContracts,
   contracts,
+  contractData,
 }: MyHiringsCardsSectionProps) {
   useEffect(() => {
     // GTM.tagManagerDataLayer("CLIENT_HIRINGS_PAGE_VIEW");
@@ -52,6 +56,11 @@ function MyHiringsCardsSection({
   return (
     <div className="container">
       <Maybe it={isCompleted} orElse={loadingSkeletons}>
+        <div className={styles.TotalResultsCounter}>
+          <p>
+            Total de Solicitudes <span>{contractData?.data?.length}</span>
+          </p>
+        </div>
         {contracts.map((contractData: MyHiringsContract) => (
           <MyHiringsCard
             key={contractData.id}
@@ -59,14 +68,17 @@ function MyHiringsCardsSection({
             contractData={contractData}
           />
         ))}
+        <Pagination
+          className={styles.PaginationButtons}
+          currentPage={1}
+          totalPage={contractData?.data?.length}
+          onChangePage={() => console.log("Go to new Page")}
+        />
       </Maybe>
     </div>
   );
 }
 
-const _MyHiringsCardsSection = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MyHiringsCardsSection);
+const _MyHiringsCardsSection = connector(MyHiringsCardsSection);
 
 export { _MyHiringsCardsSection as MyHiringsCardsSection };
