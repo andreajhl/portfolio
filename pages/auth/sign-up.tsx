0bfb21855b1041614d3da3ae84a4c2858127d46c
German Solano <german@famosos.com>
2021-05-31 19:14:58 -0400

    feat(<SignUp />): :sparkles: Añadir elementos iniciales

---
+++ pages/auth/sign-up.tsx
@@ -0,0 +1,12 @@
+import CustomHead from "react-app/src/components/common/helpers/custom-head";
+import { SignUpPage } from "react-app/src/components/pages/sign-up";
+
+function SignUp() {
+  return (
+    <>
+      <CustomHead />
+      <SignUpPage />
+    </>
+  );
+}
+export default SignUp;
