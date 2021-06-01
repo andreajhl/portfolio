72f1fa4f5eada787be929908c04ba82408cfd46c
German Solano <german@famosos.com>
2021-06-01 14:07:27 -0400

    fix(metodos-de-pago/[contract_reference]): :bug: Añadir validación de lops parametros dinamicos para evitar errores

--- pages/payments-methods/[contract_reference]/index.tsx
+++ pages/payments-methods/[contract_reference]/index.tsx
@@ -3,10 +3,17 @@ import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { PaymentMethodsPage } from "react-app/src/components/pages/payment-methods";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import LoadingPage from "react-app/src/components/layouts/loading-page";
+import { ROOT_PATH } from "react-app/src/routing/Paths";
 
-export const getServerSideProps: GetServerSideProps = async ({
-  params: { contract_reference }
-}) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const contract_reference = params?.contract_reference;
+
+  if (typeof contract_reference === "undefined") {
+    return {
+      redirect: { destination: ROOT_PATH, permanent: false }
+    };
+  }
+
   return {
     props: { contract_reference }
   };
