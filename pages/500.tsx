197b7cb29ba41b23615ea64ee6b28b43749df96b
Isaac Peña <isaac.pr99@gmail.com>
2021-06-02 12:19:59 -0400

    feat: reemplazar layout de error en page 500

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,44 +1,20 @@
 import { useRouter } from "next/router";
 import React from "react";
-import { NavLink } from "react-app/src/components/common/routing";
-import { PageContainer } from "react-app/src/components/layouts/page-container";
-import { HOME_PATH } from "react-app/src/routing/Paths";
+import ErrorReport from "react-app/src/components/layouts/error-report";
 import { FormattedMessage } from "react-intl";
 
 function ServerErrorPage() {
   const { asPath } = useRouter();
   return (
-    <PageContainer
-      applyFetchCelebrities={false}
-      showFooter={false}
-      showBotMakerFrame
-    >
-      <div className="container mx-auto d-flex flex-column align-items-center text-center p-4">
-        <h3 className="font-weight-light text-center mb-5">
-          <FormattedMessage defaultMessage="Ha ocurrido un error." />
-        </h3>
-        <img
-          width="200px"
-          style={{ opacity: "0.2" }}
-          src="/assets/img/sad-face-in-rounded-square.svg"
-          alt="sad-face"
-        />
-        <div className="mt-5">
-          <h3>
-            <FormattedMessage defaultMessage="Nos disculpamos, estamos arreglando el problema." />
-          </h3>
-          <h5 className="mt-3">
-            <FormattedMessage defaultMessage="Ruta:" /> {asPath}
-          </h5>
-        </div>
-        <br />
-        <NavLink to={HOME_PATH}>
-          <button className="btn btn-primary mt-2">
-            <FormattedMessage defaultMessage="Volver a inicio" />
-          </button>
-        </NavLink>
-      </div>
-    </PageContainer>
+    <ErrorReport
+      errorTitle="Ha ocurrido un error."
+      errorDescription={
+        <>
+          <FormattedMessage defaultMessage="Nos disculpamos, estamos arreglando el problema." />
+        </>
+      }
+      errorPath={asPath}
+    />
   );
 }
 
