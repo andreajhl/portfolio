93d4de7ce14fe18fe1db020880c7e1330de64030
Isaac Peña <isaac.pr99@gmail.com>
2021-06-02 12:19:39 -0400

    feat: reemplazar layout en page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,1 +1,20 @@
-export { FourZeroFour as default } from "react-app/src/components/pages/404";
+// export { FourZeroFour as default } from "react-app/src/components/pages/404";
+
+import { useRouter } from "next/router";
+import React from "react";
+import ErrorReport from "react-app/src/components/layouts/error-report";
+import { FormattedMessage } from "react-intl";
+
+function ServerErrorPage() {
+  const { asPath } = useRouter();
+  return (
+    <ErrorReport
+      errorTitle={
+        <FormattedMessage defaultMessage="Lo sentimos, esta página no fue encontrada" />
+      }
+      errorPath={asPath}
+    />
+  );
+}
+
+export default ServerErrorPage;
