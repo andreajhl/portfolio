ee43ade0a8432554b1173793ddfc220d36174d52
German Solano <german@famosos.com>
2021-06-03 17:01:20 -0400

    feat(<SignInFromPage />): :sparkles: Añadir elementos y estilos iniciales

---
+++ pages/auth/sign-in/from.tsx
@@ -0,0 +1,16 @@
+import CustomHead from "react-app/src/components/common/helpers/custom-head";
+import { SignInFromPage } from "react-app/src/components/pages/sign-in-from";
+
+export const getServerSideProps = async ({ query }) => {
+  return { props: { query } };
+};
+
+function SignInFrom({ query }) {
+  return (
+    <>
+      <CustomHead />
+      <SignInFromPage query={query} />
+    </>
+  );
+}
+export default SignInFrom;
