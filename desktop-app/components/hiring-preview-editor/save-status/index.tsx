1265a75db0d1aa480d449ca655cf8db540943018
German Solano <german@famosos.com>
2021-06-04 15:05:44 -0400

    feat(<SaveStatus />): :lipstick: Añadir animación de colapso antes de desaparecer

--- desktop-app/components/hiring-preview-editor/save-status/index.tsx
+++ desktop-app/components/hiring-preview-editor/save-status/index.tsx
@@ -1,4 +1,5 @@
 import classes from "classnames";
+import Collapse from "react-bootstrap/Collapse";
 import Maybe from "../../common/helpers/maybe";
 import { StatusType } from "../../common/helpers/submit-button-text";
 import styles from "./styles.module.scss";
@@ -9,22 +10,32 @@ type SaveStatusProps = {
 };
 
 function SaveStatus({ className = "", status }: SaveStatusProps) {
-  if (status === "idle") return null;
-
   return (
-    <div className={classes(styles.SaveStatusWrapper, className)}>
-      <Maybe
-        it={status === "completed"}
-        orElse={<i className={classes("fa fa-circle-notch", styles.Spinner)} />}
-      >
-        <i className={`far fa-check-circle ${styles.SavedIcon}`} />
-      </Maybe>
-      <span className={styles.SaveStatusText}>
-        <Maybe it={status === "completed"} orElse="Guardando cambios...">
-          Tus cambios se guardaron hace unos segundos.
-        </Maybe>
-      </span>
-    </div>
+    <Collapse in={status !== "idle"}>
+      <div>
+        <div
+          className={classes(
+            styles.SaveStatusWrapper,
+            status === "idle" && styles.Idle,
+            className
+          )}
+        >
+          <Maybe
+            it={status === "completed"}
+            orElse={
+              <i className={classes("fa fa-circle-notch", styles.Spinner)} />
+            }
+          >
+            <i className={`far fa-check-circle ${styles.SavedIcon}`} />
+          </Maybe>
+          <span className={styles.SaveStatusText}>
+            <Maybe it={status === "completed"} orElse="Guardando cambios...">
+              Tus cambios se guardaron hace unos segundos.
+            </Maybe>
+          </span>
+        </div>
+      </div>
+    </Collapse>
   );
 }
 
