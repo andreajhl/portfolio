3a7abc5025648e776233c8fb2d8a5fc49d9e6301
German Solano <german@famosos.com>
2021-06-05 10:20:55 -0400

    feat(<HiringShareInMail />): :sparkles: Añadir version para mobile

--- pages/my-account/hirings/[contract_reference]/share-in-mail.tsx
+++ pages/my-account/hirings/[contract_reference]/share-in-mail.tsx
@@ -2,27 +2,61 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { LoaderLayout } from "react-app/src/components/layouts/loader";
-import { HiringShareInMailPage } from "desktop-app/components/pages/hiring-share-in-mail";
 import { useDesktopClass } from "lib/hooks/useDesktopClass";
+import dynamic from "next/dynamic";
+import Maybe from "desktop-app/components/common/helpers/maybe";
+import isMobile from "lib/utils/isMobile";
+import { ROOT_PATH } from "constants/paths";
+
+const DesktopHiringShareInMailPage = dynamic<{ contractReference: string }>(
+  () =>
+    import("desktop-app/components/pages/hiring-share-in-mail").then(
+      (mod) => mod.HiringShareInMailPage
+    )
+);
+
+const MobileHiringShareInMailPage = dynamic<{ contractReference: string }>(() =>
+  import("react-app/src/components/pages/hiring-share-in-mail").then(
+    (mod) => mod.HiringShareInMailPage
+  )
+);
 
 export const getServerSideProps: GetServerSideProps = async ({
-  params: { contract_reference },
+  req,
+  params,
 }: GetServerSidePropsContext) => {
+  if (!params) {
+    return {
+      redirect: { destination: ROOT_PATH },
+      props: {},
+    };
+  }
+
   return {
-    props: { contract_reference },
+    props: {
+      contractReference: params.contract_reference,
+      isMobile: isMobile(req?.headers?.["user-agent"]),
+    },
   };
 };
 
-const HiringShareInMail = ({ contract_reference }) => {
+function HiringShareInMail({ contractReference, isMobile }) {
   useDesktopClass(true);
 
   return (
     <>
       <CustomHead />
-      <HiringShareInMailPage contractReference={contract_reference} />
+      <Maybe
+        it={isMobile}
+        orElse={
+          <DesktopHiringShareInMailPage contractReference={contractReference} />
+        }
+      >
+        <MobileHiringShareInMailPage contractReference={contractReference} />
+      </Maybe>
     </>
   );
-};
+}
 
 export default withAuthenticationRequired(HiringShareInMail, {
   onRedirecting: LoaderLayout,
