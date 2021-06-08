e74f28f682e6fa8b9b3045b23e5881a8cbc44934
German Solano <german@famosos.com>
2021-06-08 08:12:37 -0400

    refactor(<OccasionsGrid />): :recycle: Abstraer constantes para mejorar la legibilidad del codigo

--- desktop-app/components/celebrity-profile/occasions-grid/index.tsx
+++ desktop-app/components/celebrity-profile/occasions-grid/index.tsx
@@ -1,8 +1,11 @@
 import classes from "classnames";
 import occasions from "constants/occasions";
 import { OccasionType } from "desktop-app/types/contractDataType";
+import { Fragment } from "react";
 import Maybe from "../../common/helpers/maybe";
 import styles from "./styles.module.scss";
+
+const FOR_ME = 1;
 
 const occasionsOnlyToGiftContract = [
   "LOVE",
@@ -10,6 +13,32 @@ const occasionsOnlyToGiftContract = [
   "HOPE",
   "ASK_FOR_FORGIVENESS",
 ];
+
+const occasionIsAvailableForGift = (occasionKey: string) =>
+  !occasionsOnlyToGiftContract.includes(occasionKey);
+
+type OccasionTitleProps = {
+  title: string;
+};
+
+function OccasionTitle({ title }: OccasionTitleProps) {
+  return (
+    <p>
+      {title.split(" ").map((word) => (
+        <Fragment key={word}>
+          {word}
+          <br />
+        </Fragment>
+      ))}
+    </p>
+  );
+}
+
+type OccasionDataType = {
+  icon: string;
+  title: string;
+  messages: string[];
+};
 
 type OccasionsGridProps = {
   contractType: number;
@@ -27,47 +56,36 @@ function OccasionsGrid({
   return (
     <div className={classes(styles.OccasionsGrid, className)}>
       {Object.entries(occasions).map(
-        ([occasionKey, { title }]: [
-          OccasionType,
-          {
-            icon: string;
-            title: string;
-            messages: string[];
-          }
-        ]) => (
-          <Maybe
-            it={
-              contractType !== 1 ||
-              !occasionsOnlyToGiftContract.includes(occasionKey)
-            }
-            key={occasionKey}
-          >
-            <div
-              className={classes(
-                styles.OccasionsItem,
-                occasionKey === selectedOccasion && styles.OccasionsSelected
-              )}
-              onClick={() => onClickOccasion(occasionKey)}
-            >
-              <img
+        ([occasionKey, { title }]: [OccasionType, OccasionDataType]) => {
+          const canDisplayOccasion =
+            contractType !== FOR_ME || occasionIsAvailableForGift(occasionKey);
+
+          const onClickHandler = () => onClickOccasion(occasionKey);
+
+          const occasionIsSelected = occasionKey === selectedOccasion;
+
+          return (
+            <Maybe it={canDisplayOccasion} key={occasionKey}>
+              <div
                 className={classes(
-                  styles.OccasionsItemIcon,
-                  styles[`Occasions${occasionKey}`]
+                  styles.OccasionsItem,
+                  occasionIsSelected && styles.OccasionsSelected
                 )}
-                src={`/assets/img/occasions/${occasionKey}.svg`}
-                alt={title}
-              />
-              <p>
-                {title.split(" ").map((word) => (
-                  <>
-                    {word}
-                    <br />
-                  </>
-                ))}
-              </p>
-            </div>
-          </Maybe>
-        )
+                onClick={onClickHandler}
+              >
+                <img
+                  className={classes(
+                    styles.OccasionsItemIcon,
+                    styles[`Occasions${occasionKey}`]
+                  )}
+                  src={`/assets/img/occasions/${occasionKey}.svg`}
+                  alt={title}
+                />
+                <OccasionTitle title={title} />
+              </div>
+            </Maybe>
+          );
+        }
       )}
     </div>
   );
