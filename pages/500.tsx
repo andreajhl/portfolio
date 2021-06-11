0f9b75d445c55ddc8a32903ce96431113afac71a
Isaac Peña <isaac.pr99@gmail.com>
2021-06-11 14:04:32 -0400

    feat: añadir funcion para realizar reload path en page error 500

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,10 +1,35 @@
+import { NUMBER_OF_RELOAD_REALIZED } from "constants/keys";
+import { setCookie } from "lib/setCookie";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import ErrorReport from "react-app/src/components/layouts/error-report";
+import getCookie from "react-app/src/utils/getCookie";
 import { FormattedMessage } from "react-intl";
+const TEEN_SECONDS_IN_MILLISECONDS = 10000;
 
 function ServerErrorPage() {
-  const { asPath } = useRouter();
+  const { asPath, push } = useRouter();
+  useEffect(() => {
+    let IDClear;
+    const numberOfRetryRealizedInSession = Number(
+      getCookie(NUMBER_OF_RELOAD_REALIZED)
+    );
+    setCookie(
+      NUMBER_OF_RELOAD_REALIZED,
+      String(
+        numberOfRetryRealizedInSession ? numberOfRetryRealizedInSession + 1 : 1
+      ),
+      1
+    );
+    if (numberOfRetryRealizedInSession < 5) {
+      IDClear = setTimeout(() => {
+        push(asPath);
+      }, TEEN_SECONDS_IN_MILLISECONDS);
+    }
+    return () => {
+      clearTimeout(IDClear);
+    };
+  }, []);
   return (
     <ErrorReport
       errorTitle="Ha ocurrido un error."
