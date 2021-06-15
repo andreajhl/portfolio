50554763e77781304aca2f397fefbb1464a6aa47
German Solano <german@famosos.com>
2021-06-15 11:05:23 -0400

    feat(useUploadFile): :sparkles: Añadir metodo para obtener progreso de la subida

--- desktop-app/components/common/modals/file-uploader-modal/useUploadFile.ts
+++ desktop-app/components/common/modals/file-uploader-modal/useUploadFile.ts
@@ -1,17 +1,46 @@
 import { useState } from "react";
 import axios from "axios";
 
-const uploadFormData = (uploadUrl: any, fileToUpload: Blob) =>
-  axios.put(uploadUrl, fileToUpload);
+type OnUploadProgressType = (
+  progress: number,
+  progressEvent?: ProgressEvent
+) => void;
+
+function getProgress(progressEvent: ProgressEvent) {
+  const { loaded, total } = progressEvent;
+  return Math.floor((loaded / total) * 100);
+}
+
+const uploadFormData = (
+  uploadUrl: any,
+  fileToUpload: Blob,
+  onUploadProgress: OnUploadProgressType
+) =>
+  axios.put(
+    uploadUrl,
+    fileToUpload,
+    onUploadProgress
+      ? {
+          onUploadProgress(progressEvent) {
+            const progress = getProgress(progressEvent);
+            onUploadProgress(progress, progressEvent);
+          },
+        }
+      : undefined
+  );
 
 function useUploadFile() {
   const [status, setStatus] = useState<
     "idle" | "uploading" | "rejected" | "completed"
   >("idle");
 
-  async function uploadFileToBucket(uploadUrl: string, fileToUpload: Blob) {
+  async function uploadFileToBucket(
+    uploadUrl: string,
+    fileToUpload: Blob,
+    onUploadProgress: OnUploadProgressType
+  ) {
     setStatus("uploading");
-    await uploadFormData(uploadUrl, fileToUpload);
+    await uploadFormData(uploadUrl, fileToUpload, onUploadProgress);
     setStatus("completed");
   }
 
@@ -20,9 +49,13 @@ function useUploadFile() {
     throw error;
   }
 
-  async function upload(uploadUrl: string, fileToUpload: Blob) {
+  async function upload(
+    uploadUrl: string,
+    fileToUpload: Blob,
+    onUploadProgress?: OnUploadProgressType
+  ) {
     try {
-      await uploadFileToBucket(uploadUrl, fileToUpload);
+      await uploadFileToBucket(uploadUrl, fileToUpload, onUploadProgress);
     } catch (error) {
       setErrorStatus(error);
     }
