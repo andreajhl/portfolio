18ae8b2e45c53fc9fd83afb006dcd41759261e41
German Solano <german@famosos.com>
2021-06-15 08:51:56 -0400

    feat(<HiringShareSuccessModal />): :sparkles: Añadir elementos y estilos

---
+++ desktop-app/components/common/modals/hiring-share-success-modal/index.tsx
@@ -0,0 +1,47 @@
+import { CLIENT_HIRINGS, getClientHiringPreviewPath } from "constants/paths";
+import { AnimatedPopup } from "../../animated-popup";
+import { CheckedCircleIcon } from "../../icons";
+import { Link } from "../../routing/link";
+import styles from "./styles.module.scss";
+
+type HiringShareSuccessModalProps = {
+  isOpen?: boolean;
+  contractReference: string;
+};
+
+function HiringShareSuccessModal({
+  isOpen = false,
+  contractReference,
+}: HiringShareSuccessModalProps) {
+  return (
+    <AnimatedPopup
+      open={isOpen}
+      modal
+      lockScroll
+      closeOnDocumentClick={false}
+      closeOnEscape={false}
+    >
+      <div className={styles.HiringShareSuccessModal}>
+        <div className={styles.SuccessMessage}>
+          <CheckedCircleIcon />
+          <span>
+            La entrega de tu video <br /> se programó correctamente
+          </span>
+          <Link href={CLIENT_HIRINGS}>
+            <button type="button" className="btn btn-primary">
+              Ir a Mis Solicitudes
+            </button>
+          </Link>
+          <Link
+            href={getClientHiringPreviewPath(contractReference)}
+            className={styles.GoToContractDetailsLink}
+          >
+            Ver contrato
+          </Link>
+        </div>
+      </div>
+    </AnimatedPopup>
+  );
+}
+
+export { HiringShareSuccessModal };
