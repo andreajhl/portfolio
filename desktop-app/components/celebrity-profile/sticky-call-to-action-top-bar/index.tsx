54af20258db0240bd6015dc2ba9bc8de1270f1aa
German Solano <german@famosos.com>
2021-06-20 13:44:35 -0400

    feat(<StickyCallToActionTopBar />): :sparkles: Animación de desbordamiento en nombre de famoso

--- desktop-app/components/celebrity-profile/sticky-call-to-action-top-bar/index.tsx
+++ desktop-app/components/celebrity-profile/sticky-call-to-action-top-bar/index.tsx
@@ -3,6 +3,11 @@ import { CelebrityInfo } from "../celebrity-info";
 import { StickyTopBar } from "../../common/sticky-top-bar";
 import styles from "./styles.module.scss";
 import { CelebrityVideoContractPrice } from "desktop-app/components/common/helpers/celebrity-video-contract-price";
+import {
+  TextWithOverflow,
+  parentElementClass,
+} from "desktop-app/components/common/text-with-overflow";
+import classes from "classnames";
 
 type StickyCallToActionTopBarProps = {
   celebrity: celebrityType;
@@ -23,8 +28,11 @@ function StickyCallToActionTopBar({
           alt={`Foto de ${celebrity.fullName}`}
           className={styles.StickyCTAAvatar}
         />
-        <h2 className={"text-with-ellipsis " + styles.StickyCTAFullName}>
-          {celebrity.fullName}
+        <h2 className={classes(styles.StickyCTAFullName, parentElementClass)}>
+          <TextWithOverflow
+            textClassName={styles.CelebrityFullName}
+            text={celebrity.fullName}
+          />
         </h2>
         <CelebrityInfo celebrity={celebrity} className={styles.StickyCTAInfo} />
         <span className={styles.StickyCTAPrice}>
