90d337d8a6aa3d8c08aa6c7153974f0ebd4b136e
Isaac Peña <isaac.pr99@gmail.com>
2021-06-22 15:02:55 -0400

    feat: agregar version de escritorio

--- pages/purchase-summary/[contract_reference].tsx
+++ pages/purchase-summary/[contract_reference].tsx
@@ -1,11 +1,29 @@
 import LoadingPage from "react-app/src/components/layouts/loading-page";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import CustomHead from "react-app/src/components/common/helpers/custom-head";
-import { ContractWithPayments } from "react-app/src/components/pages/contract-with-payments";
+
 import { GetServerSideProps } from "next";
 import { ROOT_PATH } from "react-app/src/routing/Paths";
+import isMobile from "lib/utils/isMobile";
+import dynamic from "next/dynamic";
+import Maybe from "desktop-app/components/common/helpers/maybe";
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+const DesktopContractCreatedPage = dynamic<{ contractReference: any }>(() =>
+  import("desktop-app/components/pages/contract-created").then(
+    (mod) => mod.ContractCreated
+  )
+);
+
+const ContractWithPayments = dynamic<{ contractReference: any }>(() =>
+  import("react-app/src/components/pages/contract-with-payments").then(
+    (mod) => mod.ContractWithPayments
+  )
+);
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  req,
+}) => {
   const contractReference = params?.contract_reference;
 
   if (typeof contractReference === "undefined") {
@@ -16,15 +34,23 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   return {
     props: {
       contractReference,
+      isMobile: isMobile(req.headers["user-agent"]),
     },
   };
 };
 
-function ContractWithPaymentsPage({ contractReference }) {
+function ContractWithPaymentsPage({ contractReference, isMobile }) {
   return (
     <>
       <CustomHead />
-      <ContractWithPayments contractReference={contractReference} />
+      <Maybe
+        it={isMobile}
+        orElse={
+          <DesktopContractCreatedPage contractReference={contractReference} />
+        }
+      >
+        <ContractWithPayments contractReference={contractReference} />
+      </Maybe>
     </>
   );
 }
