3444a175f84dabe842f94d7adff5a21a860ce5d3
German Solano <german@famosos.com>
2021-06-23 10:32:02 -0400

    feat(<ContractDetailsForm />): :globe_with_meridians: Corregir occasion para obtener los mensajes internacionalizados

--- desktop-app/components/celebrity-profile/contract-details-form/index.tsx
+++ desktop-app/components/celebrity-profile/contract-details-form/index.tsx
@@ -1,25 +1,23 @@
-import occasions from "constants/occasions";
+import { getOccasionMessage, OccasionType } from "constants/occasions";
 import useForm, { ValidationsType } from "lib/hooks/useForm";
 import { useEffect, useState } from "react";
 import SubmitButton from "../../common/button/submit-button";
 import styles from "./styles.module.scss";
 import WarningMessage from "desktop-app/components/common/warning-message";
 import classes from "classnames";
-import {
-  ContractDetailsType,
-  OccasionType
-} from "desktop-app/types/contractDataType";
+import { ContractDetailsType } from "desktop-app/types/contractDataType";
 import { WizardTopNavigation } from "desktop-app/components/common/wizard-top-navigation";
 import {
   getTextContent,
-  TextInputWithPlaceholders
+  TextInputWithPlaceholders,
 } from "desktop-app/components/common/form/text-input-with-placeholders";
 import { OccasionsGrid } from "desktop-app/components/celebrity-profile/occasions-grid";
 import objectHasProperties from "lib/utils/objectHasProperties";
+import { useIntl } from "react-intl";
 
 const initialValues: ContractDetailsType = {
   occasion: "OTHER",
-  instructions: ""
+  instructions: "",
 };
 
 const validations: ValidationsType<ContractDetailsType> = {
@@ -32,7 +30,7 @@ const validations: ValidationsType<ContractDetailsType> = {
     if (value.length > 300) {
       return "Debes introducir un máximo de 300 caracteres.";
     }
-  }
+  },
 };
 
 type ContractDetailsFormProps = {
@@ -50,7 +48,7 @@ function ContractDetailsForm({
   celebrityFullName,
   initialValues: initialValuesFromProps,
   onStepChange,
-  onSubmit
+  onSubmit,
 }: ContractDetailsFormProps) {
   const {
     values,
@@ -61,15 +59,18 @@ function ContractDetailsForm({
     getTouchedFieldValues,
     validateFields,
     setFieldError,
-    validateBeforeSubmit
-  } = useForm<ContractDetailsType>({
+    submitForm,
+  } = useForm({
     initialValues: Object.assign(initialValues, initialValuesFromProps),
     validations,
-    onSubmit
+    onSubmit,
   });
+  const { locale } = useIntl();
   const [textareaText, setTextareaText] = useState(
     initialValuesFromProps?.instructions ||
-      replacePlaceHolder(occasions[values.occasion].messages[contractType - 1])
+      replacePlaceHolder(
+        getOccasionMessage(locale, values.occasion, contractType)
+      )
   );
 
   useEffect(() => {
@@ -89,30 +90,47 @@ function ContractDetailsForm({
     setFieldValue("occasion", occasionKey);
     if (touched.instructions) return;
     const text = replacePlaceHolder(
-      occasions[occasionKey].messages[contractType - 1]
+      getOccasionMessage(locale, occasionKey, contractType)
     );
     setTextareaText(text);
     setFieldValue("instructions", text, false);
+  }
+
+  function validateFormBeforeChangeStep(
+    goToClickedStep: () => void,
+    isPreviousStep: boolean
+  ): void {
+    if (!isPreviousStep && !validateFields()) return;
+    const valuesToSave = isPreviousStep ? getTouchedFieldValues() : values;
+    if (!valuesToSave) return;
+
+    onStepChange(
+      (objectHasProperties(valuesToSave)
+        ? valuesToSave
+        : null) as ContractDetailsType
+    );
+    goToClickedStep();
+  }
+
+  function changeInstructionsTouched({ key }) {
+    if (key.startsWith("Arrow")) return;
+    setFieldError("instructions", null);
+    setFieldTouched("instructions", true);
+  }
+
+  function changeInstructionsValue({ target }) {
+    // TODO: Luego de darle submit, se coloca touched,
+    // por ende en el segundo intento no valida
+    // que se haya removido el placeholder.
+    if (!touched?.instructions) return;
+    setFieldValue("instructions", getTextContent(target));
   }
 
   return (
     <section className={styles.VideoDetailsForm}>
       <WizardTopNavigation
         enableNavigation
-        onStepClick={(goToClickedStep, isPreviousStep) => {
-          if (!isPreviousStep && !validateFields()) return;
-          const valuesToSave = isPreviousStep
-            ? getTouchedFieldValues()
-            : values;
-          if (!valuesToSave) return;
-
-          onStepChange(
-            (objectHasProperties(valuesToSave)
-              ? valuesToSave
-              : null) as ContractDetailsType
-          );
-          goToClickedStep();
-        }}
+        onStepClick={validateFormBeforeChangeStep}
       />
       <h2 className={styles.VideoDetailsFormTitle}>Selecciona una ocasión</h2>
       <OccasionsGrid
@@ -131,18 +149,8 @@ function ContractDetailsForm({
           placeholder={`¡Hola ${celebrityFullName}! Me gustaría que...`}
           className={styles.VideoDetailsFormInstructionsTextarea}
           maxLength={300}
-          onKeyUp={({ key }) => {
-            if (key.startsWith("Arrow")) return;
-            setFieldError("instructions", null);
-            setFieldTouched("instructions", true);
-          }}
-          onBlur={({ target }) => {
-            // TODO: Luego de darle submit, se coloca touched,
-            // por ende en el segundo intento no valida
-            // que se haya removido el placeholder.
-            if (!touched?.instructions) return;
-            setFieldValue("instructions", getTextContent(target));
-          }}
+          onKeyUp={changeInstructionsTouched}
+          onBlur={changeInstructionsValue}
           value={textareaText}
         />
         <WarningMessage
@@ -154,7 +162,7 @@ function ContractDetailsForm({
           message={errors?.instructions || null}
         />
       </div>
-      <SubmitButton onClick={validateBeforeSubmit}>Siguiente</SubmitButton>
+      <SubmitButton onClick={submitForm}>Siguiente</SubmitButton>
     </section>
   );
 }
