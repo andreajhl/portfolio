9dc200e1d1109b8827e22edaf2acb500bccf7cb0
German Solano <german@famosos.com>
2021-06-24 16:39:46 -0400

    feat(<MyHiringsCardContractInfo />): :sparkles: Añadir validaciones para generar errores en caso de introducir datos invalidos

--- desktop-app/components/my-hirings/my-hirings-card-contract-info/index.tsx
+++ desktop-app/components/my-hirings/my-hirings-card-contract-info/index.tsx
@@ -1,10 +1,11 @@
 import MyHiringsContract from "desktop-app/types/myHiringsContract";
 import useForm from "lib/hooks/useForm";
 import { useUpdateHiredContract } from "lib/hooks/useUpdateHiredContract";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import pickPropertiesFromAObject from "react-app/src/utils/pickPropertiesFromAObject";
 import { MyHiringsCardDetails } from "../my-hirings-card-details";
 import { MyHiringsCardNotificationInfo } from "../my-hirings-card-notification-info";
+import isEmail from "validator/lib/isEmail";
 
 const initialValues = {
   deliveryTo: "",
@@ -12,7 +13,32 @@ const initialValues = {
   instructions: "",
   deliveryContact: "",
   deliveryContactCellphone: "",
-  contractType: 0,
+  contractType: 1,
+};
+
+const validations = {
+  deliveryTo(value: string) {
+    if (value.length === 0) return "Debes introducir un nombre";
+    if (value.length > 40) {
+      return "Debes introducir un máximo de 40 caracteres.";
+    }
+  },
+  deliveryFrom(value: string, { values: { contractType } }) {
+    if (contractType !== 2) return;
+    if (value.length === 0) return "Debes introducir un nombre";
+    if (value.length > 40) {
+      return "Debes introducir un máximo de 40 caracteres.";
+    }
+  },
+  instructions(value: string) {
+    if (value.length === 0) return "Debes escribir tus instrucciones.";
+    if (value.length > 300) {
+      return "Debes introducir un máximo de 300 caracteres.";
+    }
+  },
+  deliveryContact(value: string) {
+    if (!isEmail(value)) return "Ingresa un correo electrónico válido.";
+  },
 };
 
 const getInitialValuesFromContract = (contractData: MyHiringsContract) => ({
@@ -33,11 +59,13 @@ function MyHiringsCardContractInfo({
   const [isEditing, setIsEditing] = useState(false);
   const {
     values,
+    errors,
     onChangeField,
     setFieldValue,
     submitForm,
   } = useForm<InitialValuesType>({
     initialValues: getInitialValuesFromContract(contractData),
+    validations,
     onSubmit(newContractData) {
       setIsEditing(false);
       update(contractData.reference, newContractData);
@@ -51,6 +79,7 @@ function MyHiringsCardContractInfo({
         isEditing={isEditing}
         setIsEditing={setIsEditing}
         values={values}
+        errors={errors}
         onChangeField={onChangeField}
         onSave={submitForm}
       />
@@ -58,6 +87,7 @@ function MyHiringsCardContractInfo({
         contractData={contractData}
         isEditing={isEditing}
         values={values}
+        errors={errors}
         onChangeField={onChangeField}
         setFieldValue={setFieldValue}
       />
