42e0429e892170edd76c4825ae4ed2a1cd0819b2
Isaac Peña <isaac.pr99@gmail.com>
2021-06-24 03:50:05 -0400

    feat: importar componente de feedback de status

--- desktop-app/components/user-profile/update-user-gender/index.tsx
+++ desktop-app/components/user-profile/update-user-gender/index.tsx
@@ -1,11 +1,17 @@
+import { CheckIcon } from "desktop-app/components/common/check-icon";
 import Checkbox from "desktop-app/components/common/form/checkbox";
+import { LoadingSpinner } from "desktop-app/components/common/loading-spinner";
+import { SubmitStatus } from "desktop-app/components/common/submit-status";
+import useStatus from "lib/hooks/useStatus";
 import React, { useState } from "react";
 import styles from "./styles.module.scss";
+
 type genders = "male" | "female";
 type UpdateUserGenderProps = {
   gender: genders;
 };
 function UpdateUserGender({ gender }: UpdateUserGenderProps) {
+  const [status, setStatus] = useStatus("idle");
   const [currentgenderSelect, setCurrentgenderSelect] = useState(gender);
   const handlegenderChange = (gender: genders) => {
     setCurrentgenderSelect(gender);
@@ -32,6 +38,9 @@ function UpdateUserGender({ gender }: UpdateUserGenderProps) {
         checkboxLayout="circle"
         className={styles.Checkbox}
       ></Checkbox>
+      <div className={styles.SubmitIcon}>
+        <SubmitStatus status={status} />
+      </div>
     </div>
   );
 }
