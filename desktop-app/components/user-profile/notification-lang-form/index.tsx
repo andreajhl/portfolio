cabb5f72ffeac8a34c3b122ed998b6e5a5704530
Isaac Peña <isaac.pr99@gmail.com>
2021-06-24 09:48:46 -0400

    feat: conectar endpoint para actualizar idioma de notificaciones de usuario

--- desktop-app/components/user-profile/notification-lang-form/index.tsx
+++ desktop-app/components/user-profile/notification-lang-form/index.tsx
@@ -1,4 +1,9 @@
+import { AVAILABLE_LANGS } from "constants/langs";
+import { SubmitStatus } from "desktop-app/components/common/submit-status";
+import useStatus from "lib/hooks/useStatus";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { updateNotificationsLang } from "react-app/src/state/ducks/session/actions";
 import Skeleton from "react-loading-skeleton";
 import { connect } from "react-redux";
 import styles from "./styles.module.scss";
@@ -13,27 +18,43 @@ type DispatchProps = typeof mapDispatchToProps;
 type NotificationLangFormProps = StateProps & DispatchProps;
 // TODO: conectar con endpoint creado en branch master
 function NotificationLangForm(props: NotificationLangFormProps) {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), 5000);
-  }, []);
+  const [status, setStatus] = useStatus();
+  const { locale, push, pathname, query, asPath } = useRouter();
+  const updateLang = async (lang) => {
+    setStatus("loading");
+    updateNotificationsLang({ lang: lang })
+      .then((result) => {
+        setStatus("completed");
+        push({ pathname, query }, asPath, { locale: lang });
+      })
+      .catch((err) => {
+        setStatus("rejected");
+      });
+  };
   return (
     <div className={styles.NotificationSlangFormContainer}>
       <label htmlFor="notifications_slang_select">
         Idioma de la página <br></br> y notificaciones
       </label>
-      {!isLoading ? (
-        <select
-          className={styles.NotificationsSlangFormSelect}
-          name="notifications_slang"
-          id="notifications_slang_select"
-        >
-          <option value="es">Español</option>
-          <option value="en">Inglés</option>
-        </select>
-      ) : (
-        <Skeleton height={40} width={156} />
-      )}
+      <select
+        onChange={(event) => updateLang(event.target.value)}
+        className={styles.NotificationsSlangFormSelect}
+        name="notifications_slang"
+        id="notifications_slang_select"
+      >
+        {AVAILABLE_LANGS[locale]?.map((option) => (
+          <option
+            value={option.lang}
+            selected={
+              String(locale).toLocaleLowerCase() ===
+              String(option.lang).toLocaleLowerCase()
+            }
+          >
+            {option.name}
+          </option>
+        ))}
+      </select>
+      <SubmitStatus status={"loading"} />
     </div>
   );
 }
