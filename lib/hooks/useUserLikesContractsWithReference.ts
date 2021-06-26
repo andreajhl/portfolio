c1b8376432b186d52444ba74fdbd784df3ead105
Isaac Peña <isaac@famosos.com>
2021-06-26 02:30:19 -0400

    feat: :sparkles: crear hook para consultar contratos con like por el usuario en el store

---
+++ lib/hooks/useUserLikesContractsWithReference.ts
@@ -0,0 +1,25 @@
+import { toggleContractLikeFromList } from "react-app/src/state/ducks/account/actions";
+import { RootState } from "react-app/src/state/store";
+import { useDispatch, useSelector } from "react-redux";
+
+const userCelebrityLikesContractsWithReferenceSelector = ({
+  account,
+}: RootState) => account.fetchUserFavoritesContractsWithReferenceReducer.data;
+
+function useUserLikesContractsWithReference() {
+  const userContractsLikes = useSelector(
+    userCelebrityLikesContractsWithReferenceSelector
+  );
+  const dispatch = useDispatch();
+
+  function dispatchToggleContractLikeFromList(contract_reference: string) {
+    dispatch(toggleContractLikeFromList(contract_reference));
+  }
+
+  return [
+    userContractsLikes as string[],
+    dispatchToggleContractLikeFromList,
+  ] as const;
+}
+
+export default useUserLikesContractsWithReference;
