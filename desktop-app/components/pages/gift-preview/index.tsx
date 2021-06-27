08039c82099e2309341d28954f0cde77db0ade6c
German Solano <german@famosos.com>
2021-06-27 18:28:52 -0400

    feat(<GiftPreviewPage />): :sparkles: Añadir banner con copy de que se encuentra en una previsualización cuando se está en modo de previsualización

--- desktop-app/components/pages/gift-preview/index.tsx
+++ desktop-app/components/pages/gift-preview/index.tsx
@@ -9,6 +9,7 @@ import getDefaultHiringConfiguration from "constants/getDefaultHiringConfigurati
 import getObjectWithFallbackValues from "lib/utils/getObjectWithFallbackValues";
 import ClientContractType from "desktop-app/types/clientContract";
 import Maybe from "desktop-app/components/common/helpers/maybe";
+import { FormattedMessage } from "react-intl";
 
 const opcionalPreviewConfigurationProperties = [
   "pageBackgroundUrl",
@@ -51,20 +52,31 @@ function GiftPreviewPage({
 
   return (
     <PageContainer showFooter={false}>
-      <GiftAnimationWrapper
-        disableAnimation={previewMode}
-        deliveryTo={contract.deliveryTo}
-        deliveryFrom={contract.deliveryFrom}
-      >
-        <Maybe it={contractIsCompleted}>
-          <GiftPreviewMain
-            className={styles.HiringPreviewPageMain}
-            contract={contract}
-            hiringConfiguration={previewConfiguration}
-            previewMode={previewMode}
-          />
-        </Maybe>
-      </GiftAnimationWrapper>
+      <div className={styles.PageWrapper}>
+        <GiftAnimationWrapper
+          disableAnimation={previewMode}
+          deliveryTo={contract.deliveryTo}
+          deliveryFrom={contract.deliveryFrom}
+        >
+          <Maybe it={previewMode}>
+            <div className={` ${styles.PreviewModeOverlay}`}>
+              <div className={styles.PreviewModeOverlayBanner}>
+                <p>
+                  <FormattedMessage defaultMessage="Esta es una previsualización donde las funcionalidades están deshabilitadas" />
+                </p>
+              </div>
+            </div>
+          </Maybe>
+          <Maybe it={contractIsCompleted}>
+            <GiftPreviewMain
+              className={styles.HiringPreviewPageMain}
+              contract={contract}
+              hiringConfiguration={previewConfiguration}
+              previewMode={previewMode}
+            />
+          </Maybe>
+        </GiftAnimationWrapper>
+      </div>
     </PageContainer>
   );
 }
