18cec1862d4d6827ff16fc49eb1e95de34b0a9d0
Isaac Peña <isaac@famosos.com>
2021-06-29 17:41:14 -0400

    feat: add component to filter contracts by statuses

--- desktop-app/components/pages/my-hirings/index.tsx
+++ desktop-app/components/pages/my-hirings/index.tsx
@@ -1,16 +1,32 @@
 import PageContainer from "desktop-app/components/layouts/page-container";
 import { PageHeading } from "desktop-app/components/layouts/page-heading";
 import { MyHiringsCardsSection } from "desktop-app/components/my-hirings/my-hirings-cards-section";
+import CategoryFilterDropdown from "desktop-app/components/search/category-filter-dropdown";
 import { OrderByDropdown } from "desktop-app/components/search/order-by-dropdown";
 import scrollToTop from "lib/utils/scrollToTop";
 import { useRouter } from "next/router";
 import styles from "./styles.module.scss";
-
+import {
+  COMPLETED,
+  EXPIRED,
+  PAYED_BY_CLIENT,
+  REJECTED,
+  PENDING_TO_PAY,
+} from "desktop-app/constants/contractStatuses";
 type MyHiringsProps = { query: { [key: string]: any } };
 
 const orderByOptions = [
   { label: "Más recientes", value: "created_at desc" },
   { label: "Más antiguos", value: "created_at asc" },
+];
+const filterByOptions = [
+  {
+    label: "Todos",
+    value: `${COMPLETED},${EXPIRED},${PAYED_BY_CLIENT},${REJECTED}`,
+  },
+  { label: "Pendientes", value: `${PENDING_TO_PAY}` },
+  { label: "Expirados", value: `${EXPIRED}` },
+  { label: "Rechazados", value: `${REJECTED}` },
 ];
 
 const getSelectedOrderByOption = (orderByValue) =>
@@ -37,11 +53,19 @@ function MyHirings({ query }: MyHiringsProps) {
   function updateOrderBy({ value }) {
     changeQueryParams({ orderBy: value });
   }
+  function updateFilerByStatus(value) {
+    changeQueryParams({ status: value });
+  }
 
   return (
     <PageContainer>
       <PageHeading>
         <span className="font-weight-bold mr-auto">Mis solicitudes</span>
+        <CategoryFilterDropdown
+          options={filterByOptions}
+          title={"Filtrar contratos"}
+          onChange={updateFilerByStatus}
+        />
         <OrderByDropdown
           className={styles.OrderByDropdown}
           options={orderByOptions}
