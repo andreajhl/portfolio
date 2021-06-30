d0acc1eceb423b9ba6624c612cc13474e63dcebe
German Solano <german@famosos.com>
2021-06-30 07:09:43 -0400

    refactor(<LogoutButton />): :recycle: Update logout logic to sync it with the Famosos Auth logout logic

--- react-app/src/components/containers/logout-button/logout-button.js
+++ react-app/src/components/containers/logout-button/logout-button.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { useAuth } from "lib/famosos-auth";
 import * as GTM from "../../../state/utils/gtm";
-import { Session } from "../../../state/utils/session";
-const LogoutButton = (props) => {
-  const session = new Session();
-  const { children, className, redirectTo } = props;
-  const handlerLogoutSession = () => {
+import { ROOT_PATH } from "react-app/src/routing/Paths";
+
+function LogoutButton({ children, className }) {
+  const { logout } = useAuth();
+
+  function handlerLogoutSession() {
     GTM.tagManagerDataLayer("CLICK_LOGOUT");
-    session.removeSession();
-  };
+    logout({
+      returnTo: ROOT_PATH
+    });
+  }
+
   return (
-    <div
-      className={`${className ? className : ""} `}
-      onClick={() => {
-        handlerLogoutSession();
-      }}
-    >
+    <div className={className} onClick={handlerLogoutSession}>
       {children}
     </div>
   );
-};
+}
 
 export default LogoutButton;
