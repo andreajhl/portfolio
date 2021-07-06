f48b82b66f743051aa303beb5ed217ecbb1b2dfa
Isaac Peña <isaac@famosos.com>
2021-07-06 18:38:12 -0400

    feat: :poop: import react-facebook-login to handle user authentication

--- react-app/src/components/layouts/facebook-button/index.tsx
+++ react-app/src/components/layouts/facebook-button/index.tsx
@@ -2,7 +2,10 @@ import { ReactNode } from "react";
 import classes from "classnames";
 import styles from "./styles.module.scss";
 import { useIntl } from "react-intl";
+import FacebookLogin from "react-facebook-login";
 
+const clientId = process.env.NEXT_PUBLIC_FACEBOOK_LOGIN_IDENTIFIER;
+const redirectURL = process.env.NEXT_PUBLIC_FACEBOOK_LOGIN_REDIRECT;
 type FacebookButtonProps = {
   children?: ReactNode;
   className?: string;
@@ -10,18 +13,31 @@ type FacebookButtonProps = {
 
 function FacebookButton({ children, className }: FacebookButtonProps) {
   const { locale } = useIntl();
-
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
   const redirectToFacebookOAuth = () => {
     const tokenRequestURL = "https://www.facebook.com/v10.0/dialog/oauth";
     const responseType = "code";
-    const clientId = process.env.NEXT_PUBLIC_FACEBOOK_LOGIN_IDENTIFIER;
-    const redirectURL = process.env.NEXT_PUBLIC_FACEBOOK_LOGIN_REDIRECT;
     const scope = "public_profile,email";
     const state = "famosos";
     window.location.replace(
       `${tokenRequestURL}?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scope}&state=${state}&locale=${locale}`
     );
   };
+  return (
+    <FacebookLogin
+      appId={clientId}
+      responseType="code"
+      cssClass={classes("btn", styles.FacebookButton, className)}
+      fields="name,email,picture"
+      scope="public_profile,email"
+      // onClick={componentClicked}
+      icon={<img src="/assets/img/facebook-f.svg" alt="Logo de Facebook" />}
+      callback={responseFacebook}
+      redirectUri={redirectURL}
+    />
+  );
   return (
     <button
       type="button"
