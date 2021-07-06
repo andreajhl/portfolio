6474a6bc0c0e38531337d2355b0cbd814cfc775f
German Solano <german@famosos.com>
2021-07-06 09:54:49 -0400

    refactor(<PageContainer />): :recycle: Rename showSearch prop to showSearchInMobile to match its responsability properly

--- desktop-app/components/layouts/page-container/index.tsx
+++ desktop-app/components/layouts/page-container/index.tsx
@@ -16,9 +16,13 @@ type PageContainerProps = PageLayoutProps & { [key: string]: any };
 function PageContainer(props: PageContainerProps) {
   const windowWidth = useGetViewportWidthOnResize();
   const isOnDesktop = windowWidth >= 1024;
+  const showSearch = props.showSearchInMobile ?? props.showSearch;
 
   return (
-    <Maybe it={isOnDesktop} orElse={<MobilePageContainer {...props} />}>
+    <Maybe
+      it={isOnDesktop}
+      orElse={<MobilePageContainer {...props} showSearch={showSearch} />}
+    >
       <DesktopPageContainer {...props} />
     </Maybe>
   );
