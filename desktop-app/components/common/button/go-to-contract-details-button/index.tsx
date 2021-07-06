c802f0ed97bb9bae18e73e9d606b952f19fa0497
German Solano <german@famosos.com>
2021-07-06 10:07:50 -0400

    refactor(<GoToContractDetailsButton />): :recycle: Remove button child and add button styles to improve accesability

--- desktop-app/components/common/button/go-to-contract-details-button/index.tsx
+++ desktop-app/components/common/button/go-to-contract-details-button/index.tsx
@@ -1,6 +1,8 @@
 import { CLIENT_HIRINGS } from "constants/paths";
 import { Link } from "desktop-app/components/common/routing/link";
 import styles from "./styles.module.scss";
+import classes from "classnames";
+import { FormattedMessage } from "react-intl";
 
 type GoToContractDetailsButtonProps = {
   className?: string;
@@ -10,12 +12,15 @@ function GoToContractDetailsButton({
   className,
 }: GoToContractDetailsButtonProps) {
   return (
-    <Link href={CLIENT_HIRINGS} className={className}>
-      <button
-        className={`btn btn-tertiary ${styles.GoToContractDetailsButton}`}
-      >
-        Ver detalles de video
-      </button>
+    <Link
+      href={CLIENT_HIRINGS}
+      className={classes(
+        "btn btn-tertiary",
+        styles.GoToContractDetailsButton,
+        className
+      )}
+    >
+      <FormattedMessage defaultMessage="Ver detalles del video" />
     </Link>
   );
 }
