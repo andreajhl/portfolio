4a000c86ff0b5e519ec56ede77d045981f12077e
German Solano <german@famosos.com>
2021-07-05 22:23:23 -0400

    refactor(<NewsletterPopupCompleted />): :recycle: Change close button text to "Cerrar"

--- react-app/src/components/layouts/newsletter-popup-completed/index.tsx
+++ react-app/src/components/layouts/newsletter-popup-completed/index.tsx
@@ -29,7 +29,7 @@ function NewsletterPopupCompleted({
           styles.NewsletterPopupCompletedButton
         )}
       >
-        <FormattedMessage defaultMessage="Comprendido" />
+        <FormattedMessage defaultMessage="Cerrar" />
       </button>
     </section>
   );
