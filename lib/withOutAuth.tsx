3f855b4585249589173a6733d8c04356f52eb67a
Isaac Peña <isaac@famosos.com>
2021-07-07 12:54:37 -0400

    feat: :recycle: implement new HOC to use with withoutAuth HOC

--- lib/withOutAuth.tsx
+++ lib/withOutAuth.tsx
@@ -1,27 +1,17 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-
 import { NextPage } from "next";
-import { useIsAuthenticated } from "./famosos-auth";
-import withConditionalRedirect from "./withConditionalRedirect";
+import withAuthRedirect from "./withAuthRedirect";
 
 /**
  * Require the user to be unauthenticated in order to render the component.
  * If the user is authenticated, forward to the given URL.
  */
-const SESSION_NAME = process.env.NEXT_PUBLIC_FAMOSOS_AUTH_SESSION_NAME;
-
 export default function withoutAuth<P>(
   WrappedComponent: NextPage<P>,
   location = "/"
 ): NextPage<P> {
-  return withConditionalRedirect({
+  return withAuthRedirect({
     WrappedComponent,
     location,
-    clientCondition: function withoutAuthClientCondition() {
-      return useIsAuthenticated();
-    },
-    serverCondition: function withoutAuthServerCondition(ctx) {
-      return !!ctx.req?.cookies[SESSION_NAME];
-    }
+    expectedAuth: false
   });
 }
