e775efdaa4a1c9b53e66525f0b7c5d307ce3c97f
Isaac Peña <isaac@famosos.com>
2021-07-07 02:44:33 -0400

    feat: :poop: import GoogleLogin from react-google-login to test flow

--- react-app/src/components/layouts/google-button/index.tsx
+++ react-app/src/components/layouts/google-button/index.tsx
@@ -2,18 +2,25 @@ import { ReactNode } from "react";
 import classes from "classnames";
 import styles from "./styles.module.scss";
 import { useRouter } from "next/router";
+import { GoogleLogin } from "react-google-login";
+import { useWindow } from "react-app/src/utils/useWindow";
+import { IsMobile } from "react-app/src/utils/isMobile";
 
 type GoogleButtonProps = {
   children?: ReactNode;
   className?: string;
 };
 
+const responseType = "code";
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_LOGIN_IDENTIFIER;
+const redirectURL = process.env.NEXT_PUBLIC_GOOGLE_LOGIN_REDIRECT;
 function GoogleButton({ children, className }: GoogleButtonProps) {
+  const responseGoogle = (response) => {
+    console.log({ response });
+  };
+
   const redirectToGoogleOAuth = () => {
     const tokenRequestURL = "https://accounts.google.com/o/oauth2/auth";
-    const responseType = "code";
-    const clientId = process.env.NEXT_PUBLIC_GOOGLE_LOGIN_IDENTIFIER;
-    const redirectURL = process.env.NEXT_PUBLIC_GOOGLE_LOGIN_REDIRECT;
     const scope =
       "https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email";
     const state = "consent";
@@ -21,6 +28,20 @@ function GoogleButton({ children, className }: GoogleButtonProps) {
       `${tokenRequestURL}?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scope}&prompt=${state}`
     );
   };
+  return (
+    <GoogleLogin
+      clientId={clientId}
+      buttonText="Ingresar con Google"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      accessType="offline"
+      responseType="code"
+      cookiePolicy={"single_host_origin"}
+      redirectUri={redirectURL}
+      uxMode={IsMobile() ? "redirect" : "popup"}
+      className={classes("btn", styles.GoogleButton, className)}
+    />
+  );
   return (
     <button
       type="button"
