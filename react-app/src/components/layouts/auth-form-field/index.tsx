a499db8919416b6939dac08be1120252a637af7c
Isaac Peña <isaac@famosos.com>
2021-07-08 15:37:16 -0400

    feat: add iconElement to AuthFormField

--- react-app/src/components/layouts/auth-form-field/index.tsx
+++ react-app/src/components/layouts/auth-form-field/index.tsx
@@ -2,10 +2,13 @@ import { InputHTMLAttributes } from "react";
 import Maybe from "../../common/helpers/maybe";
 import classes from "classnames";
 import styles from "./styles.module.scss";
+import { ReactNode } from "react";
 
 type AuthFormFieldProps = {
   className?: string;
   label?: string;
+  iconElement?: ReactNode;
+  onIconClick?: () => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 function AuthFormField({
@@ -13,6 +16,8 @@ function AuthFormField({
   label,
   name = `AuthFormField-${Date.now()}`,
   id = name,
+  iconElement,
+  onIconClick,
   ...inputProps
 }: AuthFormFieldProps) {
   return (
@@ -22,7 +27,14 @@ function AuthFormField({
           {label}
         </label>
       </Maybe>
-      <input id={id} name={name} className={styles.Input} {...inputProps} />
+      <div className={styles.InputContainer}>
+        <input id={id} name={name} className={styles.Input} {...inputProps} />
+        {iconElement ? (
+          <div onClick={onIconClick} className={styles.IconContainer}>
+            {iconElement}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
