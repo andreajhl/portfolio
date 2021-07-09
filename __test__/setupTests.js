2ba4d2165125f2252aebf360c9a03566da765501
German Solano <german@famosos.com>
2021-07-09 08:40:16 -0400

    test: :white_check_mark: Add react-intl mock

--- __test__/setupTests.js
+++ __test__/setupTests.js
@@ -10,6 +10,19 @@ configure({ adapter: new Adapter() });
 // This allow to tests pages using 'withAuthenticationRequired' properly, without changing the tested files.
 jest.mock("@auth0/auth0-react");
 
+// Based on https://github.com/formatjs/formatjs/issues/1477#issuecomment-693206946
+jest.mock("react-intl", () => {
+  const reactIntl = jest.requireActual("react-intl");
+  const intl = reactIntl.createIntl({
+    locale: "en",
+  });
+
+  return {
+    ...reactIntl,
+    useIntl: () => intl,
+  };
+});
+
 global.beforeAll(async () => {
   await preloadAll();
 });
