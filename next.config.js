917b21f24740bf889bd436226756a9947e75dfd3
Isaac Peña <isaac@famosos.com>
2021-07-14 18:28:12 -0400

    feat: añadir nuevos iso-code faltantes de Brasil

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["es", "en", "pt"],
+    locales: ["es", "en", "pt", "por", "pt-BR"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "es"
