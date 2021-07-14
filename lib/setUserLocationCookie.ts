a13f086bd742c99e8f8c079981da91db0f6c08ec
Isaac Peña <isaac@famosos.com>
2021-07-14 14:50:13 -0400

    feat: implementar logica para verificar header con valores

--- lib/setUserLocationCookie.ts
+++ lib/setUserLocationCookie.ts
@@ -15,6 +15,7 @@ import {
   serializeCurrencyCurrentData,
   serializeUserLocationCookies
 } from "./serializeCookies";
+import setCookieParser from "set-cookie-parser";
 
 const ONE_YEAR_IN_MILLISECONDS = 365 * 24 * 3600 * 1000;
 const invalidIpAddresses = ["127.0.0.1", "::1"];
@@ -81,10 +82,7 @@ async function getLocationCookieHeader(
     userIpAddressLocation: userIpAddress
   };
 }
-async function getCurrencyCurrentTRMCookieHeader(
-  currency: string,
-  cookies: string
-) {
+async function getCurrencyCurrentTRMCookieHeader(currency: string) {
   let currencyCode = null;
   if (findAvailableCurrencyByName(currency)) {
     currencyCode = currency;
@@ -125,6 +123,7 @@ const setUserLocationCookie = async ({
   res
 }: DocumentContext): Promise<void> => {
   if (!req) return;
+
   if (isBot(req.headers["user-agent"])) {
     return debug("Este es un bot solicitando", req.url);
   }
@@ -133,23 +132,42 @@ const setUserLocationCookie = async ({
   let newCookiesSerializes = [];
 
   if (
-    !(USER_LOCATION_KEY in cookies) ||
-    !(USER_IP_ADDRESS in cookies) ||
-    !(USER_CURRENCY_CODE in cookies)
+    (!(USER_LOCATION_KEY in cookies) ||
+      !(USER_IP_ADDRESS in cookies) ||
+      !(USER_CURRENCY_CODE in cookies)) &&
+    !res.getHeader(USER_LOCATION_KEY) &&
+    !res.getHeader(USER_CURRENCY_CODE) &&
+    !res.getHeader(USER_IP_ADDRESS)
   ) {
     const locationCookies = await getLocationCookieHeader(req, res);
     const currencyCurrentData = await getCurrencyCurrentTRMCookieHeader(
-      locationCookies.currency_code,
-      cookies
+      locationCookies.currency_code
     );
     newCookiesSerializes.push(...serializeUserLocationCookies(locationCookies));
     newCookiesSerializes.push(
       ...serializeCurrencyCurrentData(currencyCurrentData)
     );
+  } else if (
+    res.getHeader(USER_IP_ADDRESS) &&
+    res.getHeader(USER_CURRENCY_CODE) &&
+    res.getHeader(USER_LOCATION_KEY)
+  ) {
+    const currencyCurrentData = await getCurrencyCurrentTRMCookieHeader(
+      String(res.getHeader(USER_CURRENCY_CODE))
+    );
+    newCookiesSerializes.push(
+      ...serializeUserLocationCookies({
+        currency_code: String(res.getHeader(USER_CURRENCY_CODE)),
+        userIpAddressLocation: String(res.getHeader(USER_IP_ADDRESS)),
+        country_code: String(res.getHeader(USER_LOCATION_KEY))
+      })
+    );
+    newCookiesSerializes.push(
+      ...serializeCurrencyCurrentData(currencyCurrentData)
+    );
   } else {
     const currencyCurrentData = await getCurrencyCurrentTRMCookieHeader(
-      cookies[CURRENT_CURRENCY_TRM_CODE] || cookies[USER_CURRENCY_CODE],
-      cookies
+      cookies[CURRENT_CURRENCY_TRM_CODE] || cookies[USER_CURRENCY_CODE]
     );
     newCookiesSerializes.push(
       ...serializeCurrencyCurrentData(currencyCurrentData)
