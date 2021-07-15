7003914e6ae17d9b2b852bd90ab50a5efa5d31ee
Isaac Peña <isaac@famosos.com>
2021-07-15 01:22:11 -0400

    feat: añadir urls de external assets para portugues

--- constants/external_assets_by_lang.ts
+++ constants/external_assets_by_lang.ts
@@ -2,6 +2,8 @@ export const hiring_proccess_img = {
   es:
     "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/es/hiring_process.png",
   en:
+    "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/en/hiring_process.png",
+  pt:
     "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/en/hiring_process.png"
 };
 
@@ -9,5 +11,7 @@ export const secure_payment_img = {
   es:
     "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/es/secure_payment.png",
   en:
-    "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/en/secure_payment.png"
+    "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/en/secure_payment.png",
+  pt:
+    "https://famosos-media.s3.amazonaws.com/famosos-com-assets/contracts/en/hiring_process.png"
 };
