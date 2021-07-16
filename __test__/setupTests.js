934ddd2cb08f08a7931e68273d126dc22a14be05
German Solano <german@famosos.com>
2021-07-16 08:46:59 -0400

    test: :wrench: Add react-async-script-loader mock and load testing environment variables

--- __test__/setupTests.js
+++ __test__/setupTests.js
@@ -2,6 +2,13 @@ const Adapter = require("enzyme-adapter-react-16");
 const { configure } = require("enzyme");
 const preloadAll = require("jest-next-dynamic");
 const enableHooks = require("jest-react-hooks-shallow").default;
+const { loadEnvConfig } = require("@next/env");
+
+const loadTestingEnvironmentVariables = async () => {
+  const projectDir = process.cwd();
+  const consoleWithoutInfoLogs = { info() {}, error: console.error };
+  loadEnvConfig(projectDir, true, consoleWithoutInfoLogs);
+};
 
 /* This package makes React Hooks (namely, useEffect() and useLayoutEffect()) work with shallow rendering. Use withoutHooks to opt-out this behaviour for mount rendering testing. */
 enableHooks(jest);
@@ -13,6 +20,9 @@ jest.mock("../react-app/src/state/utils/gtm");
 
 // This allow to tests pages using 'withAuthenticationRequired' properly, without changing the tested files.
 jest.mock("@auth0/auth0-react");
+
+// This removes the HoC of the library that does not allow to assert properly on tested components because its hide its children.
+jest.mock("react-async-script-loader", () => () => (Component) => Component);
 
 // Based on https://github.com/formatjs/formatjs/issues/1477#issuecomment-693206946
 jest.mock("react-intl", () => {
@@ -28,5 +38,6 @@ jest.mock("react-intl", () => {
 });
 
 global.beforeAll(async () => {
+  await loadTestingEnvironmentVariables();
   await preloadAll();
 });
--- desktop-app/components/payments-methods/DLocal-form-card/index.test.tsx
+++ desktop-app/components/payments-methods/DLocal-form-card/index.test.tsx
@@ -1,7 +1,10 @@
 import { shallow } from "enzyme";
+import { testPaymentMethodsAvailable } from "__test__/fake-data/testPaymentMethods";
 import DLocalFormCard from ".";
 
 it("renders without crashing", () => {
-  const wrapper = shallow(<DLocalFormCard />);
+  const wrapper = shallow(
+    <DLocalFormCard paymentsMethodsAvailable={testPaymentMethodsAvailable} />
+  );
   expect(wrapper.exists()).toBeTruthy();
 });
