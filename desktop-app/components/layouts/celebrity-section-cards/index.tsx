c9d894efd0771a97f556fb59b0eac4fdd3e320d0
German Solano <german@famosos.com>
2021-07-20 16:42:05 -0400

    refactor(<CelebritiesSection />): :chart_with_upwards_trend: Track card clicks

--- desktop-app/components/layouts/celebrity-section-cards/index.tsx
+++ desktop-app/components/layouts/celebrity-section-cards/index.tsx
@@ -4,7 +4,9 @@ import { CelebrityCard } from "desktop-app/components/common/cards/celebrity";
 import { CelebritySectionType } from "desktop-app/types/celebritySectionType";
 import { useMemo } from "react";
 import { jsonToQueryString } from "react-app/src/state/utils/apiService";
+import { analytics } from "react-app/src/state/utils/gtm";
 import getMoreFrequentIds from "react-app/src/utils/getMoreFrequentIds";
+import getWindow from "react-app/src/utils/getWindow";
 import { CardsReelSection, CardsReelSectionProps } from "../cards-section-reel";
 import CelebritySectionVideoCard from "../celebrity-section-video-card";
 import styles from "./styles.module.scss";
@@ -14,9 +16,11 @@ type CelebritiesSectionProps = {
 };
 
 const celebrityTypeReelProps: {
-  [key: string]: Omit<
+  [key: string]: {
+    getChildren: (...params: any) => CardsReelSectionProps["children"];
+  } & Omit<
     CardsReelSectionProps,
-    "itemCount" | "itemData" | "itemCount"
+    "itemCount" | "itemData" | "itemCount" | "children"
   >;
 } = {
   CELEBRITY_CARD: {
@@ -28,7 +32,29 @@ const celebrityTypeReelProps: {
       transform: "translateY(-50%)",
     },
     gap: 22.8,
-    children: (celebrity) => <CelebrityCard celebrity={celebrity} />,
+    getChildren: (celebritySectionData: CelebritySectionType) => (
+      celebrity
+    ) => {
+      function trackCelebrityCardClick() {
+        analytics.track(
+          "CLICK_ON_CELEBRITY_CARD",
+          Object.assign(
+            {
+              celebritySectionData,
+              path: getWindow().location.pathname,
+              widget: "CelebrityCard",
+            },
+            celebrity
+          )
+        );
+      }
+      return (
+        <CelebrityCard
+          celebrity={celebrity}
+          onClickLink={trackCelebrityCardClick}
+        />
+      );
+    },
   },
   MAIN_VIDEO_1: {
     itemWidth: 263,
@@ -39,17 +65,35 @@ const celebrityTypeReelProps: {
       transform: "translateY(-50%)",
     },
     gap: 27.333,
-    children: (celebrityVideo) => (
-      <CelebritySectionVideoCard
-        occasion={celebrityVideo.occasion}
-        username={celebrityVideo.username}
-        videoUrl={celebrityVideo.videoUrl}
-        fullName={celebrityVideo.fullName}
-        videoPosterUrl={celebrityVideo.videoPosterUrl}
-        avatar={celebrityVideo.avatar}
-        contract_reference={celebrityVideo.contract_reference}
-      />
-    ),
+    getChildren: (celebritySectionData: CelebritySectionType) => (
+      celebrityVideo
+    ) => {
+      function trackVideoCardClick() {
+        analytics.track(
+          "CLICK_ON_CELEBRITY_VIDEO_CARD",
+          Object.assign(
+            {
+              celebritySectionData,
+              path: getWindow().location.pathname,
+              widget: "CelebritySectionVideoCard",
+            },
+            celebrityVideo
+          )
+        );
+      }
+      return (
+        <CelebritySectionVideoCard
+          occasion={celebrityVideo.occasion}
+          username={celebrityVideo.username}
+          videoUrl={celebrityVideo.videoUrl}
+          fullName={celebrityVideo.fullName}
+          videoPosterUrl={celebrityVideo.videoPosterUrl}
+          avatar={celebrityVideo.avatar}
+          contract_reference={celebrityVideo.contract_reference}
+          onClickFooterLink={trackVideoCardClick}
+        />
+      );
+    },
   },
   CATEGORY_CARD: {
     itemWidth: 212,
@@ -60,19 +104,21 @@ const celebrityTypeReelProps: {
       transform: "translateY(-50%)",
     },
     gap: 18.5,
-    children: (category) => <CategoryCard category={category} />,
+    getChildren: () => (category) => <CategoryCard category={category} />,
   },
 };
 
 function CelebritiesSection({ celebritySection }: CelebritiesSectionProps) {
-  const { celebrities, celebritySectionType } = celebritySection;
+  const { celebrities, ...celebritySectionData } = celebritySection;
+  const { celebritySectionType } = celebritySectionData;
 
   const searchMoreResultsPath = useMemo(() => {
     if (
       (!celebrities && celebrities.length < 10) ||
       celebritySectionType === "CATEGORY_CARD"
-    )
+    ) {
       return null;
+    }
     return (
       SEARCH_PATH +
       jsonToQueryString({
@@ -84,6 +130,10 @@ function CelebritiesSection({ celebritySection }: CelebritiesSectionProps) {
   }, [celebrities, celebritySectionType]);
 
   const reelProps = celebrityTypeReelProps[celebritySectionType];
+  const children = useMemo(
+    () => reelProps.getChildren(celebritySectionData),
+    []
+  );
 
   return (
     <CardsReelSection
@@ -101,7 +151,7 @@ function CelebritiesSection({ celebritySection }: CelebritiesSectionProps) {
       gap={reelProps.gap}
       className={styles.CelebritiesSection}
     >
-      {reelProps.children}
+      {children}
     </CardsReelSection>
   );
 }
