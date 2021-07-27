2173c0c77ea49735addf8451a1afa1dfeb318e6e
German Solano <german@famosos.com>
2021-07-27 06:55:53 -0400

    feat(I18n): :sparkles: Add react-intl custom version to improve its types

---
+++ lib/custom-intl.ts
@@ -0,0 +1,42 @@
+import { localeAvailables } from "react-app/src/utils/transformUserNavigatorLanguageToISO2Code";
+import {
+  IntlFormatters,
+  MessageDescriptor,
+  useIntl as useOriginalIntl,
+} from "react-intl";
+
+const isMessageDescriptor = (object: any) =>
+  Object.keys(object).includes("defaultMessage");
+
+type TextType<
+  T extends MessageDescriptor | unknown
+> = T extends MessageDescriptor ? string : T;
+
+type FormatMessageParameters = Parameters<IntlFormatters["formatMessage"]>;
+
+type ValuesType = FormatMessageParameters[1];
+type OptionsType = FormatMessageParameters[2];
+
+function useIntl() {
+  const {
+    locale: originalLocale,
+    formatMessage: originalFormatMessage,
+    ...intl
+  } = useOriginalIntl();
+  const locale = originalLocale as localeAvailables;
+
+  function formatMessage<T extends MessageDescriptor | unknown>(
+    text: T,
+    values?: ValuesType,
+    options?: OptionsType
+  ): TextType<T> {
+    if (!isMessageDescriptor(text)) return text as TextType<T>;
+    return originalFormatMessage(text, values, options) as TextType<T>;
+  }
+
+  return { ...intl, locale, formatMessage };
+}
+
+export * from "react-intl";
+
+export { useIntl };
