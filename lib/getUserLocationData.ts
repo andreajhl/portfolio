3158b03b57b4e5474fe9733198ef825859a2b1d0
German Solano <german@famosos.com>
2021-07-29 12:58:17 -0400

    feat(<Home />): :sparkles: Add geolocation to headers

--- lib/getUserLocationData.ts
+++ lib/getUserLocationData.ts
@@ -3,27 +3,28 @@ import debug from "react-app/src/utils/debug";
 import { getIpData } from "./getIpData";
 import { getUserIp } from "./getUserIp";
 const invalidIpAddresses = ["127.0.0.1", "::1"];
+
+const fallbackIpData = {
+  geolocation: "0,0",
+  country_code: "",
+  currency_code: ""
+};
+
 export const getUserLocationData = async (
   request: IncomingMessage
 ): Promise<{
+  geolocation: string;
   country_code: string;
   currency_code: string;
 }> => {
   try {
     let userIp = getUserIp(request);
-    if (!userIp || invalidIpAddresses.includes(userIp))
-      return {
-        country_code: "",
-        currency_code: ""
-      };
+    if (!userIp || invalidIpAddresses.includes(userIp)) return fallbackIpData;
     debug("Se va a llamar a IPStack con la IP", userIp);
     const response = await getIpData(userIp);
     debug("IPStack posee response", JSON.stringify(response));
     return response;
   } catch (error) {
-    return {
-      country_code: "",
-      currency_code: ""
-    };
+    return fallbackIpData;
   }
 };
