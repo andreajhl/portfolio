52fec5da7691990299bfc037128117568231d53c
Isaac Peña <isaac.pr99@gmail.com>
2021-07-29 17:32:14 -0400

    fix: replace symbol by Decimal HTML Entity

--- react-app/src/components/layouts/celebrity-details/index.js
+++ react-app/src/components/layouts/celebrity-details/index.js
@@ -41,9 +41,8 @@ const CelebrityDetails = ({ celebrity, variant }) => {
 
   return (
     <Container
-      className={`mx-auto CelebrityDetails ${variant === "1" ? "mb-0" : ""} ${
-        variant === "2" ? "mb-4 mt-0" : ""
-      }`}
+      className={`mx-auto CelebrityDetails ${variant === "1" ? "mb-0" : ""} ${variant === "2" ? "mb-4 mt-0" : ""
+        }`}
     >
       <Row className="justify-content-md-center align-items-center align-items-md-start mt-3 mb-3 mt-lg-0">
         <Col xs="auto d-md-none" className="text-center">
@@ -107,7 +106,7 @@ const CelebrityDetails = ({ celebrity, variant }) => {
                 celebrityUsername={username}
                 text={
                   <>
-                    <Emoji label="star-struck" symbol="🤩" />
+                    <Emoji label="star-struck" symbol="&#129321" />
                     <span
                       style={{
                         fontWeight: "bold",
@@ -119,7 +118,7 @@ const CelebrityDetails = ({ celebrity, variant }) => {
                         description=""
                       />
                     </span>
-                    <Emoji label="star-struck" symbol="🤩" />
+                    <Emoji label="star-struck" symbol="&#129321" />
                   </>
                 }
                 width="100%"
