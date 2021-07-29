a2136659a5909f8cf71eadf16270799bad481844
German Solano <german@famosos.com>
2021-07-28 22:48:43 -0400

    test: :white_check_mark: Add locale and locales to useRouter hook mock

--- __mocks__/next/router.tsx
+++ __mocks__/next/router.tsx
@@ -1,6 +1,8 @@
 export * from "next/router";
 export { default } from "next/router";
 export const useRouter = () => ({
+  locale: "es",
+  locales: ["es"],
   asPath: "/",
   events: {
     on() {},
