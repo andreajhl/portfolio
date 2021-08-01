3ac1be71987f3d5608cd8f08df5022b2937fa7a3
German Solano <german@famosos.com>
2021-08-01 17:01:07 -0400

    feat(<SignUpBox />): :sparkles: Add <SignUpWithEmailWizard />

--- react-app/src/components/containers/sign-up-box/index.tsx
+++ react-app/src/components/containers/sign-up-box/index.tsx
@@ -1,29 +1,19 @@
-import { AuthFormField } from "react-app/src/components/layouts/auth-form-field";
 import { FacebookButton } from "react-app/src/components/layouts/facebook-button";
 import { GoogleButton } from "react-app/src/components/layouts/google-button";
-import { connect, ConnectedProps } from "react-redux";
 import classes from "classnames";
 import styles from "./styles.module.scss";
 import { AuthTermsAdvertise } from "react-app/src/components/layouts/auth-terms-advertise";
 import { FormattedMessage, useIntl, defineMessages } from "react-intl";
 import { Link } from "../../common/routing/link";
 import { SIGN_IN_PATH } from "react-app/src/routing/Paths";
-import { SignUpEmailPasswordForm } from "../sign-up-with-email-form";
+import { SignUpWithEmailWizard } from "../sign-up-with-email-wizard";
 
 const signInLink = (chunk: string) => <Link href={SIGN_IN_PATH}>{chunk}</Link>;
-
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-const connector = connect(mapStateToProps, mapDispatchToProps);
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type SignUpBoxProps = {
   className?: string;
   willRedirect?: boolean;
-} & PropsFromRedux;
+};
 
 const RegisterMessages = defineMessages({
   facebookMessage: { defaultMessage: "Registrarme con Facebook" },
@@ -44,7 +34,7 @@ function SignUpBox({ className, willRedirect = false }: SignUpBoxProps) {
           className={styles.AuthProviderButton}
           textButton={formatMessage(RegisterMessages.googleMessage)}
         />
-        <SignUpEmailPasswordForm willRedirect={willRedirect} />
+        <SignUpWithEmailWizard willRedirect={willRedirect} />
         <AuthTermsAdvertise className={styles.SignUpBoxAuthTermsAdvertise} />
       </div>
       <p className={styles.AlreadyRegisteredText}>
@@ -57,6 +47,4 @@ function SignUpBox({ className, willRedirect = false }: SignUpBoxProps) {
   );
 }
 
-const _SignUpBox = connector(SignUpBox);
-
-export { _SignUpBox as SignUpBox };
+export { SignUpBox };
