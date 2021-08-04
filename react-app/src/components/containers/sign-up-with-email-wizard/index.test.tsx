a222a3f181962fb66566410a900bef2d55fa34cb
German Solano <german@famosos.com>
2021-08-04 18:07:46 -0400

    feat(<SignUpBox />): :sparkles: Add wizard logic to validate email before sign up

--- react-app/src/components/containers/sign-up-with-email-wizard/index.test.tsx
+++
@@ -1,7 +0,0 @@
-import { shallow } from "enzyme";
-import { SignUpWithEmailWizard } from "./";
-
-it("should renders without crashing", () => {
-  const wrapper = shallow(<SignUpWithEmailWizard willRedirect={false} />);
-  expect(wrapper.exists()).toBeTruthy();
-});
