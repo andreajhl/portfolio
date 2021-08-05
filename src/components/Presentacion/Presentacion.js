40459d9b2d27c71b5b29a26eda4071343503083f
andreajhl <andreajhl29@gmial.com>
2021-08-05 20:49:34 -0300

    cambios responvie pantalla grande

--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -7,7 +7,7 @@ export default function Presentacion(){
     return (
             <div className="pre_cuerpo">
                 <div className="pre_cuerpo_pag">
-                    <h1 className='hi'>¡Bienvenido a mi pag!</h1>
+                    <h1 className='hi'>¡Bienvenido a mi página!</h1>
                     <p className="pre_cuerpo_pag_P">Mi nombre es Andrea, tengo 22 años, conocí el mundo de la programación gracias a un amigo. 
                         Empecé a estudiar un poco, me gusto bastante y fue allí que decidí hacer un bootcamp  
                         , así descubrí a Soy Henry, donde me he formado como Full Stack, estoy empezando en este mundo y espero poder seguir aprendiendo
