0da2c2873f1b9f5d755504c7b54c3de22026af32
German Solano <german@famosos.com>
2021-08-13 11:28:34 -0400

    refactor(useMediaQuery): :recycle: Improve hook logic to ensure proper work in any browser and to log a warning while in Server-side

--- lib/hooks/useMediaQuery.ts
+++ lib/hooks/useMediaQuery.ts
@@ -1,19 +1,56 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+// Based on https://github.com/beautifulinteractions/beautiful-react-hooks/blob/HEAD/src/useMediaQuery.js
 import { useEffect, useState } from "react";
+import isBrowserAPISupported from "../utils/isBrowserAPISupported";
 
-export function useMediaQuery(query) {
-  const [matches, setMatches] = useState(false);
+const errorMessage =
+  "matchMedia is not supported, this could happen both because window.matchMedia is not supported by" +
+  " your current browser or you're using the useMediaQuery hook whilst server side rendering.";
+
+/**
+ * Accepts a media query string then uses the
+ * [window.matchMedia](https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia) API to determine if it
+ * matches with the current document.
+ * It also monitor the document changes to detect when it matches or stops matching the media query.
+ * Returns the validity state of the given media query.
+ *
+ */
+function useMediaQuery(mediaQuery: string) {
+  if (!isBrowserAPISupported("matchMedia")) {
+    // eslint-disable-next-line no-console
+    console.warn(errorMessage);
+    return null;
+  }
+
+  const [isVerified, setIsVerified] = useState(
+    !!window.matchMedia(mediaQuery).matches
+  );
 
   useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-    const listener = () => {
-      setMatches(media.matches);
-    };
-    media.addListener(listener);
-    return () => media.removeListener(listener);
-  }, [matches, query]);
+    const mediaQueryList = window.matchMedia(mediaQuery);
+    const documentChangeHandler = () => setIsVerified(!!mediaQueryList.matches);
 
-  return matches;
+    try {
+      mediaQueryList.addEventListener("change", documentChangeHandler);
+    } catch (e) {
+      //Safari isn't supporting mediaQueryList.addEventListener
+      console.error(e);
+      mediaQueryList.addListener(documentChangeHandler);
+    }
+
+    documentChangeHandler();
+    return () => {
+      try {
+        mediaQueryList.removeEventListener("change", documentChangeHandler);
+      } catch (e) {
+        //Safari isn't supporting mediaQueryList.removeEventListener
+        console.error(e);
+        mediaQueryList.removeListener(documentChangeHandler);
+      }
+    };
+  }, [mediaQuery]);
+
+  return isVerified;
 }
+
+export { useMediaQuery };
