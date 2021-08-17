245df5e1c90fadca66d09e6f5e86450b2b330c80
Isaac Peña <isaac.pr99@gmail.com>
2021-08-17 16:41:33 -0400

    feat: add variant prop to style button with different classes

--- desktop-app/components/common/button/submit-button/index.tsx
+++ desktop-app/components/common/button/submit-button/index.tsx
@@ -5,6 +5,7 @@ import { LoadingSpinner } from "../../loading-spinner";
 
 type SubmitButtonProps = {
   loading?: boolean;
+  variant?: "primary" | "secondary" | "danger" | "tertiary";
 } & ComponentPropsWithRef<"button">;
 
 function SubmitButton({
@@ -12,12 +13,31 @@ function SubmitButton({
   type = "submit",
   className,
   loading,
+  variant = "primary",
   ...props
 }: SubmitButtonProps) {
+  const getClassNameButton = () => {
+    if (variant === "primary") {
+      return "btn-primary";
+    }
+    if (variant === "secondary") {
+      return "btn-secondary";
+    }
+    if (variant === "tertiary") {
+      return "btn-tertiary";
+    }
+    if (variant === "danger") {
+      return "btn-tertiary";
+    }
+  };
   return (
     <button
       type={type}
-      className={classes("btn btn-primary", styles.SubmitButton, className)}
+      className={classes(
+        `btn ${getClassNameButton()}`,
+        styles.SubmitButton,
+        className
+      )}
       {...props}
     >
       {children}{" "}
