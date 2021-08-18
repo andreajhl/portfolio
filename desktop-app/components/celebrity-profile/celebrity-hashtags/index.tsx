3ddb2aa7be64007717e3c9fd5e0424bbc25e0f06
German Solano <german@famosos.com>
2021-08-18 08:11:59 -0400

    feat(<CelebrityHashtags />): :sparkles: Add className prop to improve customization

--- desktop-app/components/celebrity-profile/celebrity-hashtags/index.tsx
+++ desktop-app/components/celebrity-profile/celebrity-hashtags/index.tsx
@@ -4,12 +4,14 @@ import { getSearchHashtagPath } from "constants/paths";
 import { celebrityType } from "desktop-app/types/celebrityType";
 import { getWindowPathname } from "react-app/src/utils/getWindow";
 import { analytics } from "react-app/src/state/utils/gtm";
+import classes from "classnames";
 
 type CelebrityHashtagsProps = {
+  className?: string;
   celebrity: celebrityType;
 };
 
-function CelebrityHashtags({ celebrity }: CelebrityHashtagsProps) {
+function CelebrityHashtags({ className, celebrity }: CelebrityHashtagsProps) {
   const hashtags = celebrity?.hashtags || [];
 
   function trackHashtagClick(hashtag: string) {
@@ -22,7 +24,7 @@ function CelebrityHashtags({ celebrity }: CelebrityHashtagsProps) {
   }
 
   return (
-    <div className={styles.CelebrityHashtags}>
+    <div className={classes(styles.CelebrityHashtags, className)}>
       {hashtags?.map?.((hashtag) => (
         <Link
           key={hashtag}
