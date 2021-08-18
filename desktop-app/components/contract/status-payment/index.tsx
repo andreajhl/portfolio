e17fc868100a620ff874bee963c8b31a164f5c8e
Isaac Peña <isaac.pr99@gmail.com>
2021-08-18 10:51:59 -0400

    feat: add dynamic label status

--- desktop-app/components/contract/status-payment/index.tsx
+++ desktop-app/components/contract/status-payment/index.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import moment from "moment";
 import styles from "./styles.module.scss";
 import { FormattedMessage } from "react-intl";
+import { returnPaymentStatusLabel } from "react-app/src/constants/messages";
 
 type StatusPaymentDetailsProps = {
   payment_date: string;
   contractReference: string;
+  status: number;
 };
 
 function StatusPaymentDetails({
   payment_date,
   contractReference,
+  status,
 }: StatusPaymentDetailsProps) {
   const formattedPaymentDate = moment(payment_date).format("L");
   return (
     <div className={styles.StatusPaymentDetails}>
       <p>
-        <FormattedMessage defaultMessage="ESTADO DEL PAGO: APROBADO" />
+        <FormattedMessage defaultMessage="ESTADO DEL PAGO:" />
+        {returnPaymentStatusLabel(status)[0]}
       </p>
       <p>
         <FormattedMessage
