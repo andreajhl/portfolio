6b0ff77ad70068f5be390993b214e471907b11b1
German Solano <german@famosos.com>
2021-08-19 13:54:09 -0400

    feat(<CelebrityDetails />): :sparkles: Add <CelebrityMainVideoWidget /> to variant 2

--- react-app/src/components/layouts/celebrity-details/index.js
+++ react-app/src/components/layouts/celebrity-details/index.js
@@ -2,21 +2,42 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ProfilePicture } from "../profile-picture";
 import { CelebrityInfo } from "../celebrity-info";
+import Maybe from "desktop-app/components/common/helpers/maybe";
+import dynamic from "next/dynamic";
+
+const CelebrityMainVideoWidget = dynamic(
+  import("../../celebrity-profile/celebrity-main-video-widget").then(
+    (mod) => mod.CelebrityMainVideoWidget,
+    { loading: () => <div className="CelebrityMainVideoWidget-skeleton" /> }
+  )
+);
 
 function CelebrityDetails({ celebrity, variant }) {
   const { avatar } = celebrity;
-  const profilePictureSize = variant === "1" ? "78px" : "120px";
 
   return (
     <Container className="mx-auto CelebrityDetails">
       <Row className="justify-content-md-center align-items-center align-items-md-start mt-3 mb-3 mt-lg-0">
         <Col xs="auto" className="text-center">
-          <ProfilePicture
-            avatar={avatar}
-            width={profilePictureSize}
-            height={profilePictureSize}
-            imageStyles={{ objectFit: "cover" }}
-          />
+          <Maybe
+            it={variant === "2" && celebrity?.mainVideo}
+            orElse={
+              <ProfilePicture
+                avatar={avatar}
+                width="83px"
+                height="83px"
+                imageStyles={{ objectFit: "cover" }}
+              />
+            }
+          >
+            <CelebrityMainVideoWidget
+              avatarProps={{
+                width: 120,
+                height: 120,
+              }}
+              celebrity={celebrity}
+            />
+          </Maybe>
         </Col>
         <Col>
           <CelebrityInfo variant={variant} celebrity={celebrity} />
