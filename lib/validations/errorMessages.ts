9c90c054d2acc3e2b4fe1e7d763e1e74aa132744
Isaac Peña <isaac.pr99@gmail.com>
2021-08-30 02:07:19 -0400

    feat: add new error messages

--- lib/validations/errorMessages.ts
+++ lib/validations/errorMessages.ts
@@ -5,29 +5,33 @@ const errorMessages = defineMessages({
   emptyInstructions: { defaultMessage: "Debes escribir tus instrucciones" },
   unSelectedOccasion: { defaultMessage: "Debes seleccionar una ocasión" },
   maxLengthError: {
-    defaultMessage: "Debes introducir un máximo de {maxLength} caracteres"
+    defaultMessage: "Debes introducir un máximo de {maxLength} caracteres",
   },
   invalidEmail: { defaultMessage: "Ingresa un correo electrónico válido" },
   invalidCellphoneNumber: {
-    defaultMessage: "Ingresa un número de teléfono válido"
+    defaultMessage: "Ingresa un número de teléfono válido",
   },
   invalidEmailList: {
-    defaultMessage: "Ingresa una lista de correos electrónicos válidos"
+    defaultMessage: "Ingresa una lista de correos electrónicos válidos",
   },
   emptyMessage: {
-    defaultMessage: "Ingresa tu mensaje"
+    defaultMessage: "Ingresa tu mensaje",
   },
   emptyDate: { defaultMessage: "Ingresa una fecha" },
   invalidDate: {
-    defaultMessage: "Ingresa una fecha valida. Ejemplo: 2020-06-25"
+    defaultMessage: "Ingresa una fecha valida. Ejemplo: 2020-06-25",
   },
   invalidCoupon: {
-    defaultMessage: "Debes de introducir un cupón valido"
+    defaultMessage: "Debes de introducir un cupón valido",
   },
   invalidIdentificationDocument: {
-    defaultMessage: "Debes introducir un documento de identificación valido"
+    defaultMessage: "Debes introducir un documento de identificación valido",
   },
-  emptyReview: { defaultMessage: "Debes escribir un comentario" }
+  emptyReview: { defaultMessage: "Debes escribir un comentario" },
+  emptyAddressError: { defaultMessage: "Debes introducir una dirección" },
+  emptyCountryError: { defaultMessage: "Debes seleccionar un país" },
+  emptyStateError: { defaultMessage: "Debes selecciona un Estado" },
+  emptyCityError: { defaultMessage: "Debes seleccionar una Ciudad" },
 });
 
 export default errorMessages;
