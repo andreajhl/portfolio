631b9adc27c5a35574fe7035af6d7e801ef06c89
German Solano <german@famosos.com>
2021-08-31 12:19:19 -0400

    feat(redux/subscriptions): :sparkles: Add publicListSubscriptionPosts action

--- react-app/src/state/ducks/subscriptions/paths.js
+++ react-app/src/state/ducks/subscriptions/paths.js
@@ -4,3 +4,4 @@ export const PROCESS_SUBSCRIPTION_PATH = `custom-endpoints/${MODEL}/process-subs
 export const USER_SUBSCRIPTIONS_PATH = `custom-endpoints/${MODEL}/list`;
 
 export const LIST_SUBSCRIPTION_POSTS_PATH = `custom-endpoints/celebrity-subscription-posts/list`;
+export const PUBLIC_LIST_SUBSCRIPTION_POSTS_PATH = `custom-endpoints/celebrity-subscription-posts/public-list`;
