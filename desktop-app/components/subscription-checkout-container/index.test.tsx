faf25178c14d6290ac7ab9a4daf1a23c6c3ec171
German Solano <german@famosos.com>
2021-08-31 16:37:48 -0400

    feat(<Subscription />): :sparkles: Add logic to display user's previous used cards

---
+++ desktop-app/components/subscription-checkout-container/index.test.tsx
@@ -0,0 +1,7 @@
+import { shallow } from "enzyme";
+import { SubscriptionCheckoutContainer } from "./";
+
+it("should renders without crashing", () => {
+  const wrapper = shallow(<SubscriptionCheckoutContainer />);
+  expect(wrapper.exists()).toBeTruthy();
+});
