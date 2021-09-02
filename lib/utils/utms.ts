8e4b9a51876f746847b99f597964b3a133bd72f6
German Solano <german@famosos.com>
2021-09-02 15:33:01 -0400

    feat: :sparkles: Add utils methods to save and get user UTMs

---
+++ lib/utils/utms.ts
@@ -0,0 +1,51 @@
+import objectHasValidValues from "./objectHasValidValues";
+import { USER_UTMS_KEY } from "constants/keys.js";
+import pickPropertiesFromAObject from "react-app/src/utils/pickPropertiesFromAObject";
+import { serialize } from "cookie";
+import { ONE_YEAR_IN_MILLISECONDS } from "constants/oneYearINMilliseconds";
+import getCookie from "react-app/src/utils/getCookie";
+
+function getUTMsFromQueryParams(queryParams: { [key: string]: string }) {
+  const utmSource = queryParams.utm_source;
+  const utmMedium = queryParams.utm_medium;
+  const utmCampaign = queryParams.utm_campaign;
+  const utmTerm = queryParams.utm_term;
+  const utmContent = queryParams.utm_content;
+  return {
+    utmSource,
+    utmMedium,
+    utmCampaign,
+    utmTerm,
+    utmContent,
+  };
+}
+
+type UTMsType = {
+  utmSource?: string;
+  utmMedium?: string;
+  utmCampaign?: string;
+  utmTerm?: string;
+  utmContent?: string;
+};
+
+export function getUTMsFromObject(object: { [key: string]: string }): UTMsType {
+  return pickPropertiesFromAObject(object, [
+    "utmSource",
+    "utmMedium",
+    "utmCampaign",
+    "utmTerm",
+    "utmContent",
+  ]);
+}
+
+export function saveUTMs(queryParams: { [key: string]: any }) {
+  const UTMs = getUTMsFromQueryParams(queryParams);
+  if (!objectHasValidValues(UTMs)) return;
+  document.cookie = serialize(USER_UTMS_KEY, JSON.stringify(UTMs), {
+    maxAge: ONE_YEAR_IN_MILLISECONDS,
+  });
+}
+
+export function getUTMs(): UTMsType | null {
+  return JSON.parse(getCookie(USER_UTMS_KEY));
+}
