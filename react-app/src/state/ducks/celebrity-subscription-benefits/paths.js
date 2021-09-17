e4a56cbc5dd009d79f963f9c0298a90c2a5e84e1
German Solano <german@famosos.com>
2021-09-17 12:34:50 -0400

    feat(redux/celebrity-subscription-benefits): :sparkles: Add getCountActiveBenefits to fetch total active benefits from celebrity

--- react-app/src/state/ducks/celebrity-subscription-benefits/paths.js
+++ react-app/src/state/ducks/celebrity-subscription-benefits/paths.js
@@ -2,3 +2,4 @@ export const MODEL = `celebrity-subscription-benefits`;
 
 export const LIST_SUBSCRIPTION_BENEFITS_PATH = `custom-endpoints/${MODEL}/list`;
 export const PUBLIC_LIST_SUBSCRIPTION_BENEFITS_PATH = `custom-endpoints/${MODEL}/public-list`;
+export const COUNT_ACTIVE_BENEFITS_PATH = `custom-endpoints/${MODEL}/count-active-benefits`;
