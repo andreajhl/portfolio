bb0ec932f6580b5f5ae125d5cf4e94f952941b37
German Solano <german@famosos.com>
2021-09-23 17:54:12 -0400

    feat(useReferralLink): :sparkles: Update logic to get user referral code from useGetReferralCode hook

--- lib/hooks/useReferralLink.ts
+++ lib/hooks/useReferralLink.ts
@@ -2,16 +2,19 @@ import { getReferrerInviteLink } from "constants/paths";
 import { useAuth } from "lib/famosos-auth";
 import { getFirstName } from "react-app/src/utils/getFirstName";
 import getWindow from "react-app/src/utils/getWindow";
+import useGetReferralCode from "./useGetReferralCode";
 
 function useReferralLink() {
-  const { user, isLoading } = useAuth();
+  const { code, status } = useGetReferralCode();
+  const { user } = useAuth();
+  const userFullName = user?.fullName || "";
   const referralLink =
     getWindow().location.origin +
     getReferrerInviteLink({
-      referrerCode: user?.userId,
-      referrerName: getFirstName(user?.fullName || ""),
+      referrerCode: code,
+      referrerName: getFirstName(userFullName),
     });
-  const isReady = !isLoading && typeof user?.userId === "number";
+  const isReady = userFullName && status === "completed";
 
   return { referralLink, isReady };
 }
