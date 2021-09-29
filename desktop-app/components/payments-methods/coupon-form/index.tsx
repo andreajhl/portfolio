bb53b3770edb51227478024c782df5d182bcf15d
German Solano <german@famosos.com>
2021-09-29 09:59:21 -0400

    refactor(useApplyDiscountCouponForm): :recycle: Abstract from <CouponForm /> to its reuse

--- desktop-app/components/payments-methods/coupon-form/index.tsx
+++ desktop-app/components/payments-methods/coupon-form/index.tsx
@@ -1,64 +1,29 @@
 import React from "react";
-import { connect, ConnectedProps } from "react-redux";
-import { RootState } from "react-app/src/state/store";
 import InputWithSubmitHandler from "desktop-app/components/common/form/InputWithSubmitHandler";
 import styles from "./styles.module.scss";
-import useForm, { ValidationsType } from "lib/hooks/useForm";
-import WarningMessage from "desktop-app/components/common/warning-message";
-import { discountCouponsGateways } from "react-app/src/state/ducks/payments/actions";
 import Maybe from "desktop-app/components/common/helpers/maybe";
 import { SubmitText } from "desktop-app/components/common/helpers/submit-button-text";
-import { FormattedMessage, IntlFormatters, useIntl } from "react-intl";
-import errorMessages from "lib/validations/errorMessages";
-
-const mapStateToProps = ({ payments }: RootState) => ({
-  couponData: payments.fetchDiscountCouponReducer,
-  currencyExchangeData: payments.currencyExchangeReducer.data,
-});
-
-const mapDispatchToProps = {
-  checkoutDiscountCoupon: discountCouponsGateways,
-};
-
-const connector = connect(mapStateToProps, mapDispatchToProps);
-type PropsFromRedux = ConnectedProps<typeof connector>;
+import { FormattedMessage } from "react-intl";
+import { CollapsibleErrorMessage } from "desktop-app/components/common/widgets/collapsible-error-message";
+import { useApplyDiscountCouponForm } from "../../../../lib/hooks/useApplyDiscountCouponForm";
 
 type CouponFormProps = {
   contractReference: string;
-} & PropsFromRedux;
-
-const initialValues = {
-  coupon: "",
 };
 
-function getValidations(
-  formatMessage: IntlFormatters["formatMessage"]
-): ValidationsType<{
-  coupon: string;
-}> {
-  return {
-    coupon(value) {
-      if (value.length === 0) {
-        return formatMessage(errorMessages.invalidCoupon);
-      }
-    },
-  };
-}
-
-function CouponForm({
-  contractReference,
-  checkoutDiscountCoupon,
-  couponData,
-  currencyExchangeData,
-}: CouponFormProps) {
-  const { formatMessage } = useIntl();
-  const { values, errors, submitForm, setFieldValue } = useForm({
-    initialValues: initialValues,
-    validations: getValidations(formatMessage),
-    onSubmit: (data) => {
-      checkoutDiscountCoupon(contractReference, data.coupon);
-    },
+function CouponForm({ contractReference }: CouponFormProps) {
+  const {
+    values,
+    errors,
+    submitForm,
+    setFieldValue,
+    status,
+  } = useApplyDiscountCouponForm({
+    contractReference,
   });
+
+  const isCompleted = status === "completed";
+  const submitTextStatus = status === "failed" ? "rejected" : status;
 
   return (
     <div className={styles.CouponFormWrapper}>
@@ -78,28 +43,23 @@ function CouponForm({
         placeHolderButton={
           <SubmitText
             baseText={
-              couponData.completed ? (
+              <Maybe
+                it={isCompleted}
+                orElse={<FormattedMessage defaultMessage="Aplicar" />}
+              >
                 <FormattedMessage defaultMessage="Agregado" />
-              ) : (
-                <FormattedMessage defaultMessage="Aplicar" />
-              )
+              </Maybe>
             }
-            status={couponData.loading ? "loading" : "idle"}
+            status={submitTextStatus}
           />
         }
       />
-      <Maybe
-        it={
-          (typeof errors.coupon !== "undefined" && errors.coupon !== "") ||
-          couponData.error_data !== null
-        }
-      >
-        <WarningMessage message={errors.coupon || couponData.error_data} />
-      </Maybe>
+      <CollapsibleErrorMessage
+        className={styles.ErrorMessage}
+        errorMessage={errors.coupon}
+      />
     </div>
   );
 }
 
-const _CouponForm = connector(CouponForm);
-
-export { _CouponForm as CouponForm };
+export { CouponForm };
---
+++ lib/hooks/useApplyDiscountCouponForm.ts
@@ -0,0 +1,63 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import useForm, { ValidationsType } from "lib/hooks/useForm";
+import { discountCouponsGateways } from "react-app/src/state/ducks/payments/actions";
+import { defineMessages, IntlFormatters, useIntl } from "lib/custom-intl";
+import useCouponDataState from "lib/hooks/useCouponDataState";
+import errorMessages from "lib/validations/errorMessages";
+
+const messages = defineMessages({
+  "Este cupon ha alcanzado el número de usos permitidos": {
+    defaultMessage:
+      "Este cupón ha alcanzado el número máximo de usos permitidos",
+  },
+  fallbackErrorMessage: {
+    defaultMessage: "Ha ocurrido un error agregando tu cupón",
+  },
+});
+
+export const initialValues = {
+  coupon: "",
+};
+
+export function getValidations(
+  formatMessage: IntlFormatters["formatMessage"]
+): ValidationsType<{
+  coupon: string;
+}> {
+  return {
+    coupon(value) {
+      if (value.length === 0) {
+        return formatMessage(errorMessages.invalidCoupon);
+      }
+    },
+  };
+}
+
+export function useApplyDiscountCouponForm({
+  contractReference,
+}: {
+  contractReference: string;
+}) {
+  const dispatch = useDispatch();
+  const { errorData, status } = useCouponDataState();
+  const { formatMessage } = useIntl();
+  const formState = useForm({
+    initialValues: initialValues,
+    validations: getValidations(formatMessage),
+    onSubmit(data) {
+      dispatch(discountCouponsGateways(contractReference, data.coupon));
+    },
+  });
+
+  useEffect(() => {
+    if (errorData === null) return;
+    formState.setFieldError(
+      "coupon",
+      formatMessage(messages[errorData] || messages.fallbackErrorMessage)
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [errorData]);
+
+  return { ...formState, status };
+}
