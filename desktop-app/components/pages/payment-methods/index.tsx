6eab29e36528fad73cf5878cdcebff957f687bbd
German Solano <german@famosos.com>
2021-09-29 07:28:23 -0400

    refactor(<PaymentMethods />): :recycle: Move required and shared logic for payment methods

--- desktop-app/components/pages/payment-methods/index.tsx
+++ desktop-app/components/pages/payment-methods/index.tsx
@@ -1,21 +1,15 @@
 import { ContractInfo } from "desktop-app/components/payments-methods/contract-info";
 import PageContainer from "desktop-app/components/layouts/page-container";
 import { PageHeading } from "desktop-app/components/layouts/page-heading";
-import { connect, ConnectedProps } from "react-redux";
 import styles from "./styles.module.scss";
 import { PaymentsMethodsSelectorCard } from "desktop-app/components/payments-methods/payments-methods-selector-card";
 import { PaymentMethodsSelectorCardSkeleton } from "desktop-app/components/payments-methods/payments-methods-selector-card/skeleton";
 import { WhatHappensBeforeBanner } from "desktop-app/components/payments-methods/what-happens-before-banner";
-import { paymentsOperations } from "react-app/src/state/ducks/payments";
-import { RootState } from "react-app/src/state/store";
-import { useEffect } from "react";
 import Maybe from "desktop-app/components/common/helpers/maybe";
 import { ContractInfoSkeleton } from "desktop-app/components/payments-methods/contract-info/skeleton";
-import { analytics } from "react-app/src/state/utils/gtm";
-import getWindow from "react-app/src/utils/getWindow";
-import { useRouter } from "next/router";
+import { trackPaymentMethodsBackButtonClick } from "react-app/src/state/utils/gtm";
 import { defineMessages, useIntl } from "react-intl";
-import { getPurchaseSummaryPath } from "constants/paths";
+import ContractToPayType from "desktop-app/types/contractToPayType";
 
 const messages = defineMessages({
   pageHeadingTitle: {
@@ -23,67 +17,28 @@ const messages = defineMessages({
   },
 });
 
-function trackLeave(newRoute: string) {
-  if (newRoute.includes(getPurchaseSummaryPath())) return;
-  analytics.track("PAYMENT_METHODS_LEAVE", {
-    newRoute,
-    isBackButton: false,
-    widget: "PaymentMethodsPage",
-    path: getWindow().location.pathname,
-  });
-}
-
-function trackBackButtonClick() {
-  analytics.track("PAYMENT_METHODS_LEAVE", {
-    isBackButton: true,
-    widget: "PaymentMethodsPage",
-    path: getWindow().location.pathname,
-  });
-}
-
-const mapStateToProps = (state: RootState) => ({
-  isLoading: state.payments.getContractToPayReducer.loading,
-  isCompleted: state.payments.getContractToPayReducer.completed,
-  contract: state.payments.getContractToPayReducer.data,
-});
-
-const mapDispatchToProps = {
-  getContractToPayData: paymentsOperations.getContractToPayV2,
+type PaymentMethodsProps = {
+  isLoading: boolean;
+  isCompleted: boolean;
+  contract: ContractToPayType;
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
-type PaymentMethodsProps = { contractReference: string } & PropsFromRedux;
-
 function PaymentMethodsPage({
-  contractReference,
   isLoading,
   isCompleted,
   contract,
-  getContractToPayData,
 }: PaymentMethodsProps) {
-  const router = useRouter();
   const { formatMessage } = useIntl();
-
-  useEffect(() => {
-    getContractToPayData(contractReference);
-  }, [contractReference]);
-
-  useEffect(() => {
-    router.events.on("routeChangeStart", trackLeave);
-    return () => router.events.off("routeChangeStart", trackLeave);
-  }, [router.events]);
-
   const pageHeadingTitle = formatMessage(messages.pageHeadingTitle);
 
   return (
     <PageContainer showFooter={false} showSearch={false} showBotMakerFrame>
       <PageHeading
         showHomeLink={false}
-        onBackButtonClick={trackBackButtonClick}
-        children={pageHeadingTitle}
-      />
+        onBackButtonClick={trackPaymentMethodsBackButtonClick}
+      >
+        {pageHeadingTitle}
+      </PageHeading>
       <div className={`container ${styles.PaymentMethodsPageContent}`}>
         <div className={styles.PaymentMethodsPurchaseForm}>
           <Maybe
@@ -117,15 +72,11 @@ function PaymentMethodsPage({
         </div>
         <WhatHappensBeforeBanner
           className={styles.WhatHappensBeforeBannerModifier}
-          direction={"column"}
+          direction="column"
         />
       </div>
     </PageContainer>
   );
 }
 
-const _PaymentMethodsPage = connector(PaymentMethodsPage);
-
-export default PaymentMethodsPage;
-
-export { _PaymentMethodsPage as PaymentMethodsPage };
+export { PaymentMethodsPage };
--- pages/payments-methods/[contract_reference]/index.tsx
+++ pages/payments-methods/[contract_reference]/index.tsx
@@ -2,37 +2,25 @@ import CustomHead from "react-app/src/components/common/helpers/custom-head";
 import { withAuthenticationRequired } from "lib/famosos-auth";
 import LoadingPage from "react-app/src/components/layouts/loading-page";
 import isMobile from "lib/utils/isMobile";
-import Maybe from "desktop-app/components/common/helpers/maybe";
 import { useDesktopClass } from "lib/hooks/useDesktopClass";
 import { ROOT_PATH } from "react-app/src/routing/Paths";
-import dynamic from "next/dynamic";
-// import { ValidateEmailModal } from "react-app/src/components/containers/validate-email-modal";
 import { GetServerSideProps } from "next";
-import { RootState } from "react-app/src/state/store";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { analytics } from "react-app/src/state/utils/gtm";
 import { clearCouponData } from "react-app/src/state/ducks/payments/actions";
-
-// const PaymentMethodsPage = dynamic<{ contractReference: string }>(() =>
-//   import("react-app/src/components/pages/payment-methods").then(
-//     (mod) => mod.PaymentMethodsPage
-//   )
-// );
-
-const DesktopPaymentMethodsPage = dynamic<{ contractReference: string }>(() =>
-  import("desktop-app/components/pages/payment-methods").then(
-    (mod) => mod.PaymentMethodsPage
-  )
-);
+import useFetchContractToPay from "lib/hooks/useFetchContractToPay";
+import { PaymentMethodsPage } from "desktop-app/components/pages/payment-methods";
+import useTrackPaymentMethodsLeave from "desktop-app/components/pages/payment-methods/useTrackPaymentMethodsLeave";
+// import { ValidateEmailModal } from "react-app/src/components/containers/validate-email-modal";
 
 export const getServerSideProps: GetServerSideProps = async ({
   params,
   req,
 }) => {
-  const contract_reference = params?.contract_reference;
+  const contractReference = params?.contract_reference;
 
-  if (typeof contract_reference === "undefined") {
+  if (typeof contractReference === "undefined") {
     return {
       redirect: { destination: ROOT_PATH, permanent: false },
     };
@@ -40,23 +28,20 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   return {
     props: {
-      contract_reference,
+      contractReference,
       isMobile: isMobile(req.headers["user-agent"]),
     },
   };
 };
 
-const contractToPaySelector = ({
-  payments: { getContractToPayReducer },
-}: RootState) => ({
-  isCompleted: getContractToPayReducer.completed,
-  contract: getContractToPayReducer.data,
-});
-
-const PaymentMethods = ({ contract_reference, isMobile }) => {
+function PaymentMethods({ contractReference, isMobile }) {
   useDesktopClass(!isMobile);
+  useTrackPaymentMethodsLeave();
   const dispatch = useDispatch();
-  const { contract, isCompleted } = useSelector(contractToPaySelector);
+  const { contractToPay, status } = useFetchContractToPay(contractReference);
+  const isCompleted = status === "completed";
+  const isLoading = status === "loading";
+
   useEffect(() => {
     return () => {
       dispatch(clearCouponData());
@@ -65,21 +50,24 @@ const PaymentMethods = ({ contract_reference, isMobile }) => {
 
   useEffect(() => {
     if (!isCompleted) return;
-    if (contract_reference !== contract.reference) return;
+    if (contractReference !== contractToPay.reference) return;
     analytics.trackInitiateCheckout({
-      contractPrice: contract.price,
-      celebrityId: contract.celebrity_id ?? contract.celebrityId,
+      contractPrice: contractToPay.price,
+      celebrityId: contractToPay.celebrity_id,
     });
-  }, [contract, isCompleted, contract_reference]);
+  }, [contractToPay, isCompleted, contractReference]);
 
   return (
     <>
       <CustomHead />
-      <DesktopPaymentMethodsPage contractReference={contract_reference} />
-      {/* <ValidateEmailModal /> */}
+      <PaymentMethodsPage
+        contract={contractToPay}
+        isCompleted={isCompleted}
+        isLoading={isLoading}
+      />
     </>
   );
-};
+}
 
 export default withAuthenticationRequired(PaymentMethods, {
   onRedirecting: LoadingPage,
