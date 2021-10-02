a18af2eff0de74bdf34aed6581dff31cf06f5f00
German Solano <german@famosos.com>
2021-10-02 10:26:53 -0400

    feat(<TopBar />): :sparkles: Add <UserStarsLink /> with styles for desktop

--- desktop-app/components/layouts/topbar/index.tsx
+++ desktop-app/components/layouts/topbar/index.tsx
@@ -4,7 +4,14 @@ import { AccountDropdown } from "../account-dropdown";
 import { CurrencyModal } from "../currency-modal";
 import { TopbarSearchInput } from "desktop-app/components/topbar/search-input";
 import LangDropdown from "../lang-dropdown";
+import { useAuth } from "lib/famosos-auth";
+import Maybe from "desktop-app/components/common/helpers/maybe";
+import { UserStarsLink } from "desktop-app/components/user-stars-link";
+
 function TopBar() {
+  const { user } = useAuth();
+  const showStarsLink = typeof user?.stars !== "undefined";
+
   return (
     <header className={styles.TopBar}>
       <div className="container h-100">
@@ -17,6 +24,9 @@ function TopBar() {
             >
               <CurrencyModal />
               <LangDropdown />
+              <Maybe it={showStarsLink}>
+                <UserStarsLink className={styles.TopBarStarsLink} />
+              </Maybe>
               {/* <CartIcon /> */}
               {/* <BellIcon /> */}
             </div>
--- desktop-app/components/user-stars-link/index.tsx
+++ desktop-app/components/user-stars-link/index.tsx
@@ -3,15 +3,21 @@ import { useAuth } from "lib/famosos-auth";
 import { StarWithNumber } from "react-app/src/components/common/icons/star-with-number";
 import { Link } from "react-app/src/components/common/routing/link";
 import styles from "./styles.module.scss";
+import classes from "classnames";
 
-type UserStarsLinkProps = {};
+type UserStarsLinkProps = {
+  className?: string;
+};
 
-function UserStarsLink(props: UserStarsLinkProps) {
+function UserStarsLink({ className }: UserStarsLinkProps) {
   const { user } = useAuth();
   const starsCount = user?.stars || 0;
 
   return (
-    <Link href={REFERRALS_HOME} className={styles.UserStarsLink}>
+    <Link
+      href={REFERRALS_HOME}
+      className={classes(styles.UserStarsLink, className)}
+    >
       <StarWithNumber size={26} count={starsCount} color="black" />
     </Link>
   );
