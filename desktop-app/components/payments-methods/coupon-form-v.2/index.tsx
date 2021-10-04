8acb8cbdbe1887777f11e5e43d6da245bfb340ce
German Solano <german@famosos.com>
2021-10-04 12:36:47 -0400

    feat(<CouponFormV2 />): :sparkles: Add elements and styles

---
+++ desktop-app/components/payments-methods/coupon-form-v.2/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import InputWithSubmitHandler from "desktop-app/components/common/form/InputWithSubmitHandler";
+import styles from "./styles.module.scss";
+import Maybe from "desktop-app/components/common/helpers/maybe";
+import { SubmitText } from "desktop-app/components/common/helpers/submit-button-text";
+import { FormattedMessage } from "react-intl";
+import { useApplyDiscountCouponForm } from "lib/hooks/useApplyDiscountCouponForm";
+import { CollapsibleErrorMessage } from "desktop-app/components/common/widgets/collapsible-error-message";
+
+type CouponFormProps = {
+  contractReference: string;
+  setIsOpen: (value: boolean) => void;
+};
+
+function CouponFormV2({ contractReference, setIsOpen }: CouponFormProps) {
+  const {
+    values,
+    errors,
+    submitForm,
+    setFieldValue,
+    status,
+  } = useApplyDiscountCouponForm({
+    contractReference,
+  });
+
+  const isCompleted = status === "completed";
+  const submitTextStatus = status === "failed" ? "rejected" : status;
+
+  return (
+    <div className={styles.CouponFormWrapper}>
+      <button onClick={() => setIsOpen(false)}>x</button>
+      <h3>
+        <FormattedMessage defaultMessage="Cupones" />
+      </h3>
+      <InputWithSubmitHandler
+        inputValue={values.coupon}
+        inputName="coupon"
+        onSubmit={submitForm}
+        className={styles.InputWithSubmitHandler}
+        placeHolderInput="Introduce el código promocional"
+        setInputValue={(newValue) => {
+          setFieldValue("coupon", newValue);
+        }}
+        inputID="coupon-input"
+        placeHolderButton={
+          <SubmitText
+            baseText={
+              <Maybe
+                it={isCompleted}
+                orElse={<FormattedMessage defaultMessage="Aplicar" />}
+              >
+                <FormattedMessage defaultMessage="Agregado" />
+              </Maybe>
+            }
+            status={submitTextStatus}
+          />
+        }
+      />
+      <CollapsibleErrorMessage
+        className={styles.ErrorMessage}
+        errorMessage={errors.coupon}
+      />
+    </div>
+  );
+}
+
+export { CouponFormV2 };
