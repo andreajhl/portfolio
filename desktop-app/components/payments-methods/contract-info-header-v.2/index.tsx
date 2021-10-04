0656c4d0ba265762d1d498aa996b320a96e2f3dd
German Solano <german@famosos.com>
2021-10-04 12:23:46 -0400

    feat(<ContractInfoHeaderV2 />): :sparkles: Add elements and styles

---
+++ desktop-app/components/payments-methods/contract-info-header-v.2/index.tsx
@@ -0,0 +1,53 @@
+import { ContractOccasion } from "desktop-app/components/common/widgets/contract-occasion";
+import { ProfilePicture } from "react-app/src/components/layouts/profile-picture";
+import { FormattedMessage } from "react-intl";
+import styles from "./styles.module.scss";
+
+type ContractInfoHeaderProps = {
+  occasion: string;
+  celebrityAvatar: string;
+  celebrityFullName: string;
+};
+
+function ContractInfoHeaderV2({
+  occasion,
+  celebrityAvatar,
+  celebrityFullName,
+}: ContractInfoHeaderProps) {
+  return (
+    <>
+      <div className={styles.ContractInfoCelebrityInfoDiv}>
+        <div className={styles.ContractInfoCelebrityInfo}>
+          <ProfilePicture
+            imageStyles={{
+              height: 124,
+              objectFit: "cover",
+            }}
+            width={124}
+            avatar={celebrityAvatar || "/assets/img/avatar-blank.png"}
+          />
+          <div d-flex>
+            <h1 className={styles.ContractInfoTitle}>
+              <FormattedMessage
+                defaultMessage="Tú video personalizado de <b>{celebrityFullName}</b>, está en camino!"
+                values={{
+                  b: (chunks) => <b>{celebrityFullName}</b>,
+                  celebrityFullName,
+                }}
+              />
+            </h1>
+            <label>
+              <hr />
+            </label>
+          </div>
+        </div>
+      </div>
+      <ContractOccasion
+        className={styles.ContractInfoHeaderV2Occasion}
+        occasion={occasion}
+      />
+    </>
+  );
+}
+
+export { ContractInfoHeaderV2 };
