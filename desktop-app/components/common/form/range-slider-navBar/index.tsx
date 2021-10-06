700d44833e71f98534eb0e784f348afc9468f453
andrea <andreah29@gmail.com>
2021-10-06 04:40:08 -0300

    componente filtrado de precio

---
+++ desktop-app/components/common/form/range-slider-navBar/index.tsx
@@ -0,0 +1,72 @@
+import "rheostat/initialize";
+import Slider from "rheostat";
+import "rheostat/css/rheostat.css";
+import styles from "./styles.module.scss";
+import { IconButton } from "../../button/icon-button";
+import rangeSliderLog10Algorithm from "lib/utils/rangeSliderLog10Algorithm";
+
+type ValuesType = [number, number];
+
+type StateType = {
+  max: number;
+  min: number;
+  values: ValuesType;
+};
+
+export type RangeSliderProps = {
+  className?: string;
+  algorithm?: {
+    getValue: (value: number, min: number, max: number) => number;
+    getPosition: (positionPercent: number, min: number, max: number) => number;
+  };
+  values: ValuesType;
+  onValuesUpdated?: (state: StateType) => void;
+  onClick?: () => void;
+  isTouched?: boolean;
+  [key: string]: any;
+};
+
+function NoComponent() {
+  return null;
+}
+
+const RangeSliderBackground = (props) => {
+  return <div className={styles.RangeSliderBackground} {...props} />;
+};
+
+const RangeSliderHandle = (props) => (
+  <IconButton className={styles.RangeSliderHandle} {...props}>
+    <svg width="29" height="28" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M14.1196 1.17082C14.2393 0.802296 14.7607 0.802296 14.8804 1.17082L17.8902 10.4338C17.9437 10.5987 18.0973 10.7102 18.2706 10.7102H28.0103C28.3978 10.7102 28.5589 11.2061 28.2454 11.4338L20.3658 17.1587C20.2256 17.2606 20.167 17.4411 20.2205 17.6059L23.2302 26.8689C23.35 27.2375 22.9282 27.5439 22.6147 27.3162L14.7351 21.5913C14.5949 21.4894 14.4051 21.4894 14.2649 21.5913L6.38529 27.3162C6.0718 27.5439 5.65001 27.2375 5.76975 26.8689L8.77949 17.6059C8.83304 17.4411 8.77438 17.2606 8.63418 17.1587L0.754583 11.4338C0.441097 11.2061 0.602208 10.7102 0.989697 10.7102H10.7294C10.9027 10.7102 11.0563 10.5987 11.1098 10.4338L14.1196 1.17082Z" fill="#FB177D"/>
+    </svg>
+  </IconButton>
+);
+
+const RangeSliderProgressBar = (props) => (
+  <div className={styles.RangeSliderProgressBar} {...props} />
+);
+
+function RangeSliderNavBar({
+  className = "",
+  algorithm = rangeSliderLog10Algorithm,
+  isTouched = true,
+  ...props
+}: RangeSliderProps) {
+  return (
+    <div
+      className={`${styles.RangeSlider} ${className} ${
+        isTouched ? styles.RangeSliderIsTouched : ""
+      }`}
+    >
+      <Slider
+        algorithm={algorithm}
+        background={RangeSliderBackground}
+        handle={RangeSliderHandle}
+        progressBar={isTouched ? RangeSliderProgressBar : NoComponent}
+        {...props}
+      />
+    </div>
+  );
+}
+
+export { RangeSliderNavBar };
---
+++ desktop-app/components/search/filter-price-range-navbar/index.tsx
@@ -0,0 +1,69 @@
+import PaymentMethodFormElement from 'desktop-app/components/payments-methods/form-element';
+import {updateSearchFilters} from "react-app/src/state/ducks/search-filters/actions";
+import {PriceRangeSliderNavBar} from 'desktop-app/components/search/price-range-slider-navBar';
+import { useSelector, useDispatch } from 'react-redux';
+import { FormattedMessage } from "lib/custom-intl";
+import { useState,useEffect } from "react";
+import styles from "./styles.module.scss";
+
+const defaultMinPrice = 5;
+const defaultMaxPrice = 500;
+const priceRangeSliderInitialValues = [defaultMinPrice, defaultMaxPrice];
+
+const searchFilter=({searchFilters})=>searchFilters
+
+type timeFilterProps={
+    isOpen:boolean,
+    toOpen: (value:string)=> void
+}
+
+export const PriceRangeNavBar = ({isOpen, toOpen}:timeFilterProps) => {
+
+    const dispatch = useDispatch();
+    const searchFilters=useSelector(searchFilter);
+    const { min_price, max_price } = searchFilters;
+    const [priceRangeIsTouched, setPriceRangeIsTouched] = useState(false);
+    const [priceRangeValues, setPriceRangeValues] = useState(priceRangeSliderInitialValues);
+
+    function changeIsTouched() {
+      if (!priceRangeIsTouched) setPriceRangeIsTouched(true);
+    }
+
+    useEffect(() => {
+      if (!min_price || !max_price) return;
+      changeIsTouched();
+      const minPrice = Number(min_price) ?? defaultMinPrice;
+      const maxPrice = Number(max_price) ?? defaultMaxPrice;
+      setPriceRangeValues([minPrice, maxPrice]);
+    }, [min_price, max_price]);
+  
+    function updateSearchFilterPriceRange(
+        {values: [min_price, max_price]}: {values: [any, any]}): void {
+        if(max_price !==priceRangeSliderInitialValues[1] && min_price!== priceRangeSliderInitialValues ){
+            dispatch(updateSearchFilters({min_price,max_price}))
+        }
+    }
+
+    return (
+        <div className={styles.opcion}>
+            <p className={styles.opcionTittle} onClick={()=>toOpen('price')}><FormattedMessage defaultMessage= "Precios"/></p>
+            <div className={styles.ContainerOpcion} >
+                <PaymentMethodFormElement
+                        labelId={'delivery-price-navBar'}
+                        sectionId={'delivery-price-navBar'}
+                        expanded={isOpen}
+                >
+                    <PriceRangeSliderNavBar
+                        min={defaultMinPrice}
+                        max={defaultMaxPrice}
+                        values={priceRangeValues}
+                        isTouched={priceRangeIsTouched}
+                        changeIsTouched={changeIsTouched}
+                        setValues={setPriceRangeValues}
+                        onChange={updateSearchFilterPriceRange}
+                    />                      
+                </PaymentMethodFormElement>
+            </div>
+        </div>
+    )
+}
