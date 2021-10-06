8b0e7a4837eefb52e6414ca923455a4b453a0722
andrea <andreah29@gmail.com>
2021-10-06 18:51:37 -0300

    filtrado por tiempo de entrega

--- desktop-app/components/search/delivery-time-filter-navBar/index.tsx
+++ desktop-app/components/search/delivery-time-filter-navBar/index.tsx
@@ -1,23 +1,32 @@
 import PaymentMethodFormElement from 'desktop-app/components/payments-methods/form-element';
 import { updateSearchFilters } from "react-app/src/state/ducks/search-filters/actions";
 import { CardsReelSection } from "desktop-app/components/layouts/cards-section-reel";
+import { defineMessages, useIntl } from "react-intl";
 import { FormattedMessage } from "lib/custom-intl";
 import React, {useEffect,useState} from "react";
 import styles from "./styles.module.scss";
 import {useDispatch } from "react-redux";
 
-const deliveryTimeFilter = [
-  { label: "24hrs", value: "flash" },
-  { label:"< 3 días", value: 3 },
-  { label: "< 5 días", value: 5 },
-  { label: "7 días", value: 7 }
-];
+const messages = defineMessages({
+  flash: {
+    defaultMessage: "24hrs",
+  },
+  days3: {
+      defaultMessage: "< 3 días",
+    },
+  days5: {
+    defaultMessage: "< 5 días",
+  },
+  days7: {
+    defaultMessage: "7 días",
+    }
+});
 
 const buttonStyle = {
-    size: 20,
-    top: 20,
-    opacity:1,
-    transform: "translateY(-50%)",
+  size: 25,
+  top: 15,
+  opacity:0.6,
+  transform: "translateY(-50%)",
 };
 
 type timeFilterProps={
@@ -28,7 +37,7 @@ type timeFilterProps={
 function DeliveryTimeFilterNavbar({isOpen, toOpen}:timeFilterProps) {
     
     const [deliveriesTimeChecked, setDeliveriesTimeChecked] = useState();
-    
+    const { formatMessage } = useIntl();
     const dispatch = useDispatch()
 
   useEffect(() => {
@@ -37,10 +46,17 @@ function DeliveryTimeFilterNavbar({isOpen, toOpen}:timeFilterProps) {
     }
   }, [deliveriesTimeChecked]);
 
+  const deliveryTimeFilter = [
+    { label: formatMessage(messages.flash), value: "flash" },
+    { label:formatMessage(messages.days3), value: 3 },
+    { label:formatMessage(messages.days5), value: 5 },
+    { label: formatMessage(messages.days7), value: 7 }
+  ];
+
   return (
-    <div className={styles.opcion}>
-        <p className={styles.opcionTittle} onClick={()=>toOpen('time')}><FormattedMessage defaultMessage= "Tiempo de entrega"/></p>
-        <div className={styles.ContainerOpcion} >
+    <div className={styles.option}>
+        <p className={styles.optionTittle} onClick={()=>toOpen('time')}><FormattedMessage defaultMessage= "Tiempo de entrega"/></p>
+        <div className={styles.ContainerOption} >
         <PaymentMethodFormElement
                 labelId={'delivery-time-navBar'}
                 sectionId={'delivery-time-navBar'}
@@ -50,11 +66,11 @@ function DeliveryTimeFilterNavbar({isOpen, toOpen}:timeFilterProps) {
                 itemData={deliveryTimeFilter}
                 itemCount={deliveryTimeFilter?.length}
                 itemWidth={100}
-                itemHeight={40}
+                itemHeight={35}
                 buttonsStyle={buttonStyle}
-                gap={20}
+                gap={10}
             >
-                {(e) =><FormattedMessage id={e.label}>{txt => <p onClick={()=>setDeliveriesTimeChecked(e.value)}>{txt}</p>}</FormattedMessage>}
+                {(e) =><p onClick={()=>setDeliveriesTimeChecked(e.value)}>{e.label}</p>}
             </CardsReelSection>
         </PaymentMethodFormElement>
         </div>
