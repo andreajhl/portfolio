fff3ad30e47ad2fd97feff538e97f54db2eec024
Isaac Peña <isaac@famosos.com>
2021-10-11 10:08:21 -0400

    refactor: :recycle: change <FilterRatingNavbar/> functionality and styles

--- desktop-app/components/search/filter-rating/index.tsx
+++ desktop-app/components/search/filter-rating/index.tsx
@@ -1,44 +1,39 @@
-import PaymentMethodFormElement from 'desktop-app/components/payments-methods/form-element';
+import PaymentMethodFormElement from "desktop-app/components/payments-methods/form-element";
 import { updateSearchFilters } from "react-app/src/state/ducks/search-filters/actions";
-import StarRatingDisplay from 'desktop-app/components/common/star-rating/display';
+import StarRatingDisplay from "desktop-app/components/common/star-rating/display";
 import { FormattedMessage } from "lib/custom-intl";
 import styles from "./styles.module.scss";
-import  {useState} from "react";
+import { useState } from "react";
 
-
-type timeFilterProps={
-  isOpen:boolean,
-  toOpen: (value:string)=> void
+type timeFilterProps = {
+  isOpen: boolean;
+  toOpen: (value: string) => void;
 };
 
-function FilterRatingNavbar({isOpen, toOpen}:timeFilterProps) {
-    
+function FilterRatingNavbar({ isOpen, toOpen }: timeFilterProps) {
   const [starChecked, setStarChecked] = useState(0);
 
- const onChangeRating=(value)=>{
-    setStarChecked(value)
- };
+  const onChangeRating = (value) => {
+    setStarChecked(value);
+  };
 
   return (
     <div className={styles.option}>
-        <p className={styles.optionTittle} onClick={()=>toOpen('rating')}><FormattedMessage defaultMessage= "Calificaciónes"/></p>
-        <div className={styles.ContainerOption} >
-        <PaymentMethodFormElement
-          labelId={'delivery-time-navBar'}
-          sectionId={'delivery-time-navBar'}
-          expanded={isOpen}
-        >
-      <StarRatingDisplay
-        value={starChecked}
-        editing
-        onChangeRating={onChangeRating}
-        starSchemeColor={'pink'}
-      />
-        </PaymentMethodFormElement>
+      <span className={styles.optionTittle} onClick={() => toOpen("rating")}>
+        <FormattedMessage defaultMessage="Calificaciónes" />
+      </span>
+      {isOpen && (
+        <div className={styles.ContainerOption}>
+          <StarRatingDisplay
+            value={starChecked}
+            editing
+            onChangeRating={onChangeRating}
+            starSchemeColor={"pink"}
+          />
         </div>
+      )}
     </div>
   );
 }
 
-
-export {FilterRatingNavbar };
+export { FilterRatingNavbar };
