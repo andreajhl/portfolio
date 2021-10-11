f6428cb9a01198d94af3996f1760d35a7f6ad3f5
Isaac Peña <isaac@famosos.com>
2021-10-11 10:02:51 -0400

    refactor: :recycle: change <DeliveryTimeFilterNavbar/> functionality and styles

--- desktop-app/components/search/delivery-time-filter-navBar/index.tsx
+++ desktop-app/components/search/delivery-time-filter-navBar/index.tsx
@@ -1,82 +1,96 @@
-import PaymentMethodFormElement from 'desktop-app/components/payments-methods/form-element';
+import PaymentMethodFormElement from "desktop-app/components/payments-methods/form-element";
 import { updateSearchFilters } from "react-app/src/state/ducks/search-filters/actions";
 import { CardsReelSection } from "desktop-app/components/layouts/cards-section-reel";
 import { defineMessages, useIntl } from "react-intl";
 import { FormattedMessage } from "lib/custom-intl";
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
-import {useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
+import Reel from "desktop-app/components/layouts/reel";
 
 const messages = defineMessages({
   flash: {
     defaultMessage: "24hrs",
   },
   days3: {
-      defaultMessage: "< 3 días",
-    },
+    defaultMessage: "< 3 días",
+  },
   days5: {
     defaultMessage: "< 5 días",
   },
   days7: {
     defaultMessage: "7 días",
-    }
+  },
 });
 
 const buttonStyle = {
   size: 25,
   top: 15,
-  opacity:0.6,
+  opacity: 0.6,
   transform: "translateY(-50%)",
 };
 
-type timeFilterProps={
-  isOpen:boolean,
-  toOpen: (value:string)=> void
-}
+type timeFilterProps = {
+  isOpen: boolean;
+  toOpen: (value: string) => void;
+};
 
-function DeliveryTimeFilterNavbar({isOpen, toOpen}:timeFilterProps) {
-    
-    const [deliveriesTimeChecked, setDeliveriesTimeChecked] = useState();
-    const { formatMessage } = useIntl();
-    const dispatch = useDispatch()
+function DeliveryTimeFilterNavbar({ isOpen, toOpen }: timeFilterProps) {
+  const [deliveriesTimeChecked, setDeliveriesTimeChecked] = useState();
+  const { formatMessage } = useIntl();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (deliveriesTimeChecked) {
-      dispatch(updateSearchFilters({max_delivery_time: deliveriesTimeChecked}))
+      dispatch(
+        updateSearchFilters({ max_delivery_time: deliveriesTimeChecked })
+      );
     }
   }, [deliveriesTimeChecked]);
 
   const deliveryTimeFilter = [
     { label: formatMessage(messages.flash), value: "flash" },
-    { label:formatMessage(messages.days3), value: 3 },
-    { label:formatMessage(messages.days5), value: 5 },
-    { label: formatMessage(messages.days7), value: 7 }
+    { label: formatMessage(messages.days3), value: 3 },
+    { label: formatMessage(messages.days5), value: 5 },
+    { label: formatMessage(messages.days7), value: 7 },
   ];
 
   return (
     <div className={styles.option}>
-        <p className={styles.optionTittle} onClick={()=>toOpen('time')}><FormattedMessage defaultMessage= "Tiempo de entrega"/></p>
-        <div className={styles.ContainerOption} >
-        <PaymentMethodFormElement
-                labelId={'delivery-time-navBar'}
-                sectionId={'delivery-time-navBar'}
-                expanded={isOpen}
-        >
-            <CardsReelSection
-                itemData={deliveryTimeFilter}
-                itemCount={deliveryTimeFilter?.length}
-                itemWidth={100}
-                itemHeight={35}
-                buttonsStyle={buttonStyle}
-                gap={10}
-            >
-                {(e) =><p onClick={()=>setDeliveriesTimeChecked(e.value)}>{e.label}</p>}
-            </CardsReelSection>
-        </PaymentMethodFormElement>
+      <span className={styles.optionTittle} onClick={() => toOpen("time")}>
+        <FormattedMessage defaultMessage="Tiempo de entrega" />
+      </span>
+      {isOpen && (
+        <div className={styles.ContainerOption}>
+          <Reel
+            itemSize={180}
+            height={35}
+            scrollByOffset={10}
+            itemCount={deliveryTimeFilter.length}
+            buttonsStyle={buttonStyle}
+            itemData={deliveryTimeFilter}
+          >
+            {({ data, index, style }) => {
+              const hashtag = data[index].label;
+              const value = data[index].value;
+              return (
+                <div
+                  style={{ ...style, left: Number(style.left) + 10 * index }}
+                >
+                  <span
+                    className={styles.BadgeStyle}
+                    onClick={() => setDeliveriesTimeChecked(value)}
+                  >
+                    {hashtag}
+                  </span>
+                </div>
+              );
+            }}
+          </Reel>
         </div>
+      )}
     </div>
   );
 }
 
-
-export {DeliveryTimeFilterNavbar };
+export { DeliveryTimeFilterNavbar };
