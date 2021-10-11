9c91ad0722d48fdc3faa78ab32aa9d823b78bb05
Isaac Peña <isaac@famosos.com>
2021-10-11 10:04:27 -0400

    refactor: :recycle: change <CountryFilterNavBar/> functionality and styles

--- desktop-app/components/search/country-filter-navBar/index.tsx
+++ desktop-app/components/search/country-filter-navBar/index.tsx
@@ -1,71 +1,84 @@
-import PaymentMethodFormElement from 'desktop-app/components/payments-methods/form-element';
 import { updateSearchFilters } from "react-app/src/state/ducks/search-filters/actions";
-import { CardsReelSection } from "desktop-app/components/layouts/cards-section-reel";
 import { listV2 } from "react-app/src/state/ducks/countries/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "react-app/src/state/store";
-import React, {useEffect, useState,} from "react";
-import { FormattedMessage} from "react-intl";
+import React, { useEffect, useState } from "react";
+import { FormattedMessage } from "react-intl";
 import styles from "./styles.module.scss";
-
+import Reel from "desktop-app/components/layouts/reel";
 
 const generateKeysValue = (array) => {
-  let arrayCountries=array.map((e) => ({label:e.name,value:e.id}));
+  let arrayCountries = array.map((e) => ({ label: e.name, value: e.id }));
   return arrayCountries;
 };
 
-type timeFilterProps={
-  isOpen:boolean,
-  toOpen: (value:string)=> void
-}
+type timeFilterProps = {
+  isOpen: boolean;
+  onToggle: () => void;
+};
 
-const countries = ({ countries}:RootState) =>  countries.countriesReducer.data.results
+const countries = ({ countries }: RootState) => countries.countriesReducer;
 
 const buttonStyle = {
   size: 25,
   top: 15,
-  opacity:0.6,
-  transform: "translateY(-50%)"
+  opacity: 0.6,
+  transform: "translateY(-50%)",
 };
 
-function CountryFilterNavBar({isOpen, toOpen}:timeFilterProps) {
-
+function CountryFilterNavBar({ isOpen, onToggle }: timeFilterProps) {
   const dispatch = useDispatch();
-  const countriesState= useSelector(countries);
-  const arrayCountries=generateKeysValue(countriesState);
-  const [countriesChecked, setCountriesChecked] = useState('');
+  const { completed, data, error_data, failed, loading } = useSelector(
+    countries
+  );
+  const arrayCountries = generateKeysValue(data.results);
+  const [countriesChecked, setCountriesChecked] = useState("");
 
   useEffect(() => {
     dispatch(listV2({ orderBy: "name asc" }));
   }, [dispatch]);
   useEffect(() => {
-    if(countriesChecked.length>0) dispatch(updateSearchFilters({country_id: countriesChecked}))
-  }, [dispatch,countriesChecked])
-
+    if (countriesChecked.length > 0)
+      dispatch(updateSearchFilters({ country_id: countriesChecked }));
+  }, [dispatch, countriesChecked]);
 
   return (
     <div className={styles.option}>
-        <p onClick={()=>toOpen('countrie')} className={styles.optionTittle}><FormattedMessage defaultMessage= "Paises"/></p>
+      <span onClick={() => onToggle()} className={styles.optionTittle}>
+        <FormattedMessage defaultMessage="Paises" />
+      </span>
+      {isOpen && !loading && (
         <div className={styles.ContainerOption}>
-        <PaymentMethodFormElement
-                labelId={'delivery-countrie-navBar'}
-                sectionId={'delivery-countrie-navBar'}
-                expanded={isOpen}
-        >
-            <CardsReelSection
-                itemData={arrayCountries}
-                itemCount={arrayCountries?.length}
-                itemWidth={180}
-                itemHeight={35}
-                buttonsStyle={buttonStyle}
-                gap={10}
-            >
-                {(e) =><p onClick={()=>setCountriesChecked(`${e.value}`)}><FormattedMessage id ='pais' defaultMessage = "{label}" values={{label: e.label}}/> </p>}
-            </CardsReelSection>    
-          </PaymentMethodFormElement>
+          <Reel
+            itemSize={180}
+            height={35}
+            scrollByOffset={10}
+            itemCount={arrayCountries.length}
+            // width={listWidth}
+            buttonsStyle={buttonStyle}
+            itemData={arrayCountries}
+          >
+            {({ data, index, style }) => {
+              const hashtag = data[index].label;
+              const value = data[index].value;
+              return (
+                <div
+                  style={{ ...style, left: Number(style.left) + 10 * index }}
+                >
+                  <span
+                    className={styles.BadgeStyle}
+                    onClick={() => setCountriesChecked(`${value}`)}
+                  >
+                    {hashtag}
+                  </span>
+                </div>
+              );
+            }}
+          </Reel>
         </div>
+      )}
     </div>
   );
 }
 
-export {CountryFilterNavBar };
+export { CountryFilterNavBar };
