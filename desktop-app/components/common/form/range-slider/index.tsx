7be2296c9de13ad16acb6eb469a8b360ea9da0e5
Isaac Peña <isaac@famosos.com>
2021-10-11 19:03:49 -0400

    feat: :recycle: add styles for Slider element as props

--- desktop-app/components/common/form/range-slider/index.tsx
+++ desktop-app/components/common/form/range-slider/index.tsx
@@ -5,6 +5,7 @@ import styles from "./styles.module.scss";
 import { IconButton } from "../../button/icon-button";
 import { HandleIcon, HandleIconPinkStars } from "../../icons";
 import rangeSliderLog10Algorithm from "lib/utils/rangeSliderLog10Algorithm";
+import React from "react";
 
 type ValuesType = [number, number];
 
@@ -24,6 +25,9 @@ export type RangeSliderProps = {
   onValuesUpdated?: (state: StateType) => void;
   onClick?: () => void;
   isTouched?: boolean;
+  handleIcon?: "PinkStars" | "DefaultButtons";
+  rangeSliderBackgroundStyles?: React.CSSProperties;
+  rangeSliderProgressBarStyles?: React.CSSProperties;
   [key: string]: any;
 };
 
@@ -31,8 +35,8 @@ function NoComponent() {
   return null;
 }
 
-const RangeSliderBackground = (props) => {
-  return <div className={styles.RangeSliderBackground} {...props} />;
+const RangeSliderBackground = (props, styles) => {
+  return <div {...props} style={styles} />;
 };
 
 const IconVariants = {
@@ -46,8 +50,13 @@ const RangeSliderHandle = (props, Icon = "DefaultButtons") => (
   </IconButton>
 );
 
-const RangeSliderProgressBar = (props) => (
-  <div className={styles.RangeSliderProgressBar} {...props} />
+const RangeSliderProgressBar = (props, styles) => (
+  <div
+    style={{
+      ...styles,
+      ...props.style,
+    }}
+  />
 );
 
 function RangeSlider({
@@ -56,6 +65,18 @@ function RangeSlider({
   isTouched = true,
   handleIcon = "DefaultButtons",
 
+  rangeSliderBackgroundStyles = {
+    backgroundColor: "black",
+    marginLeft: "-13px",
+    marginRight: "-5.5px",
+    height: "1px",
+  },
+  rangeSliderProgressBarStyles = {
+    position: "relative",
+    top: "-1.25px",
+    height: "2.5px",
+    backgroundImage: "linear-gradient(180deg, #fb177d 0%, #ff2063 100%)",
+  },
   ...props
 }: RangeSliderProps) {
   return (
@@ -66,9 +87,15 @@ function RangeSlider({
     >
       <Slider
         algorithm={algorithm}
-        background={RangeSliderBackground}
+        background={(props) =>
+          RangeSliderBackground(props, rangeSliderBackgroundStyles)
+        }
         handle={(props) => RangeSliderHandle(props, handleIcon)}
-        progressBar={isTouched ? RangeSliderProgressBar : NoComponent}
+        progressBar={(props) =>
+          isTouched
+            ? RangeSliderProgressBar(props, rangeSliderProgressBarStyles)
+            : NoComponent
+        }
         {...props}
       />
     </div>
