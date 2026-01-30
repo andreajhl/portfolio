5de94340fad17505a215039353eff5e259a869da
Andrea Hernandez <andreajhl29@gmail.com>
2026-01-30 02:18:22 -0300

    feat: update url cv

--- src/constants/networkLinks.js
+++ src/constants/networkLinks.js
@@ -1,4 +1,4 @@
-export const CV_URL = 'https://drive.google.com/file/d/1QKiTiwt2LFEUpYRWi5YRPtaKca9-fpyl/view?usp=sharing';
+export const CV_URL = 'https://drive.google.com/file/d/1K5qUMsXLuH9vkvDtG-F5VM3EYpEGxvkn/view?usp=sharing';
 export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/andreahernandez29/';
 export const GITHUB_PROFILE_URL = 'https://github.com/andreajhl';
 export const CONTACT_EMAIL = 'mailto:andreajhl29@gmail.com';
